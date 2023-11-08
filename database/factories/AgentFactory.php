2c43d94c123b296e44881acc69a0b46345554beb
Christopher David <chris@arcadelabs.co>
2023-11-08 11:07:41 -0500

    And set up AgentFactory

---
+++ database/factories/AgentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Agent>
+ */
+class AgentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,12 +1,19 @@
 <?php
 
-it('has many agents', function () {
+use App\Models\Agent;
+use App\Models\User;
 
-  $user = App\Models\User::factory()->create();
+it('has many agents', function () {
+  $user = User::factory()->create();
+  $agent = Agent::factory()->create(['user_id' => $user->id]);
 
   $this->assertInstanceOf(
     'Illuminate\Database\Eloquent\Collection',
     $user->agents
   );
 
+  $this->assertInstanceOf(
+    'App\Models\Agent',
+    $user->agents->first()
+  );
 });
