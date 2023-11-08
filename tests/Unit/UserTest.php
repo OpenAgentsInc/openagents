f3347115d16644bbe237cd65655b1503900d9138
Christopher David <chris@arcadelabs.co>
2023-11-08 11:09:49 -0500

    user has many conversations

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Agent;
+use App\Models\Conversation;
 use App\Models\User;
 
 it('has many agents', function () {
@@ -15,5 +16,20 @@ it('has many agents', function () {
   $this->assertInstanceOf(
     'App\Models\Agent',
     $user->agents->first()
+  );
+});
+
+it('has many conversations', function () {
+  $user = User::factory()->create();
+  $conversation = Conversation::factory()->create(['user_id' => $user->id]);
+
+  $this->assertInstanceOf(
+    'Illuminate\Database\Eloquent\Collection',
+    $user->conversations
+  );
+
+  $this->assertInstanceOf(
+    'App\Models\Conversation',
+    $user->conversations->first()
   );
 });
