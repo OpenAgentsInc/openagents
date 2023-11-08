6b969f65628206f7accd75c0d373d190a4bec752
Christopher David <chris@arcadelabs.co>
2023-11-08 11:02:09 -0500

    Reenable auth routes

--- routes/web.php
+++ routes/web.php
@@ -20,20 +20,19 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 // Add a catch-all redirect to the homepage
 Route::get('/{any}', function () {
-    return redirect('/');
+  return redirect('/');
 })->where('any', '.*');
-
-
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// require __DIR__.'/auth.php';
