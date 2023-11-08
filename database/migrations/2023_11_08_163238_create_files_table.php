940e146627bd3f6cdfc5bdbeaee44bfdee8f929c
Christopher David <chris@arcadelabs.co>
2023-11-08 11:44:03 -0500

    Conversation may have many Files.

--- database/migrations/2023_11_08_163238_create_files_table.php
+++ database/migrations/2023_11_08_163238_create_files_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('files', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('conversation_id')->cascadeOnDelete()->cascadeOnUpdate()->nullable();
             $table->timestamps();
         });
     }
