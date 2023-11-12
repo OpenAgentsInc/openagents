8024fb241197eb6f31cc5f4dd2b95cefba27ec68
Christopher David <chris@arcadelabs.co>
2023-11-11 19:46:26 -0500

    try seastead corpus

--- resources/js/Components/Start/Chat.jsx
+++ resources/js/Components/Start/Chat.jsx
@@ -19,7 +19,7 @@ export function Chat() {
     // Send an axios POST request to /api/query with the user's message and corpus ID of 4
     axios.post('/api/query', {
       query: what.content,
-      corpus_id: 4
+      corpus_id: 3
     })
       .then(function (response) {
         console.log(response);
