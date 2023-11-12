ea8a372e7a5ada16bf57ada0500dcdabed2a4dcd
Christopher David <chris@arcadelabs.co>
2023-11-11 19:40:39 -0500

    Query PDF works via UI

--- resources/js/Components/Start/Chat.jsx
+++ resources/js/Components/Start/Chat.jsx
@@ -3,16 +3,47 @@ import { EmptyScreen } from "./empty-screen"
 import { ChatPanel } from "./chat-panel"
 import { ChatList } from "./chat-list"
 import { ChatScrollAnchor } from "./chat-scroll-anchor"
+import axios from "axios"
 
 export function Chat() {
   const id = 1
   const isLoading = false
-  const append = (what) => {
-    setMessages((messages) => [...messages, what])
-  }
   const reload = () => { }
   const [messages, setMessages] = React.useState([])
   const [input, setInput] = React.useState('')
+  const append = (what) => {
+
+    // Add the user's message to the chat
+    setMessages((messages) => [...messages, what]);
+
+    // Send an axios POST request to /api/query with the user's message and corpus ID of 4
+    axios.post('/api/query', {
+      query: what.content,
+      corpus_id: 4
+    })
+      .then(function (response) {
+        console.log(response);
+
+        if (response.data && response.data.ok) {
+          // Extract the 'text' from each result and concatenate them
+          const concatenatedResponse = response.data.results.map(r => r.text).join(' ');
+
+          // Create a new message object for the response
+          const responseMessage = {
+            id: messages.length + 1, // Assuming ID is just the next number in sequence
+            content: concatenatedResponse,
+            role: 'assistant' // or any appropriate role for the response
+          };
+
+          // Append the response message to the messages state
+          setMessages((messages) => [...messages, responseMessage]);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  // console.log(messages)
   const containerRef = React.useRef(null)
   return (
     <div className="h-screen">
