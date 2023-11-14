7fc4d07d4491270a4da31c5187074a90d9adf382
Christopher David <chris@arcadelabs.co>
2023-11-14 17:53:35 -0500

    Set up Embedding model and migration

--- app/Models/Embedding.php
+++ app/Models/Embedding.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Pgvector\Laravel\Vector;
 
 class Embedding extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'embedding' => Vector::class,
+        'metadata' => 'array',
+    ];
 }
