b5d4292bf9451f28696cc4b35a6b56ab57561c1b
Christopher David <chris@arcadelabs.co>
2023-11-15 09:11:46 -0500

    configure pdf2text

--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'pdftotext' => [
+      'path' => env('PDF_TO_TEXT_PATH', '/opt/homebrew/bin/pdftotext')
+    ],
+
 ];
