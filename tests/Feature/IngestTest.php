1b61ac08bc1dfa308031f371e6b2564ba129fd3a
Christopher David <chris@arcadelabs.co>
2023-11-15 09:11:51 -0500

    Extract text from PDF

--- tests/Feature/IngestTest.php
+++ tests/Feature/IngestTest.php
@@ -1,7 +1,22 @@
 <?php
 
-// Given a PDF
-// Extract the text
+use Spatie\PdfToText\Pdf;
+
+test('can ingest pdf', function () {
+
+    // Given a PDF, extract the text
+    $fileText = Pdf::getText(
+        storage_path('app/uploads/0sYtEObUcMCnLo8zAwv7i0cJPfoWTPO4tW7ZblS0.pdf'),
+        config('services.pdftotext.path')
+    );
+
+    dd($fileText);
+
+});
+
+
+
+
 // Convert into chunks
 // Create embeddings for each chunk
 // Store the embeddings in a database
