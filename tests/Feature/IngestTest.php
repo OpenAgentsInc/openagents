44a91d21d289f6b1cef6b7a753b3195f4d7583de
Christopher David <chris@arcadelabs.co>
2023-11-15 09:03:32 -0500

    ingest comments

---
+++ tests/Feature/IngestTest.php
@@ -0,0 +1,12 @@
+<?php
+
+// Given a PDF
+// Extract the text
+// Convert into chunks
+// Create embeddings for each chunk
+// Store the embeddings in a database
+
+// Given a query
+// Convert query into embedding
+// Find nearest neighbors
+// Return results
