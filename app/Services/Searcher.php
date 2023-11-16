60f38141595a941ca187fc7c3fa53be851d61b52
Christopher David <chris@arcadelabs.co>
2023-11-16 14:38:55 -0500

    do the actual lookup

--- app/Services/Searcher.php
+++ app/Services/Searcher.php
@@ -2,11 +2,30 @@
 
 namespace App\Services;
 
-class Searcher {
-  public function query($file_id, $query) {
-    return [
-      "ok" => true,
-      "results" => "Hello there"
-    ];
-  }
+use App\Models\Embedding;
+use Pgvector\Laravel\Vector;
+
+class Searcher
+{
+    public function query($file_id, $query)
+    {
+        $gateway = new QueenbeeGateway();
+        $result = $gateway->createEmbedding($query);
+        print_r("Embedding length: " . count($result[0]["embedding"]) . "\n");
+
+        $embedding = new Vector($result[0]["embedding"]);
+
+        // Execute the query and get the results
+        $searchResults = Embedding::query()
+            ->where('file_id', $file_id)
+            ->orderByRaw('embedding <-> ?', [$embedding])
+            ->take(3)
+            ->pluck('metadata');
+
+        // Format the response with the actual search results
+        return [
+          "ok" => true,
+          "results" => $searchResults
+        ];
+    }
 }
