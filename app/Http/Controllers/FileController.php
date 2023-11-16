b20cb847a5b3adfdb1b0651ec3169f9fd06c7e7d
Christopher David <chris@arcadelabs.co>
2023-11-16 13:42:13 -0500

    Add logging to debug prod

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\Parser;
 use App\Services\Vectara;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Storage;
 
@@ -13,25 +14,32 @@ class FileController extends Controller
     public function store(Request $request)
     {
         try {
+            Log::info("Here we are.");
+
             // If we're not testing, validate the request
             if (!app()->runningUnitTests()) {
                 $request->validate([
                   'file' => 'required|mimetypes:application/pdf' // application/json,text/markdown,text/plain|max:1000240
                 ]);
+
+                Log::info('FileController:store: $request->file(): ' . print_r($request->file(), true));
             }
 
             // Store the file
             $file = $request->file('file');
             $path = Storage::putFile('uploads', $file);
+            Log::info('FileController:store: $path: ' . print_r($path, true));
 
             // Parse the file
             $parser = new Parser();
-            $parser->parsePdf($path);
+            $res = $parser->parsePdf($path);
+            Log::info('FileController:store: $res: ' . print_r($res, true));
 
             return Redirect::route('start')
               ->with('message', 'File uploaded.')
               ->with('filename', $file->getClientOriginalName());
         } catch (\Exception $e) {
+            Log::error('FileController:store: $e: ' . print_r($e, true));
             return Redirect::route('start')->with('error', 'Error uploading file.');
         }
     }
