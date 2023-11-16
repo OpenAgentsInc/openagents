32e3d794aa8d29974ae9e7a20b832f4a4b72db99
Christopher David <chris@arcadelabs.co>
2023-11-16 10:44:48 -0500

    clean up FileController store method

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,48 +9,37 @@ use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
-  public function store(Request $request)
-  {
-    try {
-      // If we're not testing
-      if (!app()->runningUnitTests()) {
-        // Validate incoming request
-        $request->validate([
-          'file' => 'required|mimetypes:application/json,application/pdf,text/markdown,text/plain', // |max:1000240
-        ]);
-      }
+    public function store(Request $request)
+    {
+        try {
+            // If we're not testing
+            if (!app()->runningUnitTests()) {
+                // Validate incoming request
+                $request->validate([
+                  'file' => 'required|mimetypes:application/pdf'
+                  // 'file' => 'required|mimetypes:application/json,application/pdf,text/markdown,text/plain', // |max:1000240
+                ]);
+            }
 
-      // Retrieve the uploaded file
-      $file = $request->file('file');
+            // Store the file
+            $file = $request->file('file');
+            $path = Storage::putFile('uploads', $file);
 
-      // Store the file
-      $path = Storage::putFile('uploads', $file);
+            // Parse the file
 
+            // $file = new \Illuminate\Http\UploadedFile(
+            //     storage_path('app/' . $path),
+            //     $file->getClientOriginalName(),
+            //     $file->getMimeType(),
+            //     null,
+            //     true
+            // );
 
-      // Create a Vectara corpus
-      $vectara = new Vectara();
-
-      // if (!app()->runningUnitTests()) {
-        // Create an UploadedFile from there
-        $file = new \Illuminate\Http\UploadedFile(
-          storage_path('app/' . $path),
-          $file->getClientOriginalName(),
-          $file->getMimeType(),
-          null,
-          true
-        );
-
-        // TODO: Send file to Vectara, save something in our local database
-
-        // $res = $vectara->upload(4, $file);
-      // }
-
-      return Redirect::route('start')
-        ->with('message', 'File uploaded.')
-        ->with('filename', $file->getClientOriginalName());
-    } catch (\Exception $e) {
-      // dd($e);
-      return Redirect::route('start')->with('error', 'Error uploading file.');
+            return Redirect::route('start')
+              ->with('message', 'File uploaded.')
+              ->with('filename', $file->getClientOriginalName());
+        } catch (\Exception $e) {
+            return Redirect::route('start')->with('error', 'Error uploading file.');
+        }
     }
-  }
 }
