363d78770b66bca5ed15dfb5ffbb2cf443e8ef30
Christopher David <chris@arcadelabs.co>
2023-11-16 10:53:43 -0500

    Move parse PDF logic out from ingest test to Parser

---
+++ app/Services/Parser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Embedding;
+use App\Services\QueenbeeGateway;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+use Spatie\PdfToText\Pdf;
+
+class Parser
+{
+    public function parsePdf()
+    {
+        // Given a PDF, extract the text
+        $fileText = Pdf::getText(
+            storage_path('app/uploads/0sYtEObUcMCnLo8zAwv7i0cJPfoWTPO4tW7ZblS0.pdf'),
+            config('services.pdftotext.path')
+        );
+
+        // Clean up and convert into chunks
+        $chunks = Str::of($fileText)
+            ->split("/\f/") // Splitting the text into pages based on the form feed character
+            ->transform(function ($page) {
+                return (string) Str::of($page)
+                    ->replace("\n", " ") // Replacing newline characters with spaces
+                    ->trim(); // Trimming leading and trailing whitespace
+            })
+            ->toArray();
+
+        // Create embeddings for each chunk
+        $gateway = new QueenbeeGateway();
+        $result = $gateway->createEmbedding($chunks);
+
+        // Store the embeddings in a database
+        foreach ($chunks as $key=>$chunk) {
+            Embedding::query()->create([
+                'embedding' => $result[$key]["embedding"],
+                'metadata' => [
+                    'text' => $chunk,
+                ]
+            ]);
+        }
+
+        return [
+            'chunks' => $chunks,
+            'embeddings' => $result,
+        ];
+    }
+}
