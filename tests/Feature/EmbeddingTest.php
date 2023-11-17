a46d5bffad0ab7a8a2aa092b03d61aa15516e221
Christopher David <chris@arcadelabs.co>
2023-11-17 17:25:59 -0500

    another embedding test moved to queenbee group

--- tests/Feature/EmbeddingTest.php
+++ tests/Feature/EmbeddingTest.php
@@ -16,4 +16,4 @@ test('can create embedding from text', function () {
     expect(is_numeric($embedding[1]))->toBeTrue();
     expect(is_numeric($embedding[2]))->toBeTrue();
     expect(is_numeric($embedding[3]))->toBeTrue();
-});
+})->group('queenbee');
