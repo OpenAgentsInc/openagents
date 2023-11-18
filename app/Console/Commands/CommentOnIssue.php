88d13b48395f4b951ee03687aea51ccdad5fbed6
Christopher David <chris@arcadelabs.co>
2023-11-18 15:12:56 -0500

    vid24 code and link

--- app/Console/Commands/CommentOnIssue.php
+++ app/Console/Commands/CommentOnIssue.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Services\OpenAIGateway;
+use App\Services\Searcher;
 use GitHub;
 use Illuminate\Console\Command;
 
@@ -35,6 +36,10 @@ class CommentOnIssue extends Command
         $body = $response['body'];
         $title = $response['title'];
 
+
+        $userAndAssistantMessages = $this->formatIssueAndCommentsAsMessages($body, $commentsResponse);
+        $context = $this->buildContextFrom($userAndAssistantMessages);
+
         $systemPrompt = "You are Faerie, an AI agent specialized in writing & analyzing code.
 
   You have been summoned to ArcadeLabsInc/openagents issue #1.
@@ -43,13 +48,17 @@ class CommentOnIssue extends Command
 
   The issue body is the first message below.
 
+  For additional context, consult the following code snippets:
+  ---
+  " . $context . "
+  ---
+
   Please respond with the comment you would like to add to the issue. Write like a senior developer would write; don't introduce yourself or use flowery text or a closing signature.";
 
         // Combine these two arrays
         $systemMessages = [
           ['role' => 'system', 'content' => $systemPrompt],
         ];
-        $userAndAssistantMessages = $this->formatIssueAndComments($body, $commentsResponse);
         $messages = array_merge($systemMessages, $userAndAssistantMessages);
 
         $gateway = new OpenAIGateway();
@@ -86,5 +95,58 @@ class CommentOnIssue extends Command
         }
 
         return $messages;
+    }
+
+    private function buildContextFrom(array $messages): string
+    {
+        $queryInput = '';
+
+        foreach ($messages as $message) {
+            $queryInput .= $message['content'] . "\n---\n";
+        }
+
+        // Let's do one LLM call to summarize queryInput with an emphasis on the types of files we need to look up
+        $gateway = new OpenAIGateway();
+        $response = $gateway->makeChatCompletion([
+          'model' => 'gpt-4',
+          'messages' => [
+            ['role' => 'system', 'content' => 'You are a helpful assistant. Speak concisely. Answer the user\'s question based only on the following context: ' . $queryInput],
+            ['role' => 'user', 'content' => 'Write 2-3 sentences explaining the types of files we should search for in our codebase to provide the next response in the conversation. Focus only on the next step, not future optimizations. Ignore mentions of video transcriptions or readme/documentation.'],
+          ],
+        ]);
+        $query = $response['choices'][0]['message']['content'];
+
+        $searcher = new Searcher();
+        $results = $searcher->queryAllFiles($query);
+
+        // Loop through the results, and for each one, add the file name and the entire content of the file to the context
+        $context = '';
+        foreach ($results["results"] as $result) {
+            $context .= "Content of " . $result['path'] . ": \n```\n";
+            $content = $this->getFileContent($result['path']);
+            $context .= $content . "\n```\n\n";
+        }
+
+        // // Loop through the results, and for each one, add the file name and the first X lines of the file to the context
+        // $context = '';
+        // foreach ($results["results"] as $result) {
+        //     $context .= "Content of " . $result['path'] . ": \n```\n";
+        //     $content = $this->getFileContent($result['path']);
+        //     $context .= implode("\n", array_slice(explode("\n", $content), 0, 20)) . "\n```\n\n";
+        // }
+
+        // dd($context);
+
+
+        return $context;
+    }
+
+    private function getFileContent(string $path): string
+    {
+        $file = fopen($path, 'r');
+        $content = fread($file, filesize($path));
+        fclose($file);
+
+        return $content;
     }
 }
