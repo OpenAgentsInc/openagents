c11da7fba0f999caf274e12fb254021266249b3e
Christopher David <chris@arcadelabs.co>
2023-11-18 09:50:35 -0500

    can fetch github issue passes

--- tests/Feature/FaerieTest.php
+++ tests/Feature/FaerieTest.php
@@ -4,6 +4,10 @@ use App\Services\QueenbeeGateway;
 
 test('can fetch github issue', function () {
 
-  dd(GitHub::repo()->show('ArcadeLabsInc', 'openagents'));
+  $response = GitHub::issues()->show('ArcadeLabsInc', 'openagents', 1);
+
+  expect($response['url'])->toBe('https://api.github.com/repos/ArcadeLabsInc/openagents/issues/1');
+  expect($response['repository_url'])->toBe('https://api.github.com/repos/ArcadeLabsInc/openagents');
+  expect($response['body'])->toContain("We will implement the 'memory stream' architecture mentioned in the Generative Agents paper, excerpted below and slightly modified to reflect our 'autodev' use case.");
 
 });
