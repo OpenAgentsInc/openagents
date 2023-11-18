a182432bd9b60c1ed0f2503948d81fcbc7bf8cc9
Christopher David <chris@arcadelabs.co>
2023-11-18 09:44:26 -0500

    Test github repo info

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'GitHub' => GrahamCampbell\GitHub\Facades\GitHub::class,
     ])->toArray(),
 
 ];
