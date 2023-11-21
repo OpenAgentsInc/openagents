f2985b57fac7aadeebd1036a9bc5ec25c20c7c6a
Christopher David <chris@arcadelabs.co>
2023-11-21 11:07:04 -0500

    we can create PRs

--- tests/Feature/CommitTest.php
+++ tests/Feature/CommitTest.php
@@ -25,3 +25,14 @@ test('can get issue patches', function () {
     expect($patches[0])->toHaveKey('content');
     expect($patches[0])->toHaveKey('new_content');
 });
+
+test('can submit issue patches to github', function () {
+  $patcher = new Patcher();
+  $issue = [
+      "title" => "Capitalize all comments",
+      "body" => "Make sure all comments are IN ALL CAPS.",
+  ];
+  $patches = $patcher->getIssuePatches($issue, 2);
+  $res = $patcher->submitPatchesToGitHub($patches, "ArcadeLabsInc/openagents", "testbranch2");
+  print_r($res);
+});
