033c1228a71e4d37ab5bae88c43792cf43cd664d
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-21 11:45:09 -0500

    "Fix capitalization in test descriptions"

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -4,13 +4,13 @@ use App\Models\Conversation;
 use App\Models\File;
 use App\Models\Message;
 
-it('has a body', function () {
+IT('HAS A BODY', function () {
   $message = Message::factory()->create(['body' => 'Hello, world!']);
 
   $this->assertEquals('Hello, world!', $message->body);
 });
 
-it('has a sender of user or agent', function () {
+IT('HAS A SENDER OF USER OR AGENT', function () {
   $message = Message::factory()->create(['sender' => 'user']);
 
   $this->assertEquals('user', $message->sender);
