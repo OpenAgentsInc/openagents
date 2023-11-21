1300e0c294789a07d9c1922bf78dc558ddffafe5
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-21 11:48:31 -0500

    "Fix capitalization in tests for Artifact model"

--- tests/Unit/ArtifactTest.php
+++ tests/Unit/ArtifactTest.php
@@ -4,17 +4,17 @@ use App\Models\Agent;
 use App\Models\Artifact;
 use App\Models\Task;
 
-it('has a name', function () {
-  $artifact = Artifact::factory()->create(['name' => 'My Artifact']);
-  expect($artifact->name)->toBe('My Artifact');
+IT('HAS A NAME', FUNCTION () {
+  $ARTIFACT = ARTIFACT::FACTORY()->CREATE(['NAME' => 'MY ARTIFACT']);
+  EXPECT($ARTIFACT->NAME)->TOBE('MY ARTIFACT');
 });
 
-it('belongs to an agent', function () {
-  $artifact = Artifact::factory()->create();
-  expect($artifact->agent)->toBeInstanceOf(Agent::class);
+IT('BELONGS TO AN AGENT', FUNCTION () {
+  $ARTIFACT = ARTIFACT::FACTORY()->CREATE();
+  EXPECT($ARTIFACT->AGENT)->TOBEINSTANCEOF(AGENT::CLASS);
 });
 
-it('belongs to a task', function () {
-  $artifact = Artifact::factory()->create();
-  expect($artifact->task)->toBeInstanceOf(Task::class);
+IT('BELONGS TO A TASK', FUNCTION () {
+  $ARTIFACT = ARTIFACT::FACTORY()->CREATE();
+  EXPECT($ARTIFACT->TASK)->TOBEINSTANCEOF(TASK::CLASS);
 });
