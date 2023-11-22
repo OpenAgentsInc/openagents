e0bfdce6b66caee1157126e3ce619d4c93c0fded
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-22 15:38:39 -0500

    Add multiple agents for user

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -5,15 +5,17 @@ use App\Models\File;
 use App\Models\Conversation;
 use App\Models\Message;
 use App\Models\User;
-
 it('has many agents', function () {
   $user = User::factory()->create();
-  $agent = Agent::factory()->create(['user_id' => $user->id]);
+  // Create multiple agents for the user
+  $agents = Agent::factory(3)->create(['user_id' => $user->id]);
 
+  // Check if the user has a collection of agents
   $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $user->agents);
+
+  // Check if the first agent in the collection is an instance of the Agent model
   $this->assertInstanceOf(Agent::class, $user->agents->first());
 });
-
 it('has many conversations', function () {
   $user = User::factory()->create();
   $conversation = Conversation::factory()->create(['user_id' => $user->id]);
