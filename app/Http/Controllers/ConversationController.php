d0e74ee489e2cb970a9094dc06b4327d66b0912f
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-22 14:32:44 -0500

    Refactor conversation controller for better readability

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -1,24 +1,35 @@
-<?php
 
+<?php
 
 namespace App\Http\Controllers;
 
-use App\Models\Agent;
-use Inertia\Inertia;
-use Inertia\Response;
+use App\Models\Conversation;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 
 class ConversationController extends Controller
 {
-  public function store() {
-    request()->validate([
-      'agent_id' => 'required',
-    ]);
+    /**
+     * Store a new conversation.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store()
+    {
+        // Validate the request data
+        Request::validate([
+            'agent_id' => 'required',
+        ]);
 
-    // Given we have an authenticated user
-    request()->user()->conversations()->create([
-      'agent_id' => request('agent_id'),
-    ]);
+        // Get the authenticated user
+        $user = Auth::user();
 
-    return response()->json([], 201);
-  }
+        // Create a new conversation for the user and agent
+        $conversation = $user->conversations()->create([
+            'agent_id' => Request::get('agent_id'),
+        ]);
+
+        // Return a success response with the created conversation ID
+        return response()->json(['conversation_id' => $conversation->id], 201);
+    }
 }
