671268b52cb88b0afa2374eb4ce75a8402279a63
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-22 14:44:00 -0500

    Fix assertDatabaseHas call in memory deletion test

--- tests/Unit/MemoryTest.php
+++ tests/Unit/MemoryTest.php
@@ -31,14 +31,21 @@ it('can update a memory', function() {
         'description' => 'Updated description',
     ]);
 });
-
 it('can delete a memory', function() {
     $memory = Memory::create([
         'description' => 'Test description',
         'last_accessed' => null,
     ]);
 
+    $this->assertDatabaseHas('memories', [
+        'description' => 'Test description',
+        'last_accessed' => null
+    ]);
+    
     $memory->delete();
-
+    
     $this->assertDatabaseMissing('memories', ['id' => $memory->id]);
 });
+```
+
+Explanation: The `assertDatabaseHas` method should be called before the `delete` method to ensure that the record exists in the database before attempting to delete it. This ensures that the test is checking for the correct behavior and avoids any potential errors.
