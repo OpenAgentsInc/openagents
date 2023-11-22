5fc26e8ef1cfe9bee073eb932b8a32d1de2cdc09
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-22 15:18:46 -0500

    Fix return value in AgentController store method

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -15,15 +15,14 @@ class AgentController extends Controller
     ]);
 
     $name = request('name');
+// create agent in database
+$agent = Agent::create([
+    'user_id' => auth()->user()->id,
+    'name' => $name,
+]);
 
-    // create agent in database
-    $agent = Agent::create([
-      'user_id' => auth()->user()->id,
-      'name' => $name,
-    ]);
-
-    return response()->json([
-      'name' => $name,
-    ], 201);
-  }
+return response()->json([
+    'name' => $agent->name, // changed to return the name of the created agent instead of the passed name variable
+], 201);
+}
 }
