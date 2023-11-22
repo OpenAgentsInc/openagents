bfcb8fbfe124c2c854fabb8b1f400aa984d1238d
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-22 14:51:47 -0500

    Add memory manager id field to agent creation

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -15,14 +15,13 @@ class AgentController extends Controller
     ]);
 
     $name = request('name');
-
-    // create agent in database
-    $agent = Agent::create([
-      'user_id' => auth()->user()->id,
-      'name' => $name,
-    ]);
-
-    return response()->json([
+// create agent in database
+$agent = Agent::create([
+  'user_id' => auth()->user()->id,
+  'name' => $name,
+  'memory_manager_id' => null, // add new field for memory manager id
+]);
+return response()->json([
       'name' => $name,
     ], 201);
   }
