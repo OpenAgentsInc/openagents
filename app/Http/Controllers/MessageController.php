b0b27f64d2bb0b0e5db2247bbc177998a90d774d
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-22 15:38:40 -0500

    Fix message sender in store method

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -14,14 +14,15 @@ class MessageController extends Controller
       'body' => 'required',
       'conversation_id' => 'required',
     ]);
+// create a message in the given conversation
+$message = request()->user()->messages()->create([
+  'body' => request('body'),
+  'conversation_id' => request('conversation_id'),
+]);
 
-    // create a message in the given conversation
-    request()->user()->messages()->create([
-      'body' => request('body'),
-      'conversation_id' => request('conversation_id'),
-      'sender' => 'user'
-    ]);
-
-    return response()->json([], 201);
+// set sender as user
+$message->sender = 'user';
+$message->save();
+return response()->json([], 201);
   }
 }
