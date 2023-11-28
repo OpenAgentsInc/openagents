1e43852915071030281a03327bce0e1dca2e788b
Christopher David <chris@arcadelabs.co>
2023-11-27 23:58:19 -0600

    darken

--- resources/js/Pages/Test.tsx
+++ resources/js/Pages/Test.tsx
@@ -8,11 +8,14 @@ export default function Test() {
   return (
     <>
       <Head title="Home" />
-      <div className="w-[700px] mx-auto mt-12">
+      <div className="dark w-[700px] mx-auto mt-12">
         <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-20 shadow-2xl">
-          <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#666666] via-[#4d4d4d] to-[#000000] bg-clip-text text-center text-7xl font-bold leading-relaxed text-transparent">
+          <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#FFFFFF] via-[#D3D3D3] to-[#A9A9A9] bg-clip-text text-center text-7xl font-bold leading-relaxed text-transparent">
             Open Agents
           </span>
+          {/* <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#666666] via-[#4d4d4d] to-[#000000] bg-clip-text text-center text-7xl font-bold leading-relaxed text-transparent">
+            Open Agents
+          </span> */}
 
           <RetroGrid />
         </div>
