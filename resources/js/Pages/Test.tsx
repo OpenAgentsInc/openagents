c8008297f6ab7ef22ad7a73fa5bbd9201180d059
Christopher David <chris@arcadelabs.co>
2023-11-27 23:38:52 -0600

    Nicer text

--- resources/js/Pages/Test.tsx
+++ resources/js/Pages/Test.tsx
@@ -7,7 +7,7 @@ export default function Test() {
       <Head title="Home" />
       <div className="w-[700px] mx-auto mt-12">
         <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-20 shadow-2xl">
-          <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#000000] via-[#4d4d4d] to-[#666666] bg-clip-text text-center text-7xl font-bold leading-relaxed text-transparent">
+          <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#666666] via-[#4d4d4d] to-[#000000] bg-clip-text text-center text-7xl font-bold leading-relaxed text-transparent">
             Open Agents
           </span>
 
