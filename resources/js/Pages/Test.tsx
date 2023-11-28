b61a55aae178aa56bf9c56dfc5da09a442c8c05e
Christopher David <chris@arcadelabs.co>
2023-11-27 23:33:56 -0600

    Test light retro grid

--- resources/js/Pages/Test.tsx
+++ resources/js/Pages/Test.tsx
@@ -1,9 +1,19 @@
 import RetroGrid from "@/Components/magicui/retro-grid";
+import { Head } from "@inertiajs/react";
 
-export default function Test () {
-    return (
-        <div>
-            <RetroGrid className="w-[600px] h-[300px]" />
+export default function Test() {
+  return (
+    <>
+      <Head title="Home" />
+      <div className="w-[700px] mx-auto mt-12">
+        <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-20 shadow-2xl">
+          <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#000000] via-[#4d4d4d] to-[#666666] bg-clip-text text-center text-7xl font-bold leading-relaxed text-transparent">
+            Open Agents
+          </span>
+
+          <RetroGrid />
         </div>
-    )
+      </div>
+    </>
+  )
 }
