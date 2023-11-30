b8d7754f3ebc5f871956f70aa6f6fe7c0004aa81
Christopher David <chris@arcadelabs.co>
2023-11-30 10:18:48 -0600

    Comingsoon page better on mobile

--- resources/js/Pages/ComingSoon.tsx
+++ resources/js/Pages/ComingSoon.tsx
@@ -1,5 +1,4 @@
 import DotPattern from "@/Components/magicui/dot-pattern";
-import RetroGrid from "@/Components/magicui/retro-grid";
 import ShimmerButton from "@/Components/magicui/shimmer-button";
 import { cn } from "@/lib/utils";
 import { Head } from "@inertiajs/react";
@@ -8,9 +7,9 @@ export default function ComingSoon() {
   return (
     <>
       <Head title="Home" />
-      <div className="dark w-[700px] mx-auto h-screen flex flex-col justify-center items-center">
-        <div className="relative flex w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-20 shadow-2xl">
-          <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#FFFFFF] via-[#D3D3D3] to-[#A9A9A9] bg-clip-text text-center text-7xl font-bold leading-relaxed text-transparent">
+      <div className="dark w-[500px] max-w-full px-12 md:w-[700px] mx-auto h-screen flex flex-col justify-center items-center">
+        <div className="relative flex w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-6 md:p-20 shadow-2xl">
+          <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#FFFFFF] via-[#D3D3D3] to-[#A9A9A9] bg-clip-text text-center text-4xl md:text-7xl font-bold leading-relaxed text-transparent">
             Open Agents
           </span>
 
@@ -25,8 +24,8 @@ export default function ComingSoon() {
             )}
           />
         </div>
-        <ShimmerButton className="mt-16 mx-auto shadow-2xl">
-          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+        <ShimmerButton className="mt-8 mx-auto p-4 md:mt-12 shadow-2xl">
+          <span className="whitespace-pre-wrap text-center text-md md:text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
             Coming soon
           </span>
         </ShimmerButton>
