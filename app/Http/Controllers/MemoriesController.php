f280e93e58cc27e66e1076617b2b9ece1d9652d1
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-12-01 18:03:51 -0500

    Update CRUD methods for MemoriesController

--- app/Http/Controllers/MemoriesController.php
+++ app/Http/Controllers/MemoriesController.php
@@ -11,18 +11,214 @@ class MemoriesController extends Controller
     {
         return view('memories.create');
     }
+public function store(Request $request)
+{
+    $memory = Memory::create([
+        'title' => $request->title,
+        'description' => $request->description,
+    ]);
 
-    public function store(Request $request)
-    {
-        $memory = new Memory();
-        $memory->title = $request->title;
-        $memory->description = $request->description;
-        $memory->save();
+    return response()->json($memory, 201);
+}
+```
 
-        return redirect()->route('memories.show', ['id' => $memory->id]);
-    }
+Before:
+```
+public function update(Request $request, $id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->title = $request->title;
+    $memory->description = $request->description;
+    $memory->save();
 
-    public function show($id)
+    return redirect()->route('memories.show', ['id' => $memory->id]);
+}
+```
+
+After:
+```
+public function update(Request $request, $id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->update([
+        'title' => $request->title,
+        'description' => $request->description,
+    ]);
+
+    return response()->json($memory, 200);
+}
+```
+
+Before:
+```
+public function destroy($id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->delete();
+
+    return redirect()->route('memories.index');
+}
+```
+
+After:
+```
+public function destroy($id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->delete();
+
+    return response()->json(null, 204);
+}
+```
+
+Before:
+```
+public function show($id)
+{
+    $memory = Memory::findOrFail($id);
+    return view('memories.show', ['memory' => $memory]);
+}
+```
+
+After:
+```
+public function show($id)
+{
+    $memory = Memory::findOrFail($id);
+    return response()->json($memory, 200);
+}
+```
+
+Before:
+```
+public function store(Request $request)
+{
+    $memory = new Memory();
+    $memory->title = $request->title;
+    $memory->description = $request->description;
+    $memory->save();
+
+    return redirect()->route('memories.show', ['id' => $memory->id]);
+}
+```
+
+After:
+```
+public function store(Request $request)
+{
+    $memory = Memory::create([
+        'title' => $request->title,
+        'description' => $request->description,
+    ]);
+
+    return response()->json($memory, 201);
+}
+```
+
+Before:
+```
+public function update(Request $request, $id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->title = $request->title;
+    $memory->description = $request->description;
+    $memory->save();
+
+    return redirect()->route('memories.show', ['id' => $memory->id]);
+}
+```
+
+After:
+```
+public function update(Request $request, $id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->update([
+        'title' => $request->title,
+        'description' => $request->description,
+    ]);
+
+    return response()->json($memory, 200);
+}
+```
+
+Before:
+```
+public function destroy($id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->delete();
+
+    return redirect()->route('memories.index');
+}
+```
+
+After:
+```
+public function destroy($id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->delete();
+
+    return response()->json(null, 204);
+}
+```
+
+Before:
+```
+public function show($id)
+{
+    $memory = Memory::findOrFail($id);
+    return view('memories.show', ['memory' => $memory]);
+}
+```
+
+After:
+```
+public function show($id)
+{
+    $memory = Memory::findOrFail($id);
+    return response()->json($memory, 200);
+}
+```
+
+Before:
+```
+public function store(Request $request)
+{
+    $memory = new Memory();
+    $memory->title = $request->title;
+    $memory->description = $request->description;
+    $memory->save();
+
+    return redirect()->route('memories.show', ['id' => $memory->id]);
+}
+```
+
+After:
+```
+public function store(Request $request)
+{
+    $memory = Memory::create([
+        'title' => $request->title,
+        'description' => $request->description,
+    ]);
+
+    return response()->json($memory, 201);
+}
+```
+
+Before:
+```
+public function update(Request $request, $id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->title = $request->title;
+    $memory->description = $request->description;
+    $memory->save();
+
+    return redirect
+public function show($id)
     {
         $memory = Memory::findOrFail($id);
         return view('memories.show', ['memory' => $memory]);
