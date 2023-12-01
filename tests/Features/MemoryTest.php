53e547981dc3acd8137959e2b06395611717de37
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-12-01 16:05:24 -0500

    Refactor MemoryTest to use closure syntax

--- tests/Features/MemoryTest.php
+++ tests/Features/MemoryTest.php
@@ -2,66 +2,57 @@
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+uses(RefreshDatabase::class);
 
-class MemoryTest extends TestCase
-{
-    use RefreshDatabase;
+it('can store memory', function() {
+    $memory = [
+        'title' => 'Test Memory',
+        'description' => 'This is a test memory',
+        'date' => '2021-01-01',
+        'location' => 'Test Location',
+        'image' => 'test_image.jpg',
+    ];
 
-    public function test_can_store_memory()
-    {
-        $memory = [
-            'title' => 'Test Memory',
-            'description' => 'This is a test memory',
-            'date' => '2021-01-01',
-            'location' => 'Test Location',
-            'image' => 'test_image.jpg',
-        ];
+    $this->post(route('memories.store'), $memory)
+        ->assertStatus(201)
+        ->assertJson($memory);
+});
 
-        $this->post(route('memories.store'), $memory)
-            ->assertStatus(201)
-            ->assertJson($memory);
-    }
+it('can get all memories', function() {
+    $memories = factory(Memory::class, 5)->create();
 
-    public function test_can_get_all_memories()
-    {
-        $memories = factory(Memory::class, 5)->create();
+    $this->get(route('memories.index'))
+        ->assertStatus(200)
+        ->assertJson($memories->toArray());
+});
 
-        $this->get(route('memories.index'))
-            ->assertStatus(200)
-            ->assertJson($memories->toArray());
-    }
+it('can get memory', function() {
+    $memory = factory(Memory::class)->create();
 
-    public function test_can_get_memory()
-    {
-        $memory = factory(Memory::class)->create();
+    $this->get(route('memories.show', $memory->id))
+        ->assertStatus(200)
+        ->assertJson($memory->toArray());
+});
 
-        $this->get(route('memories.show', $memory->id))
-            ->assertStatus(200)
-            ->assertJson($memory->toArray());
-    }
+it('can update memory', function() {
+    $memory = factory(Memory::class)->create();
 
-    public function test_can_update_memory()
-    {
-        $memory = factory(Memory::class)->create();
+    $updatedMemory = [
+        'title' => 'Updated Memory',
+        'description' => 'This is an updated memory',
+        'date' => '2021-01-02',
+        'location' => 'Updated Location',
+        'image' => 'updated_image.jpg',
+    ];
 
-        $updatedMemory = [
-            'title' => 'Updated Memory',
-            'description' => 'This is an updated memory',
-            'date' => '2021-01-02',
-            'location' => 'Updated Location',
-            'image' => 'updated_image.jpg',
-        ];
+    $this->put(route('memories.update', $memory->id), $updatedMemory)
+        ->assertStatus(200)
+        ->assertJson($updatedMemory);
+});
 
-        $this->put(route('memories.update', $memory->id), $updatedMemory)
-            ->assertStatus(200)
-            ->assertJson($updatedMemory);
-    }
+it('can delete memory', function() {
+    $memory = factory(Memory::class)->create();
 
-    public function test_can_delete_memory()
-    {
-        $memory = factory(Memory::class)->create();
-
-        $this->delete(route('memories.destroy', $memory->id))
-            ->assertStatus(204);
-    }
-}
+    $this->delete(route('memories.destroy', $memory->id))
+        ->assertStatus(204);
+});
