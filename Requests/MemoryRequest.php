0b4465d6a738d8d4a996244aa9b3ccab42a6a3f6
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-12-01 17:55:51 -0500

    Update MemoryRequest validation rules

---
+++ Requests/MemoryRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MemoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'date' => 'required|date',
+            'location' => 'required|string|max:255',
+            'image' => 'required|image|max:2048',
+        ];
+    }
+}
