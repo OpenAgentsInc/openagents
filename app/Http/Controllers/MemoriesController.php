9720936ed98bc508f8007d059642fca67ce1a9dd
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-11-30 22:49:30 -0500

    Fix memory controller methods

---
+++ app/Http/Controllers/MemoriesController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Memory;
+
+class MemoriesController extends Controller
+{
+    public function create(Request $request)
+    {
+        $memory = new Memory;
+        $memory->title = $request->title;
+        $memory->description = $request->description;
+        $memory->save();
+
+        return response()->json(['message' => 'Memory created successfully']);
+    }
+
+    public function read($id)
+    {
+        $memory = Memory::find($id);
+
+        return response()->json($memory);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $memory = Memory::find($id);
+        $memory->title = $request->title;
+        $memory->description = $request->description;
+        $memory->save();
+
+        return response()->json(['message' => 'Memory updated successfully']);
+    }
+
+    public function delete($id)
+    {
+        $memory = Memory::find($id);
+        $memory->delete();
+
+        return response()->json(['message' => 'Memory deleted successfully']);
+    }
+
+    public function store(Request $request)
+    {
+        $memory = new Memory;
+        $memory->title = $request->title;
+        $memory->description = $request->description;
+        $memory->save();
+
+        return response()->json(['message' => 'Memory created successfully']);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $memory = Memory::find($id);
+        $memory->title = $request->title;
+        $memory->description = $request->description;
+        $memory->save();
+
+        return response()->json(['message' => 'Memory updated successfully']);
+    }
+}
