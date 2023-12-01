18cc27b12658d920f2aae6a3f7d91d823d2c2e1e
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-12-01 17:55:54 -0500

    Update memories view

---
+++ resources/views/memories/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Memories</div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($memories as $memory)
+                            <tr>
+                                <th scope="row">{{ $memory->id }}</th>
+                                <td>{{ $memory->title }}</td>
+                                <td>{{ $memory->description }}</td>
+                                <td>
+                                    <a href="{{ route('memories.edit', $memory->id) }}" class="btn btn-primary">Edit</a>
+                                    <form action="{{ route('memories.destroy', $memory->id) }}" method="POST" style="display: inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
