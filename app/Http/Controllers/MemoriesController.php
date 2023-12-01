81de379a97fb24595ff0e82bbdeb6e9758588f9d
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-12-01 12:20:33 -0500

    Fix memory controller methods

--- app/Http/Controllers/MemoriesController.php
+++ app/Http/Controllers/MemoriesController.php
@@ -3,60 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Memory;
+use App\Models\Memory;
 
 class MemoriesController extends Controller
 {
-    public function create(Request $request)
+    public function store(Request $request)
     {
-        $memory = new Memory;
-        $memory->title = $request->title;
-        $memory->description = $request->description;
-        $memory->save();
-return response()->json(['message' => 'Memory created successfully'], 201);
-}
+        return Memory::create($request->all());
+    }
 
-    public function read($id)
+    public function show($id)
     {
-        $memory = Memory::find($id);
-
-        return response()->json($memory);
+        return Memory::findOrFail($id);
     }
 
     public function update(Request $request, $id)
     {
-        $memory = Memory::find($id);
-        $memory->title = $request->title;
-        $memory->description = $request->description;
-        $memory->save();
-
-        return response()->json(['message' => 'Memory updated successfully']);
+        $memory = Memory::findOrFail($id);
+        $memory->update($request->all());
+        return $memory;
     }
 
-    public function delete($id)
+    public function destroy($id)
     {
-        $memory = Memory::find($id);
+        $memory = Memory::findOrFail($id);
         $memory->delete();
-
-        return response()->json(['message' => 'Memory deleted successfully']);
-    }
-
-    public function store(Request $request)
-    {
-        $memory = new Memory;
-        $memory->title = $request->title;
-        $memory->description = $request->description;
-        $memory->save();
-return response()->json(['message' => 'Memory created successfully'], 201);
-}
-
-    public function edit(Request $request, $id)
-    {
-        $memory = Memory::find($id);
-        $memory->title = $request->title;
-        $memory->description = $request->description;
-        $memory->save();
-
-        return response()->json(['message' => 'Memory updated successfully']);
+        return 204;
     }
 }
