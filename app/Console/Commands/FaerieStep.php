7595b1b204808d519a3395e468baeeb214469c2a
Christopher David <chris@arcadelabs.co>
2023-12-01 11:29:59 -0600

    branch

--- app/Console/Commands/FaerieStep.php
+++ app/Console/Commands/FaerieStep.php
@@ -190,7 +190,7 @@ class FaerieStep extends Command
         print_r($patches);
         print_r("---");
 
-        $res = $patcher->submitPatchesToGitHub($patches, "ArcadeLabsInc/openagents", "vid32test1", false);
+        $res = $patcher->submitPatchesToGitHub($patches, "ArcadeLabsInc/openagents", "vid32test3", false);
         print_r("RESPONSE:");
         print_r($res);
 
@@ -235,7 +235,7 @@ class FaerieStep extends Command
         print_r($patches);
         print_r("---");
 
-        $res = $patcher->submitPatchesToGitHub($patches, "ArcadeLabsInc/openagents", "vid32test1");
+        $res = $patcher->submitPatchesToGitHub($patches, "ArcadeLabsInc/openagents", "vid32test3");
         print_r("RESPONSE:");
         print_r($res);
 
