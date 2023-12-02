33de835f6dceb1a2d02cec234e1a5775708e47a5
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-12-02 17:33:38 -0500

    Refactor MemoriesTest to use new MemoryFactory

--- app/Http/Controllers/MemoriesController.php
+++ app/Http/Controllers/MemoriesController.php
@@ -7,27 +7,98 @@ use App\Models\Memory;
 
 class MemoriesController extends Controller
 {
-    public function store(Request $request)
+public function store(Request $request)
+{
+    return Memory::create($request->all());
+}
+
+public function show($id)
+{
+    return Memory::findOrFail($id);
+}
+
+public function update(Request $request, $id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->update($request->all());
+    return $memory;
+}
+
+public function destroy($id)
+{
+    $memory = Memory::findOrFail($id);
+    $memory->delete();
+    return response()->json(null, 204);
+}
+```
+
+Before:
+```
+use PHPUnit\Framework\TestCase;
+
+class MemoriesTest extends TestCase
+{
+    public function test_can_create_memory()
     {
-        return Memory::create($request->all());
+        $memory = Memory::factory()->create();
+        $this->assertDatabaseHas('memories', $memory->toArray());
     }
 
-    public function show($id)
+    public function test_can_get_memory()
     {
-        return Memory::findOrFail($id);
+        $memory = Memory::factory()->create();
+        $response = $this->get('/memories/' . $memory->id);
+        $response->assertStatus(200);
     }
 
-    public function update(Request $request, $id)
+    public function test_can_update_memory()
     {
-        $memory = Memory::findOrFail($id);
-        $memory->update($request->all());
-        return $memory;
+        $memory = Memory::factory()->create();
+        $new_data = ['title' => 'New Title'];
+        $response = $this->put('/memories/' . $memory->id, $new_data);
+        $response->assertStatus(200);
+        $this->assertEquals($new_data['title'], $memory->fresh()->title);
     }
 
-    public function destroy($id)
+    public function test_can_delete_memory()
     {
-        $memory = Memory::findOrFail($id);
-        $memory->delete();
-        return response()->json(null, 204);
+        $memory = Memory::factory()->create();
+        $response = $this->delete('/memories/' . $memory->id);
+        $response->assertStatus(204);
+        $this->assertDatabaseMissing('memories', $memory->toArray());
     }
+}
+```
+
+After:
+```
+use Tests\TestCase;
+use App\Models\Memory;
+use App\Database\Factories\MemoryFactory;
+
+it('can create memory', function () {
+    $memory = MemoryFactory::new()->create();
+    expect(Memory::find($memory->id))->toBe($memory);
+});
+
+it('can get memory', function () {
+    $memory = MemoryFactory::new()->create();
+    $response = $this->get('/memories/' . $memory->id);
+    $response->assertStatus(200);
+});
+
+it('can update memory', function () {
+    $memory = MemoryFactory::new()->create();
+    $new_data = ['title' => 'New Title'];
+    $response = $this->put('/memories/' . $memory->id, $new_data);
+    $response->assertStatus(200);
+    expect($memory->fresh()->title)->toBe($new_data['title']);
+});
+
+it('can delete memory', function () {
+    $memory = MemoryFactory::new()->create();
+    $response = $this->delete('/memories/' . $memory->id);
+    $response->assertStatus(204);
+    expect(Memory::find($memory->id))->toBeNull();
+});
 }
