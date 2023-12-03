bd9bd9e7b76f51b007c996f649004afaa986fdc6
Christopher David <chris@arcadelabs.co>
2023-12-03 09:48:25 -0600

    Remove extraneous test

--- app/Services/Faerie.php
+++ app/Services/Faerie.php
@@ -9,10 +9,5 @@ class Faerie {
     public function __construct($owner = "ArcadeLabsInc", $repo = "openagents") {
         $this->owner = $owner;
         $this->repo = $repo;
-
-        // require either no params or two params
-        if (func_num_args() !== 0 && func_num_args() !== 2) {
-            throw new \Exception('Too few arguments to function App\Services\Faerie::__construct(), ' . func_num_args() . ' passed');
-        }
     }
 }
--- tests/Feature/FaerieTest.php
+++ tests/Feature/FaerieTest.php
@@ -17,15 +17,3 @@ test('Faerie can be instantiated with a different repo', function () {
     expect($faerie->owner)->toBe('foo');
     expect($faerie->repo)->toBe('bar');
 });
-
-test('Faerie needs either no params or two params', function () {
-    $faerie = new Faerie();
-    expect($faerie->owner)->toBe('ArcadeLabsInc');
-    expect($faerie->repo)->toBe('openagents');
-
-    $faerie = new Faerie('foo', 'bar');
-    expect($faerie->owner)->toBe('foo');
-    expect($faerie->repo)->toBe('bar');
-
-    $faerie = new Faerie('foo');
-})->throws('Too few arguments to function App\Services\Faerie::__construct(), 1 passed');
