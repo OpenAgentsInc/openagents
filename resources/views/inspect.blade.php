1241acf302235e3debf3c272e11cf2a9c148eb26
Christopher David <chris@arcadelabs.co>
2023-12-03 08:32:59 -0600

    group tasks by agent

--- resources/views/inspect.blade.php
+++ resources/views/inspect.blade.php
@@ -1,57 +1,50 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 <div class="max-w-4xl mx-auto py-6">
-    <div class="mb-4">
-        <h2 class="text-lg font-semibold">{{ $agents->count() }} agents</h2>
-        @foreach($agents as $agent)
-            <p class="text-gray-600">{{ $agent->name }}</p>
-        @endforeach
-    </div>
+    <h2 class="text-lg font-semibold mb-4">{{ $agents->count() }} agents</h2>
 
-    <div class="mb-4">
-        <h2 class="text-lg font-semibold">{{ $tasks->count() }} tasks</h2>
-        @foreach($tasks as $task)
-            <p class="text-gray-600">{{ $task->prompt }}</p>
-        @endforeach
-    </div>
-
-    <div>
-        <h2 class="text-lg font-semibold">{{ $steps->count() }} steps</h2>
-        @foreach($steps as $step)
-            @php
-            $inputDecoded = json_decode($step->input);
-            $outputDecoded = json_decode($step->output);
-            @endphp
-
-            <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4">
-                <div class="px-4 py-5 sm:px-6">
-                    @if($inputDecoded)
-                        <p class="text-sm text-gray-500">
-                            Type: <span class="font-medium text-gray-700">{{ $inputDecoded->type ?? 'N/A' }}</span>
-                        </p>
-                        <p class="text-sm text-gray-500">
-                            Model: <span class="font-medium text-gray-700">{{ $inputDecoded->model ?? 'N/A' }}</span>
-                        </p>
-                        <p class="text-sm text-gray-500">
-                            Instruction: <span class="font-medium text-gray-700">{{ $inputDecoded->instruction ?? 'N/A' }}</span>
-                        </p>
-                    @endif
-                </div>
-                <div class="border-t border-gray-200">
-                    <dl>
-                        @if($outputDecoded)
-                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">Response</dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $outputDecoded->response ?? 'N/A' }}</dd>
-                            </div>
-                            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">Tokens Used</dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $outputDecoded->tokens_used ?? 'N/A' }}</dd>
-                            </div>
-                        @endif
-                    </dl>
-                </div>
+    @foreach($agents as $agent)
+        <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4">
+            <div class="px-4 py-5 sm:px-6">
+                <h3 class="text-lg leading-6 font-medium text-gray-900">{{ $agent->name }}</h3>
             </div>
-        @endforeach
-    </div>
+            <div class="border-t border-gray-200">
+                @foreach($agent->tasks as $task)
+                    <div class="px-4 py-5 sm:px-6">
+                        <p class="text-sm font-medium text-gray-600">Task: {{ $task->prompt }}</p>
+
+                        @foreach($task->steps as $step)
+                            @php
+                            $inputDecoded = json_decode($step->input);
+                            $outputDecoded = json_decode($step->output);
+                            @endphp
+
+                            <div class="mt-4">
+                                @if($inputDecoded)
+                                    <p class="text-sm text-gray-500">
+                                        Type: <span class="font-medium text-gray-700">{{ $inputDecoded->type ?? 'N/A' }}</span>
+                                    </p>
+                                    <p class="text-sm text-gray-500">
+                                        Model: <span class="font-medium text-gray-700">{{ $inputDecoded->model ?? 'N/A' }}</span>
+                                    </p>
+                                    <p class="text-sm text-gray-500">
+                                        Instruction: <span class="font-medium text-gray-700">{{ $inputDecoded->instruction ?? 'N/A' }}</span>
+                                    </p>
+                                @endif
+
+                                @if($outputDecoded)
+                                    <p class="text-sm text-gray-500">
+                                        Response: <span class="font-medium text-gray-700">{{ $outputDecoded->response ?? 'N/A' }}</span>
+                                    </p>
+                                    <p class="text-sm text-gray-500">
+                                        Tokens Used: <span class="font-medium text-gray-700">{{ $outputDecoded->tokens_used ?? 'N/A' }}</span>
+                                    </p>
+                                @endif
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    @endforeach
 </div>
--- tests/Unit/StepTest.php
+++ tests/Unit/StepTest.php
@@ -5,43 +5,46 @@ use App\Models\Step;
 use App\Models\Task;
 
 it('belongs to an agent', function () {
-  $step = Step::factory()->create();
-  expect($step->agent)->toBeInstanceOf(Agent::class);
+    $step = Step::factory()->create();
+    expect($step->agent)->toBeInstanceOf(Agent::class);
 });
 
 it('belongs to a task', function () {
-  $step = Step::factory()->create();
-  expect($step->task)->toBeInstanceOf(Task::class);
+    $step = Step::factory()->create();
+    expect($step->task)->toBeInstanceOf(Task::class);
 });
 
 it('has input and output fields', function () {
-  $step = Step::factory()->create();
-  expect($step->input)->toBeNull();
-  expect($step->output)->toBeNull();
+    $step = Step::factory()->create([
+        'input' => null,
+        'output' => null
+    ]);
+    expect($step->input)->toBeNull();
+    expect($step->output)->toBeNull();
 
-  $input = ['foo' => 'bar'];
-  $output = ['result' => 'success'];
+    $input = ['foo' => 'bar'];
+    $output = ['result' => 'success'];
 
-  $step = Step::factory()->create([
-    'input' => json_encode($input),
-    'output' => json_encode($output)
-  ]);
+    $step = Step::factory()->create([
+      'input' => json_encode($input),
+      'output' => json_encode($output)
+    ]);
 
-  expect($step->input)->toBe(json_encode($input));
-  expect($step->output)->toBe(json_encode($output));
+    expect($step->input)->toBe(json_encode($input));
+    expect($step->output)->toBe(json_encode($output));
 });
 
 it('has many artifacts', function () {
-  $step = Step::factory()->create();
-  $step->artifacts()->create([
-    'name' => 'foo',
-    'path' => 'bar',
-    'agent_id' => $step->agent->id,
-  ]);
-  $step->artifacts()->create([
-    'name' => 'baz',
-    'path' => 'qux',
-    'agent_id' => $step->agent->id,
-  ]);
-  expect($step->artifacts)->toHaveCount(2);
+    $step = Step::factory()->create();
+    $step->artifacts()->create([
+      'name' => 'foo',
+      'path' => 'bar',
+      'agent_id' => $step->agent->id,
+    ]);
+    $step->artifacts()->create([
+      'name' => 'baz',
+      'path' => 'qux',
+      'agent_id' => $step->agent->id,
+    ]);
+    expect($step->artifacts)->toHaveCount(2);
 });
