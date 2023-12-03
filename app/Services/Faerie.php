21669c998c7e7fb828eeff8848ce399dcbf6c672
Christopher David <chris@arcadelabs.co>
2023-12-03 09:54:01 -0600

    can determine if repo has open pr

--- app/Services/Faerie.php
+++ app/Services/Faerie.php
@@ -10,4 +10,26 @@ class Faerie {
         $this->owner = $owner;
         $this->repo = $repo;
     }
+
+    public function repoHasOpenPR() {
+        $url = "https://api.github.com/repos/{$this->owner}/{$this->repo}/pulls?state=open";
+        $response = $this->curl($url);
+        return count($response) > 0;
+    }
+
+    private function curl ($url) {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $url,
+            CURLOPT_USERAGENT => 'ArcadeLabsInc',
+            CURLOPT_HTTPHEADER => [
+                'Accept: application/vnd.github.v3+json',
+                'Authorization: token ' . env('GITHUB_TOKEN'),
+            ],
+        ]);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response, true);
+    }
 }
--- tests/Feature/FaerieTest.php
+++ tests/Feature/FaerieTest.php
@@ -6,14 +6,31 @@ use App\Services\Faerie;
  * INSTANTIATION
  */
 
-test('Faerie defaults to the openagents repo', function () {
+it('defaults to the openagents repo', function () {
     $faerie = new Faerie();
     expect($faerie->owner)->toBe('ArcadeLabsInc');
     expect($faerie->repo)->toBe('openagents');
 });
 
-test('Faerie can be instantiated with a different repo', function () {
+it('can be instantiated with a different repo', function () {
     $faerie = new Faerie('foo', 'bar');
     expect($faerie->owner)->toBe('foo');
     expect($faerie->repo)->toBe('bar');
 });
+
+/**
+ * READ GITHUB REPO
+ */
+
+it('can determine if repo has an open PR', function () {
+    $faerie = new Faerie();
+    $response = $faerie->repoHasOpenPR();
+    expect($response)->toBeBool();
+});
+
+// it('can read a github repo', function () {
+//     $faerie = new Faerie();
+//     $response = $faerie->readRepo();
+//     expect($response)->toBeArray();
+//     expect($response['name'])->toBe('openagents');
+// });
