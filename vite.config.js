642167ca1c387cd9c5ac19141bbd9954888fb2d6
Christopher David <chris@arcadelabs.co>
2023-12-03 09:36:40 -0600

    Fix vite import (#33)
    * test
    
    * add app css to vite config
    
    * don't include app tsx there

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: ['resources/js/app.tsx', 'resources/css/app.css'],
             refresh: true,
         }),
         react(),
