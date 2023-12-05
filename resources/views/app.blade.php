eff15bd9721ad8fc7bfe531fd61402bfac15425d
Christopher David <chris@arcadelabs.co>
2023-12-05 16:00:19 -0600

    Add new font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <title inertia>{{ config('app.name', 'OpenAgents') }}</title>
 
         <!-- Scripts -->
         @routes
