59e1b10ee7a78801a8f84055f5075626c17a388c
Christopher David <chris@arcadelabs.co>
2023-12-05 16:04:07 -0600

    Start header

--- resources/js/Layouts/InspectLayout.tsx
+++ resources/js/Layouts/InspectLayout.tsx
@@ -1,12 +1,17 @@
-import { Head } from '@inertiajs/react'
+import { Head, Link } from '@inertiajs/react'
 
 export default function InspectLayout({ children, title = "Open Agents" }) {
-  return (
-    <div className="">
-      <Head title={title} />
-      <div className="min-h-screen flex flex-col sm:justify-center h-full items-center pt-6 sm:pt-0">
-        <div className="w-full overflow-hidden">{children}</div>
-      </div>
-    </div>
-  )
+    return (
+        <div className="">
+            <Head title={title} />
+            <div className="min-h-screen flex flex-col sm:justify-center h-full items-center pt-6 sm:pt-0">
+                <div className="w-full">
+                    <Link href="/">
+                        <h1 className="p-6 text-xl">OpenAgents</h1>
+                    </Link>
+                </div>
+                <div className="w-full overflow-hidden">{children}</div>
+            </div>
+        </div>
+    )
 }
