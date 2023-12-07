1308ac5a4c0db34c9134ccafbadb05ebf4d839d4
Christopher David <chris@arcadelabs.co>
2023-12-06 17:52:04 -0800

    show input and output

--- resources/js/Components/StepDetails.tsx
+++ resources/js/Components/StepDetails.tsx
@@ -7,6 +7,8 @@ export interface Step {
     name: string;
     status: string;
     description: string;
+    input: any
+    output: any
 }
 
 export interface Task {
@@ -26,9 +28,28 @@ export const StepDetails = ({ step }: { step: Step }) => {
                     <p>{step.description}</p>
                 </CardContent>
             </Card>
-            <div className="my-6">
-
-            </div>
+            <Card className="mt-6">
+                <CardContent>
+                    <div className="mt-6 flex flex-wrap -mx-4">
+                        <div className="w-full lg:w-1/2 px-4">
+                            <div className="mb-4">
+                                <h5 className="text-md font-semibold mb-2">Input</h5>
+                                <div className="rounded-lg border p-4 bg-gray-100 overflow-auto">
+                                    <pre className="text-sm whitespace-pre-wrap">{step.input}</pre>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full lg:w-1/2 px-4">
+                            <div className="mb-4">
+                                <h5 className="text-md font-semibold mb-2">Output</h5>
+                                <div className="rounded-lg border p-4 bg-gray-100 overflow-auto">
+                                    <pre className="text-sm whitespace-pre-wrap">{step.output}</pre>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
         </div>
     );
 };
