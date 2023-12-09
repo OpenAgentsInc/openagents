cb27f6edb97905eac5d9f94eeb6c2abf97214857
Christopher David <chris@arcadelabs.co>
2023-12-09 09:50:47 -0800

    splash links and favicon

--- resources/js/Pages/Splash.tsx
+++ resources/js/Pages/Splash.tsx
@@ -1,17 +1,26 @@
 import { Button } from "@/Components/ui/button";
 import { Head } from "@inertiajs/react";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 
 export default function Splash() {
     return <>
         <Head title="Home" />
         <div className="h-screen w-screen flex flex-col justify-center items-center">
-            <p className="my-8 text-3xl text-center">Soon.</p>
-            <a href="https://github.com/ArcadeLabsInc/openagents">
-                <Button variant="outline" size="icon">
-                    <GitHubLogoIcon className="w-6 h-6" />
-                </Button>
-            </a>
+            <p className="-mt-12 mb-12 text-7xl text-center tracking-widest">🌐🤖</p>
+            <p className="italic mb-12 text-xl text-center tracking-widest text-gray-500">Soon</p>
+            <div className="flex flex-row space-x-6">
+                <a href="https://github.com/ArcadeLabsInc/openagents" target="_blank">
+                    <Button variant="outline" size="icon" className="shadow-xl">
+                        <GitHubLogoIcon className="w-6 h-6" />
+                    </Button>
+                </a>
+                <a href="https://twitter.com/GPUtopia/status/1721942435125715086" target="_blank">
+                    <Button variant="outline" size="icon" className="shadow-xl">
+                        <TwitterLogoIcon className="w-6 h-6" />
+                    </Button>
+                </a>
+            </div>
+
         </div>
     </>
 }
