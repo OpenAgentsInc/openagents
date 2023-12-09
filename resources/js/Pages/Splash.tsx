a81c05687fbaed530b0022eafa25dbe3a12d853e
Christopher David <chris@arcadelabs.co>
2023-12-09 10:02:01 -0800

    use X logo

--- resources/js/Pages/Splash.tsx
+++ resources/js/Pages/Splash.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/Components/ui/button";
 import { Head } from "@inertiajs/react";
-import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function Splash() {
     return <>
         <Head title="Home" />
         <div className="h-screen w-screen flex flex-col justify-center items-center">
             <p className="-mt-12 mb-12 text-7xl text-center tracking-widest">🌐🤖</p>
-            <p className="italic mb-12 text-xl text-center tracking-widest text-gray-500">Soon</p>
+            <p className="font-mono mb-12 text-3xl text-center tracking-widest text-gray-500">Soon</p>
             <div className="flex flex-row space-x-6">
                 <a href="https://github.com/ArcadeLabsInc/openagents" target="_blank">
                     <Button variant="outline" size="icon" className="shadow-xl">
@@ -16,11 +16,10 @@ export default function Splash() {
                 </a>
                 <a href="https://twitter.com/GPUtopia/status/1721942435125715086" target="_blank">
                     <Button variant="outline" size="icon" className="shadow-xl">
-                        <TwitterLogoIcon className="w-6 h-6" />
+                        <img src="/images/x-logo-black.png" className="w-6 h-6" />
                     </Button>
                 </a>
             </div>
-
         </div>
     </>
 }
