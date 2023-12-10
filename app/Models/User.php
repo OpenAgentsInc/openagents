d129b51180af68a5f8eb349c8e0ef7b6759820e9
Christopher David <chris@arcadelabs.co>
2023-12-10 14:32:25 -0800

    Add GitHub login (#36)
    * composer require laravel/socialite
    
    * set up github secrests and redirect
    
    * initial login
    
    * Create user with that info
    
    * Re-add email_verified_at

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'github_id',
+        'github_nickname',
+        'github_avatar',
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->bigInteger('github_id')->unique()->nullable();
+            $table->string('github_nickname')->nullable();
+            $table->string('github_avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
