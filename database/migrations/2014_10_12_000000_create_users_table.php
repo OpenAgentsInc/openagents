0606cc005ad5878a63b258f1c7e8b3b16c586f22
Christopher David <chris@arcadelabs.co>
2023-12-10 16:30:00 -0600

    Re-add email_verified_at

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique()->nullable(); // Making email nullable
-            $table->string('password')->nullable(); // Making password nullable
-            $table->bigInteger('github_id')->unique()->nullable(); // GitHub ID
-            $table->string('github_nickname')->nullable(); // GitHub Nickname
-            $table->string('github_avatar')->nullable(); // GitHub Avatar URL
+            $table->string('email')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->nullable();
+            $table->bigInteger('github_id')->unique()->nullable();
+            $table->string('github_nickname')->nullable();
+            $table->string('github_avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
