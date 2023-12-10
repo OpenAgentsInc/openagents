9cda6133bd2452dcfdb6be921f84defc2cf95381
Christopher David <chris@arcadelabs.co>
2023-12-10 17:49:08 -0600

    post to trigger the run via axios

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -3,6 +3,7 @@ export interface User {
     name: string;
     email: string;
     email_verified_at: string;
+    github_nickname: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
