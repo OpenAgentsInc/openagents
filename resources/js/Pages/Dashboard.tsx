0adc552e5942651d27a03b04a5d56f05dbf6c930
Christopher David <chris@arcadelabs.co>
2023-12-11 13:48:11 -0600

    Simplify styling

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -17,13 +17,14 @@ export default function Dashboard({ auth }: PageProps) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
+            header={null}
+            // header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="min-h-[400px] bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="min-h-[400px] bg-white dark:bg-gray-800 overflow-hidden sm:rounded-lg">
                         {auth.user?.github_nickname === 'AtlantisPleb' ? (
                             <div className="p-6">
                                 <Button onClick={triggerRun}>Go Faerie</Button>
