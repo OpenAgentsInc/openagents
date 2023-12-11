b29b4a68846b4b6901b4b53b6e10c87d2a35ea39
Christopher David <chris@arcadelabs.co>
2023-12-11 13:50:27 -0600

    Show authed user avatar

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -4,6 +4,7 @@ export interface User {
     email: string;
     email_verified_at: string;
     github_nickname: string;
+    github_avatar: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
