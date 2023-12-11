90db2caa6aae084b260900cd7605e8f3d89af442
Christopher David <chris@arcadelabs.co>
2023-12-11 13:50:27 -0600

    Show authed user avatar

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -37,6 +37,8 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                                                 type="button"
                                                 className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
                                             >
+                                            <img src={user.github_avatar} alt={user.name} className="h-8 w-8 mr-3 rounded-full" />
+
                                                 {user.name}
 
                                                 <svg
