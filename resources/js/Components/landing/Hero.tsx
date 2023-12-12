2c90874c0832d1dcdf7cf5ed6fa58c4eebd0f5cc
Christopher David <chris@arcadelabs.co>
2023-12-12 15:10:08 -0600

    simplify

--- resources/js/Components/landing/Hero.tsx
+++ resources/js/Components/landing/Hero.tsx
@@ -1,15 +1,13 @@
 import { GitHubLogoIcon } from '@radix-ui/react-icons'
 import { Button } from '../ui/button'
-import { BackgroundImage } from './BackgroundImage'
 import { Container } from './Container'
 
 export function Hero() {
     return (
         <div className="flex min-h-screen relative py-20 sm:pb-24 sm:pt-36">
-            {/* <BackgroundImage className="-bottom-14 -top-36" /> */}
             <Container className="flex-grow flex justify-center items-center relative">
                 <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
-                    <h1 className="font-display text-5xl font-bold tracking-tighter text-indigo-600">
+                    <h1 className="sm:mt-0 mt-6 font-display text-5xl font-bold tracking-tighter text-indigo-600">
                         <span className="sr-only">OpenAgents - </span>An open platform for AI agents
                     </h1>
                     <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-black">
@@ -23,7 +21,7 @@ export function Hero() {
                             A closed-source megacorp with a history of monopolization and regulatory capture?
                         </p>
                         <p>
-                            Or an open cloud built on open-source models and open data?
+                            Or an open cloud built on open models and open data?
                         </p>
                         <p>
                             See you in January!
