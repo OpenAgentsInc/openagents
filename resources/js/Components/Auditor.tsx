0ed1c1a8366b6493654e2f569d4f526e30a443b7
Christopher David <chris@arcadelabs.co>
2023-12-13 11:25:19 -0600

    nice card but dont need depth yet

--- resources/js/Components/Auditor.tsx
+++ resources/js/Components/Auditor.tsx
@@ -1,3 +1,48 @@
+import { Button } from "./ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select"
+
 export const Auditor = () => {
-  return <p>Let's audit some stuff</p>
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Audit a codebase</CardTitle>
+        <CardDescription>Run an audit of your GitHub codebase</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Repo</Label>
+              <Input id="name" defaultValue="OpenAgentsInc/openagents" placeholder="OpenAgentsInc/openagents" />
+            </div>
+            {/* <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Audit level</Label>
+              <Select defaultValue="basic">
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="basic">Basic</SelectItem>
+                  <SelectItem value="deep">Deep</SelectItem>
+                </SelectContent>
+              </Select>
+            </div> */}
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        {/* <Button variant="outline">Cancel</Button> */}
+        <Button>Start</Button>
+      </CardFooter>
+    </Card>
+  )
 }
