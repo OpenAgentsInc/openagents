c97edfaf32ef661ebd183a2ee94274a937c51266
Christopher David <chris@arcadelabs.co>
2023-12-13 11:13:43 -0600

    set up clean auditor component

---
+++ resources/js/Components/Auditor.tsx
@@ -0,0 +1,3 @@
+export const Auditor = () => {
+  return <p>Let's audit some stuff</p>
+}
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,9 +1,14 @@
+import { Auditor } from '@/Components/Auditor'
+import { Container } from '@/Components/landing/Container'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
 import { PageProps } from '@/types'
 
 export default function Dashboard({ auth }: PageProps) {
   return (
     <AuthenticatedLayout user={auth.user}>
+      <Container className="mt-12">
+        <Auditor />
+      </Container>
     </AuthenticatedLayout>
   )
 }
