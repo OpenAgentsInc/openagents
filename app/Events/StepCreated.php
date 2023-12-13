f9562a236e1cd2a80025634d8e1ddae88c14894b
Christopher David <chris@arcadelabs.co>
2023-12-13 16:38:41 -0600

    ShouldBRoadcast not now

--- app/Events/StepCreated.php
+++ app/Events/StepCreated.php
@@ -6,11 +6,11 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class StepCreated implements ShouldBroadcastNow
+class StepCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
