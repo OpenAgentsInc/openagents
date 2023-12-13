028629d859d60358a4dbf78bde858d1631121ccb
Christopher David <chris@arcadelabs.co>
2023-12-13 17:28:24 -0600

    ya

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,8 +28,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
-                //
+            return in_array($user->github_nickname, [
+                "AtlantisPleb"
             ]);
         });
     }
