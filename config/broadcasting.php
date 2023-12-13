6104dceaf790a7d7f6ce483ec5757a326644b94f
Christopher David <chris@arcadelabs.co>
2023-12-13 17:42:42 -0600

    try different settings

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID', 'app-id'),
             'options' => [
                 'host' => '127.0.0.1', //'socket.openagents.com', // env('PUSHER_HOST', '127.0.0.1'),
-                'port' => 6002, // env('PUSHER_PORT', 6001),
-                'scheme' => 'http', // env('PUSHER_SCHEME', 'http'),
+                'port' => 6001, // env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
                 'encrypted' => true,
-                'useTLS' => false, // env('PUSHER_SCHEME') === 'https',
+                'useTLS' => env('PUSHER_SCHEME') === 'https',
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
