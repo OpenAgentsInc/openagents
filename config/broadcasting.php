aef32bf41fc1e3e1955f8bdd4b049d3a2a3af303
Christopher David <chris@arcadelabs.co>
2023-12-13 17:28:01 -0600

    install horizon

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,7 +36,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
             'app_id' => env('PUSHER_APP_ID', 'app-id'),
             'options' => [
-                'host' => 'socket.openagents.com', // env('PUSHER_HOST', '127.0.0.1'),
+                'host' => '127.0.0.1', //'socket.openagents.com', // env('PUSHER_HOST', '127.0.0.1'),
                 'port' => 6002, // env('PUSHER_PORT', 6001),
                 'scheme' => 'http', // env('PUSHER_SCHEME', 'http'),
                 'encrypted' => true,
