ead4c85c22eb532499d6bc9045ec5a516b2a616e
Christopher David <chris@arcadelabs.co>
2023-12-20 19:31:13 -0600

    Update logo

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -1,7 +1,13 @@
-import { SVGAttributes } from 'react';
-
-export default function ApplicationLogo(props: SVGAttributes<SVGElement>) {
-    return (
-        <h1 className="text-xl font-thin tracking-widest">OpenAgents</h1>
-    );
+export default function ApplicationLogo() {
+  const strokeColor = "black"
+  const strokeWidth = 14
+  return (
+    <div className="flex flex-row items-center" >
+      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="h-9 w-auto">
+        <circle cx="100" cy="100" r="90" stroke={strokeColor} strokeWidth={strokeWidth} fill="none" />
+        <path d="M 30 150 L 100 78 L 170 150" stroke={strokeColor} strokeWidth={strokeWidth} fill="none" />
+      </svg>
+      <h1 className="ml-1 text-2xl font- tracking-tight">Open<span className="font-bold">Agents</span></h1>
+    </div>
+  );
 }
--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -17,7 +17,7 @@ export default function Authenticated({ user, header, children }: PropsWithChild
             <div className="flex">
               <div className="shrink-0 flex items-center">
                 <Link href="/">
-                  <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                  <ApplicationLogo />
                 </Link>
               </div>
 
