325f1586a4388507bbcb6e5a3f70cfdf33a6b9c4
Christopher David <chris@arcadelabs.co>
2023-12-20 21:36:20 -0600

    decent cards in dashboard

--- resources/js/Components/UserRuns.tsx
+++ resources/js/Components/UserRuns.tsx
@@ -1,5 +1,7 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/Components/catalyst/table'
 import { Link } from './catalyst/link'
+import { Card, CardContent, CardFooter, CardHeader } from './ui/card'
+import { Text } from './catalyst/text'
 
 export function UserRuns({ runs }) {
   // Sort runs in reverse chronological order
@@ -9,13 +11,23 @@ export function UserRuns({ runs }) {
   })
 
   return (
-    <Table>
+    <Table className="-mt-4">
       <TableBody>
         {runs.map((run) => (
           <TableRow key={run.id}>
             <TableCell className="font-medium">
               <Link href={route('inspect-run', run.id)} className="block">
-                {run.created_at}
+                <Card>
+                  <CardHeader>
+                    Run #{run.id}
+                  </CardHeader>
+                  <CardContent className="-my-6">
+                    <Text>{run.description}</Text>
+                  </CardContent>
+                  <CardFooter>
+                    <Text className="opacity-75 mt-1">{timeSince(run.created_at)} ago</Text>
+                  </CardFooter>
+                </Card>
               </Link>
             </TableCell>
           </TableRow>
@@ -23,4 +35,30 @@ export function UserRuns({ runs }) {
       </TableBody>
     </Table>
   )
+}
+
+function timeSince(date) {
+  const seconds = Math.floor((new Date() - new Date(date)) / 1000);
+
+  let interval = seconds / 31536000;
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
 }
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -8,7 +8,7 @@ export default function Dashboard({ auth, runs }: PageProps) {
     <AuthenticatedLayout user={auth.user}>
       <div className="flex min-h-full flex-col">
         <div className="mx-auto flex w-full max-w-7xl items-start gap-x-8 px-4 py-10 sm:px-6 lg:px-8">
-          <aside className="sticky top-8 hidden w-44 shrink-0 lg:block">
+          <aside className="sticky top-8 hidden w-72 shrink-0 lg:block">
             <UserRuns runs={runs} />
           </aside>
           <main className="flex-1">
