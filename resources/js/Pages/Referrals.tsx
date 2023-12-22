5b7f80a260726930b5d14bc07e9e4cf2f81903d4
Christopher David <chris@arcadelabs.co>
2023-12-22 12:20:28 -0600

    Referral link card

--- resources/js/Pages/Referrals.tsx
+++ resources/js/Pages/Referrals.tsx
@@ -1,14 +1,29 @@
 import { usePage } from "@inertiajs/react"
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../Components/ui/card"
+import { Button } from "@/Components/ui/button"
 
 export default function Referrals() {
   const props = usePage().props as any
   const referrals = props.referrals
   return <AuthenticatedLayout user={props.auth.user}>
-    <h1>Your referral link:</h1>
-    <p>https://openagents.com/?r={props.auth.user.github_nickname}</p>
+
+    <Card className="mt-24 mx-auto w-[350px]">
+      <CardHeader>
+        <CardTitle>Your referral link</CardTitle>
+        <CardDescription>Earn bitcoin when your referrals spend</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <p>https://openagents.com/?r={props.auth.user.github_nickname}</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Share</Button>
+      </CardFooter>
+    </Card>
 
     <h1>Referrals ({referrals.length})</h1>
+
     <table>
       <tbody>
         {referrals.map((referral: any) => {
