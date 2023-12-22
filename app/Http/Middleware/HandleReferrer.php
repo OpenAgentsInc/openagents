baf1561689928220fef08c02fcf696db408f31d6
Christopher David <chris@arcadelabs.co>
2023-12-22 11:23:52 -0600

    handlereferrer seems to work

---
+++ app/Http/Middleware/HandleReferrer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HandleReferrer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $uri = $request->server->get('REQUEST_URI');
+
+        if (\strpos($uri, "?r=") !== false) {
+            $ref = $request->query->get('r');
+            session()->put('r', $ref);
+            $urlminusquerystring = explode('?', $uri)[0];
+            return redirect($urlminusquerystring);
+        }
+
+        return $next($request);
+    }
+}
