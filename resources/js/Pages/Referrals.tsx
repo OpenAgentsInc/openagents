8322d438b8f27239cf5480631a959a236b3fd57e
Christopher David <chris@arcadelabs.co>
2023-12-22 12:11:42 -0600

    Show your referral link

--- resources/js/Pages/Referrals.tsx
+++ resources/js/Pages/Referrals.tsx
@@ -3,8 +3,12 @@ import { usePage } from "@inertiajs/react"
 export default function Referrals() {
   const props = usePage().props as any
   const referrals = props.referrals
-  console.log(referrals)
+  console.log(props)
   return <>
+
+    <h1>Your referral link:</h1>
+    <p>https://openagents.com/?r={props.auth.user.github_nickname}</p>
+
     <h1>Referrals ({referrals.length})</h1>
     <table>
       <tbody>
