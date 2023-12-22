3ceb9de913860781ad600de3b04a5970f7d892c9
Christopher David <chris@arcadelabs.co>
2023-12-22 10:50:39 -0600

    Stats page shows user count and balance sum

--- tests/Feature/StatsTest.php
+++ tests/Feature/StatsTest.php
@@ -3,8 +3,8 @@
 use App\Models\User;
 use Inertia\Testing\AssertableInertia as Assert;
 
-test('stats page knows number of users', function () {
-    $user = User::factory(2)->create();
+test('stats page knows number of users and balances', function () {
+    $user = User::factory(2)->create(['balance' => 1000]);
     $this->get('stats')
         ->assertInertia(
             fn (Assert $page) => $page
@@ -12,5 +12,8 @@ test('stats page knows number of users', function () {
             ->has('userCount')
             ->where('userCount', User::count())
             ->where('userCount', 2)
+            ->has('userBalanceSum')
+            ->where('userBalanceSum', User::sum('balance'))
+            ->where('userBalanceSum', 2000)
         );
 });
