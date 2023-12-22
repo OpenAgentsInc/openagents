eb65acb0fe93292fa1afd74630564e2db2f0ae62
Christopher David <chris@arcadelabs.co>
2023-12-22 12:08:03 -0600

    List referral usernames

--- resources/js/Pages/Referrals.tsx
+++ resources/js/Pages/Referrals.tsx
@@ -1,3 +1,19 @@
+import { usePage } from "@inertiajs/react"
+
 export default function Referrals() {
-  return <>referrals!</>
+  const props = usePage().props as any
+  const referrals = props.referrals
+  console.log(referrals)
+  return <>
+    <h1>Referrals ({referrals.length})</h1>
+    <table>
+      <tbody>
+        {referrals.map((referral: any) => {
+          return <tr key={referral.id}>
+            <td>{referral.github_nickname}</td>
+          </tr>
+        })}
+      </tbody>
+    </table>
+  </>
 }
