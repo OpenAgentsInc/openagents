f53c7d59e20d2c6e06d2e74eb5c01e63445e9ce2
Christopher David <chris@arcadelabs.co>
2023-12-23 19:18:04 -0600

    Refactor to Trail / add missing rafz lib

--- resources/js/Components/chat/MessagesList.tsx
+++ resources/js/Components/chat/MessagesList.tsx
@@ -1,26 +1,35 @@
-import { useTransition, animated } from '@react-spring/web'
-import { useEffect } from 'react'
+import React from 'react'
+import { animated as a, useTrail } from '@react-spring/web'
 
 export const MessagesList = ({ messages }) => {
-  const [transitions, api] = useTransition(messages, () => ({
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 1 },
-  }))
-
-  useEffect(() => {
-    api.start()
-  }, [messages])
-
-  return transitions((style, item) => (
-    <animated.div className="w-full whitespace-pre-wrap mb-4 last:mb-0" style={style}>
-      {item}
-    </animated.div>
-  ))
+  return (
+    <div>
+      <Trail open={true}>
+        {messages.map((message, i) => (
+          <div key={i} className="whitespace-pre-wrap mb-4 last:mb-0">
+            <span>{message}</span>
+          </div>
+        ))}
+      </Trail>
+    </div>
+  )
 }
 
-// {messages.map((message, i) => (
-//   <div key={i} className="whitespace-pre-wrap mb-4 last:mb-0">
-//     <span>{message}</span>
-//   </div>
-// ))}
+const Trail: React.FC<{ open: boolean, children: any }> = ({ open, children }) => {
+  const items = React.Children.toArray(children)
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
+  return (
+    <div>
+      {trail.map(({ height, ...style }, index) => (
+        <a.div key={index} style={style}>
+          <a.div style={{ height }}>{items[index]}</a.div>
+        </a.div>
+      ))}
+    </div>
+  )
+}
