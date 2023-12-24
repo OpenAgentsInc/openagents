d4fe773afde53c857a315c00856607da36b659d0
Christopher David <chris@arcadelabs.co>
2023-12-24 10:28:55 -0600

    Clean up StreamController

--- app/Http/Controllers/StreamController.php
+++ app/Http/Controllers/StreamController.php
@@ -7,52 +7,10 @@ use App\Events\ChatTokenReceived;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
-// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-
-// use Illuminate\Support\Facades\Log;
-// use Nyholm\Psr7\Factory\Psr17Factory;
 
 class StreamController extends Controller
 {
-    public function readLine($stream)
-    {
-        $line = '';
-        while (!$stream->eof()) {
-            $char = $stream->read(1);
-            if ($char === "\n") {
-                break;
-            }
-            $line .= $char;
-        }
-        return $line;
-    }
-
-    public function readStream($stream)
-    {
-        while (!$stream->eof()) {
-            $line = $this->readLine($stream);
-
-            if (!str_starts_with($line, 'data:')) {
-                continue;
-            }
-
-            $data = trim(substr($line, strlen('data:')));
-
-            if ($data === '[DONE]') {
-                break;
-            }
-
-            $response = json_decode($data, true, JSON_THROW_ON_ERROR);
-
-            if (isset($response['error'])) {
-                throw new \Exception($response['error']);
-            }
-
-            yield $response;
-        }
-    }
-
     public function streamTokens()
     {
         try {
@@ -83,7 +41,6 @@ class StreamController extends Controller
 
             // Reading the streamed response
             $stream = $response->getBody();
-            // $content = '';
 
             foreach ($this->readStream($stream) as $responseLine) {
                 // $content .= $responseLine;
@@ -92,141 +49,47 @@ class StreamController extends Controller
                 dump($token);
                 broadcast(new ChatTokenReceived($token));
             }
-
-            // dump($content);
-            // dump($content);
-
-            // $stream = $response->getBody();
-            // while (!$stream->eof()) {
-            //     $line = $this->readLine($stream);
-
-            //     if (!str_starts_with($line, 'data:')) {
-            //         continue;
-            //     }
-
-            //     $data = trim(substr($line, strlen('data:')));
-
-            //     if ($data === '[DONE]') {
-            //         break;
-            //     }
-
-            //     $response = json_decode($data, true, JSON_THROW_ON_ERROR);
-            //     dump($response);
-            //     broadcast(new ChatTokenReceived($chunk));
-
-            //     if (isset($response['error'])) {
-            //         throw new \Exception($response['error']);
-            //     }
-
-            //     yield $response;
-            // }
-            // while (!$stream->eof()) {
-            //     echo $stream->read(1024);
-            // }
         } catch (RequestException $e) {
             // Handle exception or errors here
             echo $e->getMessage();
         }
     }
 
-
-    public function streamTokens2(Request $request)
+    public function readStream($stream)
     {
-        $apiUrl = 'https://api.together.xyz/inference';
-        $model = 'togethercomputer/RedPajama-INCITE-7B-Instruct';
+        while (!$stream->eof()) {
+            $line = $this->readLine($stream);
 
-        $payload = [
-            "model" => $model,
-            "prompt" => "Alan Turing was",
-            "max_tokens" => 128,
-            "stop" => ["\n\n"],
-            "temperature" => 0.7,
-            "top_p" => 0.7,
-            "top_k" => 50,
-            "repetition_penalty" => 1,
-            "stream_tokens" => true
-        ];
-
-        dump($payload);
-
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . env('TOGETHER_API_KEY'),
-            'Content-Type' => 'application/json',
-        ])->withOptions([
-            'stream' => true,
-        ])
-        ->post($apiUrl, $payload);
-
-
-
-
-        if ($response->successful()) {
-            dump("Response was successful");
-            // Open a stream to read the response
-            $stream = $response->toStream();
-
-            // Read the stream content chunk by chunk
-            while (!$stream->eof()) {
-                dump('in here');
-                $chunk = $stream->read(1024); // Read 1 KB at a time (adjust as needed)
-                // Process or output the chunk as needed
-                dump($chunk);
-                broadcast(new ChatTokenReceived($chunk));
-                echo $chunk;
+            if (!str_starts_with($line, 'data:')) {
+                continue;
             }
 
-            // Close the stream when done
-            $stream->close();
-        } else {
-            dump("Response was not successful");
-            // Handle the HTTP error response
-            $statusCode = $response->status();
-            dump($statusCode);
-            $errorMessage = $response->body();
-            dump($errorMessage);
-            // Handle the error as needed
-            // You can log, throw exceptions, or perform other error handling here
+            $data = trim(substr($line, strlen('data:')));
+
+            if ($data === '[DONE]') {
+                break;
+            }
+
+            $response = json_decode($data, true, JSON_THROW_ON_ERROR);
+
+            if (isset($response['error'])) {
+                throw new \Exception($response['error']);
+            }
+
+            yield $response;
         }
+    }
 
-
-
-
-        // dump("Did request");
-
-        // $response->throw();
-
-        // Convert Laravel HTTP client response to PSR-7 response
-        // $psr17Factory = new Psr17Factory();
-        // $psrResponse = $psr17Factory->createResponse(
-        //     $response->status(),
-        //     $response->body(),
-        //     $response->headers()
-        // );
-
-        // dump('did psr thing');
-        // $body = $response->toPsrResponse()->getBody();
-        // dump('got body');
-        // dump($body);
-
-        // $streamResponse = new StreamResponse($body);
-        // dump('got psr response');
-        // foreach ($streamResponse->getIterator() as $tokenData) {
-
-        //     dump('in a loop');
-        //     dump($tokenData);
-
-        //     // Check for final message
-        //     if (isset($tokenData['data']) && $tokenData['data'] === '[DONE]') {
-        //         // Final message handling here
-        //         break;
-        //     }
-
-        //     // Process the streaming token data here
-        //     $token = $tokenData["choices"][0]["text"];
-        //     // You can do something with $token here (e.g., save to a database, return as a response, etc.)
-
-        //     // Broadcast to the Chat channel
-        //     broadcast(new ChatTokenReceived($token));
-        // }
+    public function readLine($stream)
+    {
+        $line = '';
+        while (!$stream->eof()) {
+            $char = $stream->read(1);
+            if ($char === "\n") {
+                break;
+            }
+            $line .= $char;
+        }
+        return $line;
     }
 }
