d8350b9308b3e01d980d368b8021601659eb183a
Christopher David <chris@arcadelabs.co>
2023-12-23 23:18:04 -0600

    Try streaming chat tokens to wave

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/login/github', [AuthController::class, 'loginGithub']);
 Route::get('/github', [AuthController::class, 'githubCallback']);
 
+Route::get('/stream', [StreamController::class, 'streamTokens']);
+
 Route::get('/chat', function () {
     return Inertia::render('Chat');
 });
