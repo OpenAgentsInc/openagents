b614b5730f3cc9db81f7af44fb5074dda99085f8
Christopher David <chris@arcadelabs.co>
2023-12-24 16:52:13 -0600

    Put Card in list

--- resources/js/Pages/Nodes.tsx
+++ resources/js/Pages/Nodes.tsx
@@ -1,9 +1,25 @@
 import { DraggableList } from "@/Components/nodes/DraggableList";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/Components/ui/card";
 
 export default function Nodes() {
+  const items = [
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+      </CardHeader>
+      <CardDescription>
+        Lorem ipsum dolor sit
+      </CardDescription>
+      <CardContent>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+      </CardContent>
+    </Card>,
+    <p className="font-thin">World</p>,
+  ]
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <DraggableList items={'Lorem ipsum dolor sit'.split(' ')} />
+      <DraggableList items={items} />
     </div>
   )
 }
