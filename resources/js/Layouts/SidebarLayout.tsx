bacf656d82d6792430668cf78f0e4d3b3cdd5f24
Christopher David <chris@arcadelabs.co>
2023-12-24 21:55:43 -0600

    axe home link from sidebar

--- resources/js/Layouts/SidebarLayout.tsx
+++ resources/js/Layouts/SidebarLayout.tsx
@@ -8,10 +8,6 @@ export const SidebarLayout = ({ children }) => {
     <div className="h-dscreen w-full md:h-screen">
       <div className="flex h-dscreen bg-neutral-50">
         <div className="space-y-4 hidden w-22 flex-col items-center border-r border-neutral-300 p-3 pt-5 lg:flex">
-          <Link href="/" className="cursor-pointer mb-1 flex h-16 w-16 flex-col items-center justify-center rounded-xl text-neutral-900 hover:bg-neutral-200 hover:text-neutral-900">
-            <HomeIcon className="h-6 w-6" />
-            <div className="t-label mt-2">Home</div>
-          </Link>
           <Link href="/chat" className="cursor-pointer mb-1 flex h-16 w-16 flex-col items-center justify-center rounded-xl text-neutral-900 hover:bg-neutral-200 hover:text-neutral-900">
             <IconMessage className="h-6 w-6" />
             <div className="t-label mt-2">Chat</div>
