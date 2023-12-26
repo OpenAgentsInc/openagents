bd5a435bba20ccd916d3c8e6b9985c9a4101702b
Christopher David <chris@arcadelabs.co>
2023-12-25 20:56:36 -0600

    Refactor to Step/StepExecuted (#48)
    * New StepExecuted/TaskExecuted models
    
    * Step refactor
    
    * Step belongs to Task not Run
    
    * Initial StepExecuted test and factory
    
    * StepExecuted nice
    
    * Axe Runs
    
    * Axe Artifacts
    
    * Clean up tests
    
    * Step has order
    
    * StepExecuted has order
    
    * Initial ConciergeSeeder
    
    * Seed all Concierge steps

---
+++ database/seeders/ConciergeSeeder.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Agent;
+use App\Models\Step;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConciergeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // If there's a User, use that. If not, create a user via factory.
+        $user = User::find(1);
+        if (!$user) {
+            $user = User::factory()->create();
+        }
+
+        // Create Concierge agent
+        $agent = Agent::create([
+            'user_id' => $user->id,
+            'name' => 'The Concierge',
+        ]);
+
+        // Create main chat task
+        $task = Task::create([
+            'agent_id' => $agent->id,
+            'description' => 'Respond to user chat message after consulting knowledge base'
+        ]);
+
+        // Create the steps
+        $step1 = Step::create([
+            'agent_id' => $agent->id,
+            'category' => 'validation',
+            'description' => 'Ensure input is a valid chat message',
+            'entry_type' => 'input',
+            'error_message' => 'Could not validate input',
+            'name' => 'Validate Input',
+            'order' => 1,
+            'success_action' => 'next_node',
+            'task_id' => $task->id,
+        ]);
+
+        $step2 = Step::create([
+            'agent_id' => $agent->id,
+            'category' => 'embedding',
+            'description' => 'Convert input to vector embedding',
+            'entry_type' => 'node',
+            'error_message' => 'Could not generate embedding',
+            'name' => 'Embed Input',
+            'order' => 2,
+            'success_action' => 'next_node',
+            'task_id' => $task->id,
+        ]);
+
+        $step3 = Step::create([
+            'agent_id' => $agent->id,
+            'category' => 'similarity_search',
+            'description' => 'Compare input to knowledge base',
+            'entry_type' => 'node',
+            'error_message' => 'Could not run similarity search',
+            'name' => 'Similarity Search',
+            'order' => 3,
+            'success_action' => 'next_node',
+            'task_id' => $task->id,
+        ]);
+
+        $step4 = Step::create([
+            'agent_id' => $agent->id,
+            'category' => 'inference',
+            'description' => 'Call to LLM to generate response',
+            'entry_type' => 'node',
+            'error_message' => 'Could not call to LLM',
+            'name' => 'Call LLM',
+            'order' => 4,
+            'success_action' => 'json_response',
+            'task_id' => $task->id,
+        ]);
+    }
+}
