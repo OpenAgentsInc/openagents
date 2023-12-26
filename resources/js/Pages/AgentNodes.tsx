72856105b36a3aac87589840ec48147590b90e77
Christopher David <chris@arcadelabs.co>
2023-12-25 23:41:28 -0500

    Initial show steps

--- resources/js/Pages/AgentNodes.tsx
+++ resources/js/Pages/AgentNodes.tsx
@@ -1,5 +1,6 @@
 import { SidebarLayout } from "@/Layouts/SidebarLayout"
 import { usePage } from "@inertiajs/react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/Components/ui/card';
 
 // We show all relevant agent nodes, starting with steps.
 
@@ -8,6 +9,7 @@ interface Agent {
 }
 
 interface Task {
+  description: string
   steps: Step[]
 }
 
@@ -33,8 +35,43 @@ function AgentNodes() {
   const task = agent.tasks[0] as Task
   const steps = task.steps as Step[]
   console.log(steps)
+  // Show a Card per Step
   return (
     <div className="flex flex-col justify-center items-center h-screen">
+      <div className="pt-6 px-8 rounded-lg">
+        <Card>
+          <CardHeader>
+            <CardTitle>Task</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{task.description}</p>
+          </CardContent>
+        </Card>
+        <div className="my-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {steps.map((step, index) => (
+              <Card key={index}
+                onClick={() => {
+                  // router.get(`/step/${step.id}`)
+                }}
+
+                style={{ cursor: 'pointer' }}
+              >
+                <CardHeader>
+                  <CardTitle>Step {index + 1}</CardTitle>
+                  <CardDescription>{step.description}</CardDescription>
+                </CardHeader>
+                {/* <CardContent>
+                  <div className="flex justify-between items-center">
+                    <span className={`text-${step.status === 'success' ? 'green' : 'red'}-400`}>{step.status}</span>
+                  </div>
+                </CardContent> */}
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
