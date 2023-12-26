548d9ba2a3a25b95cf98aa8107f76ef90e47bb74
Christopher David <chris@arcadelabs.co>
2023-12-25 23:38:00 -0500

    AgentNodes types

--- resources/js/Pages/AgentNodes.tsx
+++ resources/js/Pages/AgentNodes.tsx
@@ -1,8 +1,38 @@
 import { SidebarLayout } from "@/Layouts/SidebarLayout"
 import { usePage } from "@inertiajs/react"
 
+// We show all relevant agent nodes, starting with steps.
+
+interface Agent {
+  tasks: Task[]
+}
+
+interface Task {
+  steps: Step[]
+}
+
+interface Step {
+  agent_id: number
+  category: string
+  created_at: string
+  description: string
+  entry_type: string
+  error_message: string
+  id: number
+  name: string
+  order: number
+  params: any
+  success_action: string
+  task_id: number
+  updated_at: string
+}
+
 function AgentNodes() {
-  const { agent } = usePage().props
+  const props = usePage().props as any
+  const agent = props.agent as Agent
+  const task = agent.tasks[0] as Task
+  const steps = task.steps as Step[]
+  console.log(steps)
   return (
     <div className="flex flex-col justify-center items-center h-screen">
     </div>
