874104865cdf885d9e33b4570a9ad25aecbafca5
Christopher David <chris@arcadelabs.co>
2023-12-27 11:21:09 -0500

    Fill canvas

--- resources/js/Pages/Graph.tsx
+++ resources/js/Pages/Graph.tsx
@@ -1,3 +1,28 @@
+import { LGraph, LGraphCanvas, LiteGraph } from 'litegraph.js'
+import 'litegraph.js/css/litegraph.css'
+import { useEffect } from 'react'
+
 export default function Graph() {
-  return <>Graph</>
+  useEffect(() => {
+    var graph = new LGraph();
+
+    var canvas = new LGraphCanvas("#mycanvas", graph, { autoresize: true });
+    canvas.resize()
+
+    var node_const = LiteGraph.createNode("basic/const");
+    node_const.pos = [120, 200];
+    graph.add(node_const);
+    node_const.setValue(4.5);
+
+    var node_watch = LiteGraph.createNode("basic/watch");
+    node_watch.pos = [700, 200];
+    graph.add(node_watch);
+
+    node_const.connect(0, node_watch, 0);
+
+    graph.start()
+  }, [])
+  return (
+    <canvas id="mycanvas" className="w-screen h-screen" />
+  )
 }
