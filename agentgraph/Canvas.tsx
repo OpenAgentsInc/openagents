6470a7b3a2508a7233b5ec321971a5addd642ae5
Christopher David <chris@arcadelabs.co>
2023-12-30 18:08:37 -0500

    use styledRoot

--- agentgraph/Canvas.tsx
+++ agentgraph/Canvas.tsx
@@ -1,7 +1,19 @@
+import { StyledRoot } from "./components/Agentgraph/StyledRoot"
+import { ThemeContext } from "./context"
+import { useDeepMemo } from "./hooks/useDeepMemo"
+import { getDefaultTheme, mergeTheme } from "./styles"
+
 export const Canvas = ({ children }: { children: any }) => {
+  const theme = getDefaultTheme()
+  console.log(theme)
+  const themeContext = useDeepMemo(() => mergeTheme(theme), [theme])
   return (
     <div className="w-full h-full">
-      {children}
+      <ThemeContext.Provider value={themeContext}>
+        <StyledRoot>
+          {children}
+        </StyledRoot>
+      </ThemeContext.Provider>
     </div>
   )
 }
