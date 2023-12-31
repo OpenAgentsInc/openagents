3294daf152876ac728b503937f8c2ce743e5de8b
Christopher David <chris@arcadelabs.co>
2023-12-31 13:03:13 -0500

    Add dark purple theme (#54)
    * some hooks
    
    * use styledRoot
    
    * Styled controls
    
    * shh
    
    * Swap in leva titlebar
    
    * fuckaround
    
    * panels darker
    
    * disable filter
    
    * darker bg grid
    
    * purple maybe

--- agentgraph/TreeWrapper.tsx
+++ agentgraph/TreeWrapper.tsx
@@ -1,22 +1,27 @@
 import { Control } from "./Control"
+import { StyledContent, StyledWrapper } from "./components/Folder/StyledFolder"
 
 export type Tree = {
   [key: string]: { path: string, data: any } | Tree
 }
 
 type TreeWrapperProps = {
+  isRoot?: boolean
+  toggled: boolean
   tree: Tree
 }
 
-export const TreeWrapper = ({ tree }: TreeWrapperProps) => {
+export const TreeWrapper = ({ isRoot, toggled, tree }: TreeWrapperProps) => {
   const entries = Object.entries(tree)
   // console.log("TreeWrapper with entries:", entries)
   return (
-    <>
-      {entries.map(([key, value]) => (
-        // @ts-ignore
-        <Control key={key} path={value.path} data={value.data} />
-      ))}
-    </>
+    <StyledWrapper isRoot={isRoot}>
+      <StyledContent toggled={toggled}>
+        {entries.map(([key, value]) => (
+          // @ts-ignore
+          <Control key={key} path={value.path} data={value.data} />
+        ))}
+      </StyledContent>
+    </StyledWrapper>
   )
 }
