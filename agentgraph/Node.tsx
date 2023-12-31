cfdf2d61500796768f1382acc16264be1339aee8
Christopher David <chris@arcadelabs.co>
2023-12-31 13:03:13 -0500

    Add dark purple theme (#54)
    * some hooks
    
    * use styledRoot
    
    * Styled controls
    
    * shh
    
    * Swap in leva titlebar
    
    * fuckaround
    
    * panels darker
    
    * disable filter
    
    * darker bg grid
    
    * purple maybe

--- agentgraph/Node.tsx
+++ agentgraph/Node.tsx
@@ -1,10 +1,10 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useTransform } from './hooks/useTransform'
-import { TitleBar } from './TitleBar';
 import { buildTree } from './tree'
 import { Tree, TreeWrapper } from './TreeWrapper'
 import { PanelSettingsContext } from './context';
-import { globalStyles } from './styles';
+import { globalStyles, styled } from './styles';
+import { TitleWithFilter } from './components/Agentgraph/Filter';
 
 interface NodeProps {
   data: any // All data passed to the node will be rendered
@@ -25,9 +25,12 @@ export const Node = React.memo(
     },
     hideCopyButton = false,
   }: NodeProps) => {
+    const [filter, setFilter] = useState('')
+    const [toggled, setToggle] = useState(true)
     const tree = useMemo(() => buildTree(data), [data]) as Tree
     const [rootRef, set, currentPos] = useTransform<HTMLDivElement>()
     const drag = typeof titleBar === 'object' ? titleBar.drag ?? true : true
+    const filterEnabled = typeof titleBar === 'object' ? titleBar.filter ?? true : true
     const position = typeof titleBar === 'object' ? titleBar.position || undefined : undefined
     const onDragStart = typeof titleBar === 'object' ? titleBar.onDragStart || undefined : undefined
     const onDragEnd = typeof titleBar === 'object' ? titleBar.onDragEnd || undefined : undefined
@@ -39,9 +42,9 @@ export const Node = React.memo(
 
     return (
       <PanelSettingsContext.Provider value={{ hideCopyButton }}>
-        <div ref={rootRef} className="absolute w-64 bg-gray-200 rounded-lg shadow-lg">
+        <PanelMaybe ref={rootRef} className="absolute w-64 rounded-lg shadow-lg">
           {titleBar && (
-            <TitleBar
+            <TitleWithFilter
               onDrag={(point) => {
                 set(point)
               }}
@@ -49,18 +52,30 @@ export const Node = React.memo(
               from={currentPos}
               onDragStart={(point) => onDragStart?.(point)}
               onDragEnd={(point) => onDragEnd?.(point)}
+              setFilter={setFilter}
+              toggle={(flag?: boolean) => setToggle((t) => flag ?? !t)}
+              toggled={toggled}
+              title={data.name}
+              filterEnabled={false}
             />
           )}
           <div className="p-4">
             <h1 className="text-lg font-semibold">{data.order}. {data.name}</h1>
             <p className="text-sm">{data.description}</p>
             <div className="mt-4">
-              <TreeWrapper tree={tree} />
+              <TreeWrapper isRoot tree={tree} toggled={toggled} />
               {/* <pre className="text-xs whitespace-pre-wrap">{JSON.stringify(data, null, 2)}</pre> */}
             </div>
           </div>
-        </div>
+        </PanelMaybe>
       </PanelSettingsContext.Provider>
+
     )
   }
 )
+
+const PanelMaybe = styled('div', {
+  color: '$highlight2',
+  backgroundColor: '$elevation2',
+  borderRadius: '4px'
+})
