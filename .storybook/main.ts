e38122546a987acee14e1bd0d65f2e75a5d8ef0b
Christopher David <chris@arcadelabs.co>
2023-12-31 13:55:21 -0500

    Clear everything but Node and colocate w compo

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,8 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    // "../stories/**/*.mdx",
+    // "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../agentgraph/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
