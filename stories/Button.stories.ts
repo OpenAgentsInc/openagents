896b377ea0dc10342ccd7ea5af8ed1a8bab3bb26
Christopher David <chris@arcadelabs.co>
2023-12-31 13:41:44 -0500

    tutorial story

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -48,3 +48,12 @@ export const Small: Story = {
     label: 'Button',
   },
 };
+
+
+export const Warning: Story = {
+  args: {
+    primary: true,
+    label: 'Delete now',
+    backgroundColor: 'red',
+  }
+};
