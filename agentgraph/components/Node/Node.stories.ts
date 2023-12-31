dd948a36c837fd52b7492e52404b9b4072e93349
Christopher David <chris@arcadelabs.co>
2023-12-31 15:18:44 -0500

    Story cleanup

--- agentgraph/components/Node/Node.stories.ts
+++ agentgraph/components/Node/Node.stories.ts
@@ -20,29 +20,14 @@ const demoStep: Step = {
 const meta = {
   title: 'AgentGraph/Node',
   component: Node,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  parameters: { layout: 'centered' },
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    // backgroundColor: { control: 'color' },
-  },
+  argTypes: {},
 } satisfies Meta<typeof Node>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
-  // args: {
-  // primary: true,
-  // label: 'Button',
-  // },
-};
 
 export const StepNode: Story = {
   args: {
