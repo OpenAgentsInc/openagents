6a1f13454f1530ec869a248966fedafa4ace8ec6
Christopher David <chris@arcadelabs.co>
2023-12-31 16:43:45 -0500

    New Node styling (#56)
    * Demo node step for story
    
    * Remove onboarding addon
    
    * Story cleanup
    
    * Show step basics
    
    * Font works
    
    * Initial Node styles
    
    * Decent padding
    
    * better title

--- agentgraph/components/Node/Node.tsx
+++ agentgraph/components/Node/Node.tsx
@@ -1,5 +1,18 @@
-export const Node = () => {
+import { NodeProps } from "./Node.props"
+import { NodeContent, NodePanel, NodeTitleBar } from "./Node.styles"
+
+export const Node = ({ step }: NodeProps) => {
+  // Only Step nodes supported for now, so return null if no step
+  if (!step) return null
+
   return (
-    <p>I'm a node</p>
+    <NodePanel>
+      <NodeTitleBar>
+        <p>{step.order}. {step.name}</p>
+      </NodeTitleBar>
+      <NodeContent>
+        <p>{step.description}</p>
+      </NodeContent >
+    </NodePanel >
   )
 }
