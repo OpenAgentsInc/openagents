52a7066cbc8014b676371447c0eb14c532d0e5fd
Christopher David <chris@arcadelabs.co>
2024-01-03 18:20:51 -0600

    initial BitcoinBalance

---
+++ agentgraph/components/Wallet/BitcoinBalance/BitcoinBalance.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { BitcoinBalance } from '.';
+
+const meta = {
+  title: 'AgentGraph/Wallet/BitcoinBalance',
+  component: BitcoinBalance,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Node>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    sats: 100000
+  }
+}
---
+++ agentgraph/components/Wallet/BitcoinBalance/BitcoinBalance.tsx
@@ -0,0 +1,3 @@
+export const BitcoinBalance = () => {
+  return <>bitcoin balance</>
+}
