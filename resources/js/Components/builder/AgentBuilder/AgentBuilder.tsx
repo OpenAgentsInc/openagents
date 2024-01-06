a6a5cebeced486a9b8534181098aa5e85d134a7d
Christopher David <chris@arcadelabs.co>
2024-01-05 21:11:39 -0600

    convert to radiogroup

--- resources/js/Components/builder/AgentBuilder/AgentBuilder.tsx
+++ resources/js/Components/builder/AgentBuilder/AgentBuilder.tsx
@@ -1,3 +1,5 @@
+import * as RadioGroup from '@radix-ui/react-radio-group';
+
 export const AgentBuilder = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center">
@@ -6,17 +8,18 @@ export const AgentBuilder = () => {
         <div className="flex w-full justify-center md:w-1/2">
           <div className="h-full grow overflow-hidden">
             <div className="flex h-full flex-col px-2 pt-2">
-              <div role="radiogroup" aria-required="false" dir="ltr" className="flex w-full overflow-hidden rounded-xl bg-token-surface-secondary p-1.5 dark:bg-token-surface-tertiary md:w-1/2 mb-2 flex-shrink-0 self-center" tabIndex={0} style={{ outline: 'none' }}>
-                <button type="button" role="radio" aria-checked="true" data-state="checked" value="magic" className="text-md w-1/3 flex-grow rounded-lg border-token-border-light p-1.5 font-medium text-token-text-tertiary transition hover:text-token-text-primary radix-state-checked:border radix-state-checked:bg-token-surface-primary radix-state-checked:text-token-text-primary radix-state-checked:shadow-[0_0_2px_rgba(0,0,0,.03)] radix-state-checked:dark:bg-token-surface-secondary md:w-1/2" tabIndex={0} data-radix-collection-item="">
+
+              <RadioGroup.Root role="radiogroup" aria-required="false" dir="ltr" className="flex w-full overflow-hidden rounded-xl bg-token-surface-secondary p-1.5 dark:bg-token-surface-tertiary md:w-1/2 mb-2 flex-shrink-0 self-center" tabIndex={0} style={{ outline: 'none' }} onValueChange={(wat) => console.log(wat)}>
+                <RadioGroup.Item type="button" role="radio" aria-checked="true" data-state="checked" value="create" className="text-md w-1/3 flex-grow rounded-lg border-token-border-light p-1.5 font-medium text-token-text-tertiary transition hover:text-token-text-primary radix-state-checked:border radix-state-checked:bg-token-surface-primary radix-state-checked:text-token-text-primary radix-state-checked:shadow-[0_0_2px_rgba(0,0,0,.03)] radix-state-checked:dark:bg-token-surface-secondary md:w-1/2" tabIndex={0} data-radix-collection-item="">
                   Create
-                </button>
-                <button type="button" role="radio" aria-checked="false" data-state="unchecked" value="advanced" className="text-md w-1/3 flex-grow rounded-lg border-token-border-light p-1.5 font-medium text-token-text-tertiary transition hover:text-token-text-primary radix-state-checked:border radix-state-checked:bg-token-surface-primary radix-state-checked:text-token-text-primary radix-state-checked:shadow-[0_0_2px_rgba(0,0,0,.03)] radix-state-checked:dark:bg-token-surface-secondary md:w-1/2" tabIndex={-1} data-radix-collection-item="">Configure
-                </button>
-                <div className="flex w-1/3 md:hidden"><button type="button" role="radio" aria-checked="false" data-state="unchecked" value="preview" className="text-md w-1/3 flex-grow rounded-lg border-token-border-light p-1.5 font-medium text-token-text-tertiary transition hover:text-token-text-primary radix-state-checked:border radix-state-checked:bg-token-surface-primary radix-state-checked:text-token-text-primary radix-state-checked:shadow-[0_0_2px_rgba(0,0,0,.03)] radix-state-checked:dark:bg-token-surface-secondary md:w-1/2" tabIndex={-1} data-radix-collection-item="">
+                </RadioGroup.Item>
+                <RadioGroup.Item type="button" role="radio" aria-checked="false" data-state="unchecked" value="configure" className="text-md w-1/3 flex-grow rounded-lg border-token-border-light p-1.5 font-medium text-token-text-tertiary transition hover:text-token-text-primary radix-state-checked:border radix-state-checked:bg-token-surface-primary radix-state-checked:text-token-text-primary radix-state-checked:shadow-[0_0_2px_rgba(0,0,0,.03)] radix-state-checked:dark:bg-token-surface-secondary md:w-1/2" tabIndex={-1} data-radix-collection-item="">Configure
+                </RadioGroup.Item>
+                <div className="flex w-1/3 md:hidden"><RadioGroup.Item type="button" role="radio" aria-checked="false" data-state="unchecked" value="preview" className="text-md w-1/3 flex-grow rounded-lg border-token-border-light p-1.5 font-medium text-token-text-tertiary transition hover:text-token-text-primary radix-state-checked:border radix-state-checked:bg-token-surface-primary radix-state-checked:text-token-text-primary radix-state-checked:shadow-[0_0_2px_rgba(0,0,0,.03)] radix-state-checked:dark:bg-token-surface-secondary md:w-1/2" tabIndex={-1} data-radix-collection-item="">
                   Preview
-                </button>
+                </RadioGroup.Item>
                 </div>
-              </div>
+              </RadioGroup.Root>
               <div className="grow overflow-hidden">
                 <div className="h-full w-full pb-5">
                   <div className="relative flex h-full grow overflow-auto">
