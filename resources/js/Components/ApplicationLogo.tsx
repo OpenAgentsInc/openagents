a45c1f8261926628361fc37af60abaff8babc375
Christopher David <chris@arcadelabs.co>
2024-01-06 10:29:38 -0600

    initial navbar

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -7,7 +7,7 @@ export default function ApplicationLogo() {
         <circle cx="100" cy="100" r="90" stroke={strokeColor} strokeWidth={strokeWidth} fill="none" />
         <path d="M 30 150 L 100 78 L 170 150" stroke={strokeColor} strokeWidth={strokeWidth} fill="none" />
       </svg>
-      <h1 className="ml-1 text-2xl font- tracking-tight">Open<span className="font-bold">Agents</span></h1>
+      <h1 className="ml-1 text-2xl font-light tracking-tight">Open<span className="font-bold">Agents</span></h1>
     </div>
   );
 }
---
+++ resources/js/Components/nav/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import ApplicationLogo from "@/Components/ApplicationLogo"
+import { Link } from "@inertiajs/react"
+
+export const Navbar = () => {
+  return (
+    <header className="fixed w-full bg-background/80 shadow backdrop-blur" style={{ zIndex: 9000 }}>
+      <nav className="mx-auto flex max-w-7xl items-center justify-between h-16 px-6 lg:px-8" aria-label="Global">
+        <Link href="/" className="-m-1.5 p-1.5">
+          <ApplicationLogo />
+        </Link>
+      </nav>
+    </header>
+  )
+}
