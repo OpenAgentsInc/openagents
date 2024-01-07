596db45cc2d39ecc82771244802a297d8de6818f
Christopher David <chris@arcadelabs.co>
2024-01-07 16:32:41 -0600

    basic agent info card on agentview

--- resources/js/Components/agent/AgentView/AgentView.tsx
+++ resources/js/Components/agent/AgentView/AgentView.tsx
@@ -1,4 +1,5 @@
 import { ChatPane } from "@/Components/builder/ChatPane"
+import { Card, CardDescription, CardHeader, CardTitle } from "@/Components/ui/card"
 import { Agent } from "@/types/agents"
 
 interface AgentViewProps {
@@ -9,6 +10,13 @@ export const AgentView = ({ agent }: AgentViewProps) => {
   const initialMessages = [{ id: 0, role: "assistant", content: agent.welcome_message, tokens: [] }]
   return (
     <div className="h-full">
+      <Card className="fixed left-[20px] top-[20%] w-[350px]">
+        <CardHeader>
+          <CardTitle>{agent.name}</CardTitle>
+          <CardDescription>{agent.description}</CardDescription>
+          <CardDescription>By [author]</CardDescription>
+        </CardHeader>
+      </Card>
       <ChatPane initialMessages={initialMessages} />
     </div>
   )
