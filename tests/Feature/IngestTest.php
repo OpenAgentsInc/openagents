488bb788d67e6e83581556c7216711a1efa3025a
Christopher David <chris@arcadelabs.co>
2024-01-09 13:28:46 -0600

    cleanup

--- tests/Feature/IngestTest.php
+++ tests/Feature/IngestTest.php
@@ -1,20 +1,17 @@
 <?php
 
 use App\Jobs\IngestPDF;
-use App\Models\Embedding;
-use App\Services\Parser;
-use App\Services\QueenbeeGateway;
-use Illuminate\Http\UploadedFile;
-use Spatie\PdfToText\Pdf;
+use App\Models\Datapoint;
+
+// use App\Models\Embedding;
+// use App\Services\Parser;
 
 test('can ingest pdf to database as chunked embeddings', function () {
     $path = 'uploads/0sYtEObUcMCnLo8zAwv7i0cJPfoWTPO4tW7ZblS0.pdf';
     $job = new IngestPDF($path);
     $job->handle();
 
-    // $parser = new Parser();
-    // $parsed = $parser->parsePdf($path);
-    // expect(Embedding::query()->count())->toBe(count($parsed["chunks"]));
+    expect(Datapoint::count())->toBe(4);
 });
 
 // test('can ingest pdf to database as chunked embeddings (old)', function () {
