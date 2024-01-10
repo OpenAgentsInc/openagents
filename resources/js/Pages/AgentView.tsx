9c0fe0ca6cfcd4cb85a6356e5c62c652c6459626
Christopher David <chris@arcadelabs.co>
2024-01-09 21:36:35 -0600

    subscribe to EmbeddingCreated event and console.log

--- resources/js/Pages/AgentView.tsx
+++ resources/js/Pages/AgentView.tsx
@@ -2,6 +2,7 @@ import { usePage } from "@inertiajs/react"
 import { Agent } from "@/types/agents"
 import { NavLayout } from "@/Layouts/NavLayout"
 import { AgentView } from "@/Components/agent/AgentView"
+import { useEffect } from "react"
 
 function AgentViewPage() {
   const props = usePage().props as any
@@ -9,6 +10,17 @@ function AgentViewPage() {
   const conversation = props.conversation as any
   const files = props.files as any
   const owner = props.owner as string
+
+  useEffect(() => {
+    if (import.meta.env.VITE_ENV === "local") return
+    if (!agent) return
+    // @ts-ignore
+    window.Echo.channel(`Agent.${agent.id}`)
+      .listen('EmbeddingCreated', (e) => {
+        console.log(e)
+      });
+  }, [agent.id]);
+
   return (
     <AgentView agent={agent} conversation={conversation} files={files} owner={owner} />
   )
