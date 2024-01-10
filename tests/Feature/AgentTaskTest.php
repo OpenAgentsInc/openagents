5a09046a4f42ebd63176814620a048f048817924
Christopher David <chris@arcadelabs.co>
2024-01-10 08:35:58 -0600

    new agents are created with default task and step - fails

--- tests/Feature/AgentTaskTest.php
+++ tests/Feature/AgentTaskTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Agent;
+use App\Models\Step;
+use App\Models\Task;
 use App\Models\User;
 
 test('new agents are created with default task and step', function () {
@@ -9,36 +11,22 @@ test('new agents are created with default task and step', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
 
-    // And we have no agents
+    // And we have no agents or tasks/steps
     $this->assertCount(0, Agent::all());
+    $this->assertCount(0, Task::all());
+    $this->assertCount(0, Step::all());
 
     // When we create an agent
-    $this->postJson('/api/agents', [
+    $this->postJson(route('agents.store'), [
         'name' => 'John Doe',
         'description' => 'This is a description',
         'instructions' => 'This is a set of instructions',
         'welcome_message' => 'This is a welcome message'
     ])
-        ->assertStatus(201)
-        ->assertJsonStructure(['id', 'name', 'description', 'instructions', 'welcome_message', 'created_at', 'updated_at']);
+        ->assertStatus(201);
 
     // Then we have 1 agent
     $this->assertCount(1, Agent::all());
-
-    // And the agent has 1 task
-    $agent = Agent::first();
-    $this->assertCount(1, $agent->tasks);
-
-    // And the task has 1 step
-    $task = $agent->tasks->first();
-    $this->assertCount(1, $task->steps);
-
-    // And the step has 1 message
-    $step = $task->steps->first();
-    $this->assertCount(1, $step->messages);
-
-    // And the message is the welcome message
-    $message = $step->messages->first();
-    $this->assertEquals('This is a welcome message', $message->body);
-
-})->skip();
+    $this->assertCount(1, Task::all());
+    $this->assertCount(2, Step::all());
+});
