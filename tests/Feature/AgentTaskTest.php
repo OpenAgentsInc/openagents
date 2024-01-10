58d47d12e33e70f1709ef4bdfcf42abdf096b421
Christopher David <chris@arcadelabs.co>
2024-01-10 09:23:19 -0600

    chatting with an agent without task creates defaults - fails

--- tests/Feature/AgentTaskTest.php
+++ tests/Feature/AgentTaskTest.php
@@ -6,7 +6,6 @@ use App\Models\Task;
 use App\Models\User;
 
 test('new agents are created with default task and step', function () {
-
     // Given we have a user
     $user = User::factory()->create();
     $this->actingAs($user);
@@ -27,6 +26,30 @@ test('new agents are created with default task and step', function () {
 
     // Then we have 1 agent
     $this->assertCount(1, Agent::all());
+    $this->assertCount(1, Task::all());
+    $this->assertCount(2, Step::all());
+});
+
+test('chatting with an agent without task creates defaults', function () {
+
+    // Given we have a user
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    // And we have an agent
+    $agent = Agent::factory()->create();
+
+    // And we have no tasks or steps
+    $this->assertCount(0, Task::all());
+    $this->assertCount(0, Step::all());
+
+    // When we chat with the agent
+    $this->post(route('agent.chat', $agent->id), [
+        'input' => "Hello world"
+    ])
+        ->assertStatus(200);
+
+    // Then we have 1 task and 2 steps
     $this->assertCount(1, Task::all());
     $this->assertCount(2, Step::all());
 });
