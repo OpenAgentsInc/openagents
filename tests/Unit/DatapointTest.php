e0cc0bea91b214f2cfa06f94546b2e121f49653a
Christopher David <chris@arcadelabs.co>
2024-01-11 11:03:41 -0600

    datapoint tests

--- tests/Unit/DatapointTest.php
+++ tests/Unit/DatapointTest.php
@@ -7,3 +7,13 @@ it('belongs to a brain', function () {
     $datapoint = Datapoint::factory()->create();
     expect($datapoint->brain)->toBeInstanceOf(Brain::class);
 });
+
+test('it has data', function () {
+    $datapoint = Datapoint::factory()->create(['data' => 'Hello world!']);
+    expect($datapoint->data)->toBe('Hello world!');
+});
+
+it('has an embedding', function () {
+    $datapoint = Datapoint::factory()->create();
+    expect($datapoint->embedding->toArray())->toBeArray();
+});
