54f0b973a162727c9b0a30013ee2139032eb5b07
Christopher David <chris@arcadelabs.co>
2024-01-11 14:05:48 -0600

    fix broken stories

--- resources/js/Components/nav/Navbar/Navbar.tsx
+++ resources/js/Components/nav/Navbar/Navbar.tsx
@@ -20,10 +20,7 @@ export const Navbar = ({ user }: HeaderProps) => {
   const name = user?.name || user?.github_nickname || 'Guest'
   const avatar = user?.github_avatar || user?.twitter_avatar || 'https://placekitten.com/100/100'
 
-  // If route is undefined (in storybook), set it to no-op, otherwise dont change it
-  const route2 = route || ((route?: any) => {
-    current: () => false
-  })
+  const areWeLocal = import.meta.env.VITE_ENV === "local"
 
   return (
     <Disclosure as="nav" className="fixed w-full bg-white/50 backdrop-blur shadow" style={{ zIndex: 9990 }}>
@@ -50,9 +47,13 @@ export const Navbar = ({ user }: HeaderProps) => {
                 </div>
                 <div className="hidden md:ml-6 md:flex md:space-x-8">
                   {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                  <NavLink href={route('agents')} active={route2().current('agents')}>
-                    Agents
-                  </NavLink>
+                  {!areWeLocal && (
+                    <NavLink href={route('agents')} active={route().current('agents')}>
+                      Agents
+                    </NavLink>
+                  )}
+
+
                   {/* <NavLink href={route('referrals')} active={route2().current('referrals')}>
                     Referrals
                   </NavLink> */}
