e647e10bf16fdabb62c1ded1885c5404c01e2d2c
Christopher David <chris@arcadelabs.co>
2024-01-16 16:21:03 -0600

    revert til debug prod sse

--- resources/views/bitcoin.blade.php
+++ resources/views/bitcoin.blade.php
@@ -1,9 +1,7 @@
 <script src="https://unpkg.com/htmx.org@1.9.10"
     integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous">
 </script>
-<script src="https://unpkg.com/htmx.org/dist/ext/sse.js"></script>
 
-
-<h1 hx-ext="sse" sse-connect="/bitcoin-price-ticker" sse-swap="message">
-    Loading bitcoin price...
-</h1>
+<div hx-get="{{ route('bitcoin-price') }}" hx-trigger="every 5s" id="bitcoin-price">
+    @include('bitcoin-price', ['price' => $price])
+</div>
