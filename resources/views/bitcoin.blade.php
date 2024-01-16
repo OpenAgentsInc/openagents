5eeb54505c269e1bcf9e4e684081635bdbf2be15
Christopher David <chris@arcadelabs.co>
2024-01-16 15:43:03 -0600

    Price ticker uses cache (#80)
    * move bitcoin methods to new controller
    
    * bitcoin price ticker returns price
    
    * use cache
    
    * skip dat

--- resources/views/bitcoin.blade.php
+++ resources/views/bitcoin.blade.php
@@ -1,7 +1,9 @@
 <script src="https://unpkg.com/htmx.org@1.9.10"
     integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous">
 </script>
+<script src="https://unpkg.com/htmx.org/dist/ext/sse.js"></script>
 
-<div hx-get="{{ route('bitcoin-price') }}" hx-trigger="every 5s" id="bitcoin-price">
-    @include('bitcoin-price', ['price' => $price])
-</div>
+
+<h1 hx-ext="sse" sse-connect="/bitcoin-price-ticker" sse-swap="message">
+    Loading bitcoin price...
+</h1>
