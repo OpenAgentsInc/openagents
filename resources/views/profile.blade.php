6cbf40ed74c7189ffd07728cdb7f5d4a08013330
Christopher David <chris@arcadelabs.co>
2024-01-16 10:41:31 -0600

    Basic profile form

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,38 @@
-<script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="en">
 
-im a profile
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Profile</title>
+    <script src="https://unpkg.com/htmx.org@1.9.10"
+        integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+    <h1>Edit Profile</h1>
+    <form id="edit-profile-form" hx-post="/update-profile" hx-target="#edit-profile-form" hx-swap="outerHTML">
+        @csrf
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" autocomplete="email" required>
+        </div>
+        <div>
+            <label for="password">New Password:</label>
+            <input type="password" id="password" name="password" autocomplete="new-password">
+        </div>
+        <div>
+            <label for="password_confirmation">Confirm Password:</label>
+            <input type="password" id="password_confirmation" name="password_confirmation" autocomplete="new-password">
+        </div>
+        <button type="submit">Update Profile</button>
+    </form>
+</body>
+
+</html>
