4aca772841e0f10a8ef0ed2860798977bf450e75
Christopher David <chris@arcadelabs.co>
2024-01-16 10:30:50 -0600

    set up profile blade view

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,6 +11,6 @@ class ProfileController extends Controller
 {
     public function index()
     {
-        return "hello";
+        return view('profile');
     }
 }
--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,10 +2,11 @@
 
 use App\Models\User;
 
-test('profile page returns 200 status code', function () {
+test('profile page returns 200 status code and profile view', function () {
     $user = User::factory()->create();
 
     $response = $this->actingAs($user)->get('/profile');
 
-    $response->assertStatus(200);
+    $response->assertStatus(200)
+    ->assertViewIs('profile');
 });
