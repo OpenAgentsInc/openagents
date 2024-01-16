d16d1141acec474a288216a8d0ff223822b02830
Christopher David <chris@arcadelabs.co>
2024-01-16 14:48:30 -0600

    fake it during test

--- app/Services/Bitcoin.php
+++ app/Services/Bitcoin.php
@@ -8,6 +8,10 @@ class Bitcoin
 {
     public static function getUsdPrice(): float
     {
+        if (app()->environment('testing')) {
+            return 10000.00;
+        }
+
         $fmpKey = "W52QFnXt2v2xIDXFmlUHnQ3EWwlMncbl";
         $url = "https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey={$fmpKey}";
         $response = Http::get($url)->json();
