f0ff8875290fdbd14951a0e54a0ec222afae6213
Christopher David <chris@arcadelabs.co>
2024-01-17 19:27:39 -0600

    Load and parse Extism WASM plugins (#82)
    * can read what functions a plugin has - fails
    
    * php artisan make:model -m --factory Plugin
    
    * initial pass
    
    * initial attempt w extism - exists but no list
    
    * can execute plugin function
    
    * got raw protobuf data
    
    * composer require google/protobuf
    
    * extism proto classes
    
    * hmm fail to push limit error
    
    * move old protos to sep dir
    
    * api proto and change header
    
    * composer require "protobuf-php/protobuf"
    
    * diff header maybe
    
    * plugin hm
    
    * protoc --proto_path=./app/Services/Extism --php_out=./app/Services/Extism ./app/Services/Extism/api.proto
    
    * delete all old
    
    * protoc --proto_path=./app/Services/Extism --php_out=./app/Services/Extism ./app/Services/Extism/api.proto
    
    * regen
    
    * have a module
    
    * module with hash maybe workin
    
    * close
    
    * we got it
    
    * Parse works - cleanup
    
    * can be parsed
    
    * and can return just the functions
    
    * plugin tests to integration
    
    * vid53link

---
+++ app/Models/Plugin.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+require_once __DIR__ . "/../Services/Extism/GPBMetadata/Api.php";
+
+use App\Services\Extism\Proto\Module;
+use App\Services\Extism\Proto\SourceLanguage;
+use Extism\Plugin as ExtismPlugin;
+use Extism\Manifest;
+use Extism\UrlWasmSource;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plugin extends Model
+{
+    use HasFactory;
+
+    private function wasmBytes()
+    {
+        return file_get_contents($this->wasm_url);
+    }
+
+    public function parse()
+    {
+        $wasm = new UrlWasmSource("https://cdn.modsurfer.dylibso.com/api/v1/module/0c20c61f67108ebccae1db0be6df7c7d14b2567d5606154278ee390f43e1f408.wasm");
+        $manifest = new Manifest($wasm);
+        $plugin = new ExtismPlugin($manifest, true);
+        $protobufData = $plugin->call("parse_module", $this->wasmBytes());
+        $module = new Module();
+        $module->mergeFromString($protobufData);
+
+        $exports = $module->getExports();
+        $moduleMetadata = [
+            'module_id' => $module->getId(),
+            'module_hash' => $module->getHash(),
+            'exports_count' => count($exports),
+            'size' => $module->getSize(),
+            'module_location' => $module->getLocation(),
+            'source_language' => SourceLanguage::name($module->getSourceLanguage()),
+            'exports' => []
+        ];
+
+        foreach ($exports as $export) {
+            if ($export->hasFunc()) {
+                $function = $export->getFunc(); // $function is an instance of PBFunction
+                $moduleMetadata['exports'][] = $function->getName();
+            }
+        }
+
+        return $moduleMetadata;
+    }
+
+    public function call(string $function, string $input): mixed
+    {
+        $wasm = new UrlWasmSource($this->wasm_url);
+        $manifest = new Manifest($wasm);
+
+        $plugin = new ExtismPlugin($manifest, true);
+        return $plugin->call($function, $input);
+    }
+
+    public function functions(): array
+    {
+        $parsed = $this->parse();
+        return $parsed['exports'];
+    }
+}
---
+++ tests/Unit/PluginTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Plugin;
+
+it('has a name', function () {
+    $plugin = Plugin::factory()->create([
+        'name' => 'Count Vowels'
+    ]);
+    expect($plugin->name)->toBe('Count Vowels');
+})->group('integration');
+
+it('has a wasm_url', function () {
+    $plugin = Plugin::factory()->create([
+        'wasm_url' => 'https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm'
+    ]);
+    expect($plugin->wasm_url)->toBe('https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm');
+})->group('integration');
+
+test('can execute plugin function', function () {
+    $plugin = Plugin::factory()->create();
+    $output = $plugin->call("count_vowels", "Yellow, World!");
+    expect($output)->toBe('{"count":3,"total":3,"vowels":"aeiouAEIOU"}');
+})->group('integration');
+
+test('can return its module functions', function () {
+    $plugin = Plugin::factory()->create([
+        'name' => 'Count Vowels',
+        'wasm_url' => 'https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm',
+    ]);
+    $functions = $plugin->functions();
+    expect($functions)->toBeArray();
+    expect($functions)->toContain('count_vowels');
+})->group('integration');
+
+it('can be parsed', function () {
+    $plugin = Plugin::factory()->create();
+    $parsed = $plugin->parse();
+    expect($parsed)->toBeArray();
+    expect($parsed["module_hash"])->toBeString();
+    expect($parsed["module_hash"])->toBe('93898457953d30d016f712ccf4336ce7e9971db5f7f3aff1edd252764f75d5d7');
+})->group('integration');
