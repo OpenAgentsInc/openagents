99b1dd319078f764c208953ea0a243cfc0701fe6
Christopher David <chris@arcadelabs.co>
2024-01-18 10:10:34 -0600

    php artisan make:component PluginUploadForm

--- resources/views/plugins.blade.php
+++ resources/views/plugins.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Profile</title>
+    <title>Plugins</title>
     <script src="https://unpkg.com/htmx.org@1.9.10"
         integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous">
     </script>
