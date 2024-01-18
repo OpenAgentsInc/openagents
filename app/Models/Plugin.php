fb95b2f822e6b28456cce4147ae37c1938b9d843
Christopher David <chris@arcadelabs.co>
2024-01-17 18:29:28 -0600

    Parse works - cleanup

--- app/Models/Plugin.php
+++ app/Models/Plugin.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 require_once __DIR__ . "/../Services/Extism/GPBMetadata/Api.php";
-// require_once __DIR__ . "/../Services/Extism/Proto/Module.php";
 
 use App\Services\Extism\Proto\Module;
 use Extism\Plugin as ExtismPlugin;
@@ -18,20 +17,51 @@ class Plugin extends Model
 
     private function wasmBytes()
     {
-        dd($this->wasm_url);
         return file_get_contents($this->wasm_url);
     }
 
     public function parse()
     {
+        $valTypeMap = [
+            0 => 'I32',
+            1 => 'I64',
+            2 => 'F32',
+            3 => 'F64',
+            4 => 'V128',
+            5 => 'FuncRef',
+            6 => 'ExternRef'
+        ];
+
         $wasm = new UrlWasmSource("https://cdn.modsurfer.dylibso.com/api/v1/module/0c20c61f67108ebccae1db0be6df7c7d14b2567d5606154278ee390f43e1f408.wasm");
         $manifest = new Manifest($wasm);
         $plugin = new ExtismPlugin($manifest, true);
-        $protobufData = $plugin->call("parse_module", file_get_contents("https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm"));
+        $protobufData = $plugin->call("parse_module", $this->wasmBytes());
         $module = new Module();
         $module->mergeFromString($protobufData);
+
+        $moduleId = $module->getId();
+        $moduleHash = $module->getHash();
         $exports = $module->getExports();
-        dd($exports);
+        $exportsCount = count($exports);
+        $moduleSize = $module->getSize();
+        $moduleLocation = $module->getLocation();
+        $sourceLanguage = $module->getSourceLanguage(); // This will be an enum value
+        $metadata = $module->getMetadata();
+
+        // Display or process the metadata
+        echo "Module ID: $moduleId\n";
+        echo "Module Hash: $moduleHash\n";
+        echo "Exports Count: $exportsCount\n";
+        echo "Module Size: $moduleSize bytes\n";
+        echo "Module Location: $moduleLocation\n";
+
+        foreach ($exports as $export) {
+            if ($export->hasFunc()) {
+                $function = $export->getFunc(); // $function is an instance of PBFunction
+                $functionName = $function->getName();
+                echo "Function Name: $functionName\n";
+            }
+        }
     }
 
     public function call(string $function, string $input): mixed
