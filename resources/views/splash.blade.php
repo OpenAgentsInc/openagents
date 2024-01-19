87d5a39ceae0ebc4de6559cdc9f539ecc1c0b3a7
Christopher David <chris@arcadelabs.co>
2024-01-19 15:39:52 -0600

    simplify

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -2,15 +2,16 @@
 <html>
 
 <head>
-    <style>
-        body {
-            font-family: monospace;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OpenAgents</title>
+    @include('partials.css')
 </head>
 
-<body>
+<body class="font-mono">
     <h1>OpenAgents</h1>
+    <h2>An open platform for AI agents</h2>
+    <p>Built in public from scratch</p>
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,137 +1,13 @@
 import forms from "@tailwindcss/forms"
-import defaultTheme from "tailwindcss/defaultTheme"
-import { scrollbarColor, scrollbarGutter, scrollbarWidth } from "tailwind-scrollbar-utilities"
 
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
   content: [
-    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-    "./storage/framework/views/*.php",
+    // "./resources/views/**/*.blade.php",
     "./resources/views/**/*.blade.php",
-    "./resources/js/**/*.tsx",
-    "./resources/js/**/*.jsx",
-    "./agentgraph/**/*.tsx",
   ],
-
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-
-        primary: `hsl(var(--primary))`,
-        'primary-light': `hsl(var(--primary-light))`,
-        'primary-dark': `hsl(var(--primary-dark))`,
-        'background-light': `hsl(var(--background-light))`,
-        'background-dark': `hsl(var(--background-dark))`,
-        'gray-50': `hsl(var(--gray-50))`,
-        'gray-100': `hsl(var(--gray-100))`,
-        'gray-200': `hsl(var(--gray-200))`,
-        'gray-300': `hsl(var(--gray-300))`,
-        'gray-400': `hsl(var(--gray-400))`,
-        'gray-500': `hsl(var(--gray-500))`,
-        'gray-600': `hsl(var(--gray-600))`,
-        'gray-700': `hsl(var(--gray-700))`,
-        'gray-800': `hsl(var(--gray-800))`,
-        'gray-900': `hsl(var(--gray-900))`,
-        'gray-950': `hsl(var(--gray-950))`,
-
-        // border: "hsl(var(--border))",
-        // input: "hsl(var(--input))",
-        // ring: "hsl(var(--ring))",
-        // background: "hsl(var(--background))",
-        // foreground: "hsl(var(--foreground))",
-        // primary: {
-        //   DEFAULT: "hsl(var(--primary))",
-        //   foreground: "hsl(var(--primary-foreground))",
-        // },
-        // secondary: {
-        //   DEFAULT: "hsl(var(--secondary))",
-        //   foreground: "hsl(var(--secondary-foreground))",
-        // },
-        // destructive: {
-        //   DEFAULT: "hsl(var(--destructive))",
-        //   foreground: "hsl(var(--destructive-foreground))",
-        // },
-        // muted: {
-        //   DEFAULT: "hsl(var(--muted))",
-        //   foreground: "hsl(var(--muted-foreground))",
-        // },
-        // accent: {
-        //   DEFAULT: "hsl(var(--accent))",
-        //   foreground: "hsl(var(--accent-foreground))",
-        // },
-        // popover: {
-        //   DEFAULT: "hsl(var(--popover))",
-        //   foreground: "hsl(var(--popover-foreground))",
-        // },
-        // card: {
-        //   DEFAULT: "hsl(var(--card))",
-        //   foreground: "hsl(var(--card-foreground))",
-        // },
-      },
-      borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
-        sm: "calc(var(--radius) - 4px)",
-      },
-      fontFamily: {
-        sans: ["Inter", "BeausiteClassic", ...defaultTheme.fontFamily.sans],
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-        grid: {
-          "0%": { transform: "translateY(-50%)" },
-          "100%": { transform: "translateY(0)" },
-        },
-        spin: {
-          "0%": {
-            transform: "translateZ(0) rotate(0)",
-          },
-          "15%, 35%": {
-            transform: "translateZ(0) rotate(90deg)",
-          },
-          "65%, 85%": {
-            transform: "translateZ(0) rotate(270deg)",
-          },
-          "100%": {
-            transform: "translateZ(0) rotate(360deg)",
-          },
-        },
-        slide: {
-          to: {
-            transform: "translate(calc(100cqw - 100%), 0)",
-          },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        grid: "grid 15s linear infinite",
-        spin: "spin calc(var(--speed) * 2) infinite linear",
-        slide: "slide var(--speed) ease-in-out infinite alternate",
-      },
-    },
-  },
-
   plugins: [
     forms,
-    require("tailwindcss-animate"),
-    scrollbarGutter(), // no options to configure
-    scrollbarWidth(), // no options to configure
-    scrollbarColor(), // no options to configure
   ],
 }
