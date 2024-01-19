cc77577bee9426022516a431371f83d55572d562
Christopher David <chris@arcadelabs.co>
2024-01-19 10:38:24 -0600

    first failing plugin see test

---
+++ tests/Feature/PluginRegistryTest.php
@@ -0,0 +1,9 @@
+<?php
+
+test('plugins page shows list of all plugins', function () {
+    $plugin = Plugin::factory(3)->create();
+    $response = $this->get('/plugins');
+    $response->assertSee($plugin[0]->name);
+    $response->assertSee($plugin[1]->name);
+    $response->assertSee($plugin[2]->name);
+});
