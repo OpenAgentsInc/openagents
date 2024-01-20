5c4c78105cb54c1fb4791187c74b888744c03cf0
Christopher David <chris@arcadelabs.co>
2024-01-19 18:30:42 -0600

    simple and add tweetar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,35 +1,71 @@
-<div class="relative max-w-8xl mx-auto">
-    <div class="py-5 lg:py-4 lg:px-12 border-b border-gray-500/10 lg:border-0 dark:border-gray-300/10 mx-4 lg:mx-0">
-        <div class="relative flex items-center">
-            <div class="flex-1 flex items-center space-x-4"><a href="/"><span class="sr-only">OpenAgents home
-                        page</span><img class="w-auto h-7 relative block dark:hidden"
-                        src="{{ asset('images/light.svg') }}" alt="light logo"><img
-                        class="w-auto h-7 relative hidden dark:block"
-                        src="{{ asset('images/dark.svg') }}" alt="dark logo"></a></div>
-            <div class="flex-1 relative hidden lg:flex items-center ml-auto justify-end">
-                <nav class="text-sm leading-6 font-semibold text-gray-700 dark:text-gray-200">
-                    <ul class="flex space-x-8 items-center"></ul>
-                </nav>
-                <div class="flex items-center">
-                    <!--
+<header class="relative">
+    <div class="px-4 sm:px-6 md:px-8">
+        <div class="absolute inset-0 bottom-10 bg-bottom bg-no-repeat bg-slate-50 dark:bg-haiti">
+            <div class="absolute inset-0 bg-grid-slate-900/[0.04] bg-[bottom_1px_center] dark:bg-grid-slate-400/[0.05]"
+                style="mask-image:linear-gradient(to bottom, transparent, black);-webkit-mask-image:linear-gradient(to bottom, transparent, black)">
+            </div>
+        </div>
+        <div
+            class="relative pt-6 flex items-center justify-between text-slate-700 font-semibold text-sm leading-6 dark:text-slate-200">
+            <div class="text-slate-900 dark:text-white w-44">
+                @include('partials.logo')
+            </div>
+            <div class="flex items-center">
+                <button type="button"
+                    class="text-slate-500 hover:text-slate-600 w-8 h-8 -my-1 flex items-center justify-center md:hidden dark:hover:text-slate-300">
+                    <span class="sr-only">Search</span>
+                    <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+                        <path d="m19 19-3.5-3.5"></path>
+                        <circle cx="11" cy="11" r="6"></circle>
+                    </svg>
+                </button>
+                <div class="-my-1 ml-2 -mr-1 md:hidden">
+                    <button type="button"
+                        class="text-slate-500 w-8 h-8 flex items-center justify-center hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300">
+                        <span class="sr-only">Navigation</span><svg width="24" height="24" fill="none"
+                            aria-hidden="true">
+                            <path
+                                d="M12 6v.01M12 12v.01M12 18v.01M12 7a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                            </path>
+                        </svg>
+                    </button>
                     <div
-                        class="border-l hidden lg:flex border-gray-100 ml-6 pl-6 dark:border-background-dark dark:brightness-200 h-6">
+                        style="position:fixed;top:1px;left:1px;width:1px;height:0;padding:0;margin:-1px;overflow:hidden;clip:rect(0, 0, 0, 0);white-space:nowrap;border-width:0;display:none">
                     </div>
-                    -->
-                    <x-theme-switcher />
+                </div>
+                <div class="hidden md:flex items-center">
+                    <nav>
+                        <ul class="flex items-center gap-x-8">
+                            <li><a class="hover:text-purple-500 dark:hover:text-purple-400"
+                                    href="/docs/installation">Docs</a></li>
+                            <li><a class="hover:text-purple-500 dark:hover:text-purple-400" href="/blog">Blog</a></li>
+                        </ul>
+                    </nav>
+
+                    <div class="flex items-center border-l border-slate-200 ml-6 pl-6 dark:border-portgore">
+                        <label class="sr-only">Theme</label>
+                        <x-theme-switcher />
+                        <a href="https://github.com/OpenAgentsInc/openagents" target="_blank"
+                            class="ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300"><span
+                                class="sr-only">OpenAgents on GitHub</span><svg viewBox="0 0 16 16" class="w-5 h-5"
+                                fill="currentColor" aria-hidden="true">
+                                <path
+                                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z">
+                                </path>
+                            </svg>
+                        </a>
+                    </div>
                 </div>
             </div>
-            <!--
-            <div class="flex lg:hidden items-center"><button type="button"
-                    class="ml-auto text-gray-500 w-8 h-8 -my-1 items-center justify-center hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"><span
-                        class="sr-only">Search</span><svg
-                        class="h-4 w-4 bg-gray-500 dark:bg-gray-400 hover:bg-gray-600 dark:hover:bg-gray-300"
-                        style="-webkit-mask-image:url(https://mintlify.b-cdn.net/v6.4.0/solid/magnifying-glass.svg);-webkit-mask-repeat:no-repeat;-webkit-mask-position:center"></svg></button><button
-                    class="h-7 w-5 flex items-center justify-end"><svg
-                        class="h-4 w-4 bg-gray-500 dark:bg-gray-400 hover:bg-gray-600 dark:hover:bg-gray-300"
-                        style="-webkit-mask-image:url(https://mintlify.b-cdn.net/v6.4.0/solid/ellipsis-vertical.svg);-webkit-mask-repeat:no-repeat;-webkit-mask-position:center"></svg></button>
-            </div>
-            -->
+        </div>
+        <div class="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+            <h1
+                class="text-slate-900 font-extrabold text-3xl sm:text-4xl lg:text-5xl tracking-tight text-center dark:text-white">
+                An open platform for AI agents</h1>
+            <p class="mt-6 text-xl text-slate-600 text-center max-w-3xl mx-auto dark:text-bluebell">Built in public
+                from scratch. Launching soon!</p>
         </div>
     </div>
-</div>
+</header>
--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -8,11 +8,17 @@
     @include('partials.css')
 </head>
 
-<body class="font-mono dark:text-white dark:bg-black">
-    <h1>OpenAgents</h1>
-    <h2>An open platform for AI agents</h2>
-    <p>Built in public from scratch</p>
-    <x-theme-switcher />
+<body class="antialiased font-mono dark:text-white bg-slate-50 dark:bg-haiti">
+    <x-header />
+
+
+    <div class="mt-12 mx-auto w-[560px] max-w-full">
+        <blockquote class="twitter-tweet border-portgore" data-theme="dark" data-media-max-width="560">
+            <a href="https://twitter.com/OpenAgentsInc/status/1721942435125715086?ref_src=twsrc%5Etfw"></a>
+        </blockquote>
+    </div>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
 </body>
 
 </html>
