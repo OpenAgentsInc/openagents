e64b7c20b12b19a7b4709ec37aaa0fba336d8258
Christopher David <chris@arcadelabs.co>
2024-01-20 00:39:42 -0600

    talk notes (#88)
    * padding and add tailwind typography
    
    * initial thesis
    
    * thesis view

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\StaticController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [StaticController::class, 'newsplash']);
+Route::get('/thesis', [StaticController::class, 'thesis']);
 
 // Plugin uploading
 Route::get('/plugins', [PluginController::class, 'index'])->name('plugins');
