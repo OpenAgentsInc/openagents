fdfb5c8d4831f0b98fecc80bf8004049f07622a2
Christopher David <chris@arcadelabs.co>
2024-01-20 21:40:48 -0600

    homepage use splash layout

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -1,32 +1,20 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OpenAgents</title>
-    @include('partials.css')
-</head>
+@section('title', 'OpenAgents')
 
-<body class="antialiased font-mono dark:text-white bg-slate-50 dark:bg-haiti">
-    <x-header />
+@section('content')
 
-    <div class="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-        <h1
-            class="text-slate-900 font-extrabold text-3xl sm:text-4xl lg:text-5xl tracking-tight text-center dark:text-white">
-            An open platform for AI agents</h1>
-        <p class="mt-6 text-xl text-slate-600 text-center max-w-3xl mx-auto dark:text-bluebell">Built in public
-            from scratch. Launching soon!</p>
-    </div>
+<h1 class="mt-12 text-slate-900 font-extrabold text-3xl sm:text-4xl lg:text-5xl text-center dark:text-white">
+    An open platform for AI agents</h1>
+<p class="mt-6 text-xl text-slate-600 text-center max-w-3xl tracking-wide mx-auto dark:text-bluebell">Built in public
+    from scratch. Launching soon!</p>
 
-    <!-- Episode 1 video -->
-    <div class="mt-12 mx-auto w-[560px] max-w-full">
-        <blockquote class="twitter-tweet" data-theme="dark" data-media-max-width="560">
-            <a href="https://twitter.com/OpenAgentsInc/status/1721942435125715086?ref_src=twsrc%5Etfw"></a>
-        </blockquote>
-    </div>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<!-- Episode 1 video -->
+<div class="mt-12 mx-auto w-[560px] max-w-full">
+    <blockquote class="twitter-tweet" data-theme="dark" data-media-max-width="560">
+        <a href="https://twitter.com/OpenAgentsInc/status/1721942435125715086?ref_src=twsrc%5Etfw"></a>
+    </blockquote>
+</div>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-</body>
-
-</html>
+@endsection
