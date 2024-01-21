b2d7b8a6633f557d38f1954ea182da10cc772477
Christopher David <chris@arcadelabs.co>
2024-01-21 13:27:41 -0600

    colors

--- resources/views/plugin-show.blade.php
+++ resources/views/plugin-show.blade.php
@@ -3,10 +3,24 @@
 @section('title', 'Plugins')
 
 @section('content')
+
+<div class="md:flex md:items-center md:justify-between">
+    <div class="min-w-0 flex-1">
+        <h2 class="text-2xl font-bold leading-7 dark:text-white sm:truncate sm:text-3xl sm:tracking-tight">
+            {{ $plugin->name }}
+        </h2>
+    </div>
+    <div class="mt-4 flex md:ml-4 md:mt-0">
+        <button type="button"
+            class="inline-flex items-center rounded-md bg-indigo-400 dark:bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">Edit</button>
+        <button type="button"
+            class="ml-3 inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Publish</button>
+    </div>
+</div>
+
 <div class="prose dark:prose-invert">
-    {{ $plugin->name }}
-    {{ $plugin->description }}
-    {{ $plugin->fee }}
-    {{ $plugin->wasm_url }}
+    <p>{{ $plugin->description }}</p>
+    <p>{{ $plugin->fee }}</p>
+    <p>{{ $plugin->wasm_url }}</p>
 </div>
 @endsection
