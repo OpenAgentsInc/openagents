713b6bdffa01ca8c5f1cc6f45d1ec04cf1d9e9a3
Christopher David <chris@arcadelabs.co>
2024-01-21 14:53:41 -0600

    show fee and not url

--- resources/views/plugin-show.blade.php
+++ resources/views/plugin-show.blade.php
@@ -5,10 +5,11 @@
 @section('content')
 
 <div class="md:flex md:items-center md:justify-between">
-    <div class="min-w-0 flex-1">
-        <h2 class="text-2xl font-bold leading-7 dark:text-white sm:truncate sm:text-3xl sm:tracking-tight">
+    <div class="min-w-0 flex-1 flex items-center">
+        <h2 class="text-2xl font-bold leading-7 dark:text-white sm:truncate sm:text-3xl sm:tracking-tight mr-4">
             {{ $plugin->name }}
         </h2>
+        <x-bitcoin-amount :amount="$plugin->fee" class="text-lg" />
     </div>
     <div class="mt-4 flex md:ml-4 md:mt-0">
         <x-button variant="secondary" class="mr-2">
@@ -23,7 +24,6 @@
 
 <div class="prose dark:prose-invert">
     <p>{{ $plugin->description }}</p>
-    <p>{{ $plugin->fee }}</p>
-    <p>{{ $plugin->wasm_url }}</p>
+    <!-- <p><a href="{{ $plugin->wasm_url }}" class="text-blue-500 hover:text-blue-700">{{ $plugin->wasm_url }}</a></p> -->
 </div>
 @endsection
