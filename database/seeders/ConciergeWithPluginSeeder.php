21d4127df6b7ff6e25b1151c7dedeba0d2b02a18
Christopher David <chris@arcadelabs.co>
2024-01-22 11:55:51 -0600

    seeder ehehe

--- database/seeders/ConciergeWithPluginSeeder.php
+++ database/seeders/ConciergeWithPluginSeeder.php
@@ -45,7 +45,7 @@ class ConciergeWithPluginSeeder extends Seeder
         // Create main chat task
         $task = Task::create([
             'agent_id' => $agent->id,
-            'description' => 'Respond to user chat message after consulting knowledge base'
+            'description' => 'Respond to user chat message after consulting knowledge base, but only count the vowels of what you would respond with'
         ]);
 
         // Create the steps
