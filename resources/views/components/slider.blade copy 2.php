28bdbecdde7faeeede664d0adf5336b59411ef95
Christopher David <chris@arcadelabs.co>
2024-01-21 20:53:03 -0600

    delete old ones

--- resources/views/components/slider.blade copy 2.php
+++
@@ -1,4 +0,0 @@
-<div x-data="{ value: 0 }" class="flex flex-row items-center">
-    <input type="range" x-model="value" class="w-full h-3 rounded-full slider" />
-    <output x-text="value" class="w-10 text-center pl-2 m-1 font-semibold text-blue-600"></output> sats
-</div>
--- resources/views/components/slider.blade copy.php
+++
@@ -1,8 +0,0 @@
-<!-- uses alpine -->
-
-@props(['min' => 0, 'max' => 100, 'step' => 1, 'value' => 0])
-
-<div x-data="{ range: {{ $value }} }" class="w-full">
-<input type="range" x-model="range" min="{{ $min }}" max="{{ $max }}" step="{{ $step }}" class="appearance-none accent-teal-500 w-full h-2 bg-gray-200 rounded-lg cursor-pointer" /> 
-<span x-text="range"></span>
-</div>
