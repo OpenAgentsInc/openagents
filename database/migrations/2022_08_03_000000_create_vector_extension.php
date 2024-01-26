6beb072137288193f7f038e038503c891b6389da
Christopher David <chris@arcadelabs.co>
2024-01-26 16:16:57 -0600

    and vector down too

--- database/migrations/2022_08_03_000000_create_vector_extension.php
+++ database/migrations/2022_08_03_000000_create_vector_extension.php
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down()
     {
-        // DB::statement('DROP EXTENSION vector');
+        DB::statement('DROP EXTENSION vector');
     }
 };
