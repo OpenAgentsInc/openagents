d4fdccb0570e172cff76e09ebc0ac60fed96acb6
Christopher David <chris@arcadelabs.co>
2024-01-28 10:39:12 -0600

    L402 class buildout

--- app/Services/L402.php
+++ app/Services/L402.php
@@ -9,10 +9,25 @@ class L402
     public function __construct()
     {
         $this->wallet = new Client(env('ALBY_ACCESS_TOKEN'));
-        // die and dump the length of the access token
-        dd(config('env'));
-        dd(strlen(env('TOGETHER_API_KEY')));
+    }
 
-        dd($this->wallet->getInfo());
+    public function sendRequest($url, $l402Token = null)
+    {
+        $headers = [];
+        if ($l402Token) {
+            $headers['Authorization'] = 'L402 '.$l402Token;
+        }
+
+        return Http::withHeaders($headers)->get($url);
+    }
+
+    public function parseAuthHeader($authHeader)
+    {
+        return explode(':', str_replace('L402 ', '', $authHeader));
+    }
+
+    public function assembleL402Token($macaroon, $preimage)
+    {
+        return $macaroon.':'.$preimage;
     }
 }
