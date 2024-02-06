60a657c414518dc167652989fbbc25789a89f2a5
Christopher David <chris@arcadelabs.co>
2024-02-05 19:11:16 -0600

    Add Extism host functions support (#105)
    * error hm
    
    * host funcs work w php 8.3
    
    * axe docker 8.2 configs
    
    * plugin HF tests to integration
    
    * try CI use 8.3 and extism cli
    
    * nah - just try 8.3
    
    * moar integ
    
    * pluginregistry to integration
    
    * moar integ
    
    * and that one

--- tests/Feature/PluginViewTest.php
+++ tests/Feature/PluginViewTest.php
@@ -6,7 +6,7 @@ test('can view plugin show page', function () {
     $plugin = Plugin::factory()->create();
     $response = $this->get(route('plugins.show', $plugin));
     $response->assertStatus(200);
-});
+})->group('integration');
 
 test('plugin show page shows plugin name, description, and fee', function () {
     $plugin = Plugin::factory()->create();
@@ -15,9 +15,9 @@ test('plugin show page shows plugin name, description, and fee', function () {
     $response->assertSee($plugin->description);
     $response->assertSee($plugin->fee);
     // $response->assertSee($plugin->wasm_url);
-});
+})->group('integration');
 
 test('if plugin not found, redirect to home page', function () {
     $response = $this->get(route('plugins.show', 1));
-    $response->assertRedirect("/");
-});
+    $response->assertRedirect('/');
+})->group('integration');
