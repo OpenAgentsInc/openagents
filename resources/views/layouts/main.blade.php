0cb7507129356a80c83f8ebde958efb45261c74c
Christopher David <chris@arcadelabs.co>
2024-02-07 09:39:31 -0600

    bad script tag thing

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'OpenAgents')</title>
-    </script>
     @include('partials.vite')
     @include('analytics')
 </head>
