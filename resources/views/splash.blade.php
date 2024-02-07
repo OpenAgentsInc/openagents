c207730b8655b490eb755c8f7bdf02abce1ccfa2
Christopher David <chris@arcadelabs.co>
2024-02-07 08:44:59 -0600

    splash stfu

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -1,13 +1,16 @@
-@extends('layouts.main')
+@extends('layouts.simple')
 
 @section('title', 'OpenAgents')
 
 @section('content')
 
-<h1 class="mt-12 text-grey-900 font-extrabold text-3xl sm:text-4xl lg:text-5xl text-center dark:text-white">
-    An open platform for AI agents</h1>
-<p class="mt-6 text-xl text-grey-600 text-center max-w-3xl tracking-wide mx-auto dark:text-teal-300">Built in
-    public
-    from scratch. Launching soon!</p>
+<div class="h-[80vh] flex flex-col justify-center items-center font-mono">
+    <div class="fixed text-center">
+        <div class="border py-4 px-5 mx-auto">
+            <h1 class="text-2xl">OpenAgents</h1>
+        </div>
+        <p class="text-gray mt-6">Coming soon</p>
+    </div>
+</div>
 
 @endsection
