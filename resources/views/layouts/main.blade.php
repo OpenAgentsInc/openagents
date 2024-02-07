f598287a7e14ed3cecb8095f53aeb9ab11af769c
Christopher David <chris@arcadelabs.co>
2024-02-07 10:11:49 -0600

    footer no bg

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,7 @@
             @yield('content')
         </div>
     </div>
-    <footer class="text-sm px-6 sm:px-8 py-4 fixed bottom-0 w-full bg-white dark:bg-black">
+    <footer class="text-sm px-6 sm:px-8 py-4 fixed bottom-0 w-full">
         <div class="mx-auto flex justify-end space-x-4">
             <a href="https://twitter.com/OpenAgentsInc" target="_blank"
                 class="text-gray hover:text-black dark:hover:text-white mx-2">Twitter</a>
