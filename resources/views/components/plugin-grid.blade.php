d18c843c189fdef4f6c6e7038a87b240a2949721
Christopher David <chris@arcadelabs.co>
2024-02-07 07:48:40 -0600

    shhhh

--- resources/views/components/plugin-grid.blade.php
+++ resources/views/components/plugin-grid.blade.php
@@ -1,5 +1,4 @@
 @fragment('plugin-grid')
-    <h1 class="text-2xl font-bold mb-4 text-center">Plugins</h1>
     <div id="plugin-grid" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
         @forelse($plugins as $plugin)
             <x-plugin :plugin="$plugin" />
--- resources/views/components/plugin.blade.php
+++ resources/views/components/plugin.blade.php
@@ -1,17 +1,8 @@
 @props(['plugin'])
 
     <a href="/plugin/{{ $plugin->id }}">
-        <x-card class="relative">
-            <div class="absolute top-0 right-0 mt-2 mr-2">
-                <x-bitcoin-amount :amount="$plugin->fee" class="text-lg" />
-            </div>
-            <x-card-header>
-                <x-card-title>{{ $plugin->name }}</x-card-title>
-                <x-card-description>{{ $plugin->description }}</x-card-description>
-            </x-card-header>
-            <x-card-content>
-                <p class="text-sm text-grey-500 dark:text-grey-400">Created:
-                    {{ $plugin->created_at->format('M d, Y') }}</p>
-            </x-card-content>
-        </x-card>
+        <p>{{ $plugin->name }}</p>
+        <p>{{ $plugin->description }}</p>
+        <p>{{ $plugin->fee }}</p>
+        <p>{{ $plugin->created_at->format('M d, Y') }}</p>
     </a>
