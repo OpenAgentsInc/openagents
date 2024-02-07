b8c2b159c6a8e2f3c482eb642124e69718143b19
Christopher David <chris@arcadelabs.co>
2024-02-07 08:31:04 -0600

    axe fat video embed

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -10,12 +10,4 @@
     public
     from scratch. Launching soon!</p>
 
-<!-- Episode 1 video -->
-<div class="mt-12 mx-auto w-[560px] max-w-full">
-    <blockquote class="twitter-tweet" data-theme="dark" data-media-max-width="560">
-        <a href="https://twitter.com/OpenAgentsInc/status/1721942435125715086?ref_src=twsrc%5Etfw"></a>
-    </blockquote>
-</div>
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-
 @endsection
