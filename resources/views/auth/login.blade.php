487ce5d8da38d938969505de9d0f0c608a8e9eba
Christopher David <chris@arcadelabs.co>
2024-02-07 15:09:17 -0500

    start restyling compoennts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,16 +27,14 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox"
-                    class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
-                    name="remember">
-                <span
-                    class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                    class="rounded border-gray text-black shadow-sm focus:ring-black" name="remember">
+                <span class="ms-2 text-sm text-gray">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if(Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                <a class="underline text-sm text-gray hover:text-black dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-black"
                     href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     @include('analytics')
 </head>
 
-<body class="font-sans text-black antialiased">
+<body class="font-mono text-black antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray dark:bg-black">
         <div>
             <a href="/">
@@ -24,7 +24,7 @@
             </a>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
