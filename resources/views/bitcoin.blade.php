1b195345defc0544551e83a7db34d01832f1385a
Christopher David <chris@arcadelabs.co>
2024-02-07 12:49:48 -0500

    axe unused bitcoin views

--- resources/views/bitcoin.blade.php
+++
@@ -1,7 +0,0 @@
-<script src="https://unpkg.com/htmx.org@1.9.10"
-    integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous">
-</script>
-
-<div hx-get="{{ route('bitcoin-price') }}" hx-trigger="every 5s" id="bitcoin-price">
-    @include('bitcoin-price', ['price' => $price])
-</div>
