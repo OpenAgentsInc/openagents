97603f1c56779a9859b311311631a860264076e0
Christopher David <chris@arcadelabs.co>
2024-02-07 08:21:20 -0600

    plugin shh

--- resources/views/components/plugin.blade.php
+++ resources/views/components/plugin.blade.php
@@ -3,6 +3,4 @@
     <a href="/plugin/{{ $plugin->id }}" class="font-mono border border-black dark:border-white p-4">
         <p class="font-bold text-lg">{{ $plugin->name }}</p>
         <p class="mt-2 text-sm">{{ $plugin->description }}</p>
-        <!--<p>{{ $plugin->fee }}</p>-->
-        <!--<p>{{ $plugin->created_at->format('M d, Y') }}</p>-->
     </a>
