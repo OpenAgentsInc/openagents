a2d04f2df46deb2439dfdfeec1677d0185eee7a1
Christopher David <chris@arcadelabs.co>
2024-02-08 00:07:40 -0500

    agent create via existing ya

--- resources/views/agent-create.blade.php
+++ resources/views/agent-create.blade.php
@@ -1,12 +1,30 @@
 @extends('layouts.main')
 
-@section('title', 'Create Plugin')
+@section('title', 'Create Agent')
 
 @section('content')
 
 <div class="my-8 mx-auto max-w-xl">
     <div class="font-bold text-xl">Create Agent</div>
     <div class="mt-1 text-sm text-gray">Let's fucking go</div>
+    <form id="upload-agent" method="POST" action="{{ route('agents.store') }}"
+        enctype="multipart/form-data" class="space-y-6">
+        @csrf
+        <div>
+            <x-label for="name">Name</x-label>
+            <x-input id="name" name="name" placeholder="Agent Name" autofocus />
+        </div>
+        <div>
+            <x-label for="description">Description</x-label>
+            <x-textarea id="description" name="description" placeholder="Agent Description">
+            </x-textarea>
+        </div>
+        <div class="flex justify-center">
+            <x-button variant="outline" size="lg" type="submit">
+                Create
+            </x-button>
+        </div>
+    </form>
 </div>
 
 @endsection
--- tests/Feature/AgentCreateTest.php
+++ tests/Feature/AgentCreateTest.php
@@ -1,8 +1,39 @@
 <?php
 
+use App\Models\Agent;
+use App\Models\User;
+
 test('user can see create agent form', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
     $this->get('/agents/create')
         ->assertOk()
         ->assertSee('Create Agent')
         ->assertViewIs('agent-create');
+});
+
+test('unauthed user cannot create agent', function () {
+    $this->post('/agents', [
+        'name' => 'Count Vowels',
+        'description' => 'Count the vowels in a string',
+        'instructions' => 'Count the vowels in a string',
+        'welcome_message' => 'Welcome to the Count Vowels agent!',
+    ])->assertRedirect('/login');
+});
+
+test('user can create agent', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    expect(Agent::all())->toHaveCount(0);
+
+    $this->post('/agents', [
+        'name' => 'Count Vowels',
+        'description' => 'Count the vowels in a string',
+        'instructions' => 'Count the vowels in a string',
+        'welcome_message' => 'Welcome to the Count Vowels agent!',
+    ]);
+
+    expect(Agent::all())->toHaveCount(1);
 });
