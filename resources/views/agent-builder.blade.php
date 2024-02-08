795e00423431312bbfd43f65420d10de1052574c
Christopher David <chris@arcadelabs.co>
2024-02-08 13:53:30 -0500

    alpine

--- resources/views/agent-builder.blade.php
+++ resources/views/agent-builder.blade.php
@@ -1,2 +1,17 @@
-{{ $agent->name }}
-{{ $agent->description }}
+@extends('layouts.main')
+
+@section('title', 'Create Agent')
+
+@section('content')
+
+<div class="my-8 mx-auto max-w-xl">
+    <div class="font-bold text-xl">{{ $agent->name }}</div>
+    <div class="mt-1 text-sm text-gray">{{ $agent->description }}</div>
+
+    <!-- button to add a plugin -->
+    <x-button variant="outline" size="lg" class="mt-8">
+        Add Plugin
+    </x-button>
+</div>
+
+@endsection
