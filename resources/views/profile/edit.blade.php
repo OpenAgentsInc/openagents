d4efe645c26e362e643a3ffbfe0bedaca6c5fdf0
Christopher David <chris@arcadelabs.co>
2024-02-09 09:56:32 -0500

    center profile edit

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,22 +6,24 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center">
+            <div class="space-y-6">
+                <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
+                    <div class="max-w-xl mx-auto">
+                        @include('profile.partials.update-profile-information-form')
+                    </div>
                 </div>
-            </div>
 
-            <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+                <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
+                    <div class="max-w-xl mx-auto">
+                        @include('profile.partials.update-password-form')
+                    </div>
                 </div>
-            </div>
 
-            <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
+                    <div class="max-w-xl mx-auto">
+                        @include('profile.partials.delete-user-form')
+                    </div>
                 </div>
             </div>
         </div>
