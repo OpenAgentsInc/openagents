fa950a6b64be98ca8c7a0883d882a9c195600f4e
Christopher David <chris@arcadelabs.co>
2024-02-09 09:39:48 -0500

    dashboard non horrible

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray" />
                     </a>
                 </div>
