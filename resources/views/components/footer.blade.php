f252dcae92d879de23088ada123345d3b918b3df
Christopher David <chris@arcadelabs.co>
2024-02-09 09:15:51 -0500

    footer formatting match header

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,7 @@
-<footer class="text-sm px-6 sm:px-8 py-4 fixed bottom-0 w-full">
-    <div class="mx-auto flex justify-end space-x-4">
-        <a href="https://twitter.com/OpenAgentsInc" target="_blank"
-            class="text-gray hover:text-black dark:hover:text-white mx-2">Twitter</a>
+<footer class="bg-black text-white h-12 text-sm fixed bottom-0 w-full">
+    <div class="max-w-7xl mx-auto w-full flex justify-end space-x-4 px-4 sm:px-6 lg:px-8">
+        <a href="https://twitter.com/OpenAgentsInc" target="_blank" class="text-gray hover:text-white mx-2">Twitter</a>
         <a href="https://github.com/OpenAgentsInc/openagents" target="_blank"
-            class="text-gray hover:text-black dark:hover:text-white mx-2">GitHub</a>
+            class="text-gray hover:text-white mx-2">GitHub</a>
     </div>
 </footer>
