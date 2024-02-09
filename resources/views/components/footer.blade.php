bb3e6ab1a33df8c53042609e88ac9fb30614b522
Christopher David <chris@arcadelabs.co>
2024-02-09 08:54:04 -0500

    extract footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-sm px-6 sm:px-8 py-4 fixed bottom-0 w-full">
+    <div class="mx-auto flex justify-end space-x-4">
+        <a href="https://twitter.com/OpenAgentsInc" target="_blank"
+            class="text-gray hover:text-black dark:hover:text-white mx-2">Twitter</a>
+        <a href="https://github.com/OpenAgentsInc/openagents" target="_blank"
+            class="text-gray hover:text-black dark:hover:text-white mx-2">GitHub</a>
+    </div>
+</footer>
