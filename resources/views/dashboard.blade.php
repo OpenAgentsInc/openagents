1f8063f3fc78fa355b1dd5d46ce812fe4b55f055
Christopher David <chris@arcadelabs.co>
2024-02-09 09:57:14 -0500

    center dashboard intro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center">
             <div class="bg-black overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray">
+                <div class="p-6 text-gray text-center">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
