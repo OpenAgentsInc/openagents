df414d8a3552d2f46c6472097c2ac150b8e15358
Christopher David <chris@arcadelabs.co>
2024-02-10 17:33:38 -0500

    list of all agents, linking to each build

--- resources/views/agent-list.blade.php
+++ resources/views/agent-list.blade.php
@@ -1,7 +1,17 @@
-<h1>all agents!</h1>
+@extends('layouts.main')
 
-<!-- loop through $agents and show each name -->
+@section('title', 'Create Agent')
 
-@foreach($agents as $agent)
-    <p>{{ $agent->name }}</p>
-@endforeach
+@section('content')
+
+<div class="grid grid-cols-3 gap-4">
+    @foreach($agents as $agent)
+        <a href="{{ route('agent.build', $agent) }}">
+            <div class="border-2 border-offblack rounded-[16px] p-4">
+                <p>{{ $agent->name }}</p>
+            </div>
+        </a>
+    @endforeach
+</div>
+
+@endsection
