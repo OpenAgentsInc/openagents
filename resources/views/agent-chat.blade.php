ab0e6287332e3c9d5a127418fb5dfe2c047087d9
Christopher David <chris@arcadelabs.co>
2024-02-15 13:04:27 -0600

    better and box

--- resources/views/agent-chat.blade.php
+++ resources/views/agent-chat.blade.php
@@ -17,12 +17,40 @@
         <main class="flex-1 overflow-hidden">
             <div class="flex flex-row w-screen h-full">
                 <div class="w-1/5 border-r border-offblack shadow-xl nice-scrollbar overflow-y-auto">
-                    what
+
                 </div>
                 <div class="w-4/5 flex flex-col px-2">
                     <div id="chatbox-container" class="grow nice-scrollbar weird-height">
-                        hi
+
                     </div>
+<div>
+    <form id="sendMessageForm" onsubmit="sendMessage(event)">
+        <div class="flex flex-row items-center h-auto rounded-xl w-full px-4">
+            <div class="flex-grow ml-4">
+                <div class="relative w-full">
+                    <x-textarea id="message-input" name="body"
+                        autofocus
+                        onkeydown="if(event.keyCode == 13 && !event.shiftKey) { event.preventDefault(); document.getElementById('send-message').click(); }"
+                        class="text-md w-full rounded-lg border focus:outline-none disabled:opacity-50 dark:text-white focus:ring-0"
+                        rows="3"></x-textarea>
+                </div>
+            </div>
+            <div class="ml-4">
+                <button id="send-message" type="submit"
+                    class="flex items-center justify-center bg-teal-500 hover:bg-teal-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+                    <span>Send</span>
+                    <span class="ml-2">
+                        <svg class="w-4 h-4 transform rotate-45 -mt-px" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+                        </svg>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
                 </div>
             </div>
         </main>
