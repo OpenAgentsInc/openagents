fecac4f7049a78eeca89a67dcaf5acfe8d9db948
Christopher David <chris@arcadelabs.co>
2024-02-16 08:59:06 -0600

    initial form handler with body and clear input

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -6,8 +6,18 @@ use Livewire\Component;
 
 class Chat extends Component
 {
+    public $body = '';
+
+    public function sendMessage()
+    {
+        // Send the message
+
+        // Clear the input
+        $this->body = '';
+    }
+
     public function render()
-   {
+    {
         return view('livewire.chat')->layout('components.layouts.chat');
     }
 }
