b8b7f02581ce62384e2e0dbe8e2b2ba246741e08
Christopher David <chris@arcadelabs.co>
2024-02-16 21:52:19 -0600

    headerrrrs

--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -4,7 +4,7 @@
     <div class="mt-12">
         <h1>Brand</h1>
         <div class="my-2 p-8 rounded-[8px] bg-offblack w-[350px]">
-            <h3 class="mb-4">Logo</h3>
+            <h3 class="mb-4 text-gray">Logo</h3>
             <x-logo />
         </div>
     </div>
