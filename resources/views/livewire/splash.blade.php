42eb16f7fbd65e84507c5e4fafade49bc0629763
Christopher David <chris@arcadelabs.co>
2024-02-17 14:12:33 -0600

    splash

--- resources/views/livewire/splash.blade.php
+++ resources/views/livewire/splash.blade.php
@@ -1,8 +1,7 @@
 <div class="h-full flex flex-col justify-center items-center">
     <div class="fixed text-center">
         <h1>Build your own AI workforce</h1>
-        <h4 class="text-gray mt-6 mb-12">Multiply your productivity and get paid</h4>
-        <x-button variant="outline" size="lg" class="mx-1 py-6">View agents</x-button>
-        <x-button size="lg" class="mx-1 py-6">Get started</x-button>
+        <h4 class="text-gray mt-6 mb-8">Multiply your productivity and get paid</h4>
+        <x-button variant="outline" size="lg" class="mx-1 py-6">Get started</x-button>
     </div>
 </div>
