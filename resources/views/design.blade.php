ce698f2512e186f75ffeb6e8c1f8ef86647ef9dc
Christopher David <chris@arcadelabs.co>
2024-02-16 22:34:55 -0600

    logomark

--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -3,9 +3,17 @@
     <h1 class="title">Design system</h1>
     <div class="mt-12">
         <h1>Brand</h1>
-        <div class="my-2 p-8 rounded-[8px] bg-offblack w-[350px]">
+        <div class="my-2 p-8 rounded-[8px] bg-offblack w-[450px]">
             <h3 class="mb-4 text-gray">Logo</h3>
             <x-logo />
+            <h3 class="mt-8 mb-2 text-gray">Mark</h3>
+            <div class="space-y-4">
+                <x-logomark size="1" />
+                <x-logomark size="2" />
+                <x-logomark size="3" />
+                <x-logomark size="4" />
+                <x-logomark size="5" />
+            </div>
         </div>
     </div>
 </x-blank-layout>
