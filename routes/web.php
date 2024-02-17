75d4ac57a324cae6a8945cc23336362163287492
Christopher David <chris@arcadelabs.co>
2024-02-17 14:06:52 -0600

    catchall

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ use App\Livewire\Splash;
 Route::get('/', Splash::class)->name('home');
 
 Route::get('/design', [StaticController::class, 'design'])->name('design');
+
+// Add a catch-all redirect to the homepage
+Route::get('/{any}', function () {
+    return redirect('/');
+})->where('any', '.*');
