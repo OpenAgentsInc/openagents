511cfa3f56f13317ffaab0cfa7079eba03c39fd3
Christopher David <chris@arcadelabs.co>
2024-02-17 14:06:02 -0600

    design route

--- resources/views/livewire/splash.blade.php
+++ resources/views/livewire/splash.blade.php
@@ -1,7 +1,7 @@
 <div class="h-full flex flex-col justify-center items-center">
     <div class="fixed text-center">
         <h1>Build your own AI workforce</h1>
-        <h4 class="text-gray mt-4 mb-8">Multiply productivity and get paid</h4>
+        <h4 class="text-gray mt-4 mb-8">Multiply your productivity and get paid</h4>
         <x-button variant="outline" size="lg">Get started</x-button>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\StaticController;
 use App\Livewire\Splash;
 
 Route::get('/', Splash::class)->name('home');
+
+Route::get('/design', [StaticController::class, 'design'])->name('design');
