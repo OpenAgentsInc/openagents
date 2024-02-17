0525cff0f3ef0cf4d2576c0b98c6fea22564c534
Christopher David <chris@arcadelabs.co>
2024-02-17 17:50:54 -0600

    nicer stream replace thar

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\AgentController;
 use App\Http\Controllers\StaticController;
 use App\Livewire\Chat;
 use App\Livewire\Splash;
 
 Route::get('/', Splash::class)->name('home');
-Route::get('/chat', Chat::class)->name('chat');
 
+// Agent chat
+Route::get('/chat', Chat::class)->name('chat');
+Route::post('/agent/{id}/run', [AgentController::class, 'run_task'])->name('agent.run_task');
+
+// Dev only
 Route::get('/design', [StaticController::class, 'design'])->name('design');
 
 // Include breeze auth routes
