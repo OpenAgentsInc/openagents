24be4c724931409ba026b454c143ce7b5981abf3
Christopher David <chris@arcadelabs.co>
2024-02-17 14:16:39 -0600

    Zerobase (#131)
    * all tests are old
    
    * cleanup and start fresh
    
    * php artisan make:livewire Navbar --pest
    
    * lander hehe
    
    * design route
    
    * catchall
    
    * splash
    
    * splash

--- resources/views/components/layouts/chat.blade.php
+++ resources/views/components/layouts/chat.blade.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-        @stack('scripts')
-        @include('partials.vite')
-        @include('analytics')
-    </head>
-    <body class="bg-black text-white h-screen font-mono">
-        <div class="flex flex-col h-full">
-            @include('layouts.navigation')
-            <main class="flex-1 overflow-hidden">
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+    @stack('scripts')
+    @include('partials.vite')
+    @include('analytics')
+</head>
+
+<body>
+    <div class="flex flex-col h-full">
+        @include('layouts.navigation')
+        <main class="flex-1 overflow-hidden">
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
 </html>
