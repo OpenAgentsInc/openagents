d3efd18d7927186b00a3a31b14d546fc176e002d
Christopher David <chris@arcadelabs.co>
2024-02-18 13:23:15 -0600

    works well with circleoffset

--- app/Livewire/Graph.php
+++ app/Livewire/Graph.php
@@ -13,13 +13,15 @@ class Graph extends Component
     {
         // Initialize your nodes and edges here
         $this->nodes = [
-            ['id' => 1, 'x' => 100, 'y' => 50, 'width' => 300, 'height' => 200, 'title' => 'Start'],
-            ['id' => 2, 'x' => 600, 'y' => 100, 'width' => 300, 'height' => 200, 'title' => 'End'],
+            ['id' => 1, 'x' => 50, 'y' => 50, 'width' => 300, 'height' => 100, 'title' => 'Start'],
+            ['id' => 2, 'x' => 400, 'y' => 75, 'width' => 300, 'height' => 150, 'title' => 'Middle'],
+            ['id' => 3, 'x' => 750, 'y' => 100, 'width' => 300, 'height' => 100, 'title' => 'End'],
         ];
 
         // Assuming each edge connects the centers of the right side of one node to the left side of the next node
         $this->edges = [
-            ['from' => 1, 'to' => 2]
+            ['from' => 1, 'to' => 2],
+            ['from' => 2, 'to' => 3],
         ];
     }
 
