2d0f524d0d80bfbbac7012cf7812ce767a8f07c2
Christopher David <chris@arcadelabs.co>
2024-02-18 17:43:19 -0600

    axe prev edge

--- resources/views/components/edge.blade.php
+++
@@ -1,6 +0,0 @@
-@props(['from', 'to'])
-
-    <!-- Edge SVG line -->
-    <line x1="{{ $from['x'] }}" y1="{{ $from['y'] }}"
-        x2="{{ $to['x'] }}" y2="{{ $to['y'] }}" stroke="white"
-        stroke-width="2" />
