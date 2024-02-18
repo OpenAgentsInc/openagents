795a6c08d01854dc73efb1df65f45611ef185514
Christopher David <chris@arcadelabs.co>
2024-02-18 00:03:12 -0600

    Initial HUD scratchpad (#133)
    * empty hud page
    
    * initial lines
    
    * ok
    
    * weird
    
    * flash
    
    * initial hud

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -11,6 +11,11 @@ class StaticController extends Controller
         return view('splash');
     }
 
+    public function hud()
+    {
+        return view('hud');
+    }
+
     public function design()
     {
         return view('design');
