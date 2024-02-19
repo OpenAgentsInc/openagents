160436e54311a64892e739e7f5a7d40bb86bcaf1
Christopher David <chris@arcadelabs.co>
2024-02-19 12:03:55 -0600

    loadspinner

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -2,10 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Login extends Component
 {
+    public $email = '';
+
+    public function submit()
+    {
+        $this->validate([
+            'email' => 'required|email',
+        ]);
+
+        sleep(1);
+
+        // Check if there's a user with this email
+        $user = User::where('email', $this->email)->first();
+    }
+
     public function render()
     {
         return view('livewire.login')->layout('layouts.blank');
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -7,18 +7,25 @@
         </div>
 
         <div class="w-[476px]">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
+            <form wire:submit="submit">
                 <div>
                     <x-input-label for="email" :value="__('Email')" />
-                    <x-input id="email" type="email" name="email" :value="old('email')" required autofocus
+                    <x-input wire:model="email" id="email" type="email" name="email" required autofocus
                         autocomplete="username" class="h-[48px] border-offblack" placeholder="satoshi@vistomail.com" />
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
 
                 <div class="flex items-center justify-end mt-[32px]">
-                    <x-button variant="default" class="w-full h-[48px] text-[24px]">
+                    <x-button variant="default"
+                        class="w-full h-[48px] text-[20px] disabled:cursor-not-allowed disabled:opacity-75">
                         Get started
+                        <svg wire:loading class="absolute right-0 animate-spin -ml-1 mr-3 h-5 w-5 text-black"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+                            </circle>
+                            <path class="opacity-75" fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                            </path>
+                        </svg>
                     </x-button>
                 </div>
             </form>
