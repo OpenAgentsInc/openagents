bdd045d4cf418acb2a079ec8452c15ce23cdd481
Christopher David <chris@arcadelabs.co>
2024-02-19 20:24:40 -0600

    php artisan vendor:publish --provider="Spatie\Sheets\SheetsServiceProvider" --tag="config"

---
+++ config/sheets.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'default_collection' => null,
+
+    'collections' => [
+
+        /* An example collection. All keys are optional.
+
+        'posts' => [
+            'disk' => 'posts',
+            'sheet_class' => App\Models\Post::class,
+            'path_parser' => Spatie\Sheets\PathParsers\SlugWithDateParser::class,
+            'content_parser' => Spatie\Sheets\ContentParsers\MarkdownParser::class,
+            'extension' => 'txt',
+        ], */
+    ],
+];
