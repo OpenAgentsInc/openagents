a17a9fadc6ef3cd3c31fb7723d7a95016d0f4304
Christopher David <chris@arcadelabs.co>
2024-02-20 19:39:50 -0600

    font

--- resources/views/docs/show.blade.php
+++ resources/views/docs/show.blade.php
@@ -3,12 +3,12 @@
 
     <div class="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
         <div
-            class="z-20 hidden lg:block fixed bottom-0 right-auto w-[18rem] pl-4 pr-6 pb-10 overflow-y-auto stable-scrollbar-gutter top-[7.3rem]">
+            class="z-20 hidden lg:block fixed bottom-0 right-auto w-[18rem] pl-4 pr-6 pb-10 overflow-y-auto stable-scrollbar-gutter top-[85px]">
             <div class="relative lg:text-sm lg:leading-6">
                 <ul>
                     @foreach($documentsList as $slug => $title)
                         <li
-                            class="font-bold my-2 {{ $activePage === $slug ? 'text-white' : 'text-gray' }}">
+                            class="text-lg font-bold my-4 {{ $activePage === $slug ? 'text-white' : 'text-gray' }}">
                             <a wire:navigate
                                 href="{{ route('docs.show', $slug) }}">{{ $title }}</a>
                         </li>
@@ -17,8 +17,8 @@
             </div>
         </div>
 
-        <div class="flex justify-center min-h-screen">
-            <div class="docs w-full max-w-5xl pt-20 px-12">
+        <div class="flex justify-center min-h-screen pt-[62px]">
+            <div class="docs w-full max-w-5xl px-12">
                 {!! $content->contents !!}
             </div>
         </div>
