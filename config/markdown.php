efc97d7d45691c3063320735f93c09bd631a234a
Christopher David <chris@arcadelabs.co>
2024-02-22 15:34:44 -0600

    tokyo night default

--- config/markdown.php
+++ config/markdown.php
@@ -14,7 +14,7 @@ return [
          *
          * More info: https://github.com/shikijs/shiki/blob/main/docs/themes.md
          */
-        'theme' => 'vitesse-black',
+        'theme' => 'tokyo-night',
     ],
 
     /*
