70b6342f2607ba427d948062ca7c29c322d80502
Christopher David <chris@arcadelabs.co>
2024-02-21 18:56:35 -0600

    ok

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -1,16 +1,13 @@
 <div class="relative z-0 flex h-full w-full overflow-hidden">
-    <div class="flex-shrink-0 overflow-x-hidden bg-offblack w-[300px]">
+    <div class="fixed h-full flex-shrink-0 overflow-x-hidden bg-offblack w-[300px]">
         <div class="h-full w-[300px]">
-            <div class="flex h-full min-h-0 flex-col">
-                <div class="flex h-full min-h-0 flex-col transition-opacity opacity-100">
-                    <div class="scrollbar-trigger relative h-full w-full flex-1 items-start border-white/20">
-                        <h2
-                            style="position: absolute; border: 0px; width: 1px; height: 1px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; overflow-wrap: normal;">
-                            Chat history</h2>
-                        <nav class="flex h-full w-full flex-col px-3 pb-3.5" aria-label="Chat history">
-                        </nav>
-                    </div>
-                </div>
+            <x-button variant="ghost" size="lg" class="my-4">New session</x-button>
+            <div class="px-4">
+                <p class="text-gray">Recent</p>
+                <p>You chattin about stuff</p>
+                <p>Component system</p>
+                <p>Google tag manager</p>
+                <p>Backend testing</p>
             </div>
         </div>
     </div>
