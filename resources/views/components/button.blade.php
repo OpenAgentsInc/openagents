368254ea4ef41d3f27a171aa233ced2128a5d0c9
Christopher David <chris@arcadelabs.co>
2024-02-23 16:15:14 -0600

    New button design & some typography  (#144)
    * format dat
    
    * start buttons
    
    * button setup
    
    * button style
    
    * button label etc
    
    * icon classes
    
    * not bad
    
    * better
    
    * try darker bg

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,32 +1,34 @@
-@props(['variant' => 'ghost', 'size' => 'default', 'icon' => null])
+@props(['variant' => 'primary', 'size' => 'default', 'icon' => null])
 
 @php
-    $baseClasses = 'inline-flex items-center justify-center rounded-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50';
+    $baseClasses = 'rounded-[6px] font-bold inline-flex items-center justify-between focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50';
     $variantClasses = match($variant) {
-        'ghost' => 'hover:bg-accent hover:text-accent-foreground',
-        'outline' => 'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
-        default => 'bg-white text-black shadow hover:bg-white/90'
+        'primary' => 'bg-white text-black',
+        'secondary' => 'border border-2 bg-transparent',
+        'ghost' => '',
+        default => 'bg-white text-black'
     };
     $sizeClasses = match($size) {
-        'sm' => 'h-8 px-3 text-xs',
-        'lg' => 'h-[48px] px-6 w-full text-lg',
-        'icon' => 'h-9 w-9',
-        default => 'h-9 px-4'
+        'sm' => 'h-[24px] px-[6px]',
+        'md' => 'h-[32px] px-[8px]',
+        'lg' => 'h-[48px] px-[12px]',
+        default => 'h-[32px] px-[8px]'
     };
     $iconClasses = match($size) {
-        'sm' => 'h-4 w-4',
-        'lg' => 'h-8 w-8',
-        'icon' => 'h-5 w-5',
-        default => 'h-4 w-4'
+        'sm' => 'w-[12px] h-[12px]',
+        'md' => 'w-[16px] h-[16px]',
+        'lg' => 'w-[24px] h-[24px]',
+        default => 'w-[16px] h-[16px]'
     };
 @endphp
 
 <button {{ $attributes->merge(['class' => "$baseClasses $variantClasses $sizeClasses"]) }}>
-    <span class="flex-1 text-left">
+    <x-label :size="$size">
         {{ $slot }}
+    </x-label>
     </span>
     @if($icon)
-        <span class="ml-2">
+        <span class="ml-3">
             <x-icon :name="$icon" :class="$iconClasses" />
         </span>
     @endif
