1104983666ee642c21b6c112048c6da6a6e3e647
Christopher David <chris@arcadelabs.co>
2024-02-23 09:33:45 -0600

    cleanup inferencer

--- app/Services/Inferencer.php
+++ app/Services/Inferencer.php
@@ -4,8 +4,6 @@ namespace App\Services;
 
 use App\Models\Conversation;
 use App\Models\Message;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use OpenAI;
 
 class Inferencer
@@ -14,8 +12,8 @@ class Inferencer
     {
         $messages = [
             [
-                "role" => "system",
-                "content" => "You are a helpful AI agent named " . $conversation->agent->name . ". Your description is " . $conversation->agent->description
+                'role' => 'system',
+                'content' => 'You are a helpful AI agent named '.$conversation->agent->name.'. Your description is '.$conversation->agent->description,
             ],
         ];
 
@@ -25,38 +23,35 @@ class Inferencer
             ->get()
             ->toArray();
 
-        // Add previous messages to the array
         foreach ($previousMessages as $msg) {
             $messages[] = [
-                "role" => $msg['sender'] === 'user' ? 'user' : 'assistant',
-                "content" => $msg['body']
+                'role' => $msg['sender'] === 'user' ? 'user' : 'assistant',
+                'content' => $msg['body'],
             ];
         }
 
-        // But shave off the most recent message
-        // array_pop($messages);
+        // But shave off the most recent message (since we added it to DB already)
+        array_pop($messages);
 
         // Add the current user input as the last element
-        $messages[] = ["role" => "user", "content" => $input['input']];
+        $messages[] = ['role' => 'user', 'content' => $input['input']];
 
-        // \dd($messages);
-
-        $client = OpenAI::client(env("OPENAI_API_KEY"));
+        $client = OpenAI::client(env('OPENAI_API_KEY'));
         $stream = $client->chat()->createStreamed([
             'model' => 'gpt-4',
             'messages' => $messages,
             'max_tokens' => 3024,
         ]);
 
-        $content = "";
-        foreach($stream as $response) {
-            // $response->choices[0]->toArray();
-            $token = $response['choices'][0]['delta']['content'] ?? "";
+        $content = '';
+        foreach ($stream as $response) {
+            $token = $response['choices'][0]['delta']['content'] ?? '';
             $streamFunction($response);
             $content .= $token;
         }
+
         return [
-            'output' => $content
+            'output' => $content,
         ];
     }
 }
