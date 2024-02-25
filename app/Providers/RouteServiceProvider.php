38315e785bf9cf24fac87373dfc29310c4381c1a
Christopher David <chris@arcadelabs.co>
2024-02-25 17:29:26 -0600

    Test setup and disable markdown cache to debug (#146)
    * api routes
    
    * api route
    
    * initial dummy can create agent via api
    
    * test and disable cache for debugging

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,15 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
         $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->group(base_path('routes/api.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
