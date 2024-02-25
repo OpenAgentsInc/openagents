dc92e10c98cf128cc62e2121ff9ac40d8dd362fa
Christopher David <chris@arcadelabs.co>
2024-02-25 16:56:09 -0600

    initial dummy can create agent via api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
+                ->prefix('api/v1')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/agents', function (Request $request) {
+        return response()->json([
+            'name' => $request->name,
+            'description' => $request->description,
+        ], 201);
+    })->name('api.agents.store');
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
