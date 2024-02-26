e96166847958a0f3faaa2656101b5ec18ec44759
Christopher David <chris@arcadelabs.co>
2024-02-25 22:44:59 -0600

    Initial doc markdown sidebar ok (#148)
    * agent create route
    
    * o dam
    
    * proper
    
    * curl smaller

--- resources/views/apidoc.blade.php
+++ resources/views/apidoc.blade.php
@@ -1,21 +1,29 @@
 <x-apidoc-layout>
     <livewire:navbar />
 
-    <div class="pt-24 px-24">
+    <div class="py-16 docs flex flex-wrap -mx-4 max-w-[95vw]">
+        <div class="w-full lg:w-1/4 px-4">
+            placeholder
+        </div>
 
-        <h3>Curl:</h3>
-        {!! $curl !!}
+        <div class="w-full lg:w-1/2 px-4">
+            <div class="px-24">
+                {!! $content->contents !!}
+            </div>
+        </div>
 
-        <h3 class="mt-8">Responses:</h3>
-        <p>Response 200:</p>
-        {!! $responseSuccessHtml !!}
+        <div class="w-full lg:w-1/4 px-4">
+            <h3>Curl:</h3>
+            <div class="verysmall">
+                {!! $curl !!}
+            </div>
 
-        <p>Response 400:</p>
-        {!! $responseErrorHtml !!}
+            <h3 class="mt-8">Responses:</h3>
+            <p>Response 200:</p>
+            {!! $responseSuccessHtml !!}
 
-    </div>
-
-    <div class="docs w-full max-w-5xl px-12 py-20">
-        {!! $content->contents !!}
+            <p>Response 400:</p>
+            {!! $responseErrorHtml !!}
+        </div>
     </div>
 </x-apidoc-layout>
