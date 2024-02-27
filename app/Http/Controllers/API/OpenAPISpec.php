30079aebb3d1d8ec1090363ea26ede51ee145a8d
Christopher David <chris@arcadelabs.co>
2024-02-27 09:55:10 -0600

    add missing component defs and regen openapi.json

--- app/Http/Controllers/API/OpenAPISpec.php
+++ app/Http/Controllers/API/OpenAPISpec.php
@@ -18,6 +18,48 @@ namespace App\Http\Controllers\API;
  *     url="https://openagents.com/api/v1/",
  *     description="OpenAgents API server"
  * )
+ *
+ * @OA\Components(
+ *
+ *     @OA\Schema(
+ *         schema="Agent",
+ *         type="object",
+ *
+ *         @OA\Property(
+ *             property="id",
+ *             type="integer",
+ *             format="int64",
+ *             description="Agent ID"
+ *         ),
+ *         @OA\Property(
+ *             property="name",
+ *             type="string",
+ *             description="Agent name"
+ *         ),
+ *         @OA\Property(
+ *             property="description",
+ *             type="string",
+ *             description="Agent description"
+ *         ),
+ *         @OA\Property(
+ *             property="instructions",
+ *             type="string",
+ *             description="Agent instructions"
+ *         ),
+ *         @OA\Property(
+ *             property="welcome_message",
+ *             type="string",
+ *             description="Agent welcome message"
+ *         )
+ *     ),
+ *
+ *     @OA\Schema(
+ *         schema="AgentList",
+ *         type="array",
+ *
+ *         @OA\Items(ref="#/components/schemas/Agent")
+ *     )
+ * )
  */
 class OpenApiSpec
 {
