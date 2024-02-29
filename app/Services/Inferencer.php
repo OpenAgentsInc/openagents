31fe633f8238ad5178c25cb24358b4d6f0f0e16e
Christopher David <chris@arcadelabs.co>
2024-02-28 21:08:29 -0600

    ahahhahaha it works

--- app/Services/Inferencer.php
+++ app/Services/Inferencer.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-use App\AI\MistralAIGateway;
+use App\AI\GroqAIGateway;
 use App\Models\Conversation;
 use OpenAI;
 
@@ -34,7 +34,7 @@ class Inferencer
                 'model' => $model,
                 'messages' => $messages,
                 'max_tokens' => 3024,
-                'stream_function' => $streamFunction
+                'stream_function' => $streamFunction,
             ]);
 
             $content = '';
@@ -48,48 +48,21 @@ class Inferencer
             // It's plain text or not properly decoded, proceed as before
             $text = $input['input'];
             // $model = 'gpt-4';
-            $model = 'mistral-large-latest';
+            //            $model = 'mistral-large-latest';
+            $model = 'mixtral-8x7b-32768';
             $messages = self::prepareTextInference($text, $conversation);
-            $client = new MistralAIGateway();
+            //            $client = new MistralAIGateway();
+            $client = new GroqAIGateway();
             $content = $client->chat()->createStreamed([
                 'model' => $model,
                 'messages' => $messages,
                 'max_tokens' => 3024,
-                'stream_function' => $streamFunction
+                'stream_function' => $streamFunction,
             ]);
         }
 
         return ['output' => $content];
     }
-
-    private static function prepareTextInference($text, Conversation $conversation)
-    {
-        // Fetch previous messages
-        $previousMessages = $conversation->messages()
-            ->orderBy('created_at', 'asc')
-            ->get()
-            ->map(function ($message) {
-                // Map 'user' and 'agent' to 'user' and 'assistant' respectively
-                $role = $message->sender === 'agent' ? 'assistant' : 'user';
-                return [
-                    'role' => $role,
-                    'content' => $message->body,
-                ];
-            })
-            ->toArray();
-
-        // Add the current user message to the array of previous messages
-        // $previousMessages[] = ['role' => 'user', 'content' => $text];
-
-        // Prepend system message
-        array_unshift($previousMessages, [
-            'role' => 'system',
-            'content' => 'You are a helpful AI agent named ' . $conversation->agent->name . '. Your description is ' . $conversation->agent->description,
-        ]);
-
-        return $previousMessages;
-    }
-
 
     private static function prepareMultiModalInference($input, Conversation $conversation)
     {
@@ -129,6 +102,35 @@ class Inferencer
         ];
 
         return [$systemMessage, $userMessage];
+    }
+
+    private static function prepareTextInference($text, Conversation $conversation)
+    {
+        // Fetch previous messages
+        $previousMessages = $conversation->messages()
+            ->orderBy('created_at', 'asc')
+            ->get()
+            ->map(function ($message) {
+                // Map 'user' and 'agent' to 'user' and 'assistant' respectively
+                $role = $message->sender === 'agent' ? 'assistant' : 'user';
+
+                return [
+                    'role' => $role,
+                    'content' => $message->body,
+                ];
+            })
+            ->toArray();
+
+        // Add the current user message to the array of previous messages
+        // $previousMessages[] = ['role' => 'user', 'content' => $text];
+
+        // Prepend system message
+        array_unshift($previousMessages, [
+            'role' => 'system',
+            'content' => 'You are a helpful AI agent named '.$conversation->agent->name.'. Your description is '.$conversation->agent->description,
+        ]);
+
+        return $previousMessages;
     }
 
     private static function old_prepareMultiModalInference($input, Conversation $conversation)
