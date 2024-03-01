fd9058e56e7f0fbb8b53d662587c8b806df03218
Christopher David <chris@arcadelabs.co>
2024-03-01 04:24:08 -0500

    thread has many messages

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Thread extends Model
 {
     use HasFactory;
+
+    // has many messages
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
 }
