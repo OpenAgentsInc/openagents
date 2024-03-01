65bcedef35b341f1f59071676f9094d09a9a7386
Christopher David <chris@arcadelabs.co>
2024-02-29 23:16:16 -0700

    conflict fix wut

--- tests/Feature/API/AgentListTest.php
+++ tests/Feature/API/AgentListTest.php
@@ -15,17 +15,6 @@ test('authenticated user can retrieve list of agents', function () {
 
     get('/api/v1/agents')
         ->assertStatus(200)
-<<<<<<< HEAD
-        ->assertJsonCount(5) // Directly assert the count of agents in the root of the JSON response
-        ->assertJsonStructure([
-            '*' => [
-                'id',
-                'name',
-                'description',
-                'instructions',
-                'welcome_message',
-                'user_id',
-=======
         ->assertJsonStructure([
             'success',
             'data' => [
@@ -37,7 +26,6 @@ test('authenticated user can retrieve list of agents', function () {
                     'welcome_message',
                     'user_id',
                 ],
->>>>>>> 76f4603b487876635961671e7ac4af1745e7ad8e
             ],
         ])
         ->assertJsonPath('success', true)
