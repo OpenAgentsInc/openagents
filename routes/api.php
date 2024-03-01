d908e0170c458eeac9cb82b999fca9d16a6c71e3
Christopher David <chris@arcadelabs.co>
2024-03-01 13:27:37 -0500

    ya

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,21 @@
 
 use App\Http\Controllers\API\AgentController;
 use App\Http\Controllers\API\AgentFileController;
+use App\Http\Controllers\API\AgentThreadController;
 use App\Http\Controllers\API\FileController;
+use App\Http\Controllers\API\FileThreadController;
 use App\Http\Controllers\API\FlowController;
+use App\Http\Controllers\API\FlowNodeController;
 use App\Http\Controllers\API\MessageController;
 use App\Http\Controllers\API\NodeController;
 use App\Http\Controllers\API\PluginController;
 use App\Http\Controllers\API\RunController;
 use App\Http\Controllers\API\ThreadController;
+use App\Http\Controllers\API\UserThreadController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    // Core API routes
+    // Core CRUD API routes
     Route::apiResource('agents', AgentController::class);
     Route::apiResource('files', FileController::class);
     Route::apiResource('flows', FlowController::class);
@@ -22,8 +26,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('runs', RunController::class);
     Route::apiResource('threads', ThreadController::class);
 
-    Route::apiResource('agents.files', AgentFileController::class)->shallow();
-
-    // Add file to agent
-    //    Route::post('/agents/{agent}/files', [AgentFileControllerPrev::class, 'store']);
+    // Many-to-many relationships
+    Route::apiResource('agents.files', AgentFileController::class);
+    Route::apiResource('files.threads', FileThreadController::class);
+    Route::apiResource('flows.nodes', FlowNodeController::class);
+    Route::apiResource('agents.threads', AgentThreadController::class);
+    Route::apiResource('users.threads', UserThreadController::class);
 });
