a862ae56c47296caffeea1d959ec1bec24e1ae16
Christopher David <chris@arcadelabs.co>
2024-03-01 12:51:04 -0500

    Add missing routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\API\AgentController;
 use App\Http\Controllers\API\AgentFileController;
 use App\Http\Controllers\API\FileController;
+use App\Http\Controllers\API\FlowController;
+use App\Http\Controllers\API\MessageController;
+use App\Http\Controllers\API\NodeController;
+use App\Http\Controllers\API\PluginController;
+use App\Http\Controllers\API\RunController;
 use App\Http\Controllers\API\ThreadController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,11 +16,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/agents/{agent}/files', [AgentFileController::class, 'store']);
 
     Route::apiResource('files', FileController::class);
+    Route::apiResource('flows', FlowController::class);
+    Route::apiResource('messages', MessageController::class);
+    Route::apiResource('nodes', NodeController::class);
+    Route::apiResource('plugins', PluginController::class);
+    Route::apiResource('runs', RunController::class);
     Route::apiResource('threads', ThreadController::class);
-
-    //    Route::apiResource('flows', FlowController::class);
-    //    Route::apiResource('messages', MessageController::class);
-    //    Route::apiResource('nodes', NodeController::class);
-    //    Route::apiResource('plugins', PluginController::class);
-    //    Route::apiResource('runs', RunController::class);
 });
