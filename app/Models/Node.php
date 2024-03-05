b0fdd11bc7703bac26484dff91dc484bf98a9767
Christopher David <chris@arcadelabs.co>
2024-03-05 22:50:10 +0000

    run belongs to flow etc

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Node extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    // Each node can have multiple ports.
+    public function ports(): HasMany
+    {
+        return $this->hasMany(Port::class);
+    }
 }
