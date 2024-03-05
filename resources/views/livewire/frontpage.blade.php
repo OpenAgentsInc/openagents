02e193db0d16cbd8d181308d51e54382c2f32829
Christopher David <chris@arcadelabs.co>
2024-03-05 13:15:07 +0000

    different homepage

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,3 +1,8 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="h-full flex flex-col justify-center items-center">
+    <h1>Do something epic</h1>
+    <h4 class="mt-4 mb-8 text-gray">What do you want to accomplish?</h4>
+
+    <div class="w-[400px]">
+        <x-input autofocus placeholder="I want an agent for..."></x-input>
+    </div>
 </div>
--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,13 +1,13 @@
 <div
-    class="fixed w-full border-b border-offblack px-5 py-2 flex flex-row items-center justify-between bg-black z-[300]">
+        class="fixed w-full border-b border-offblack px-5 py-2 flex flex-row items-center justify-between bg-black z-[300]">
     <div class="flex flex-row items-center">
         <a href="/" wire:navigate>
-            <x-logomark size="4" />
+            <x-logomark size="4"/>
         </a>
         @auth
             <a href="/chat" wire:navigate class="ml-8 text-gray hover:text-white">Chat</a>
         @endauth
-        <a href="/docs" wire:navigate class="ml-8 text-gray hover:text-white">Docs</a>
+        {{--        <a href="/docs" wire:navigate class="ml-8 text-gray hover:text-white">Docs</a>--}}
     </div>
     <div class="flex flex-row items-center">
         @auth
