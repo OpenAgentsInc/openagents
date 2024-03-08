04851cae87108a55cb2fe94c415362d4f417f904
Christopher David <chris@arcadelabs.co>
2024-03-08 09:23:42 -0500

    show dat img

--- resources/views/livewire/pay-bitcoin.blade.php
+++ resources/views/livewire/pay-bitcoin.blade.php
@@ -5,19 +5,19 @@
         <h1>Add agent credit</h1>
         <h4 class="mt-4 text-gray">Pay with Bitcoin Lightning</h4>
 
-        <form wire:submit="generateInvoice">
-            <div class="mt-16 w-[350px] flex flex-col text-left">
-                <x-label for="amount">Amount (sats)</x-label>
-                <div class="w-full">
-                    <input type="hidden" name="amount" id="slider-input"/>
-                    <x-slider min="10" max="5000" step="10" value="{{ $amount }}"/>
+        @if (!$qr)
+            <form wire:submit="generateInvoice">
+                <div class="mt-16 w-[350px] flex flex-col text-left">
+                    <x-label for="amount">Amount (sats)</x-label>
+                    <div class="w-full">
+                        <input type="hidden" name="amount" id="slider-input"/>
+                        <x-slider min="10" max="5000" step="10" value="{{ $amount }}"/>
+                    </div>
                 </div>
-            </div>
 
-            <x-button size="lg" class="mt-4">Generate invoice</x-button>
-        </form>
-
-        @if (!!$qr)
+                <x-button size="lg" class="mt-4">Generate invoice</x-button>
+            </form>
+        @else
             <img class="mt-12" src="{{ $qr }}" alt="QR code">
         @endif
     </div>
