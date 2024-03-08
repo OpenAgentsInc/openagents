d108fa6bf8eba79ad38d3e6bab443c2bfdefeb9a
Christopher David <chris@arcadelabs.co>
2024-03-08 09:14:40 -0500

    button

--- app/Livewire/PayBitcoin.php
+++ app/Livewire/PayBitcoin.php
@@ -8,17 +8,18 @@ class PayBitcoin extends Component
 {
     public $qr;
 
-    public function mount()
+    public function generateInvoice()
     {
-        // Create invoice by hitting the Alby API - via POST to https://api.getalby.com/invoices
-        //        $response = Http::withHeaders([
-        //            'Authorization' => 'Bearer '.env('ALBY_ACCESS_TOKEN'),
-        //        ])->post('https://api.getalby.com/invoices', [
-        //            'amount' => 100,
-        //            'description' => 'Payment for the product',
-        //        ]);
-        //
-        //        $this->qr = $response->json()['qr_code_png'];
+        dd('we here');
+        //         Create invoice by hitting the Alby API - via POST to https://api.getalby.com/invoices
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer '.env('ALBY_ACCESS_TOKEN'),
+        ])->post('https://api.getalby.com/invoices', [
+            'amount' => 100,
+            'description' => 'Payment for the product',
+        ]);
+
+        $this->qr = $response->json()['qr_code_png'];
     }
 
     public function render()
--- resources/views/livewire/pay-bitcoin.blade.php
+++ resources/views/livewire/pay-bitcoin.blade.php
@@ -5,13 +5,17 @@
         <h1>Add agent credit</h1>
         <h4 class="mt-4 text-gray">Pay with Bitcoin Lightning</h4>
 
-        <div class="mt-16 w-[350px] flex flex-col text-left">
-            <x-label for="fee">Amount (sats)</x-label>
-            <div class="w-full">
-                <input type="hidden" name="fee" id="slider-input" value="0"/>
-                <x-slider min="10" max="5000" step="10" value="100"/>
+        <form wire:submit="generateInvoice">
+            <div class="mt-16 w-[350px] flex flex-col text-left">
+                <x-label for="fee">Amount (sats)</x-label>
+                <div class="w-full">
+                    <input type="hidden" name="fee" id="slider-input" value="0"/>
+                    <x-slider min="10" max="5000" step="10" value="100"/>
+                </div>
             </div>
-        </div>
+
+            <x-button type="submit" size="lg" class="mt-4">Generate invoice</x-button>
+        </form>
 
         @if (!!$qr)
             <img class="mt-12" src="{{ $qr }}" alt="QR code">
