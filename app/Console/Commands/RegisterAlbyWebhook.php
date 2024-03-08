aca5773bdd4f4257acfb571b32501d6b75bc0f3f
Christopher David <chris@arcadelabs.co>
2024-03-07 19:52:39 -0500

    php artisan make:command RegisterAlbyWebhook

---
+++ app/Console/Commands/RegisterAlbyWebhook.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RegisterAlbyWebhook extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:register-alby-webhook';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
