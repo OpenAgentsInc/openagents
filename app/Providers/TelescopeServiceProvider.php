40ece26e9c432a50e55e298b7ff076d3fa63b11a
Christopher David <chris@arcadelabs.co>
2024-03-07 20:09:17 -0500

    1mo

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -19,9 +19,9 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment('local')) {
-                return true;
-            }
+            //            if ($this->app->environment('local')) {
+            return true;
+            //            }
 
             return $entry->isReportableException() ||
                    $entry->isFailedRequest() ||
