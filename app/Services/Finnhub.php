7607af4b45dc3f0cf38929db80f405866cb3262a
Christopher David <chris@arcadelabs.co>
2024-03-10 16:27:53 -0400

    example finnhub call works

--- app/Services/Finnhub.php
+++ app/Services/Finnhub.php
@@ -2,18 +2,22 @@
 
 namespace App\Services;
 
+use Finnhub\Api\DefaultApi;
+use Finnhub\Configuration;
+use GuzzleHttp\Client;
+
 class Finnhub
 {
     public function __construct()
     {
-        $config = Finnhub\Configuration::getDefaultConfiguration()->setApiKey('token', 'YOUR API KEY');
-        $client = new Finnhub\Api\DefaultApi(
-            new GuzzleHttp\Client(),
+        $config = Configuration::getDefaultConfiguration()->setApiKey('token', env('FINNHUB_API_KEY'));
+        $client = new DefaultApi(
+            new Client(),
             $config
         );
 
         // Stock candles
-        print_r($client->stockCandles('AAPL', 'D', 1590988249, 1591852249));
+        print_r($client->symbolSearch('AAPL'));
 
     }
 }
