8fcf5cb001befcf17ce541355a6927234fe273f6
Christopher David <chris@arcadelabs.co>
2024-03-12 11:07:24 -0500

    Axe most prev

--- resources/views/components/dropdown-link.blade.php
+++
@@ -1,2 +0,0 @@
-<a
-    {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-white hover:bg-gray focus:outline-none focus:bg-gray transition duration-150 ease-in-out bg-black']) }}>{{ $slot }}</a>
--- resources/views/components/secondary-button.blade.php
+++
@@ -1,4 +0,0 @@
-<button
-    {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-black border border-gray rounded-md font-semibold text-xs text-gray uppercase tracking-widest shadow-sm hover:bg-gray focus:outline-none focus:ring-2 focus:ring-gray focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
