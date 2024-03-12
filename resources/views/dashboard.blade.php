8fad9336406d496707e48e43afb188b8e72ca5cc
Christopher David <chris@arcadelabs.co>
2024-03-12 18:21:56 -0500

    duotone dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            <div class="bg-black overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome/>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,12 @@
 <body class="font-mono antialiased">
 <x-banner/>
 
-<div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+<div class="min-h-screen bg-black">
     @livewire('navigation-menu')
 
     <!-- Page Heading -->
     @if (isset($header))
-        <header class="bg-white dark:bg-gray-800 shadow">
+        <header class="bg-black shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
