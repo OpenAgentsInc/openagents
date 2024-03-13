4ab671193b418a1479ba822d4c558a32950295f8
Christopher David <chris@arcadelabs.co>
2024-03-13 09:56:22 -0500

    Extend agent to check stock price (#173)
    * Look for Finnhub flow
    
    * handle route via node
    
    * function calling works
    
    * re-add finnhub client
    
    * got data from finnhub
    
    * works end to end

---
+++ app/AI/FunctionCaller.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\AI;
+
+class FunctionCaller
+{
+    public static function parsedTools($tools)
+    {
+        $parsedTools = [];
+        foreach ($tools as $tool) {
+            if ($tool['type'] === 'function') {
+                $parsedFunction = ['type' => $tool['type']];
+                // Assuming your Function class has a method to export its properties as an array
+                $parsedFunction['function'] = $tool['function'];
+                $parsedTools[] = $parsedFunction;
+            }
+        }
+
+        return $parsedTools;
+    }
+
+    public static function prepareFunctions()
+    {
+        return [
+            [
+                'type' => 'function',
+                'function' => [
+                    'name' => 'check_stock_price',
+                    'description' => 'Check the current price of a stock given its ticker symbol',
+                    'parameters' => [
+                        'type' => 'object',
+                        'properties' => [
+                            'ticker_symbol' => [
+                                'type' => 'string',
+                                'description' => 'The stock ticker symbol.',
+                            ],
+                        ],
+                        'required' => ['ticker_symbol'],
+                    ],
+                ],
+            ],
+
+            //            [
+            //                'type' => 'function',
+            //                'function' => [
+            //                    'name' => 'check_news_sentiment',
+            //                    'description' => 'Check the current market news sentiment given its ticker symbol',
+            //                    'parameters' => [
+            //                        'type' => 'object',
+            //                        'properties' => [
+            //                            'ticker_symbol' => [
+            //                                'type' => 'string',
+            //                                'description' => 'The stock ticker symbol.',
+            //                            ],
+            //                        ],
+            //                        'required' => ['ticker_symbol'],
+            //                    ],
+            //                ],
+            //            ],
+            //
+        ];
+    }
+}
