926bc7993ab55afc73111d256cf15c4159ce42cb
Christopher David <chris@arcadelabs.co>
2024-03-13 16:02:38 -0500

    Share button (#177)
    * twitter button NOICE
    
    * test this
    
    * share lol

---
+++ resources/views/partials/ogtags.blade.php
@@ -0,0 +1,11 @@
+<!-- Twitter Card tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@OpenAgentsInc">
+<meta name="twitter:creator" content="@OpenAgentsInc">
+
+<!-- Open Graph tags for fallback -->
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:title" content="Chat with OpenAgents">
+<meta property="og:description"
+      content="It's the coolest AI chat. Literally 1000x better than the rest. Try it now or else.">
+<meta property="og:image" content="https://staging.openagents.com/openagents.png">
