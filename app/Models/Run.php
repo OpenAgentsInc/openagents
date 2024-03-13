289659af1b70a7d8b3afed6c886325fc03fa444e
Christopher David <chris@arcadelabs.co>
2024-03-12 22:43:46 -0500

    disable routing for now

--- app/Models/Run.php
+++ app/Models/Run.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use App\AI\MistralAIGateway;
-use App\Services\SemanticRouter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -48,13 +46,13 @@ class Run extends Model
 
         // First see if we should override node with a semantic route
         // Vectorize the input
-        $gateway = new MistralAIGateway();
-        $vectorizedInput = $gateway->embed($input);
+        //        $gateway = new MistralAIGateway();
+        //        $vectorizedInput = $gateway->embed($input);
+        //
+        //        $router = new SemanticRouter();
+        //        $route = $router->route($vectorizedInput);
 
-        $router = new SemanticRouter();
-        $route = $router->route($vectorizedInput);
-
-        dd($route);
+        //        dd($route);
 
         // Execute each node in sequence (assuming nodes are already sorted by their execution order)
         foreach ($this->flow->nodes as $node) {
