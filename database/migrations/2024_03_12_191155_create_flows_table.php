f7b89c1f90e597110ee9f4d490e5252b1367bba0
Christopher David <chris@arcadelabs.co>
2024-03-13 10:13:11 -0500

    Flow name/fillable

--- database/migrations/2024_03_12_191155_create_flows_table.php
+++ database/migrations/2024_03_12_191155_create_flows_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('flows', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
             $table->timestamps();
         });
     }
