3dcbabb389208e21db3986aad89d074d8539909c
Christopher David <chris@arcadelabs.co>
2024-03-13 15:12:18 -0500

    twitter button NOICE

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -10,6 +10,15 @@
         {{--        <a href="/docs" wire:navigate class="ml-8 text-gray hover:text-white">Docs</a>--}}
     </div>
     <div class="flex flex-row items-center">
+        {{--        <a href="/share" wire:navigate class="ml-8 text-gray hover:text-white">Share</a>--}}
+
+        <a class="twitter-share-button"
+           target="_blank"
+           href="https://twitter.com/intent/tweet?text=Hello%20world"
+           data-size="large">
+         
+        </a>
+
         @auth
         @else
             {{--            <a href="/login" wire:navigate class="ml-8 text-gray hover:text-white">Login</a>--}}
