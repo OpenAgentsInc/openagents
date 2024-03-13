b05599d6c2a31c1ce390d2a5d600832551073a3f
Christopher David <chris@arcadelabs.co>
2024-03-12 19:08:01 -0500

    fix test

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -14,7 +14,7 @@ test('registration screen can be rendered', function () {
 test('registration screen cannot be rendered if support is disabled', function () {
     $response = $this->get('/register');
 
-    $response->assertStatus(404);
+    $response->assertStatus(302);
 })->skip(function () {
     return Features::enabled(Features::registration());
 }, 'Registration support is enabled.');
