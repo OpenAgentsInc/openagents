dff38b3f53807b4fd3b694c784f4dc0040666cde
Christopher David <chris@arcadelabs.co>
2024-03-14 15:43:06 -0500

    php artisan telescope:install

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
