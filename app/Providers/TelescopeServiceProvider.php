a9476c8eb678de96a86f055cd03772efa1da15bf
Christopher David <chris@arcadelabs.co>
2024-03-14 15:54:36 -0500

    show more telescope stuffs for now

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -20,13 +20,14 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 
         $isLocal = $this->app->environment('local');
 
-        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
-            return $isLocal ||
-                   $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
+        Telescope::filter(function (IncomingEntry $entry) {
+            return true;
+            //            return $isLocal ||
+            //                   $entry->isReportableException() ||
+            //                   $entry->isFailedRequest() ||
+            //                   $entry->isFailedJob() ||
+            //                   $entry->isScheduledTask() ||
+            //                   $entry->hasMonitoredTag();
         });
     }
 
