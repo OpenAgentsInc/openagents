ed192b94ce528be416c67b958ae8efd24b106018
Christopher David <chris@arcadelabs.co>
2024-03-14 14:01:38 -0500

    Fix syntax highlighting / start imagegen route (#180)
    * initial imagegen semantic route
    
    * php artisan vendor:publish --provider=SpatieLaravelMarkdownMarkdownServiceProvider --tag=markdown-config
    
    * fix shiki highlighting

---
+++ app/AI/StabilityAIGateway.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\AI;
+
+class StabilityAIGateway
+{
+    public function text_to_image(string $input): string
+    {
+        return "I'm learning to create images, check back soon!";
+    }
+}
