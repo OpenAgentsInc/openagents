0703f3dbc31229daf5e4f159d68377042e9af1f8
Christopher David <chris@arcadelabs.co>
2024-03-20 14:38:14 -0500

    it works - return raw data

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -52,8 +52,19 @@ class Node extends Model
                 $config = json_decode($this->config, true);
                 $plugin_id = $config['plugin_id'];
                 $plugin = Plugin::find($plugin_id);
-                $output = $plugin->call('run', $input);
-                dd($output);
+
+                // Extract the zipcode from the input. Could be many forms, so just look for any amount of digits
+                $matches = [];
+                preg_match('/\d+/', $input, $matches);
+                $zipcode = $matches[0] ?? null;
+
+                $params = json_encode([
+                    'zipcode' => $zipcode,
+                    'apikey' => env('ZIPCODESTACK_API_KEY'),
+                ]);
+
+                $output = "```\n".$plugin->call('run', $params)."\n```";
+
                 break;
 
             default:
