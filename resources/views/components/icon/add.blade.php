778970883500fba76fe94a858c425bc39d121507
moneya <moneya@users.noreply.github.com>
2024-03-20 18:06:47 +0100

    Added new components (#184)
    * added 2 new component bitcoin and iput with icon support
    
    * added new componenents
    
    * removed hasError

---
+++ resources/views/components/icon/add.blade.php
@@ -0,0 +1,5 @@
+
+
+<svg {{ $attributes }} width="100%" height="100%" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M24 4C12.9531 4 4 12.9531 4 24C4 35.0469 12.9531 44 24 44C35.0469 44 44 35.0469 44 24C44 12.9531 35.0469 4 24 4ZM32 26H26V32H22V26H16V22H22V16H26V22H32V26Z" fill="white"/>
+    </svg>
---
+++ resources/views/components/icon/expand-down.blade.php
@@ -0,0 +1,3 @@
+<svg {{ $attributes }} width="100%" height="100%" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M14.8594 19L11.8594 22L24 34.1406L36.1406 22L33.1406 19L24 28.1406L14.8594 19Z" fill="white"/>
+    </svg>
