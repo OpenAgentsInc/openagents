64d0e43f5dce54e82d36c1f2a6fb289b10fbb9ee
Christopher David <chris@arcadelabs.co>
2024-03-23 13:39:22 -0500

    initial pay command

--- app/Console/Commands/SendPrismPayment.php
+++ app/Console/Commands/SendPrismPayment.php
@@ -2,29 +2,32 @@
 
 namespace App\Console\Commands;
 
+use App\Services\PrismService;
 use Illuminate\Console\Command;
 
 class SendPrismPayment extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:send-prism-payment';
+    protected $signature = 'prism:pay';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    protected $description = 'Sends a demo payment to two users via Prism API';
 
-    /**
-     * Execute the console command.
-     */
+    protected $prismService;
+
+    public function __construct(PrismService $prismService)
+    {
+        parent::__construct();
+        $this->prismService = $prismService;
+    }
+
     public function handle()
     {
-        //
+        $recipients = [
+            ['rblb@blink.sv', 50], // Example recipient format
+            ['mcdonald55@bitnob.io', 50],
+        ];
+
+        $result = $this->prismService->sendPayment(100, 'SAT', $recipients);
+
+        $this->info('Payment response: '.print_r($result, true));
     }
 }
