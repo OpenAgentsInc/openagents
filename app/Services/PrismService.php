6b33b62ac375d87b1c0127cb51b87ecd5b7f1e45
Christopher David <chris@arcadelabs.co>
2024-03-23 17:51:11 -0500

    half right

--- app/Services/PrismService.php
+++ app/Services/PrismService.php
@@ -57,8 +57,8 @@ class PrismService
         // Assuming $recipients is an array of lightning addresses
         $response = Http::withToken($this->apiKey)
             ->post("{$this->baseUrl}/payment/prism", [
-                'senderId' => '67c2cc15-d90f-4af5-b16f-06cebd9e8e5d', // atlantispleb
-                //                'senderId' => '1e90c130-8bce-4aa8-abd0-92329d57fafe', // openagents
+                //                'senderId' => '67c2cc15-d90f-4af5-b16f-06cebd9e8e5d', // atlantispleb
+                'senderId' => '68f5d9c3-9260-4fdc-b29f-8e5e8edcb849', // openagents
                 'amount' => $amount,
                 'currency' => $currency,
                 'prism' => $recipients,
