d18e78f8c986a06a370664c96426d88b41f90e7e
Christopher David <chris@arcadelabs.co>
2024-03-24 11:57:53 -0500

    Initial prism payment UI (#193)
    * Initial ascii dashboard look
    
    * css version
    
    * recent txs and before delete
    
    * nice
    
    * nice
    
    * hardcodes
    
    * initial
    
    * ok

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\PrismController;
 use App\Http\Controllers\StaticController;
 use App\Livewire\Chat;
 use App\Livewire\Frontpage;
+use App\Livewire\PrismDashboard;
 use App\Livewire\ReverbDemo;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Frontpage::class);
 Route::get('/chat/{id}', Chat::class);
+Route::get('/prism', PrismDashboard::class);
 Route::get('/pro', [StaticController::class, 'pro']);
 Route::get('/launch', [StaticController::class, 'launch']);
 Route::get('/docs', [StaticController::class, 'docs']);
