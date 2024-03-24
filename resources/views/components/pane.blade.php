21aab9237a60766a95134bf64d8cecc19ab6b0cb
Christopher David <chris@arcadelabs.co>
2024-03-24 11:57:53 -0500

    Initial prism payment UI (#193)
    * Initial ascii dashboard look
    
    * css version
    
    * recent txs and before delete
    
    * nice
    
    * nice
    
    * hardcodes
    
    * initial
    
    * ok

---
+++ resources/views/components/pane.blade.php
@@ -0,0 +1,10 @@
+@props(['title'])
+
+<div class="bg-black text-white font-mono w-full max-w-[1050px] mx-auto">
+    <div class="border-2 border-white relative pt-[18px] px-[16px] pb-[14px] mb-5">
+        <div class="absolute font-bold top-[-13px] left-[6px] bg-black px-2.5">{{ $title }}</div>
+        <div class="pt-[2px]">
+            {{ $slot }}
+        </div>
+    </div>
+</div>
