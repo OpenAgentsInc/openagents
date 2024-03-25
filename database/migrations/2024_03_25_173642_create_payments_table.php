054731f1c57f61547a83ff47630f5ab56868c0b5
Christopher David <chris@arcadelabs.co>
2024-03-25 12:44:08 -0500

    payments table

--- database/migrations/2024_03_25_173642_create_payments_table.php
+++ database/migrations/2024_03_25_173642_create_payments_table.php
@@ -14,6 +14,26 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            // Prism SinglePayment fields
+            $table->uuid('prism_id')->unique(); // Same as "id" from prism API (not the "prism payment" id)
+            $table->unsignedBigInteger('prism_created_at');
+            $table->unsignedBigInteger('prism_updated_at');
+            $table->unsignedBigInteger('expires_at');
+            $table->string('receiver_id');
+            $table->string('sender_id');
+            $table->string('type');
+            $table->bigInteger('amount_msat');
+            $table->string('status');
+            $table->boolean('resolved');
+            $table->unsignedBigInteger('resolved_at')->nullable();
+            $table->string('prism_payment_id')->nullable();
+            $table->text('bolt11')->nullable();
+            $table->text('preimage')->nullable();
+            $table->string('failure_code')->nullable();
+
+            // Indexes for faster searching on frequently queried fields
+            $table->index(['sender_id', 'receiver_id', 'status']);
         });
     }
 
