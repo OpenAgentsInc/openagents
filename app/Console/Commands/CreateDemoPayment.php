f8228b909d7c24e7247a310d38044f7682a147dd
Christopher David <chris@arcadelabs.co>
2024-03-25 13:48:23 -0500

    php artisan make:command CreateDemoPayment

---
+++ app/Console/Commands/CreateDemoPayment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateDemoPayment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-demo-payment';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
