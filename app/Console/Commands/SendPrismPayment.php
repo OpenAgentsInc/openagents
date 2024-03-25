168051b523a5863f06c65852d26858fae5c99cf3
Christopher David <chris@arcadelabs.co>
2024-03-25 13:23:06 -0500

    ensure recipient and sender ids

--- app/Console/Commands/SendPrismPayment.php
+++ app/Console/Commands/SendPrismPayment.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
 use App\Services\PrismService;
 use Illuminate\Console\Command;
 
@@ -21,12 +22,29 @@ class SendPrismPayment extends Command
 
     public function handle()
     {
+        // Ensure the sender exists or create them
+        $sender = User::firstOrCreate(
+            ['prism_user_id' => '68f5d9c3-9260-4fdc-b29f-8e5e8edcb849'],
+            ['name' => 'OpenAgents', 'email' => 'chris+oa@openagents.com'] // Adjust default attributes as needed
+        );
+
         $recipients = [
-            ['3cf9098c-283f-4843-8ee8-ada9a907a75f', 50], // rblb@blink.sv
-            ['9a96fe41-30a6-4b84-96a2-7184f107be96', 50], // mcdonald55@bitnob.io
+            ['rblb@blink.sv', '3cf9098c-283f-4843-8ee8-ada9a907a75f', 50],
+            ['mcdonald55@bitnob.io', '9a96fe41-30a6-4b84-96a2-7184f107be96', 50],
         ];
 
-        $result = $this->prismService->sendPayment(100, 'SAT', $recipients);
+        // Ensure each recipient exists
+        foreach ($recipients as $recipient) {
+            User::firstOrCreate(
+                ['prism_user_id' => $recipient[1]],
+                ['name' => $recipient[0], 'email' => $recipient[0], 'ln_address' => $recipient[0]],  // Adjust default attributes as needed
+            );
+        }
+
+        // Prism recipient object is $recipients minus the first element
+        $prism_recipients = array_map(fn ($recipient) => array_slice($recipient, 1), $recipients);
+
+        $result = $this->prismService->sendPayment(100, 'SAT', $prism_recipients);
 
         $this->info('Payment response: '.print_r($result, true));
     }
