a0a3db6c210ff156b3d0813f569deaa8ca340a7c
Christopher David <chris@arcadelabs.co>
2024-03-25 12:51:53 -0500

    prism user migrations

--- database/migrations/2024_03_25_174813_add_prism_fields_to_users_table.php
+++ database/migrations/2024_03_25_174813_add_prism_fields_to_users_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->uuid('prism_user_id')->nullable()->after('id');
+            $table->unsignedBigInteger('prism_created_at')->nullable()->after('profile_photo_path');
+            $table->unsignedBigInteger('prism_updated_at')->nullable()->after('prism_created_at');
+            $table->string('ln_address')->nullable()->after('prism_updated_at');
+            $table->string('nwc_id')->nullable()->after('ln_address');
+            $table->string('nwc_connector_type')->nullable()->after('nwc_id');
         });
     }
 
@@ -22,7 +27,12 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('prism_user_id');
+            $table->dropColumn('prism_created_at');
+            $table->dropColumn('prism_updated_at');
+            $table->dropColumn('ln_address');
+            $table->dropColumn('nwc_id');
+            $table->dropColumn('nwc_connector_type');
         });
     }
 };
