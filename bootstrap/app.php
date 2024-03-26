5c705434cb1960588b8fe88369bd1031b5df5ad2
Christopher David <chris@arcadelabs.co>
2024-03-26 16:04:15 -0500

    Install Cashier (#200)
    * composer require laravel/cashier
    
    * php artisan vendor:publish --tag="cashier-migrations"
    
    * php artisan vendor:publish --tag=cashier-config
    
    * stripe webhook exclude csrf
    
    * billing redirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'stripe/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
