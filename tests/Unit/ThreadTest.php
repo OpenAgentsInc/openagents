f68cfaa52fc354e6585c8c5e32ab028197a886ec
Christopher David <chris@arcadelabs.co>
2024-03-26 09:54:02 -0500

    threaduser

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Agent;
 use App\Models\Message;
 use App\Models\Thread;
+use App\Models\User;
 
 it('can be created', function () {
     $thread = Thread::factory()->create();
@@ -12,4 +14,20 @@ it('has many messages', function () {
     $thread = Thread::factory()->create();
     Message::factory(2)->create(['thread_id' => $thread->id]);
     expect($thread->messages)->toHaveCount(2);
+});
+
+it('belongs to many agents', function () {
+    $thread = Thread::factory()->create();
+    $agent = Agent::factory()->create();
+    $thread->agents()->attach($agent);
+
+    expect($thread->agents)->toHaveCount(1);
+});
+
+it('belongs to many users', function () {
+    $thread = Thread::factory()->create();
+    $user = User::factory()->create();
+    $thread->users()->attach($user);
+
+    expect($thread->users)->toHaveCount(1);
 });
