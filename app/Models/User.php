bfc5fdf30ec293c1cd18338afb8035313271a368
Christopher David <chris@arcadelabs.co>
2024-03-26 16:04:15 -0500

    Install Cashier (#200)
    * composer require laravel/cashier
    
    * php artisan vendor:publish --tag="cashier-migrations"
    
    * php artisan vendor:publish --tag=cashier-config
    
    * stripe webhook exclude csrf
    
    * billing redirect

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use Billable;
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
