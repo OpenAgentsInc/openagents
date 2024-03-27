b41cde07dfae1125a269449551ff8a4c6afb2c00
Christopher David <chris@arcadelabs.co>
2024-03-27 17:31:38 -0500

    start modals hidden to avoid loading flash

--- resources/views/livewire/layouts/sidebar/navbar.blade.php
+++ resources/views/livewire/layouts/sidebar/navbar.blade.php
@@ -102,7 +102,7 @@
                         <x-secondary-button x-data @click="$dispatch('open-login-modal')" id="hs-dropdown-with-header"
                                             type="button"
                                             class="py-3 text-[20px] inline-flex justify-center items-center gap-x-2 font-semibold rounded-full disabled:opacity-50 disabled:pointer-events-none">
-                            Log In
+                            Log in
                         </x-secondary-button>
 
 
