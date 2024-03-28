586612c8cb18db4e63b83481766c97ada2704498
Christopher David <chris@arcadelabs.co>
2024-03-28 13:28:13 -0500

    Start happy path dusk test (#207)
    * hey
    
    * happypath a bit

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -3,14 +3,15 @@
 
     <div class="h-full flex flex-col justify-center items-center">
         <div class="fixed w-full px-8 sm:w-[584px]">
-            <h2 class="-mt-8 mb-12 text-center">What can we do for you?</h2>
+            <h2 class="-mt-8 mb-12 text-center">How can we help you today?</h2>
 
             <form wire:submit.prevent="sendFirstMessage">
-                <x-chat.input autofocus placeholder="Message OpenAgents..." :showIcon="true" iconName="send"
+                <x-chat.input dusk="first-message-input" autofocus placeholder="Message OpenAgents..." :showIcon="true"
+                              iconName="send"
                               wire:model="first_message"
                               onkeydown="if(event.keyCode == 13 && !event.shiftKey) { event.preventDefault(); document.getElementById('send-message').click(); }"
                 />
-                <button class="hidden" id="send-message" type="submit"></button>
+                <button dusk="send-message" class="hidden" id="send-message" type="submit"></button>
 
             </form>
             <p class="text-center text-gray mt-12 text-sm">
--- tests/Browser/HappyPathTest.php
+++ tests/Browser/HappyPathTest.php
@@ -4,7 +4,16 @@ use Laravel\Dusk\Browser;
 
 test('happy path', function () {
     $this->browse(function (Browser $browser) {
+
+        // Homepage loads and allows chat
         $browser->visit('/')
-            ->assertSee('OpenAgents');
+            ->assertSee('OpenAgents')
+            ->assertSee('How can we help you today?')
+            ->typeSlowly('@first-message-input', 'Who are you?', 50)
+            ->script("document.getElementById('send-message').click();");
+
+        // We've moved to a chat page
+        $browser->waitForText('OpenAgents')
+            ->pause(100);
     });
 });
