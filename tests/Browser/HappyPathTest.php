327b4d79199b8f148a0f94a34311892194d67a80
Christopher David <chris@arcadelabs.co>
2024-03-28 13:21:38 -0500

    hey

--- tests/Browser/HappyPathTest.php
+++ tests/Browser/HappyPathTest.php
@@ -5,6 +5,12 @@ use Laravel\Dusk\Browser;
 test('happy path', function () {
     $this->browse(function (Browser $browser) {
         $browser->visit('/')
-            ->assertSee('OpenAgents');
+            ->assertSee('OpenAgents')
+            ->assertSee('How can we help you today?')
+            ->typeSlowly('@first-message-input', 'Your message here', 50)
+            ->script("document.getElementById('send-message').click();");
+
+        $browser->waitForText('OpenAgents')
+            ->pause(5000);
     });
 });
