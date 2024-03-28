df94837c8b581c11a9b29eba5bdedc9a0b762827
Moneya <moneya59@gmail.com>
2024-03-28 11:03:29 +0100

    added login flow

---
+++ app/Livewire/Auth/Login.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Livewire\Component;
+
+class Login extends Component
+{
+
+    public bool $show = false;
+
+    // Toggle the value of $show
+    public function showLogin(){
+
+        $this->show = !$this->show;
+    }
+
+    public function render()
+    {
+        return view('livewire.auth.login');
+    }
+}
