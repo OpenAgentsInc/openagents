bb4c0e68289f4ef5d5a3e177fd83e9ff5945bbb2
Moneya <moneya59@gmail.com>
2024-03-28 17:05:06 +0100

    complete forgot password flow

--- resources/views/livewire/auth/forget-password.blade.php
+++ resources/views/livewire/auth/forget-password.blade.php
@@ -1,61 +1,73 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    @livewire('navbar')
-    <div class="flex items-center justify-end w-full h-screen">
-        <div class="border border-[#3C3E42] bg-black rounded-lg p-6  max-w-lg h-auto mx-auto  shadow-lg transform transition-all duration-300">
-            <!-- Modal Header -->
+    <!-- Modal Header -->
 
-            <div class="">
-                <h2 class="block text-xl text-center font-bold text-gray-800 dark:text-gray-200">Forgot Password</h2>
-            </div>
-
-            <div class="p-4 sm:p-7">
-
-                <div class="mb-4 flex justify-center">
-                    <span class="mb-4 inline-flex justify-center items-center  rounded-full">
-                        <x-icon.padlock class="w-[100px] h-[100px]">
-                        </x-icon.padlock>
-                    </span>
-                </div>
+    <div class="flex justify-end items-center  pb-4">
+        <button @click="open = false" class="text-gray-500 hover:text-gray-700 focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+        </button>
+    </div>
 
 
+    <div class="">
+        <h2 class="block text-md md:text-xl lg:text-2xl text-center font-bold text-white">{{$this->show ? 'Reset password' : 'Forgot Password'}}</h2>
+    </div>
 
-                    <div class="mb-4">
-                        <x-input id="email" class="block mt-1 w-full"  type="email" name="email" :value="old('email')"
-                                      required autofocus autocomplete="username" placeholder="email"/>
-                    </div>
+    @if(!$this->show)
+    <div class="p-4 sm:p-7">
 
 
 
-                <div class="mt-5">
-
-                    <x-secondary-button class="w-full flex justify-center gap-2 hover:bg-gray">
-                        <x-icon.agent-white class="h-5 w-5 text-white"></x-icon.agent-white>
-                       Reset account Password
-                    </x-secondary-button>
 
 
-
-                </div>
-
-                <div class="text-center">
-                    <p class="mt-4 text-sm text-gray">
-                        You will receive a password reset link if you have an
-                        <a href="#" class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" >
-                           account
-                        </a>
-                            with us.
+        <div class="mb-4">
+            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="email" />
+        </div>
 
 
 
-                    </p>
-                </div>
-            </div>
+        <div class="my-5">
 
-
+            <x-button class="w-full flex justify-center gap-2 " wire:click='sendResetLink()'>
+                Reset account
+            </x-button>
 
 
 
         </div>
+
+        <div class="text-center">
+            <p class="mt-4 text-sm text-gray">
+                You will receive a password reset link if you have an
+                <a href="#" class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                    account
+                </a>
+                with us.
+
+
+
+            </p>
+        </div>
     </div>
+    @elseif ($this->show)
+    <div class="p-4 sm:p-7">
+
+
+
+        <div class="text-center">
+            <p class="mt-2 text-sm md:text-md text-gray">
+                We sent a reset link to satoshi@nakamoto.com.
+            </p>
+            <p>
+                <a class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+                   Resend Email
+                </a>
+            </p>
+        </div>
+    </div>
+
+    @endif
+
 </div>
--- resources/views/partials/modals.blade.php
+++ resources/views/partials/modals.blade.php
@@ -44,75 +44,18 @@
  <!-- Forgot Password modal -->
  <div x-data="{ open: false }" @open-resetpassword-modal.window="open = true" @close-resetpassword-modal.window="open = false">
      <!-- Trigger button -->
+
      <!-- Modal -->
-     <div x-show="open" @click.away="$dispatch('close-resetpassword-modal')" class="fixed inset-0 flex items-center justify-center  bg-black/90 w-full h-full z-[500]">
+
+     <div x-show="open" class="fixed inset-0 flex items-center justify-center  bg-black/90 w-full h-full z-[50]">
 
 
-         <!-- Overlay -->
-         <div class="fixed bg-black/20 rounded-lg p-6  w-full max-w-full shadow-lg transform transition-all duration-300 z-[5]" x-show.transition.opacity="open"></div>
-
-         <div class="fixed border border-[#3C3E42] bg-black rounded-lg p-6  max-w-md   shadow-lg transform transition-all duration-300 z-[5000]" x-show.transition.opacity="open">
-             <!-- Modal Header -->
-
-             <div class="flex justify-end items-center  pb-4">
-                 {{-- <h2 class="text-2xl font-semibold">Get Started</h2> --}}
-                 <button @click="open = false" class="text-gray-500 hover:text-gray-700 focus:outline-none">
-                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
-                         <line x1="18" y1="6" x2="6" y2="18"></line>
-                         <line x1="6" y1="6" x2="18" y2="18"></line>
-                     </svg>
-                 </button>
-             </div>
-
-             <div class="">
-                 <h2 class="block text-xl text-center font-bold text-gray-800 dark:text-gray-200">Reset Password</h2>
-             </div>
+        <!-- Overlay -->
+        <div class="fixed bg-black/20 rounded-lg p-6  w-full max-w-full shadow-lg transform transition-all duration-300 z-[5]" x-show.transition.opacity="open"></div>
+        <div @click.away="$dispatch('close-resetpassword-modal')" class="fixed border  border-[#3C3E42] bg-black rounded-lg p-6  max-w-md   shadow-lg transform transition-all duration-300 z-[5000]" x-show.transition.opacity="open">
+            @livewire('auth.forget-password')
+        </div>
 
 
-
-             <div class="p-4 sm:p-7">
-
-                 <div class=" flex justify-center">
-                     <span class="mb-4 inline-flex justify-center items-center  rounded-full">
-                         <x-icon.padlock class="w-[100px] h-[100px]">
-                         </x-icon.padlock>
-                     </span>
-                 </div>
-
-                 <div class="mb-4">
-                     <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="email" />
-                 </div>
-
-
-                 <div class="mt-5">
-                     {{-- <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-
-                        </a> --}}
-                     <x-button class="w-full flex justify-center gap-2 hover:bg-gray">
-                         <x-icon.agent class="h-5 w-5 text-black"></x-icon.agent>
-                         Reset Account
-                     </x-button>
-
-
-
-                 </div>
-
-                 <div class="text-center">
-                     <p class="mt-2 text-sm text-gray">
-                         Do you have an account?
-                         <a @click="$dispatch('close-resetpassword-modal')" class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-                             Login
-                         </a>
-
-
-                     </p>
-                 </div>
-             </div>
-
-
-
-
-
-         </div>
-     </div>
+    </div>
  </div>
