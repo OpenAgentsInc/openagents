2a776399afb6796dc7ec08b5fc6c04c3e3f1079a
Christopher David <chris@arcadelabs.co>
2024-03-28 22:12:56 -0500

    Gemini writes test & passing code for multimodal inference (#210)
    * gemini api overview
    
    * hmm
    
    * part 1 - suggest addition and gimme code
    
    * attempt 2
    
    * 3
    
    * bump php thing down 1 line
    
    * HARM_CATEGORY_SEXUALLY_EXPLICIT HIGH - lol
    
    * works there - and php markdown
    
    * multimodal

--- tests/Feature/AnalysisTest.php
+++ tests/Feature/AnalysisTest.php
@@ -22,15 +22,38 @@ test('can generate prompt from filepaths', function () {
 })->skip($skipThese);
 
 test('can pass to gemini for analysis', function () {
-    $filepaths = CodeAnalyzer::getAllCodebaseFilePaths(base_path()); // first just markdown
+    //    $filepaths = CodeAnalyzer::getAllCodebaseFilePaths(base_path()); // first just markdown
+    $filepaths = [
+        'app/AI/GeminiAIGateway.php',
+        'resources/markdown/docs.md',
+        'resources/markdown/gemini.md',
+        'resources/markdown/launch.md',
+        'tests/Feature/AnalysisTest.php',
+        'tests/Feature/GeminiTest.php',
+        'tests/Feature/GitHubTest.php',
+        'resources/markdown/20240328-gemini.md',
+        'resources/markdown/20240329-024442-gemini.md',
+        'resources/markdown/20240329-024920-gemini.md',
+    ];
 
     $prompt = CodeAnalyzer::generatePrompt($filepaths);
     $gemini = new GeminiAIGateway();
-    $text = "Analyze the following documents. Summarize the project in one paragraph, the API in one paragraph, and recommendations for future directions in a third paragraph. \n".$prompt;
+    $text = "Continue the conversation based on 20240329-024442-gemini.md. You gave a partial code snippet. I need you to write the entire contents of the file so I can copy-paste it in entirely. Ensure that the tests in GeminiTest will continue to pass. \n".$prompt;
     //    $text = 'Analyze the following code. Write names of feature and unit tests we should write to cover all mentioned functionality. \n '.$prompt;
     $response = $gemini->inference($text, 'new');
-    dump($response['candidates'][0]['content']['parts'][0]['text']);
+
+    $response = $response['candidates'][0]['content']['parts'][0]['text'];
+
+    dump($response);
+
+    // Write $text to a file with the current timestamp like "20240328-123456-gemini.md" in the resources/markdown folder
+    $filename = 'resources/markdown/'.date('Ymd-His').'-gemini.md';
+
+    // Prepend the prompt to the text
+    $texttowrite = "# Prompt \n".$text."\n\n".$response;
+
+    file_put_contents($filename, $texttowrite);
 
     expect($response)->toBeArray();
     expect($response)->toHaveKey('candidates');
-})->skip($skipThese);
+})->skip();
