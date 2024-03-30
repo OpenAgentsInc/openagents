98e0d0013abc230810b1b7cc4fa9637fa20f5c54
Christopher David <chris@arcadelabs.co>
2024-03-30 16:43:16 -0500

    avatar and share button in navbar when authed

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,4 +1,14 @@
 <div class="fixed w-full px-5 py-3 flex flex-row items-center justify-between z-[300]">
     <livewire:model-selector/>
-    <x-login-button/>
+    @auth
+        <div class="flex flex-row items-center">
+            <x-icon.share class="cursor-pointer w-[24px] h-[24px] mr-[56px]"/>
+            <div class="cursor-pointer bg-darkgray w-[32px] h-[32px] rounded-full text-[#d7d8e5] flex items-center justify-center">
+                C
+            </div>
+        </div>
+
+    @else
+        <x-login-button/>
+    @endauth
 </div>
