241f1bce4a635b50cc3685603b4d01d1edbe4b3b
Christopher David <chris@arcadelabs.co>
2024-03-30 09:57:12 -0500

    simplify navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,13 +1,4 @@
-<div
-        class="fixed w-full px-5 py-2 flex flex-row items-center justify-between bg-black z-[300]">
-    <div class="flex flex-row items-center">
-        <livewire:model-selector/>
-    </div>
-    <div class="flex flex-row items-center">
-        <x-secondary-button x-data @click="$dispatch('open-login-modal')" id="hs-dropdown-with-header"
-                            type="button"
-                            class="py-2 text-[20px] inline-flex justify-center items-center gap-x-2 font-semibold rounded-full disabled:opacity-50 disabled:pointer-events-none">
-            Log in
-        </x-secondary-button>
-    </div>
+<div class="fixed w-full px-5 py-3 flex flex-row items-center justify-between z-[300]">
+    <livewire:model-selector/>
+    <x-login-button/>
 </div>
