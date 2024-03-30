6c7d4b85c3b6a678992120e1b0c22ad442db6dfb
Christopher David <chris@arcadelabs.co>
2024-03-30 11:05:08 -0500

    improve frontpage layout

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,24 +1,24 @@
-<div class="h-full">
+<div class="h-full flex flex-col">
     <livewire:navbar/>
 
-    <div class="h-full flex flex-col justify-center items-center">
-        <div class="fixed w-full px-8 sm:w-[584px]">
-            <h2 class="-mt-8 mb-12 text-center">How can we help you today?</h2>
-
-            <form wire:submit.prevent="sendFirstMessage">
-                <x-chat.input dusk="first-message-input" autofocus placeholder="Message OpenAgents..." :showIcon="true"
-                              iconName="send"
-                              wire:model="first_message"
-                              onkeydown="if(event.keyCode == 13 && !event.shiftKey) { event.preventDefault(); document.getElementById('send-message').click(); }"
-                />
-                <button dusk="send-message" class="hidden" id="send-message" type="submit"></button>
-
-            </form>
-            <p class="text-center text-gray mt-12 text-sm">
-                All conversations are public (read & write).<br/>Don't share sensitive info.
-            </p>
-
-        </div>
+    <!-- Container for centering the content vertically and horizontally -->
+    <div class="flex-grow flex flex-col justify-center items-center px-8 sm:w-[584px] lg:w-[768px] mx-auto">
+        <x-logomark :size="2"/>
+        <h3 class="mt-[16px] mb-12">How can we help you today?</h3>
     </div>
 
+    <!-- Input bar at the bottom -->
+    <div class="w-full px-8 sm:w-[584px] lg:w-[768px] mx-auto">
+        <form wire:submit.prevent="sendFirstMessage" class="mb-6">
+            <x-chat.input dusk="first-message-input" autofocus placeholder="Message OpenAgents..." :showIcon="true"
+                          iconName="send"
+                          wire:model="first_message"
+                          onkeydown="if(event.keyCode == 13 && !event.shiftKey) { event.preventDefault(); document.getElementById('send-message').click(); }"
+            />
+            <button dusk="send-message" class="hidden" id="send-message" type="submit"></button>
+        </form>
+        <p class="text-center text-gray text-sm">
+            Chat agents make mistakes. Don't share sensitive info.
+        </p>
+    </div>
 </div>
