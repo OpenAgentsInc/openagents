f83e4fe7825731c99bc2abcf5e4869bb0c94720e
Christopher David <chris@arcadelabs.co>
2024-03-30 09:45:46 -0500

    stripe billing redir if not customer

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -9,6 +9,12 @@ class BillingController extends Controller
 {
     public function stripe_billing_portal(Request $request)
     {
+        // User is not a Stripe customer yet. See the createAsStripeCustomer method.
+        $user = $request->user();
+        if ($user->stripe_id === null) {
+            return redirect('/');
+        }
+
         return request()->user()?->redirectToBillingPortal() ?? redirect('/');
     }
 }
