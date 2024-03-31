601852f671d46125ed45a41f5608e2a13e544bcc
Christopher David <chris@arcadelabs.co>
2024-03-31 14:06:08 -0500

    update text colors

--- resources/views/components/chat/messagestreaming.blade.php
+++ resources/views/components/chat/messagestreaming.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full text-lightgray">
+<div class="w-full text-text">
     <div class="px-1 py-2 justify-center text-base md:gap-6 m-auto">
         <div
                 class="flex flex-1 text-base mx-auto gap-3 md:px-5 lg:px-1 xl:px-5 md:max-w-3xl lg:max-w-[800px]">
@@ -11,14 +11,14 @@
                 <span class="mb-1 font-semibold select-none text-white">{{ $author }}</span>
                 <div class="flex-col gap-1 md:gap-3">
                     <div class="flex flex-grow flex-col max-w-[936px]">
-                        <div wire:stream="taskProgress" class="text-sm text-gray"></div>
+                        <div wire:stream="taskProgress" class="text-sm text-text"></div>
                         <x-markdown wire:stream="streamtext" class="text-md">
 
                         </x-markdown>
                     </div>
                     <div class="flex justify-start gap-3 empty:hidden">
                         <div
-                                class="text-gray flex self-end lg:self-center justify-center lg:justify-start mt-0 -ml-1 h-7 visible">
+                                class="text-text flex self-end lg:self-center justify-center lg:justify-start mt-0 -ml-1 h-7 visible">
                         </div>
                     </div>
                 </div>
