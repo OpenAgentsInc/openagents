1ac167aad9b8427f4588b999b74554283dac373a
Christopher David <chris@arcadelabs.co>
2024-03-31 13:52:48 -0500

    axe pencil

--- resources/views/livewire/model-selector.blade.php
+++ resources/views/livewire/model-selector.blade.php
@@ -1,15 +1,15 @@
-<ol class="ms-3 flex items-center whitespace-nowrap" aria-label="Breadcrumb">
+<ol class="mt-2 ms-3 flex items-center whitespace-nowrap" aria-label="Breadcrumb">
     @auth
 
     @else
-        <li class="flex items-center text-sm text-gray">
-            <a href="/" wire:navigate class="h-[48px] w-[48px] flex justify-center items-center">
-                <div class="border rounded w-[32px] h-[32px] flex justify-center items-center">
-                    <x-icon.logo class="w-[20px] h-[20px]"></x-icon.logo>
-                </div>
-            </a>
-            <h3 class="ml-2 mr-4">/</h3>
-        </li>
+        {{--        <li class="flex items-center text-sm text-gray">--}}
+        {{--            <a href="/" wire:navigate class="h-[48px] w-[48px] flex justify-center items-center">--}}
+        {{--                <div class="border rounded w-[32px] h-[32px] flex justify-center items-center">--}}
+        {{--                    <x-icon.logo class="w-[20px] h-[20px]"></x-icon.logo>--}}
+        {{--                </div>--}}
+        {{--            </a>--}}
+        {{--            <h3 class="ml-2 mr-4">/</h3>--}}
+        {{--        </li>--}}
     @endauth
     <li class="text-sm font-semibold text-gray-800 truncate select-none" x-data="{ dropdown: false }"
         aria-current="page">
