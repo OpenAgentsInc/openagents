b9476e7318944d090ac02c6034a4325f3a5e3ffb
Christopher David <chris@arcadelabs.co>
2024-03-31 14:21:04 -0500

    and autofocus

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -4,24 +4,29 @@
             <div class="relative"
                  x-data="{ pending: @entangle('pending').live }"
                  x-init="
-                            let chatbox = $refs.chatboxContainer;
-                            setTimeout(() => {
-                                chatbox.scrollTo({ top: chatbox.scrollHeight, behavior: 'smooth' });
-                            }, 250);
-                            let lastScrollHeight = chatbox.scrollHeight;
+                    let chatbox = $refs.chatboxContainer;
+                    setTimeout(() => {
+                        chatbox.scrollTo({ top: chatbox.scrollHeight, behavior: 'smooth' });
+                    }, 250);
+                    let lastScrollHeight = chatbox.scrollHeight;
 
-                            $watch('pending', value => {
-                                if (value) {
-                                    let scrollInterval = setInterval(() => {
-                                        if (chatbox.scrollHeight != lastScrollHeight) {
-                                            chatbox.scrollTo({ top: chatbox.scrollHeight, behavior: 'smooth' });
-                                            lastScrollHeight = chatbox.scrollHeight;
-                                        }
-
-                                        if (!pending) clearInterval(scrollInterval);
-                                    }, 1);
+                    $watch('pending', value => {
+                        if (value) {
+                            let scrollInterval = setInterval(() => {
+                                if (chatbox.scrollHeight != lastScrollHeight) {
+                                    chatbox.scrollTo({ top: chatbox.scrollHeight, behavior: 'smooth' });
+                                    lastScrollHeight = chatbox.scrollHeight;
                                 }
-                    });">
+
+                                if (!pending) clearInterval(scrollInterval);
+                            }, 1);
+                        }
+                    });
+
+                    $nextTick(() => {
+                        document.getElementById('message-input').focus(); // Focus the textarea
+                    });
+                ">
                 <div class="flex flex-col text-sm pb-9" style="">
                     <div class="sticky top-0 mb-1.5 flex items-center justify-between z-10 h-14 p-2 font-semibold bg-black">
                         <div class="absolute left-1/2 -translate-x-1/2"></div>
@@ -46,6 +51,7 @@
         <div class="sm:w-[584px] lg:w-[768px] mx-auto">
             <form wire:submit.prevent="sendMessage">
                 <x-chat.input dusk="message-input" autofocus placeholder="Message OpenAgents..." :showIcon="true"
+                              id="message-input"
                               iconName="send"
                               wire:model="message_input"
                               onkeydown="if(event.keyCode == 13 && !event.shiftKey) { event.preventDefault(); document.getElementById('send-message').click(); }"
