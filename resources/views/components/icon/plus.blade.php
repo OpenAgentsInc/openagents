3cb5ed8e58dfdf7c9fa34d73f5c9124132147f6b
moneya <moneya@users.noreply.github.com>
2024-04-01 17:50:39 +0100

    Fixed sidebar UI (#217)
    * added sidebar ui
    
    * made some ui changes
    
    * ok
    
    * less top margin
    
    ---------
    
    Co-authored-by: Christopher David <chris@arcadelabs.co>

---
+++ resources/views/components/icon/plus.blade.php
@@ -0,0 +1,3 @@
+<svg {{ $attributes }} width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 2C6.47656 2 2 6.47656 2 12C2 17.5234 6.47656 22 12 22C17.5234 22 22 17.5234 22 12C22 6.47656 17.5234 2 12 2ZM16 13H13V16H11V13H8V11H11V8H13V11H16V13Z" fill="white"/>
+    </svg>
