a04929001eb6560f67f14f0b4d73256a308a368e
Moneya <moneya59@gmail.com>
2024-04-01 18:29:57 +0100

    fixed UI changes

--- resources/views/livewire/sidebar-content.blade.php
+++ resources/views/livewire/sidebar-content.blade.php
@@ -3,7 +3,7 @@
 
     <div class="w-full">
         <div class="flex gap-2 items-center  overflow-hidden" x-bind:class="{
-            'justify-between': !collapsed,
+            'justify-between': sidebarOpen,
             'justify-center': collapsed
            }">
             <button class="z-50 absolute top-0 left-0 cursor-pointer h-[28px] w-[28px] m-4 mt-[18px] mr-12" @click="sidebarOpen = !sidebarOpen">
@@ -55,13 +55,12 @@
         'rounded-full p-2 mx- w-10 h-10': sidebarOpen
      }">
 
-            <li class="mt-4">
+     <li class="mt-4">
         <span class="text-left leading-6 font-sm text-sm text-[#777A82] px-2" x-cloak x-show="sidebarOpen">
             Today
         </span>
-            </li>
-            <li x-cloak x-show="sidebarOpen"
-                class="text-white bg-white/15 group flex gap-x-2 rounded-md p-2 text-sm leading-6 font-semibold">
+    </li>
+            <li x-cloak x-show="sidebarOpen" class="text-white bg-white/15 group flex gap-x-2 rounded-md p-2 text-sm leading-6 font-semibold">
                 <a href="#" class="flex w-full">
                     Why we did it
                 </a>
@@ -97,8 +96,7 @@
                 </span>
             </li>
 
-            <li x-cloak x-show="sidebarOpen"
-                class="text-white hover:text-white bg-black  hover:bg-white/15 group flex gap-x-2 rounded-md p-2 text-sm leading-6 font-semibold">
+            <li x-cloak x-show="sidebarOpen" class="text-white hover:text-white bg-black  hover:bg-white/15 group flex gap-x-2 rounded-md p-2 text-sm leading-6 font-semibold">
                 <a href="#" class="flex w-full">
                     Tim Cook is CEO
                 </a>
@@ -128,8 +126,7 @@
                 </div>
             </li>
 
-            <li x-cloak x-show="sidebarOpen"
-                class="text-white hover:text-white bg-black  hover:bg-white/15 group flex gap-x-2 rounded-md p-2 text-sm leading-6 font-semibold">
+            <li x-cloak x-show="sidebarOpen" class="text-white hover:text-white bg-black  hover:bg-white/15 group flex gap-x-2 rounded-md p-2 text-sm leading-6 font-semibold">
                 <a href="#" class="flex w-full">
                     Why we did it
                 </a>
