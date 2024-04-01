20b0a8dd9f13caa3cfc7dc0214d85e40a0d297b9
Moneya <moneya59@gmail.com>
2024-04-01 15:11:49 +0100

    added sidebar ui

--- resources/views/components/layouts/new.blade.php
+++ resources/views/components/layouts/new.blade.php
@@ -14,18 +14,17 @@
 <body class="h-screen bg-black antialiased" x-cloak x-data="{ sidebarOpen: false, collapsed: false }">
 
 <div class="relative z-0 flex h-full w-full overflow-hidden min-h-screen">
-    <button class="z-50 absolute top-0 left-0 cursor-pointer h-[28px] w-[28px] m-4 mt-[18px] mr-12"
-            @click="sidebarOpen = !sidebarOpen">
-        <x-icon.menu/>
-    </button>
-    <div class="flex-shrink-0 overflow-x-hidden sidebar"
+
+    <div class="flex-shrink-0 overflow-x-hidde sidebar"
          x-cloak
          x-bind:class="{
             'sidebar-open': sidebarOpen,
             'sidebar-closed': !sidebarOpen
            }"
     >
+    <nav class="flex-grow w-full">
         <livewire:layouts.sidebar.content/>
+    </nav>
     </div>
     <div class="relative flex h-full max-w-full flex-1 flex-col overflow-hidden hmmm"
          :style="`margin-left: ${sidebarOpen ? '0' : '50px'}`"
