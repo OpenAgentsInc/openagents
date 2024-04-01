8950a8422824e21f37015b2aee6f5313a77bfc0c
Moneya <moneya59@gmail.com>
2024-04-01 18:23:47 +0100

    fixed merge conflict

--- resources/views/livewire/sidebar-content.blade.php
+++ resources/views/livewire/sidebar-content.blade.php
@@ -5,6 +5,31 @@
 
     <div class="w-full">
         <div class="flex gap-2 items-center  overflow-hidden" x-bind:class="{
+            'justify-between': !collapsed,
+            'justify-center': collapsed
+           }">
+            <button class="z-50 absolute top-0 left-0 cursor-pointer h-[28px] w-[28px] m-4 mt-[18px] mr-12" @click="sidebarOpen = !sidebarOpen">
+                <x-icon.menu />
+            </button>
+
+
+            <div  class="relative flex-1 text-right" x-data="{ dropdown: false }">
+                    <button @click="dropdown= !dropdown" x-cloak  x-show="sidebarOpen" class="mt-4 p-1.5 rounded-md text-white hover:bg-gray-50 active:bg-gray-100">
+                        <x-icon.plus class="h-6 w-6"></x-icon.plus>
+                    </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-24 w-[260px] p-4">
+        <ul x-cloak x-bind:class="{
+        'hidden': !sidebarOpen
+       }" x-bind:class="{
+<div>
+
+
+    <div class="w-full">
+        <div class="flex gap-2 items-center  overflow-hidden" x-bind:class="{
             'justify-between': sidebarOpen,
             'justify-center': collapsed
            }">
