7e135d6549da17b7a6ed7eaeba5cb438872d45e1
moneya <moneya@users.noreply.github.com>
2024-04-02 22:17:33 +0100

    Added Share links modal (#223)
    * fixed merge conflict
    
    * fixed UI changes
    
    * merged modals
    
    * merged new sidebar
    
    * finsihed the auth flow
    
    * added share links modal

---
+++ resources/views/components/login-button.blade.php
@@ -0,0 +1,7 @@
+<x-secondary-button x-data
+                    wire:click="$dispatch('openModal', { component: 'auth.login' })"
+                    id="hs-dropdown-with-header"
+                    type="button"
+                    class="focus:outline-none active:outline-none outline-none focus:ring-0 active:ring-0 h-[48px] py-2 text-[20px] inline-flex justify-center items-center gap-x-2 font-semibold rounded-full disabled:opacity-50 disabled:pointer-events-none">
+    Sign In
+</x-secondary-button>
---
+++ resources/views/livewire/modals/chat/share.blade.php
@@ -0,0 +1,102 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+
+    <div class="w-full my-2">
+
+        <div class="my-4 px-6">
+            <h2 class="block text-md md:text-xl lg:text-2xl  font-bold text-white">Share link to chat</h2>
+        </div>
+
+        <div class="my-4 px-6 text-left text-md">
+            Messages you send after creating the link will not be shared. Anyone with the link can view.
+        </div>
+
+
+        <div class="p-4 sm:p-7">
+            <div class="mb-4 border-2 border-[#3C3E42] rounded">
+                <!-- Chat section -->
+                <div class="max-h-[315px] overflow-y-auto">
+                    <div class="z-[-1] w-full text-lightgray">
+                        <div class="px-1 py-2 justify-center text-base md:gap-6 m-auto">
+                            <div class="flex flex-1 text-base mx-auto gap-3 md:px-5 lg:px-1 xl:px-5 md:max-w-3xl lg:max-w-[800px]">
+                                <div class="flex-shrink-0 flex flex-col relative items-end">
+
+                                    <div class="m-[2px] w-[36px] p-1 border border-darkgray rounded">
+
+                                        <div class="">
+                                            <svg class="" width="100%" height="100%" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M24 8C19.5807 8 16 11.5807 16 16C16 20.4193 19.5807 24 24 24C28.4193 24 32 20.4193 32 16C32 11.5807 28.4193 8 24 8ZM24 28C11.7995 28 8 36 8 36V40H40V36C40 36 36.2005 28 24 28Z" fill="white"></path>
+                                            </svg>
+                                        </div>
+                                        <!--[if ENDBLOCK]><![endif]-->
+                                    </div>
+                                    <!--[if ENDBLOCK]><![endif]-->
+                                </div>
+                                <div class="relative flex w-full flex-col">
+                                    <span class="mb-1 font-semibold select-none text-white">You</span>
+                                    <div class="flex-col gap-1 md:gap-3">
+                                        <div class="-mt-4 flex flex-grow flex-col max-w-[936px]">
+
+                                            <div class="text-md text-text">
+                                                <p>what's the concept of openagents?</p>
+                                            </div>
+                                            <!--[if ENDBLOCK]><![endif]-->
+                                        </div>
+                                        <div class="flex justify-start gap-3 empty:hidden">
+                                            <div class="text-gray flex self-end lg:self-center justify-center lg:justify-start mt-0 -ml-1 h-7 visible">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="z-[-1] w-full text-lightgray">
+                        <div class="px-1 py-2 justify-center text-base md:gap-6 m-auto">
+                            <div class="flex flex-1 text-base mx-auto gap-3 md:px-5 lg:px-1 xl:px-5 md:max-w-3xl lg:max-w-[800px]">
+                                <div class="flex-shrink-0 flex flex-col relative items-end">
+                                    <div class="m-[2px] w-[36px] p-2 rounded">
+
+                                            <x-icon.chatgpt class="w-[50px]" />
+
+                                    </div>
+                                </div>
+                                <div class="relative flex w-full flex-col">
+                                    <span class="mb-1 font-semibold select-none text-white">OpenAgents</span>
+                                    <div class="flex-col gap-1 md:gap-3">
+                                        <div class="-mt-4 flex flex-grow flex-col max-w-[936px]">
+                                            <div class="text-md text-text">
+                                                <p>OpenAgents is a versatile platform that hosts a dynamic swarm of AI agents. Each agent is tailored to excel in specific tasks, ranging from website development to financial analysis. The platform is designed to intelligently route your queries to the most suitable agent based on your needs. Would you like to know more about how OpenAgents can assist you, or are you interested in creating your own AI agent?</p>
+                                            </div>
+
+                                        </div>
+                                        <div class="flex justify-start gap-3 empty:hidden">
+                                            <div class="text-gray flex self-end lg:self-center justify-center lg:justify-start mt-0 -ml-1 h-7 visible">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end chat section -->
+
+                <div class="h-[52px] flex justify-end items-center px-4">
+                    <x-icon.openagent class="w-4/12"/>
+
+                </div>
+            </div>
+
+
+
+            <div class="mt-5 w-full">
+                <x-button class="w-full text-center justify-center flext items-center gap-2 py-2">
+                 <x-icon.link class="h-6 w-6" />  Copy link
+                </x-button>
+            </div>
+
+
+        </div>
+    </div>
+</div>
