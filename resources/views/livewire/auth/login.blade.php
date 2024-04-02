490c7c453d6bcef4026e48be62d5ad06ef797901
Christopher David <chris@arcadelabs.co>
2024-04-02 13:31:06 -0500

    login modal works great

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,64 +1,41 @@
 <div class="bg-black text-white p-[32px]">
     <div class="">
-        <h2 class="block text-md md:text-xl lg:text-2xl text-center font-bold text-white">{{$this->show ? 'Enter Password' : 'Get started'}}</h2>
+        <h2 class="block text-md md:text-xl lg:text-2xl text-center font-bold text-white">{{$this->show ? 'Enter Password' : 'Log in'}}</h2>
     </div>
 
-    <div class="p-4 sm:p-7">
-        <div class="mb-4">
-            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="email" />
+    <div class="p-4">
+        <div>
+            <x-label for="email" value="{{ __('Email') }}"/>
+            <x-chat.input id="email" class="block mt-[6px] w-full" type="email" name="email" :value="old('email')"
+                          required autofocus autocomplete="username" placeholder="Enter email"/>
+        </div>
+        <div class="mt-6">
+            <div class="flex flex-row justify-between">
+                <x-label for="password" value="{{ __('Password') }}"/>
+                <a class="text-sm text-gray cursor-pointer"
+                   wire:click="$dispatch('openModal', { component: 'auth.forget-password' })"
+                >Forgot?</a>
+            </div>
+            <x-chat.input id=" password" class="block mt-[6px] w-full" type="password" name="password" required
+                          autocomplete="current-password" placeholder="Enter password"/>
         </div>
 
-        @if($this->show)
-        <div class="mb-4">
-            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autofocus autocomplete="password" placeholder="password" />
-        </div>
-        @endif
-
-        <div class="my-5">
+        <div class="mt-6">
             <x-button class="w-full text-center justify-center gap-2 py-2" wire:click='showLogin()'>
-                {{$this->show ? 'Enter Password' : 'Get started'}}
+                {{$this->show ? 'Enter Password' : 'Log in'}}
             </x-button>
 
             @if(!$this->show)
-            <div class="py-3 my-2 flex items-center text-xs text-[#777A82] uppercase before:flex-[1_1_0%]  before:me-6 after:flex-[1_1_0%] after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">
-                Or
-            </div>
+                <div class="py-3 my-2 flex flex-col items-center text-sm text-[#777A82]">
+                    or
+                </div>
 
-            <x-secondary-button class="w-full flex justify-center gap-2">
-                <x-icon.google class="h-5 w-5"></x-icon.google>
-                <span class="text-sm md:text-md">Continue with Google</span>
-            </x-secondary-button>
+                <x-secondary-button class="w-full flex justify-center gap-2 mb-0">
+                    <x-icon.google class="h-5 w-5"></x-icon.google>
+                    <span class="text-sm md:text-md">Log in with Google</span>
+                </x-secondary-button>
             @endif
 
         </div>
-
-        @if(!$this->show)
-        <!-- the best place to add sign up -->
-        <div class="text-center">
-            <p class="mb-0 mt-6 text-sm text-gray">
-                By continuing you agree to our
-                <a target="_blank"
-                   class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                   href="/terms">
-                    Terms of Service
-                </a>
-                and
-                <a target="_blank"
-                   class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                   href="/privacy">
-                    Privacy Policy.
-                </a>
-            </p>
-        </div>
-        @else
-        <div class="text-center">
-            <p class="mt-4 text-sm text-gray">
-                <a role="button" wire:click="$dispatch('openModal', { component: 'auth.forget-password' })" id="hs-dropdown-with-header" type="button" class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" >
-                    Forget Password
-                </a>
-            </p>
-        </div>
-        @endif
-
     </div>
 </div>
