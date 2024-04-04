f6afc49870b0758650d3bd1ee8f4009444facee2
Moneya <moneya59@gmail.com>
2024-04-04 23:04:40 +0100

    completed The password reset modal

--- app/Livewire/Auth/ForgetPassword.php
+++ app/Livewire/Auth/ForgetPassword.php
@@ -2,18 +2,68 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
 use LivewireUI\Modal\ModalComponent;
+use Illuminate\Support\Facades\Password;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
 class ForgetPassword extends ModalComponent
 {
     public bool $show = false;
+    public string $email;
 
-    // Toggle the value of $show
-    public function sendResetLink()
+    use LivewireAlert;
+
+
+    public function resetPassword()
     {
 
-        $this->show = ! $this->show;
+        $this->validate([
+            'email' => ['required', 'email'],
+        ]);
+
+        $user = User::where('email', $this->email)->first();
+
+        if ($user && !empty($user)) {
+
+            // We will send the password reset link to this user. Once we have attempted
+            // to send the link, we will examine the response then see the message we
+            // need to show to the user. Finally, we'll send out a proper response.
+            $status = Password::sendResetLink([
+                'email' => $this->email
+            ]);
+
+            if ($status  == Password::RESET_LINK_SENT){
+
+
+                    $this->show = true;
+                $this->alert('success', 'Reset link sent!');
+            } else {
+                $this->alert('error', 'Reset link sent!');
+            }
+        }
     }
+
+
+    public function resendLink()
+    {
+
+        $user = User::where('email',$this->email)->first();
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        // $status = Password::broker()->sendResetLink(['email' => $this->email]);
+
+        if ($user && !is_null($user)) {
+            $token = app('auth.password.broker')->createToken($user);
+            $user->sendPasswordResetNotification($token);
+            $this->alert('success', 'Reset link sent!');
+        } else {
+            $this->alert('error', 'Reset Link not sent cause an error occured');
+        }
+    }
+
 
     public function render()
     {
