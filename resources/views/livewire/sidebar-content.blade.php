90c504aabf95e58821ccc5b2308799caa321abba
Moneya <moneya59@gmail.com>
2024-04-04 03:21:24 +0100

    reverted the sidebar

--- resources/views/livewire/sidebar-content.blade.php
+++ resources/views/livewire/sidebar-content.blade.php
@@ -1,40 +1,16 @@
-<div>
-
-
-
-
-    <div class="w-full">
-        <div class="flex gap-2 items-center  overflow-hidden" x-bind:class="{
-            'justify-between': sidebarOpen,
-            'justify-center': collapsed
-           }">
-            <button class="z-50 absolute top-0 left-0 cursor-pointer h-[28px] w-[28px] m-4 mt-[18px] mr-12" @click="sidebarOpen = !sidebarOpen">
-                <x-icon.menu />
-            </button>
-
-
-            <div  class="relative flex-1 text-right" x-data="{ dropdown: false }">
-                    <button @click="dropdown= !dropdown" x-cloak  x-show="sidebarOpen" class="mt-4 p-1.5 rounded-md text-white hover:bg-gray-50 active:bg-gray-100">
-                        <x-icon.plus class="h-6 w-6"></x-icon.plus>
-                    </button>
-            </div>
+<div class="w-full relative z-50">
+    <div class="justify-between flex gap-2 items-center overflow-hidden z-50">
+        <div class="relative flex-1 text-right" x-data="{ dropdown: false }">
+            <a href="/" wire:navigate>
+                <button class="mt-3 p-1.5 rounded-md text-white">
+                    <x-icon.plus class="h-6 w-6"></x-icon.plus>
+                </button>
+            </a>
         </div>
     </div>
 
-
-
-    <div class="mt-8 w-[260px] p-4">
-        <ul x-cloak x-bind:class="{
-        'hidden': !sidebarOpen
-       }" x-bind:class="{
-        'my-2 flex flex-col gap-2 items-stretch': true,
-        'rounded-md p-2 mx- gap-4': !sidebarOpen,
-        'rounded-full p-2 mx- w-10 h-10': sidebarOpen
-     }">
-
-     @if($threads)
-     <li class="mt-4">
-        <span class="text-left leading-6 font-sm text-sm text-[#777A82] px-2" x-cloak x-show="sidebarOpen">
+    <div class="flex flex-col gap-2 mt-8 py-4 px-1">
+        <span class="text-left text-sm text-[#777A82] px-2" x-cloak>
             Today
         </span>
         <ol>
@@ -42,6 +18,5 @@
                 <livewire:sidebar-thread :thread="$thread" :key="$thread->id"/>
             @endforeach
         </ol>
-        @endif'
     </div>
 </div>
