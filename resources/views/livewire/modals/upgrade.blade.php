8254b6176207b3e84032031ba9148090703a3f19
Christopher David <chris@arcadelabs.co>
2024-04-05 12:52:40 -0500

    ok

--- resources/views/livewire/modals/upgrade.blade.php
+++ resources/views/livewire/modals/upgrade.blade.php
@@ -5,12 +5,12 @@
 
     <div class="p-4 sm:px-7 pb-6">
         <p class="text-left my-2 text-md lg:text-lg text-[#D7D8E5]">
-            Upgrade to Pro for $10/month and receive 100 responses per day. Billing secured with Stripe.
+            Upgrade to Pro for $10/month and receive 100 responses per day. Secure billing via Stripe.
         </p>
 
         <div class="mt-8 mb-2">
             <a class="w-full" href="https://pay.openagents.com/b/14kg0d0nt2TCegEcMP">
-                <x-button class="w-full justify-center font-medium">Sign up</x-button>
+                <x-button class="w-full justify-center font-medium">Upgrade plan</x-button>
             </a>
         </div>
     </div>
