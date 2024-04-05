a0b73f2d436da56d54b21179a6365ee9b0bfae39
Christopher David <chris@arcadelabs.co>
2024-04-05 16:50:18 -0500

    Delete non-MVP code (#235)
    * axe unused migrations / consolidate
    
    * axe factories
    
    * clean up tests
    
    * no cache
    
    * no personal access tokens
    
    * cleanup
    
    * axe old models
    
    * axe dat console thing
    
    * axe all old services
    
    * just put username there
    
    * axe unused tests
    
    * nonprivate no
    
    * thread simplify
    
    * axe stability api
    
    * dupe slash
    
    * no agent shaz in chat
    
    * Axe services entirely
    
    * temp remove extism/finnhub
    
    * axe funcgion caller
    
    * axe inferencer
    
    * command

--- database/migrations/2024_03_12_171259_create_threads_table.php
+++ database/migrations/2024_03_12_171259_create_threads_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('threads', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
+            $table->boolean('private')->default(false);
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/chat/{id}', Chat::class)->name('chat');
 
 // AUTH
 Route::get('/reset/account/{token}', ChangePassword::class)->name('password.reset');
-Route::get('/email/verify//{id}/{hash}', VerifyAccount::class)->name('verification.verify');
+Route::get('/email/verify/{id}/{hash}', VerifyAccount::class)->name('verification.verify');
 
 // AUTH - SOCIAL
 Route::get('/login/x', [SocialAuthController::class, 'login_x']);
