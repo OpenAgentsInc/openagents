ebceb79c9fd8e3fa6e2cabdfcb6b406d6602a0a1
Christopher David <chris@arcadelabs.co>
2024-04-05 11:21:50 -0500

    top buttons a lil smaller

--- resources/views/components/login-buttons.blade.php
+++ resources/views/components/login-buttons.blade.php
@@ -3,7 +3,7 @@
             wire:click="$dispatch('openModal', { component: 'auth.login' })"
             id="hs-dropdown-with-header"
             type="button"
-            class="px-4 focus:outline-none active:outline-none outline-none focus:ring-0 active:ring-0 h-[48px] py-2 text-[20px] inline-flex justify-center items-center gap-x-2 font-semibold rounded-full disabled:opacity-50 disabled:pointer-events-none">
+            class="px-4 focus:outline-none active:outline-none outline-none focus:ring-0 active:ring-0 h-[44px] py-2 text-[18px] inline-flex justify-center items-center gap-x-2 font-semibold rounded-full disabled:opacity-50 disabled:pointer-events-none">
         Log in
     </button>
 
@@ -11,7 +11,7 @@
                         wire:click="$dispatch('openModal', { component: 'auth.register' })"
                         id="hs-dropdown-with-header"
                         type="button"
-                        class="focus:outline-none active:outline-none outline-none focus:ring-0 active:ring-0 h-[48px] py-[1.5] text-[20px] inline-flex justify-center items-center gap-x-2 font-semibold rounded-full disabled:opacity-50 disabled:pointer-events-none">
+                        class="focus:outline-none active:outline-none outline-none focus:ring-0 active:ring-0 h-[44px] py-1 text-[18px] inline-flex justify-center items-center gap-x-2 font-semibold rounded-full disabled:opacity-50 disabled:pointer-events-none">
         Sign up
     </x-secondary-button>
 </div>
