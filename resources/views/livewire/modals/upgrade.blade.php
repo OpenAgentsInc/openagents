de3b340ba58cde82ebbf738966578e747092b8da
Christopher David <chris@arcadelabs.co>
2024-04-05 12:48:46 -0500

    sign up

---
+++ resources/views/livewire/modals/upgrade.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <div class="mt-6 px-6 sm:px-7">
+        <h2 class="block text-md  md:text-xl lg:text-2xl font-bold text-white">Upgrade your plan</h2>
+    </div>
+
+    <div class="p-4 sm:px-7 pb-6">
+        <p class="text-left my-2 text-md lg:text-lg text-[#D7D8E5]">
+            Upgrade to Pro for $10/month and receive 100 responses per day. Billing secured with Stripe.
+        </p>
+
+        <div class="mt-8 mb-2">
+            <a class="w-full">
+                <x-button class="w-full justify-center font-medium">Sign up</x-button>
+            </a>
+        </div>
+    </div>
+</div>
