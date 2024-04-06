0b86e4d0f3637b6e1d966ab2439b60475180c017
Christopher David <chris@arcadelabs.co>
2024-04-06 17:26:36 -0500

    update test & factory

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Thread extends Model
@@ -18,8 +18,8 @@ class Thread extends Model
         return $this->hasMany(Message::class);
     }
 
-    public function users(): BelongsToMany
+    public function user(): BelongsTo
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsTo(User::class);
     }
 }
--- database/migrations/2024_03_12_171259_create_threads_table.php
+++ database/migrations/2024_03_12_171259_create_threads_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('threads', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
             $table->string('title')->nullable();
             $table->boolean('private')->default(false);
             $table->timestamps();
