f4fa3f94b0cba2f128d59060349bb0415cd5513a
Christopher David <chris@arcadelabs.co>
2024-04-07 10:47:34 -0500

    Add logout popover (#248)
    * clean up sidebar-thread
    
    * add logout popover

--- resources/views/livewire/sidebar-thread.blade.php
+++ resources/views/livewire/sidebar-thread.blade.php
@@ -38,46 +38,5 @@
                             </span> Delete
             </button>
         </div>
-
-
-        {{--        <div x-data x-popover class="relative">--}}
-        {{--            <button x-popover:button type="button"--}}
-        {{--                    class="flex items-center gap-2 px-5 py-2.5 rounded-md shadow bg-black border border-darkgray">--}}
-        {{--                Company--}}
-
-        {{--                <!-- Heroicon: chevron-down -->--}}
-        {{--                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"--}}
-        {{--                     fill="currentColor">--}}
-        {{--                    <path fill-rule="evenodd"--}}
-        {{--                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"--}}
-        {{--                          clip-rule="evenodd"/>--}}
-        {{--                </svg>--}}
-        {{--            </button>--}}
-
-        {{--            <div--}}
-        {{--                    x-popover:panel--}}
-        {{--                    x-cloak--}}
-        {{--                    x-transition.origin.top.left--}}
-        {{--                    class="relative left-0 mt-2 w-[400px] rounded-md shadow-md text-white bg-black border border-darkgray"--}}
-        {{--            >--}}
-        {{--                <button x-on:click="Livewire.dispatch('openModal', { component: 'modals.chat.rename' })"--}}
-        {{--                        class="w-full hover:bg-white/20 gap-4 flex px-4 py-2 text-sm text-white" role="menuitem"--}}
-        {{--                        tabindex="-1" id="user-menu-item-0">--}}
-        {{--                            <span>--}}
-        {{--                                <x-icon.pen class="h-4 w-4"></x-icon.pen>--}}
-        {{--                            </span> Rename--}}
-        {{--                </button>--}}
-
-        {{--                <a href="#"--}}
-        {{--                   class="block w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">--}}
-        {{--                    Team--}}
-        {{--                </a>--}}
-
-        {{--                <a href="#"--}}
-        {{--                   class="block w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">--}}
-        {{--                    Careers--}}
-        {{--                </a>--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
     </div>
 </div>
