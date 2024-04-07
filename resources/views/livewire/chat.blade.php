4487894aa4fd7ad784b2be311044c2ee8d41c762
Christopher David <chris@arcadelabs.co>
2024-04-07 10:47:34 -0500

    Add logout popover (#248)
    * clean up sidebar-thread
    
    * add logout popover

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -39,19 +39,29 @@
                         <livewire:model-selector/>
 
                         @auth
-                            <div class="flex flex-row items-center">
-                                <x-icon.share wire:click="$dispatch('openModal', { component: 'modals.chat.share' })"
-                                              class="cursor-pointer w-[24px] h-[24px] mr-[56px]"/>
-                                <a href="/logout">
-                                    <a href="/logout">
-                                        @if(Auth::user()->profile_photo_path)
-                                            <img src="{{ Auth::user()->profile_photo_path }}" alt="Profile"
-                                                 class="rounded-full w-[32px] h-[32px] object-cover">
-                                        @else
-                                            {{ strtoupper(Auth::user()->name[0] ?? '-') }}
-                                        @endif
-                                    </a>
-                                </a>
+                            <div class="relative z-[15]">
+                                <div x-data="{ open: false }" class="flex flex-row items-center">
+                                    <x-icon.share
+                                            wire:click="$dispatch('openModal', { component: 'modals.chat.share' })"
+                                            class="cursor-pointer w-[24px] h-[24px] mr-[56px]"/>
+                                    <div x-popover @click.outside="open = false" class="relative">
+                                        <button x-popover:button @click="open = !open" class="focus:outline-none">
+                                            @if(Auth::user()->profile_photo_path)
+                                                <img src="{{ Auth::user()->profile_photo_path }}" alt="Profile"
+                                                     class="rounded-full w-[32px] h-[32px] object-cover">
+                                            @else
+                                                {{ strtoupper(Auth::user()->name[0] ?? '-') }}
+                                            @endif
+                                        </button>
+                                        <div x-popover:panel x-cloak x-transition x-show="open"
+                                             class="fixed right-[8px] mt-2 rounded-md shadow-md text-white bg-black border border-darkgray">
+                                            <a href="/logout"
+                                               class="block px-4 py-2 text-sm text-white hover:bg-white/20">
+                                                Logout
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
                         @else
