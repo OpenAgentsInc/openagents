6bb86ff4f1596e7ad0673035a632410b0ef9959b
Christopher David <chris@arcadelabs.co>
2024-04-06 20:22:27 -0500

    fix load indicator

--- resources/views/components/chat/messagestreaming.blade.php
+++ resources/views/components/chat/messagestreaming.blade.php
@@ -11,9 +11,8 @@
                 <span class="mb-1 font-semibold select-none text-white">{{ $author }}</span>
                 <div class="flex-col gap-1 md:gap-3">
                     <div class="flex flex-grow flex-col max-w-[936px]">
-                        <x-markdown wire:stream="streamtext" class="text-md whitespace-pre-wrap">
-                            <div class="dot-flashing"></div>
-                        </x-markdown>
+                        <x-markdown wire:stream="streamtext" class="text-md whitespace-pre-wrap"></x-markdown>
+                        <div class="dot-flashing"></div>
                     </div>
                     <div class="flex justify-start gap-3 empty:hidden">
                         <div
