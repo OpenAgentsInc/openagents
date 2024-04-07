46a7e4cf76e40959f4112c80365476d756a54ccf
Christopher David <chris@arcadelabs.co>
2024-04-06 23:40:14 -0500

    register modal yaaa

--- app/Livewire/ModelSelector.php
+++ app/Livewire/ModelSelector.php
@@ -24,20 +24,28 @@ class ModelSelector extends Component
     {
         $userAccess = $this->getUserAccess();
 
-        // Check if the selected model requires "pro" access and the user is not a "pro" user
-        if ($this->isProModelSelected($model) && $userAccess !== 'pro') {
-            $this->dispatch('openModal', 'modals.upgrade');
+        // If the user is not logged in, show the login modal for any model they don't have access to
+        if ($userAccess === 'guest' && ! $this->hasModelAccess($model, $userAccess)) {
+            $this->dispatch('openModal', 'auth.register');
 
             return;
         }
 
-        // Check if the user has access to the selected model
+        // Check if the selected model requires "pro" access
+        if ($this->isProModelSelected($model)) {
+            // If the user is logged in but not a "pro" user, show the upgrade modal
+            if ($userAccess !== 'pro') {
+                $this->dispatch('openModal', 'modals.upgrade');
+
+                return;
+            }
+        }
+
+        // If the user has access to the selected model, update the selected model
         if ($this->hasModelAccess($model, $userAccess)) {
             $this->selectedModel = $model;
             $this->formattedModel = Models::getModelName($model);
             $this->dispatch('select-model', $model);
-        } else {
-            dd('no access to that model!');
         }
     }
 
@@ -52,17 +60,6 @@ class ModelSelector extends Component
         }
     }
 
-    protected function isProModelSelected($model)
-    {
-        $modelDetails = Models::MODELS[$model] ?? null;
-
-        if ($modelDetails) {
-            return $modelDetails['access'] === 'pro';
-        }
-
-        return false;
-    }
-
     protected function hasModelAccess($model, $userAccess)
     {
         $modelDetails = Models::MODELS[$model] ?? null;
@@ -74,6 +71,17 @@ class ModelSelector extends Component
             $requiredAccessIndex = array_search($requiredAccess, $accessLevels);
 
             return $userAccessIndex >= $requiredAccessIndex;
+        }
+
+        return false;
+    }
+
+    protected function isProModelSelected($model)
+    {
+        $modelDetails = Models::MODELS[$model] ?? null;
+
+        if ($modelDetails) {
+            return $modelDetails['access'] === 'pro';
         }
 
         return false;
