7ce7365cdd81f8b14fa1fbf09cd3a2c82fd9c1c7
Christopher David <chris@arcadelabs.co>
2024-04-08 11:38:25 -0500

    update link styling

--- resources/views/livewire/sidebar-content.blade.php
+++ resources/views/livewire/sidebar-content.blade.php
@@ -10,9 +10,6 @@
     </div>
 
     <div class="flex flex-col gap-2 mt-8 py-4 px-1" @thread_updated="$refresh">
-        {{--        <span class="text-left text-sm text-[#777A82] px-2" x-cloak>--}}
-        {{--            Today--}}
-        {{--        </span>--}}
         <ol>
             @foreach($threads as $thread)
                 <livewire:sidebar-thread :thread="$thread" :key="$thread->id"/>
@@ -20,11 +17,20 @@
         </ol>
     </div>
 
-    <!-- New section for links -->
     <div class="flex flex-col gap-2 py-4 px-1 mt-auto">
         <ol>
             @auth
-                <li class="ml-4"><a target="_blank" href="/billing">Billing</a></li>
+                <li>
+                    <div class="relative z-[15]">
+                        <div class="group relative rounded-lg active:opacity-90 px-3">
+                            <a href="/billing" target="_blank" class="flex items-center gap-2 py-2" wire:navigate>
+                                <div class="relative grow overflow-hidden whitespace-nowrap">
+                                    Billing
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </li>
             @endauth
         </ol>
     </div>
