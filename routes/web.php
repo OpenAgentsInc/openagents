bc850a2dfc7c742851f55b86a33339e2022fd3b0
Christopher David <chris@arcadelabs.co>
2024-04-08 18:11:52 -0500

    Terms and privacy yuck (#255)
    * terms and privacy links
    
    * routes
    
    * terms and privacy
    
    * axe old gemini docs
    
    * bleh terms
    
    * terms privacy yuck

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,17 @@ Route::get('/callback/x', [SocialAuthController::class, 'login_x_callback']);
 // BILLING
 Route::get('/subscription', [BillingController::class, 'stripe_billing_portal']);
 Route::get('/upgrade', [BillingController::class, 'stripe_subscribe']);
+Route::get('/pro', [BillingController::class, 'pro'])->name('pro');
 
-// STATIC
-Route::get('/pro', [StaticController::class, 'pro'])->name('pro');
+// BLOG
 Route::get('/blog', [StaticController::class, 'blog']);
 Route::get('/launch', [StaticController::class, 'launch']);
 Route::get('/goodbye-chatgpt', [StaticController::class, 'goodbye']);
+
+// MISC
 Route::get('/docs', [StaticController::class, 'docs']);
+Route::get('/terms', [StaticController::class, 'terms']);
+Route::get('/privacy', [StaticController::class, 'privacy']);
 
 // Add GET logout route
 Route::get('/logout', [AuthenticatedSessionController::class, 'destroy']);
