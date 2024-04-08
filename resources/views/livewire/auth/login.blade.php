f4559dd748d94bed3987de4b7acad525afaafcb5
Christopher David <chris@arcadelabs.co>
2024-04-08 15:58:45 -0500

    Hide share button / consolidate login buttons (#252)

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,44 +1,27 @@
 <div class="bg-black text-white p-[32px]">
     <h2 class="block text-md md:text-xl lg:text-2xl text-center font-bold text-white">Log in</h2>
 
-    <form wire:submit.prevent="login" class="p-4">
-        <div>
-            <x-label for="email" value="{{ __('Email') }}"/>
-            <x-chat.input wire:model='email' id="email"
-                          class="block mt-[6px] w-full  @error('email') border border-red-500 @enderror" type="email"
-                          name="email" :value="old('email')"
-                          required autofocus autocomplete="username" placeholder="Enter email"/>
-            @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
-        </div>
-        <div class="mt-6">
-            <div class="flex flex-row justify-between">
-                <x-label for="password" value="{{ __('Password') }}"/>
-                <a class="text-sm text-gray cursor-pointer"
-                   wire:click="$dispatch('openModal', { component: 'auth.forget-password' })"
-                >Forgot?</a>
-            </div>
-            <div>
-            <x-chat.input wire:model='password' id="password" class="block mt-[6px] w-full" type="password" name="password" required
-                          autocomplete="current-password" placeholder="Enter password"/>
-                          @error('password') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-        </div>
+    <div class="my-6">
+        <a href="/login/x">
+            <x-secondary-button class="w-full flex justify-center gap-2 mb-0 h-[44px]">
+                <x-icon.x class="h-5 w-5"></x-icon.x>
+                <span class="">Log in with X</span>
+            </x-secondary-button>
+        </a>
+    </div>
 
-        <div class="mt-6">
-            <x-button class="w-full text-center justify-center gap-2 py-2">
-                Log in
-            </x-button>
-
-            <div class="py-3 my-2 flex flex-col items-center text-sm text-[#777A82]">
-                or
-            </div>
-
-            <a href="/login/x">
-                <x-secondary-button class="w-full flex justify-center gap-2 mb-0 h-[44px]">
-                    <x-icon.x class="h-5 w-5"></x-icon.x>
-                    <span class="">Log in with X</span>
-                </x-secondary-button>
+    <div class="text-center">
+        <p class="mt-2 mb-0 text-sm text-gray">
+            By continuing you agree to our
+            <a class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+               href="/terms" target="_blank">
+                Terms of Service
             </a>
-        </div>
-    </form>
+            and
+            <a class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+               href="/privacy" target="_blank">
+                Privacy Policy.
+            </a>
+        </p>
+    </div>
 </div>
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,94 +1,27 @@
 <div class="bg-black text-white p-[32px]">
+    <h2 class="block text-md md:text-xl lg:text-2xl text-center font-bold text-white">Sign up</h2>
 
-    @if(!$this->verification)
+    <div class="my-6">
+        <a href="/login/x">
+            <x-secondary-button class="w-full flex justify-center gap-2 mb-0 h-[44px]">
+                <x-icon.x class="h-5 w-5"></x-icon.x>
+                <span class="">Sign up with X</span>
+            </x-secondary-button>
+        </a>
+    </div>
 
-        <h2 class="block text-md md:text-xl lg:text-2xl text-center font-bold text-white">{{$this->show ? 'Create Password' : 'Sign up'}}</h2>
-
-    @else
-        <div class="py-4 mt-2">
-            <h2 class="block text-md md:text-xl lg:text-2xl text-center font-bold text-white">Verify your email</h2>
-        </div>
-    @endif
-
-    @if(!$this->show)
-        {{-- Step 1 input email --}}
-        <div class="p-4 pb-0">
-            <div class="mb-4">
-                <x-label for="register-email" value="{{ __('Email') }}"/>
-                <x-input wire:model='email' id="register-email" class="block mt-[6px] w-full" type="email" name="email"
-                         :value="old('email')" required autofocus autocomplete="username" placeholder="Enter email"/>
-            </div>
-
-            <div class="mt-6 mb-3">
-                <x-button class="w-full flex justify-center gap-2 text-center" wire:click='showpassword()'>
-                    Continue
-                </x-button>
-
-                <div class="mt-2 py-3 flex flex-col items-center text-sm text-[#777A82]">
-                    or
-                </div>
-
-                <a href="/login/x">
-                    <x-secondary-button class="w-full flex justify-center gap-2 my-2">
-                        <x-icon.x class="h-5 w-5"></x-icon.x>
-                        <span class="text-sm md:text-md">Continue with X</span>
-                    </x-secondary-button>
-                </a>
-            </div>
-
-            <div class="text-center">
-                <p class="mt-2 mb-0 text-sm text-gray">
-                    By continuing you agree to our
-                    <a class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                       href="/terms" target="_blank">
-                        Terms of Service
-                    </a>
-                    and
-                    <a class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                       href="/privacy" target="_blank">
-                        Privacy Policy.
-                    </a>
-                </p>
-            </div>
-        </div>
-    @endif
-    @if ($this->show && !$this->verification)
-    {{-- Step 2 Input password --}}
-    <form wire:submit.prevent='create'>
-        <div class="p-2">
-            <div class="mb-6">
-                <x-input id="password" wire:model='password' class="block mt-1 w-full" type="password" name="password" required autofocus placeholder="Enter password..." />
-                @error('password') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="mb-6">
-                <x-input id="password" wire:model='password_confirmation' class="block mt-1 w-full" type="password" name="password_confirmation" required autofocus placeholder="Confirm password..." />
-                @error('password_confirmation') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-
-                <div class="mt-8">
-                    <x-button class="w-full flex justify-center text-center gap-2">
-                        Create Password
-                    </x-button>
-                </div>
-            </div>
-        </form>
-    @endif
-    @if ($this->show && $this->verification)
-        {{-- Send Verification --}}
-        <div class="p-4 sm:p-7">
-            <div class="text-center">
-                <p class="mt-2 text-sm md:text-md text-gray">
-                    We sent a verification link to <span class="text-white">satoshi@nakamoto.com</span>
-                </p>
-                <p>
-                    <a role="button" wire:click='resend()'
-                       class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                       href="#">
-                        Resend Email
-                    </a>
-                </p>
-            </div>
-        </div>
-    @endif
+    <div class="text-center">
+        <p class="mt-2 mb-0 text-sm text-gray">
+            By continuing you agree to our
+            <a class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+               href="/terms" target="_blank">
+                Terms of Service
+            </a>
+            and
+            <a class="text-white decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+               href="/privacy" target="_blank">
+                Privacy Policy.
+            </a>
+        </p>
+    </div>
 </div>
