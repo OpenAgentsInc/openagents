379b56c5b6e8ea4399ac9921eb5478ddb9b5d136
Christopher David <chris@arcadelabs.co>
2024-04-08 15:58:45 -0500

    Hide share button / consolidate login buttons (#252)

--- resources/views/components/login-buttons.blade.php
+++ resources/views/components/login-buttons.blade.php
@@ -1,12 +1,4 @@
 <div>
-    <button x-data
-            wire:click="$dispatch('openModal', { component: 'auth.login' })"
-            id="hs-dropdown-with-header"
-            type="button"
-            class="px-4 focus:outline-none active:outline-none outline-none focus:ring-0 active:ring-0 h-[44px] py-2 text-[18px] inline-flex justify-center items-center gap-x-2 font-semibold rounded-full disabled:opacity-50 disabled:pointer-events-none">
-        Log in
-    </button>
-
     <x-secondary-button x-data
                         wire:click="$dispatch('openModal', { component: 'auth.register' })"
                         id="hs-dropdown-with-header"
