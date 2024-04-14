aea3970f7287ba7a4951f7844d005d2ec7e5a564
Christopher David <chris@arcadelabs.co>
2024-04-14 15:01:26 -0400

    Code block copy button (#264)
    * Initial Copy code button
    
    * Nice styling
    
    * almost
    
    * almost2
    
    * kinda works
    
    * remove dupe listener
    
    * extract to partial
    
    * Console cleanup

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -16,7 +16,8 @@
 </head>
 
 <body class="h-screen bg-black antialiased" x-cloak
-      x-data="{  sidebarOpen: window.innerWidth > 768, collapsed: false }">
+      x-data="{  sidebarOpen: window.innerWidth > 768, collapsed: false }"
+>
 
 <div class="relative z-0 flex h-full w-full overflow-hidden min-h-screen">
     <button class="z-[9001] absolute top-0 left-0 cursor-pointer h-[28px] w-[28px] m-4 mt-[14px] mr-12"
@@ -53,6 +54,7 @@
 @livewire('wire-elements-modal')
 <x-livewire-alert::scripts/>
 
+@include('partials.copyblocks')
 
 </body>
 
