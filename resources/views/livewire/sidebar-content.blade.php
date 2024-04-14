7b255d9dfd7d5f845c7bf575796ea9c0c977c2c5
Christopher David <chris@arcadelabs.co>
2024-04-14 17:07:33 -0400

    almostfix

--- resources/views/livewire/sidebar-content.blade.php
+++ resources/views/livewire/sidebar-content.blade.php
@@ -1,15 +1,15 @@
 <div class="flex flex-col w-full relative z-50 h-full">
-    <div class="justify-between flex gap-2 items-center overflow-hidden z-50">
-        <div class="relative flex-1 text-right" x-data="{ dropdown: false }">
+    <div class="fixed top-0 left-0 right-0 z-50 bg-black py-2 px-4">
+        <div class="flex justify-end">
             <a href="/" wire:navigate>
-                <button class="mt-2 p-1.5 rounded-md text-white">
+                <button class="p-1.5 rounded-md text-white">
                     <x-icon.plus class="h-6 w-6"></x-icon.plus>
                 </button>
             </a>
         </div>
     </div>
 
-    <div class="flex flex-col gap-2 mt-8 py-4 px-1" @thread_updated="$refresh">
+    <div class="flex flex-col gap-2 mt-16 py-4 px-1" @thread_updated="$refresh">
         <ol>
             @foreach($threads as $thread)
                 <livewire:sidebar-thread :thread="$thread" :key="$thread->id"/>
