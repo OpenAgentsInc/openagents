a8a413db44ae098f17ef2acd1d2c34a8897ac102
Christopher David <chris@arcadelabs.co>
2024-04-15 11:08:54 -0500

    changelog moar

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -12,7 +12,7 @@
             <h1 class="mt-12 text-center">Changelog</h1>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-black shadow-md overflow-hidden sm:rounded-lg prose prose-invert">
-                <div class="mt-6 grid grid-cols-1 gap-12">
+                <div class="mt-6 grid grid-cols-1 gap-10">
 
                     <x-pane title="April 15, 2024" borderColor="border-darkgray">
                         <x-changelog-item code="https://github.com/OpenAgentsInc/openagents/pull/267">
@@ -36,6 +36,43 @@
                             Added chat model Satoshi 7B
                         </x-changelog-item>
                     </x-pane>
+
+                    <x-pane title="April 11, 2024" borderColor="border-darkgray">
+                        <x-changelog-item
+                                code="https://github.com/OpenAgentsInc/openagents/pull/260"
+                                post="https://twitter.com/OpenAgentsInc/status/1778420801253101652"
+                        >
+                            Added Cohere chat models Command R and R+
+                        </x-changelog-item>
+                    </x-pane>
+
+                    <x-pane title="April 10, 2024" borderColor="border-darkgray">
+                        <x-changelog-item
+                                code="https://github.com/OpenAgentsInc/openagents/pull/259"
+                                post="https://twitter.com/OpenAgentsInc/status/1778150350383440316"
+                        >
+                            Added Perplexity Sonar online chat models
+                        </x-changelog-item>
+                    </x-pane>
+
+                    <x-pane title="April 9, 2024" borderColor="border-darkgray">
+                        <x-changelog-item
+                                code="https://github.com/OpenAgentsInc/openagents/commit/76a0be7e3fc408726ec03226984ecab635338f10"
+                                post="https://twitter.com/OpenAgentsInc/status/1777863950014570549"
+                        >
+                            Added "majorly improved" GPT-4 Turbo model
+                        </x-changelog-item>
+                    </x-pane>
+
+                    <x-pane title="April 8, 2024" borderColor="border-darkgray">
+                        <x-changelog-item
+                                code="https://github.com/OpenAgentsInc/openagents/tree/e6ba003c4dfca4668a49527b1b268ea5d05b96ff"
+                                post="https://twitter.com/OpenAgentsInc/status/1777496991099998302"
+                        >
+                            Launch new chat interface
+                        </x-changelog-item>
+                    </x-pane>
+
 
                 </div>
                 <p class="text-sm text-center text-gray">
