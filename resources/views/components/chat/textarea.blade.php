36cae13dba9d58ae83d78796712d92094029dd74
Christopher David <chris@arcadelabs.co>
2024-04-14 20:59:59 -0400

    And fix the textbox staying big after submit

--- resources/views/components/chat/textarea.blade.php
+++ resources/views/components/chat/textarea.blade.php
@@ -6,10 +6,11 @@
     'minRows' => 1,
     'maxRows' => null,
     'default' => '',
+    'wireModel' => '',
 ])
 
 <div class="relative" x-data="{
-    inputVal: '',
+    inputVal: @entangle($attributes->wire('model')),
     isDisabled: true,
     height: 'auto',
     minRows: @js($minRows),
@@ -19,6 +20,7 @@
         this.$refs.textarea.style.height = 'auto';
         let newHeight = this.$refs.textarea.scrollHeight;
         let maxHeight = this.viewportMaxHeight;
+        console.log('Updating with new height', newHeight, 'and max height', maxHeight);
 
         if (this.maxRows !== null) {
             let maxRowsHeight = this.maxRows * this.lineHeight() + this.scrollbarWidth();
@@ -39,16 +41,24 @@
     },
     scrollbarWidth() {
         return this.$refs.textarea.offsetWidth - this.$refs.textarea.clientWidth;
+    },
+    resetHeight() {
+        console.log('trying to reset height')
+        this.$refs.textarea.style.height = '48px';
+        this.$refs.textarea.style.overflowY = 'hidden';
+        this.isDisabled = true;
     }
-}" x-init="update()"
+}" x-init="update()" wire:key="{{ $wireModel }}"
 >
 <textarea
         x-ref="textarea"
         autocomplete="off"
         spellcheck="false"
         wire:ignore
-        @input="update(); inputVal = $event.target.value; isDisabled = inputVal.length === 0; $wire.set('message_input', inputVal)"
+        @input="update(); isDisabled = inputVal.length === 0"
         placeholder="{{ $default }}"
+        x-model="inputVal"
+        x-effect="console.log('inputVal:', inputVal); if (inputVal === '') resetHeight()"
         :rows="minRows"
             {{ $attributes->merge([
                 'class' => "resize-none flex w-full rounded-md border-2 bg-transparent px-3 py-[0.65rem] pr-10 text-[16px] placeholder:text-[#777A81] focus-visible:outline-none focus-visible:ring-0 focus-visible:border-white focus-visible:ring-white " . ($hasError ? 'border-red' : 'border-[#3D3E42]') . " $className transition-all duration-300 ease-in-out",
@@ -66,4 +76,3 @@
         </button>
     @endif
 </div>
-
