a7a1794834ec3fb22fd7268980673222c65c992b
Christopher David <chris@arcadelabs.co>
2024-04-17 11:50:39 -0500

    Initial Plugins UI  (#273)
    * plugins UI
    
    * plugins-WIP
    
    * plugins-registry-UI-WIP
    
    * plugins-registry-UI-WIP
    
    * plugins-registry-UI-WIP-fix to placeholders
    
    * plugin_registry_UI-fixed_placeholders-WIP
    
    * plugin_reg_UI-target_blank_urls-WIP
    
    * clean_stuff-plugin_reg_UI_WIP
    
    * capitalize Plugins dir
    
    * move to controller
    
    * convert to vanilla blade
    
    ---------
    
    Co-authored-by: DJrOf <notanumber8333@gmail.com>
    Co-authored-by: DJrOf <danieljrofosu@gmail.com>

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -9,6 +9,32 @@ use Laravel\Jetstream\Jetstream;
 
 class StaticController extends Controller
 {
+    public function plugins(Request $request)
+    {
+        $plugins = [
+            [
+                'name' => 'RSS Feed Reader Plugin',
+                'description' => 'An RSS feed reader extism plugin',
+                'author' => 'Daniel Ofosu',
+                'url' => 'https://github.com/DJrOf/rss-feed-plugin',
+            ],
+            [
+                'name' => 'US Zip Code Information Plugin',
+                'description' => 'The US Zip Code Information plugin retrieves detailed information about a zip code in the United States using the Zippopotam API.',
+                'author' => 'McDonald Aladi',
+                'url' => 'https://github.com/moneya/plugin-zipcode-finder',
+            ],
+            [
+                'name' => 'Key-Value storage Plugin',
+                'description' => 'A Key-Value storage plugin for OpenAgents that is eventually-consistent on nostr using NIP-78.',
+                'author' => 'Riccardo Balbo',
+                'url' => 'https://github.com/riccardobl/openagents-plugins-kv',
+            ],
+        ];
+
+        return view('plugins', ['plugins' => $plugins]);
+    }
+
     public function changelog(Request $request)
     {
         return view('changelog');
---
+++ resources/views/plugins.blade.php
@@ -0,0 +1,40 @@
+<x-blog-layout>
+    <div class="pt-4 pb-24 bg-gray-100 dark:bg-gray-900">
+        <div class="flex flex-col items-center pt-6 sm:pt-0">
+            <a href="/" wire:navigate class="mt-12">
+                <x-icon.logo class="w-20 h-20 text-white"/>
+            </a>
+
+            <a href="/" wire:navigate>
+                <h3 class="text-[16px] fixed top-[18px] left-[24px] text-gray"> &larr; Back to chat</h3>
+            </a>
+
+            <h1 class="mt-12 text-center">Plugin Registry</h1>
+
+            <div class="mt-0 max-w-2xl">
+
+                @foreach ($plugins as $plugin)
+                    <div class="m-16">
+                        <x-pane title="{{$plugin['name']}}">
+                            <p>
+                                <b> Description: </b>
+                                <i>"{{$plugin['description']}}"</i>
+                            </p>
+
+                            <a href="{{$plugin['url']}}" target="_blank">
+                                <b>Source code: </b>
+                                {{$plugin['url']}}
+                            </a>
+
+                            <p>
+                                <b>Author: </b>
+                                {{$plugin['author']}}
+                            </p>
+                        </x-pane>
+                    </div>
+                @endforeach
+            </div>
+
+        </div>
+    </div>
+</x-blog-layout>
