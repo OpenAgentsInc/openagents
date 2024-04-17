66b82d4467a16aae1350b85d88a9d987fc4c249d
Christopher David <chris@arcadelabs.co>
2024-04-17 11:53:54 -0500

    changelog and plugin doc link

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -13,6 +13,13 @@
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-black shadow-md overflow-hidden sm:rounded-lg prose prose-invert">
                 <div class="mt-6 grid grid-cols-1 gap-10">
+                    <x-pane title="April 17, 2024" borderColor="border-darkgray">
+                        <x-changelog-item
+                                code="https://github.com/OpenAgentsInc/openagents/pull/273"
+                        >
+                            Added plugin registry
+                        </x-changelog-item>
+                    </x-pane>
 
                     <x-pane title="April 16, 2024" borderColor="border-darkgray">
                         <x-changelog-item
