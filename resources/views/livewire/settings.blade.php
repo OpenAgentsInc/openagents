efa75714d60aac1b4cd43e5fa9933c99bcdd0f54
Christopher David <chris@arcadelabs.co>
2024-04-19 14:35:36 -0700

    Add autoscroll setting (#283)
    * title selectnone
    
    * rename that migration
    
    * php artisan make:migration add_autoscroll_to_users_table --table=users
    
    * touch enable disable nice
    
    * works

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -1,9 +1,19 @@
 <div class="p-12 mx-auto flex flex-col justify-center w-full items-center" x-data="{ dropdown: false }">
     <div class="max-w-3xl min-w-[600px]">
-        <h3 class="mb-16 font-bold text-3xl text-center">Settings</h3>
+        <h3 class="mb-16 font-bold text-3xl text-center select-none">Settings</h3>
         <x-pane title="Default model for new chats">
-            <livewire:model-dropdown :selected-model="$this->selectedModel" :models="$models"
-                                     action="setDefaultModel"/>
+            <livewire:model-dropdown :selected-model="$this->selectedModel" :models="$models" action="setDefaultModel"/>
+        </x-pane>
+
+        <div class="my-12"/>
+
+        <x-pane title="Autoscroll to bottom in chats">
+            <div class="flex items-center justify-between cursor-pointer" wire:click="toggleAutoscroll">
+                <span>Autoscroll</span>
+                <div class="{{ $autoscroll ? 'bg-green-500' : 'bg-gray-200' }} rounded-full p-2">
+                    {{ $autoscroll ? 'ENABLED' : 'DISABLED' }}
+                </div>
+            </div>
         </x-pane>
     </div>
 </div>
