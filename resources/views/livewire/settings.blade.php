d130f9add876d0fa4a11842b71f5e8f8510cd92b
Christopher David <chris@arcadelabs.co>
2024-04-19 14:10:09 -0700

    title selectnone

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -1,6 +1,6 @@
 <div class="p-12 mx-auto flex flex-col justify-center w-full items-center" x-data="{ dropdown: false }">
     <div class="max-w-3xl min-w-[600px]">
-        <h3 class="mb-16 font-bold text-3xl text-center">Settings</h3>
+        <h3 class="mb-16 font-bold text-3xl text-center select-none">Settings</h3>
         <x-pane title="Default model for new chats">
             <livewire:model-dropdown :selected-model="$this->selectedModel" :models="$models"
                                      action="setDefaultModel"/>
