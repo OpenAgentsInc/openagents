94ee8a299f96649bf84870d346d769fd6228f879
Christopher David <chris@arcadelabs.co>
2024-04-19 11:41:32 -0700

    Add settings menu

--- resources/views/components/user-menu.blade.php
+++ resources/views/components/user-menu.blade.php
@@ -12,6 +12,10 @@
             <div x-popover:panel x-cloak x-transition x-show="open"
                  class="fixed right-[8px] mt-2 shadow-md text-white bg-black border border-darkgray"
                  style="border-radius: 3px !important;">
+                <a wire:navigate href="{{ route('settings') }}"
+                   class="block px-4 py-2 text-sm text-white hover:bg-white/20">
+                    Settings
+                </a>
                 <a href="/logout"
                    class="block px-4 py-2 text-sm text-white hover:bg-white/20">
                     Log out
