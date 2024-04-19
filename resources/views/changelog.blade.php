6ee91a3b0818f04a1a3b69088770e39988d9d97f
Christopher David <chris@arcadelabs.co>
2024-04-19 13:30:51 -0700

    fix that

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -16,7 +16,7 @@
                 <div class="mt-6 grid grid-cols-1 gap-10">
                     <x-pane title="April 19, 2024" borderColor="border-darkgray">
                         <x-changelog-item
-                                code="https://github.com/OpenAgentsInc/openagents/pull/280"
+                                code="https://github.com/OpenAgentsInc/openagents/pull/281"
                         >
                             Add Settings page; set default model
                         </x-changelog-item>
--- tests/Feature/Livewire/SettingsTest.php
+++ tests/Feature/Livewire/SettingsTest.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Livewire\Settings;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
+    $this->actingAs(User::factory()->create());
+
     Livewire::test(Settings::class)
         ->assertStatus(200);
 });
