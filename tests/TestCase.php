42af72ff494a1ada0ad048604f27aafab6665b15
Christopher David <chris@arcadelabs.co>
2024-04-19 07:10:15 -0700

    yeah try that

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    // define skipInProduction method
+    public function skipInProduction()
+    {
+        if (app()->environment('production')) {
+            $this->markTestSkipped('Skipping this test in production.');
+        }
+    }
 }
