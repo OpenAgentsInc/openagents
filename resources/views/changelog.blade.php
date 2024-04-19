141fce34de47e82c6b66524c9255961dcd1bc256
Christopher David <chris@arcadelabs.co>
2024-04-19 07:41:37 -0700

    revert to every-minute logic

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -14,6 +14,14 @@
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-black shadow-md overflow-hidden sm:rounded-lg prose prose-invert">
                 <div class="mt-6 grid grid-cols-1 gap-10">
+                    <x-pane title="April 19, 2024" borderColor="border-darkgray">
+                        <x-changelog-item
+                                code="https://github.com/OpenAgentsInc/openagents/pull/276"
+                        >
+                            Improve AI thread titles
+                        </x-changelog-item>
+                    </x-pane>
+
                     <x-pane title="April 18, 2024" borderColor="border-darkgray">
                         <x-changelog-item
                                 code="https://github.com/OpenAgentsInc/openagents/pull/276"
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,8 @@ abstract class TestCase extends BaseTestCase
     // define skipInProduction method
     public function skipInProduction()
     {
+        dump('env is '.app()->environment());
+
         if (app()->environment('production')) {
             $this->markTestSkipped('Skipping this test in production.');
         }
