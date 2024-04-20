bb89a6a053ab3f26a8dfcc1ef443652669c58f61
Christopher David <chris@arcadelabs.co>
2024-04-20 07:49:14 -0700

    fillable

--- database/migrations/2024_04_20_144458_add_lightning_address_to_users_table.php
+++ database/migrations/2024_04_20_144458_add_lightning_address_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('lightning_address')->nullable()->after('email');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('lightning_address');
         });
     }
 };
