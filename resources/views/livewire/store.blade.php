fc1bdf081fc777d542c9fce2eadc861165785a9b
Christopher David <chris@arcadelabs.co>
2024-04-19 20:08:38 -0700

    initial store layout

--- resources/views/livewire/store.blade.php
+++ resources/views/livewire/store.blade.php
@@ -1,8 +1,20 @@
 <div>
-    <div class="text-center mt-24 mx-auto w-[768px] p-[15px]">
-        <h1>Who would you like to<br/>speak with today?</h1>
+    <div class="px-4 py-2 flex flex-row justify-between">
+        <x-logomark size="2"/>
+        <x-login-buttons/>
+    </div>
 
-        <p class="text-gray">Discover, try, and create AI chat agents...<br/>
-            on the world’s <span class="italic">openest</span> open AI platform.</p>
+    <div class="w-[768px] mx-auto">
+        <div class="text-center mt-24 p-[15px]">
+            <h1>Who would you like to<br/>speak with today?</h1>
+
+            <p class="text-gray">Discover, try, and create AI chat agents...<br/>
+                on the world’s <span class="italic">openest</span> open AI platform.</p>
+        </div>
+
+        <h2 class="font-bold">LLMs</h2>
+        <p class="text-gray">Large language models (LLMs) are general-purpose chat agents that can help with a wide
+            variety
+            of tasks.</p>
     </div>
 </div>
