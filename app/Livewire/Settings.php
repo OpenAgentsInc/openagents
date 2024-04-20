50a13fe1b5b65f5d73de8040bfe9cdb7c1afc200
Christopher David <chris@arcadelabs.co>
2024-04-20 08:05:56 -0700

    User can add Lightning Address (#285)
    * php artisan make:migration add_lightning_address_to_users_table --table=users
    
    * fillable
    
    * add lightning address box
    
    * decent
    
    * require emailish
    
    * lightning address add ehehehe
    
    * changelog

--- app/Livewire/Settings.php
+++ app/Livewire/Settings.php
@@ -3,14 +3,20 @@
 namespace App\Livewire;
 
 use App\AI\Models;
+use Exception;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Settings extends Component
 {
+    use LivewireAlert;
+
     public $selectedModel;
 
-    public $autoscroll; // Add this line
+    public $autoscroll;
+
+    public $lightning_address;
 
     public $models = Models::MODELS;
 
@@ -20,8 +26,28 @@ class Settings extends Component
             return redirect('/');
         }
 
+        $this->lightning_address = auth()->user()->lightning_address;
+
         $this->selectedModel = auth()->user()->default_model ?? Models::getDefaultModel();
         $this->autoscroll = auth()->user()->autoscroll; // Initialize autoscroll
+
+    }
+
+    public function updateLightningAddress()
+    {
+        try {
+            $this->validate([
+                'lightning_address' => 'nullable|string|email',
+            ]);
+        } catch (Exception $e) {
+            $this->alert('error', 'Invalid lightning address');
+
+            return;
+        }
+
+        auth()->user()->update(['lightning_address' => $this->lightning_address]);
+
+        $this->alert('success', 'Updated Lightning address');
     }
 
     #[On('select-model')]
@@ -29,12 +55,14 @@ class Settings extends Component
     {
         auth()->user()->update(['default_model' => $modelKey]);
         $this->selectedModel = $modelKey;
+        $this->alert('success', 'Updated default model');
     }
 
     public function toggleAutoscroll() // Add this method
     {
         $this->autoscroll = ! $this->autoscroll;
         auth()->user()->update(['autoscroll' => $this->autoscroll]);
+        $this->alert('success', 'Updated autoscroll');
     }
 
     public function render()
