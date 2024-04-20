3499031537e84aac9bc070d33ad2a30a216f3774
Christopher David <chris@arcadelabs.co>
2024-04-20 08:00:56 -0700

    require emailish

--- app/Livewire/Settings.php
+++ app/Livewire/Settings.php
@@ -3,11 +3,15 @@
 namespace App\Livewire;
 
 use App\AI\Models;
+use Exception;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Settings extends Component
 {
+    use LivewireAlert;
+
     public $selectedModel;
 
     public $autoscroll;
@@ -26,15 +30,24 @@ class Settings extends Component
 
         $this->selectedModel = auth()->user()->default_model ?? Models::getDefaultModel();
         $this->autoscroll = auth()->user()->autoscroll; // Initialize autoscroll
+
     }
 
     public function updateLightningAddress()
     {
-        $this->validate([
-            'lightning_address' => 'nullable|string|email',
-        ]);
+        try {
+            $this->validate([
+                'lightning_address' => 'nullable|string|email',
+            ]);
+        } catch (Exception $e) {
+            $this->alert('error', 'Invalid lightning address');
+
+            return;
+        }
 
         auth()->user()->update(['lightning_address' => $this->lightning_address]);
+
+        $this->alert('success', 'Updated Lightning address');
     }
 
     #[On('select-model')]
@@ -42,12 +55,14 @@ class Settings extends Component
     {
         auth()->user()->update(['default_model' => $modelKey]);
         $this->selectedModel = $modelKey;
+        $this->alert('success', 'Updated default model');
     }
 
     public function toggleAutoscroll() // Add this method
     {
         $this->autoscroll = ! $this->autoscroll;
         auth()->user()->update(['autoscroll' => $this->autoscroll]);
+        $this->alert('success', 'Updated autoscroll');
     }
 
     public function render()
