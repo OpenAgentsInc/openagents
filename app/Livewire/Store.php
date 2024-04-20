ea15676a78d6ea5d97e97d7135fcbc94eda3831a
Christopher David <chris@arcadelabs.co>
2024-04-19 20:19:58 -0700

    initial ugly model cards

--- app/Livewire/Store.php
+++ app/Livewire/Store.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire;
 
+use App\AI\Models;
 use Livewire\Component;
 
 class Store extends Component
 {
+    public $models = Models::MODELS;
+
     public function render()
     {
         return view('livewire.store')
--- resources/views/livewire/store.blade.php
+++ resources/views/livewire/store.blade.php
@@ -1,3 +1,4 @@
+@php use App\AI\Models; @endphp
 <div>
     <div class="px-4 py-2 flex flex-row justify-between">
         <x-logomark size="2"/>
@@ -16,5 +17,25 @@
         <p class="text-gray">Large language models (LLMs) are general-purpose chat agents that can help with a wide
             variety
             of tasks.</p>
+
+        <!-- grid of two columns of cards -->
+        <div class="grid grid-cols-2 gap-4 mt-4">
+            @foreach($models as $modelKey => $modelDetails)
+                <div class="bg-darkgray p-4 rounded-lg">
+                    <h3 class="text-lg font-bold">{{ $modelDetails['name'] }}</h3>
+                    <p class="text-gray">Gateway: {{ $modelDetails['gateway'] }}</p>
+                    <p class="text-gray">Access: {{ $modelDetails['access'] }}</p>
+                    <p class="text-gray">Max Tokens: {{ $modelDetails['max_tokens'] }}</p>
+                    @php
+                        $userAccess = Models::getUserAccess();
+                        $indicator = Models::getModelIndicator($modelKey, $userAccess);
+                    @endphp
+                    @if($indicator)
+                        <span class="ml-auto text-gray-500">{{ $indicator }}</span>
+                    @endif
+                </div>
+            @endforeach
+        </div>
+
     </div>
 </div>
