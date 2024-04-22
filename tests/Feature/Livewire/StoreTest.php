01edad084fc6a6b206243e8df5b46d53d057cafd
Svemir Brkic <svemir@symplicity.com>
2024-04-22 11:03:34 -0400

    test: add more checks to existing livewire tests (#286)
    * test: add more checks to existing livewire tests
    
    * test: pass Guzzle client to SimpleInferencer for mocking
    
    Reactive inference feature test and remove unused get_previous_messages
    
    * refactor: make it possible to test GenerateThreadTitles

--- tests/Feature/Livewire/StoreTest.php
+++ tests/Feature/Livewire/StoreTest.php
@@ -5,5 +5,15 @@ use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(Store::class)
-        ->assertStatus(200);
+        ->assertStatus(200)
+        ->assertSeeInOrder([
+            'Who would you like to', 'speak with today?',
+            'Discover, try, and create AI chat agents...',
+            'LLMs',
+            'Llama 3 8B', 'Free',
+            'Mistral Large', 'Pro',
+            'GPT-4', 'Pro',
+            'Command-R', 'Free',
+            'Satoshi 7B', 'Free',
+        ]);
 });
