c838edcb57970361cbeb407236a6f3ccdababdc7
Christopher David <chris@arcadelabs.co>
2024-04-22 18:15:30 -0500

    Send prism payments via dashboard (#292)
    * row selector
    
    * good neough
    
    * clickstop
    
    * confirm delete users
    
    * php artisan make:migration add_prism_user_id_to_users_table --table=users
    
    * prism user id etc
    
    * get ready
    
    * a make:model PrismMultiPayment -mf
    
    * a make:model PrismSinglePayment -mf
    
    * lets do a test
    
    * does this work

--- app/Services/PrismService.php
+++ app/Services/PrismService.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\PrismMultiPayment;
+use App\Models\PrismSinglePayment;
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class PrismService
@@ -52,19 +55,61 @@ class PrismService
         return $response->json();
     }
 
-    public function sendPayment($amount, $currency, array $recipients)
+    public function sendPayment($amount, array $recipients)
     {
-        // Assuming $recipients is an array of lightning addresses
         $response = Http::withToken($this->apiKey)
             ->post("{$this->baseUrl}/payment/prism", [
                 //                'senderId' => '67c2cc15-d90f-4af5-b16f-06cebd9e8e5d', // atlantispleb
                 'senderId' => '68f5d9c3-9260-4fdc-b29f-8e5e8edcb849', // openagents
                 'amount' => $amount,
-                'currency' => $currency,
+                'currency' => 'SAT',
                 'prism' => $recipients,
             ]);
 
-        return $response->json();
+        $json = $response->json();
+
+        if ($response->status() === 200) {
+            $this->savePrism($json);
+        } else {
+            dd($json);
+        }
+
+        return $json;
+    }
+
+    public function savePrism($prismResponse)
+    {
+        try {
+            $prismMultiPayment = new PrismMultiPayment();
+            $prismMultiPayment->prism_id = $prismResponse['prismId'];
+            $prismMultiPayment->save();
+
+            foreach ($prismResponse['payments'] as $payment) {
+                $prismSinglePayment = new PrismSinglePayment();
+                $prismSinglePayment->payment_id = $payment['id'];
+                $prismSinglePayment->prism_multi_payment_id = $prismMultiPayment->id;
+                $prismSinglePayment->prism_id = $prismResponse['prismId'];
+                $prismSinglePayment->expires_at = $payment['expiresAt'];
+                $prismSinglePayment->sender_id = $payment['senderId'];
+                $prismSinglePayment->receiver_id = $payment['receiverId'];
+                $prismSinglePayment->amount_msat = $payment['amountMsat'];
+                $prismSinglePayment->status = $payment['status'];
+                $prismSinglePayment->resolved_at = $payment['resolvedAt'];
+                $prismSinglePayment->resolved = $payment['resolved'];
+                $prismSinglePayment->prism_payment_id = $payment['prismPaymentId'];
+                $prismSinglePayment->bolt11 = $payment['bolt11'];
+                $prismSinglePayment->preimage = $payment['preimage'];
+                $prismSinglePayment->failure_code = $payment['failureCode'];
+                $prismSinglePayment->type = $payment['type'];
+                $prismSinglePayment->reason = $payment['reason'];
+                $prismSinglePayment->save();
+            }
+        } catch (Exception $e) {
+            // Log the error
+            dd($e->getMessage());
+            // Return a response indicating failure
+        }
+
     }
 
     public function getTransactionDetails($transactionId)
---
+++ database/migrations/2024_04_22_225635_create_prism_single_payments_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prism_single_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('prism_multi_payment_id')->constrained();
+            $table->string('prism_id'); // prismId from the response
+            $table->string('payment_id'); // id from the payments array
+            //            $table->integer('created_at'); // createdAt from the response
+            //            $table->integer('updated_at')->nullable(); // updatedAt from the response
+            $table->integer('expires_at')->nullable(); // expiresAt from the response
+            $table->string('sender_id'); // senderId from the response
+            $table->string('receiver_id'); // receiverId from the response
+            $table->integer('amount_msat'); // amountMsat from the response
+            $table->string('status'); // status from the response
+            $table->integer('resolved_at')->nullable(); // resolvedAt from the response
+            $table->boolean('resolved'); // resolved from the response
+            $table->string('prism_payment_id')->nullable(); // prismPaymentId from the response
+            $table->string('bolt11')->nullable(); // bolt11 from the response
+            $table->string('preimage')->nullable(); // preimage from the response
+            $table->string('failure_code')->nullable(); // failureCode from the response
+            $table->string('type'); // type from the response
+            $table->string('reason')->nullable(); // reason from the response
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prism_single_payments');
+    }
+};
