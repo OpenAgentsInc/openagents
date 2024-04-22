ee78b4861c243de405ae457c05e4dd1fd8322d9d
Christopher David <chris@arcadelabs.co>
2024-04-22 12:45:58 -0500

    colors

--- resources/views/livewire/admin.blade.php
+++ resources/views/livewire/admin.blade.php
@@ -5,25 +5,25 @@
         <table class="min-w-full table-fixed divide-y divide-offblack text-white">
             <thead>
             <tr>
-                <th class="p-3 text-left text-sm font-semibold text-gray">
+                <th class="p-2 text-left text-sm font-semibold text-gray">
                     <div>ID</div>
                 </th>
-                <th class="p-3 text-left text-sm font-semibold text-gray">
+                <th class="p-2 text-left text-sm font-semibold text-gray">
                     <div>Name</div>
                 </th>
-                <th class="p-3 text-left text-sm font-semibold text-gray">
+                <th class="p-2 text-left text-sm font-semibold text-gray">
                     <div>Username</div>
                 </th>
-                <th class="p-3 text-left text-sm font-semibold text-gray">
+                <th class="p-2 text-left text-sm font-semibold text-gray">
                     <div># Messages</div>
                 </th>
-                <th class="p-3 text-left text-sm font-semibold text-gray">
+                <th class="p-2 text-left text-sm font-semibold text-gray">
                     <div>Pro</div>
                 </th>
-                <th class="p-3 text-left text-sm font-semibold text-gray">
+                <th class="p-2 text-left text-sm font-semibold text-gray">
                     <div>Joined</div>
                 </th>
-                <th class="p-3 text-left text-sm font-semibold text-gray">
+                <th class="p-2 text-left text-sm font-semibold text-gray">
                     {{-- Dropdown--}}
                 </th>
             </tr>
@@ -31,25 +31,24 @@
             <tbody class="divide-y divide-offblack bg-black text-gray">
             @foreach($users as $user)
                 <tr wire:key="{{ $user->id }}">
-                    <td class="whitespace-nowrap p-3 text-sm">{{ $user->id }}</td>
-                    <td class="whitespace-nowrap p-3 text-sm">{{ $user->name }}</td>
-                    <td class="whitespace-nowrap p-3 text-sm">{{ $user->username }}</td>
-                    <td class="whitespace-nowrap p-3 text-sm">{{ $user->messages_count }}</td>
-                    <td class="whitespace-nowrap p-3 text-sm">{{ $user->is_pro ? "Yes" : "No" }}</td>
-                    <td class="whitespace-nowrap p-3 text-sm">{{ $user->dateForHumans() }}</td>
-                    <td class="whitespace-nowrap p-3 text-sm">
+                    <td class="whitespace-nowrap p-2 text-sm">{{ $user->id }}</td>
+                    <td class="whitespace-nowrap p-2 text-sm">{{ $user->name }}</td>
+                    <td class="whitespace-nowrap p-2 text-sm">{{ $user->username }}</td>
+                    <td class="whitespace-nowrap p-2 text-sm">{{ $user->messages_count }}</td>
+                    <td class="whitespace-nowrap p-2 text-sm">{{ $user->is_pro ? "Yes" : "No" }}</td>
+                    <td class="whitespace-nowrap p-2 text-sm">{{ $user->dateForHumans() }}</td>
+                    <td class="whitespace-nowrap p-2 text-sm">
                         <div class="flex items-center justify-end">
                             <div x-data x-menu class="relative">
                                 <button x-menu:button
                                         class="flex items-center gap-2 pl-5 pr-3 py-2.5 rounded-md shadow">
                                     <span>...</span>
-
                                 </button>
 
                                 <div
                                         x-menu:items
                                         x-transition.origin.top.right
-                                        class="absolute right-0 w-48 mt-2 z-10 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-md py-1 outline-none"
+                                        class="absolute right-0 w-48 mt-2 z-10 origin-top-right bg-black border border-offblack divide-y divide-offblack rounded-md shadow-md py-1 outline-none"
                                         x-cloak
                                 >
                                     <a
@@ -98,22 +97,4 @@
             </tbody>
         </table>
     </div>
-
-
-    {{--    <table>--}}
-    {{--        <thead>--}}
-    {{--        <tr>--}}
-    {{--            <th>User</th>--}}
-    {{--            <th>Messages</th>--}}
-    {{--        </tr>--}}
-    {{--        </thead>--}}
-    {{--        <tbody>--}}
-    {{--        @foreach($users as $user)--}}
-    {{--            <tr>--}}
-    {{--                <td>{{ $user->name }}</td>--}}
-    {{--                <td>{{ $user->messages_count }}</td>--}}
-    {{--            </tr>--}}
-    {{--        @endforeach--}}
-    {{--        </tbody>--}}
-    {{--    </table>--}}
 </div>
