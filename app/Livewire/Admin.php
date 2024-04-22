a43046e8895df63bc448df20e314f19acee62fa6
Christopher David <chris@arcadelabs.co>
2024-04-22 10:43:38 -0500

    redir if not admin

---
+++ app/Livewire/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Admin extends Component
+{
+    public function mount()
+    {
+        if (! auth()->user() || ! auth()->user()->isAdmin()) {
+            return redirect()->route('home');
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.admin');
+    }
+}
