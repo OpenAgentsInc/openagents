f0453573c2829284c3dc52c6011b05e9da9a4100
Svemir Brkic <svemir@symplicity.com>
2024-04-22 11:03:34 -0400

    test: add more checks to existing livewire tests (#286)
    * test: add more checks to existing livewire tests
    
    * test: pass Guzzle client to SimpleInferencer for mocking
    
    Reactive inference feature test and remove unused get_previous_messages
    
    * refactor: make it possible to test GenerateThreadTitles

--- tests/Feature/Livewire/JoinTest.php
+++ tests/Feature/Livewire/JoinTest.php
@@ -5,5 +5,11 @@ use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(Join::class)
-        ->assertStatus(200);
+        ->assertStatus(200)
+        ->assertSeeHtml('>Join OpenAgents</h2>')
+        ->assertSee('Continue with X')
+        ->assertSee('Continue with Nostr')
+        ->assertSee('By continuing you agree to our')
+        ->assertSee('Terms of Service')
+        ->assertSee('Privacy Policy');
 });
