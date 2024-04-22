748081daff7d3b1e5bf711666049807a19981a44
Christopher David <chris@arcadelabs.co>
2024-04-22 13:21:00 -0500

    dont dupe alpine

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'OpenAgents' }}</title>
-    <script defer src="https://unpkg.com/@alpinejs/ui@3.13.8-beta.0/dist/cdn.min.js"></script>
-    <script defer src="https://unpkg.com/@alpinejs/focus@3.13.8/dist/cdn.min.js"></script>
+    <script defer src="https://unpkg.com/@alpinejs/ui@3.13.9-beta.0/dist/cdn.min.js"></script>
+    <script defer src="https://unpkg.com/@alpinejs/focus@3.13.9/dist/cdn.min.js"></script>
     @stack('scripts')
     @include('partials.vite')
     @include('partials.analytics')
