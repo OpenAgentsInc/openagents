3c35a7aa0aa90ab40b5c199f09f0950b095395aa
Christopher David <chris@arcadelabs.co>
2024-04-23 13:07:57 -0500

    Update prism status job (#295)
    * update the status of prisms ok
    
    * update prism status scheduled

---
+++ app/Console/Commands/UpdatePrismStatuses.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\PrismSinglePayment;
+use App\Services\PrismService;
+use Illuminate\Console\Command;
+
+class UpdatePrismStatuses extends Command
+{
+    protected $signature = 'prism:update';
+
+    protected $description = 'Update prism statuses';
+
+    public function handle()
+    {
+        // Fetch all PrismSinglePayments where status is 'sending'
+        $payments = PrismSinglePayment::where('status', 'sending')->get();
+
+        dump('Updating '.$payments->count().' payments...');
+
+        $prismService = new PrismService();
+
+        // loop through them
+        foreach ($payments as $payment) {
+            // Assume we have a PrismService class with a method to check the status of a payment
+            $wat = $prismService->getTransactionDetails($payment->payment_id);
+
+            // If status is different than the one we have, update it
+            if ($wat['status'] !== $payment->status) {
+                $this->info('Updating payment '.$payment->payment_id.' to status '.$wat['status']);
+                $payment->update(['status' => $wat['status']]);
+            }
+
+            sleep(1);
+        }
+    }
+}
