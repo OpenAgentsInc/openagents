0d09e8cbea4473d5abdb726b672c5984496b8efd
Moneya <moneya59@gmail.com>
2024-04-24 18:17:56 +0100

    finished the webhook setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'stripe/*',
+            'webhook/nostra',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
