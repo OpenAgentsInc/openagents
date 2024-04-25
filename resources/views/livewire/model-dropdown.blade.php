55f111d2f9906dc940a969f040e2770a27c1f5f6
Christopher David <chris@arcadelabs.co>
2024-04-25 12:59:50 -0500

    add exploreagents flag to modeldropdown

--- resources/views/livewire/model-dropdown.blade.php
+++ resources/views/livewire/model-dropdown.blade.php
@@ -12,6 +12,14 @@
              class="mt-3 -ml-[125px] fixed z-[50] divide-y divide-white/15 min-w-60 shadow-md rounded-lg p-2 bg-black border border-white/25"
              aria-labelledby="hs-dropdown-with-header">
             <div class="py-0 first:pt-0 last:pb-0 bg-black">
+                @if($showAgents)
+                    <a
+                            class="flex items-center gap-x-3.5 py-1 px-3 rounded-lg text-sm focus:ring-2 focus:ring-gray-500 text-gray-400 hover:text-gray-400 hover:bg-white/15">
+                        <div class="flex flex-col">
+                            <span class="text-indigo-50 my-0 text-sm">Explore agents</span>
+                        </div>
+                    </a>
+                @endif
                 @foreach($models as $modelKey => $modelName)
                     @php
                         $userAccess = $this->getUserAccess();
