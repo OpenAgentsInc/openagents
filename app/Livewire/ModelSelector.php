96d2d01fbf5be81b9c1aad0f496a5c5e3bd7e280
Christopher David <chris@arcadelabs.co>
2024-04-25 12:26:35 -0500

    Fix newchat & cleanup old modelselect code (#299)
    * fix newchat button
    
    * clean up modelselector
    
    * selectedmodel once only

--- app/Livewire/ModelSelector.php
+++ app/Livewire/ModelSelector.php
@@ -3,14 +3,11 @@
 namespace App\Livewire;
 
 use App\AI\Models;
-use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ModelSelector extends Component
 {
     public $selectedModel = '';
-
-    public $formattedModel = '';
 
     public $models;
 
@@ -22,62 +19,13 @@ class ModelSelector extends Component
 
         $this->selectedModel = Models::getModelForThread($this->thread);
 
-        $this->formattedModel = Models::getModelName($this->selectedModel);
-    }
-
-    #[On('model-selected')]
-    public function selectModel($model)
-    {
-        dd("selected model: $model");
-        $userAccess = $this->getUserAccess();
-
-        // If the user is not logged in, show the login modal for any model they don't have access to
-        if ($userAccess === 'guest' && ! $this->hasModelAccess($model, $userAccess)) {
-            $this->dispatch('openModal', 'auth.join');
-
-            return;
+        if (session()->has('selectedModel')) {
+            session()->forget('selectedModel');
         }
-
-        // Check if the selected model requires "pro" access
-        if ($this->isProModelSelected($model)) {
-            // If the user is logged in but not a "pro" user, show the upgrade modal
-            if ($userAccess !== 'pro') {
-                $this->dispatch('openModal', 'modals.upgrade');
-
-                return;
-            }
-        }
-
-        // If the user has access to the selected model, update the selected model
-        if ($this->hasModelAccess($model, $userAccess)) {
-            $this->selectedModel = $model;
-            $this->formattedModel = Models::getModelName($model);
-            $this->dispatch('select-model', $model);
-        }
-    }
-
-    protected function getUserAccess()
-    {
-        return Models::getUserAccess();
-    }
-
-    protected function hasModelAccess($model, $userAccess)
-    {
-        return Models::hasModelAccess($model, $userAccess);
-    }
-
-    protected function isProModelSelected($model)
-    {
-        return Models::isProModelSelected($model);
     }
 
     public function render()
     {
         return view('livewire.model-selector');
-    }
-
-    protected function getModelIndicator($model, $userAccess)
-    {
-        return Models::getModelIndicator($model, $userAccess);
     }
 }
