af8d8105058313d50206d201e4e03f79bb42a127
Christopher David <chris@arcadelabs.co>
2024-04-25 15:18:12 -0500

    agent create button and fields

--- resources/views/livewire/agents/create.blade.php
+++ resources/views/livewire/agents/create.blade.php
@@ -10,18 +10,20 @@
                 <div class="col-span-full flex items-center gap-x-8 my-5">
                     @if ($image)
                         <img src="{{ $image->temporaryUrl() }}"
-                            class="h-24 w-24 flex-none rounded-lg bg-gray-800 object-cover">
+                             class="h-24 w-24 flex-none rounded-lg bg-gray-800 object-cover">
                     @else
                         <img src="{{ url('/images/no-image.jpg') }}" alt=""
-                            class="h-24 w-24 flex-none rounded-lg bg-gray-800 object-cover">
+                             class="h-24 w-24 flex-none rounded-lg bg-gray-800 object-cover">
                     @endif
 
                     <div>
                         <button type="button" x-on:click="$refs.imageUpload.click()"
-                            class="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">Change
-                            image</button>
+                                class="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">
+                            Change
+                            image
+                        </button>
                         <input type="file" x-ref="imageUpload" class="hidden" accept="image/jpg, image/png"
-                            wire:model="image">
+                               wire:model="image">
                         <p class="mt-2 text-xs leading-5 text-gray-400">JPG, PNG. 2MB max.</p>
                         @error('image')
                         <div class="my-2">
@@ -34,64 +36,64 @@
                 <div class="mt-5">
                     <label for="name">Name</label>
                     <x-input id="name" class="block mt-1 w-full " type="text" name="name" wire:model='name'
-                        required placeholder="Agent name" />
+                             required placeholder="Name your agent"/>
                     @error('name')
-                        <span class="text-red mt-2 text-xs">{{ $message }}</span>
+                    <span class="text-red mt-2 text-xs">{{ $message }}</span>
                     @enderror
                 </div>
 
                 <div class="mt-5">
                     <label for="about"> About</label>
-                    <x-textarea wire:model='about' placeholder="about" id="about" class="block mt-1 w-full"
-                        rows="1" min-rows="1" name="about" required />
+                    <x-textarea wire:model='about' placeholder="Add a short description about what this agent does"
+                                id="about" class="block mt-1 w-full"
+                                rows="1" min-rows="1" name="about" required/>
                     @error('about')
-                        <span class="text-red mt-2 text-xs">{{ $message }}</span>
+                    <span class="text-red mt-2 text-xs">{{ $message }}</span>
+                    @enderror
+                </div>
+                
+                <div class="mt-5">
+                    <label for="prompt"> Instructions</label>
+                    <x-textarea wire:model='prompt'
+                                placeholder="What does this agent do? How does it behave? What should it avoid doing?"
+                                id="about" class="block mt-1 w-full" rows="5" min-rows="5" name="prompt"
+                                required/>
+                    @error('prompt')
+                    <span class="text-red mt-2 text-xs">{{ $message }}</span>
                     @enderror
                 </div>
 
                 <div class="mt-5">
                     <label for="message"> Welcome Message</label>
-                    <x-textarea wire:model='message' placeholder="How this agent starts conversation." id="message"
-                        class="block mt-1 w-full" rows="5" min-rows="5" name="message" required />
+                    <x-textarea wire:model='message' placeholder="How this agent starts conversations" id="message"
+                                class="block mt-1 w-full" rows="5" min-rows="5" name="message" required/>
                     @error('message')
-                        <span class="text-red mt-2 text-xs">{{ $message }}</span>
-                    @enderror
-                </div>
-
-                <div class="mt-5">
-                    <label for="prompt"> Instructions</label>
-                    <x-textarea wire:model='prompt'
-                        placeholder="What does this agent do? How does it behave? What should it avoid doing?"
-                        id="about" class="block mt-1 w-full" rows="5" min-rows="5" name="prompt"
-                        required />
-                    @error('prompt')
-                        <span class="text-red mt-2 text-xs">{{ $message }}</span>
+                    <span class="text-red mt-2 text-xs">{{ $message }}</span>
                     @enderror
                 </div>
 
                 <div class="mt-5">
                     <label for="rag_prompt"> RAG Prompt</label>
                     <x-textarea wire:model='rag_prompt'
-                        placeholder="Add your RAG prompt?"
-                        id="rag_prompt" class="block mt-1 w-full" rows="5" min-rows="5" name="rag_prompt"
-                        required />
+                                placeholder="Add your RAG prompt?"
+                                id="rag_prompt" class="block mt-1 w-full" rows="5" min-rows="5" name="rag_prompt"
+                                required/>
                     @error('rag_prompt')
-                        <span class="text-red mt-2 text-xs">{{ $message }}</span>
+                    <span class="text-red mt-2 text-xs">{{ $message }}</span>
                     @enderror
                 </div>
-
-
 
 
                 <div class="my-5">
                     <label for="files"> Documents</label>
 
                     @error('files.*')
-                        <span class="error">{{ $message }}</span>
+                    <span class="error">{{ $message }}</span>
                     @enderror
                     <x-filepond ref="myFilepond" wire:model="files" multiple allowFileTypeValidation
-                        imagePreviewMaxHeight="300"  acceptedFileTypes="['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'text/csv', 'text/plain']"
-                        allowFileSizeValidation maxFileSize="10MB" />
+                                imagePreviewMaxHeight="300"
+                                acceptedFileTypes="['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'text/csv', 'text/plain']"
+                                allowFileSizeValidation maxFileSize="10MB"/>
                 </div>
 
                 <div class="mt-5 flex justify-end">
