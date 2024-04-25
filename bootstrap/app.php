bc20e50533d3023c508315e15911175ce2529354
moneya <moneya@users.noreply.github.com>
2024-04-25 16:48:32 +0100

    Agents Task (#293)
    * added documents API
    
    * replaced public disk with S3 wrong disk
    
    * fix: avoid false negatives in PR test runs (#287)
    
    Posting comments from PRs coming from forks is proving too complicated.
    Use regular action error messages instead.
    
    * test: add more checks to existing livewire tests (#286)
    
    * test: add more checks to existing livewire tests
    
    * test: pass Guzzle client to SimpleInferencer for mocking
    
    Reactive inference feature test and remove unused get_previous_messages
    
    * refactor: make it possible to test GenerateThreadTitles
    
    * Initial admin panel w user message counts (#288)
    
    * admin menu for me
    
    * redir if not admin
    
    * axe nostr testing routes
    
    * admin layout
    
    * whos doin what
    
    * Better user list (#289)
    
    * nice basic user table
    
    * name username
    
    * fix ispro attribute
    
    * joined date for huamns
    
    * Delete user (#290)
    
    * colors ok and dropdown
    
    * colors
    
    * nice
    
    * nicer dropdown
    
    * delete individual
    
    * and email/lnaddress
    
    * dont dupe alpine
    
    * Mass delete users (#291)
    
    * dark checkbox
    
    * delete button
    
    * mass delete
    
    * mass delete
    
    * show num selected
    
    * Send prism payments via dashboard (#292)
    
    * row selector
    
    * good neough
    
    * clickstop
    
    * confirm delete users
    
    * php artisan make:migration add_prism_user_id_to_users_table --table=users
    
    * prism user id etc
    
    * get ready
    
    * a make:model PrismMultiPayment -mf
    
    * a make:model PrismSinglePayment -mf
    
    * lets do a test
    
    * does this work
    
    * add support for error handling
    
    ---------
    
    Co-authored-by: Svemir Brkic <svemir@symplicity.com>
    Co-authored-by: Christopher David <chris@arcadelabs.co>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +18,84 @@ return Application::configure(basePath: dirname(__DIR__))
             'stripe/*',
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    ->withExceptions(function ( Exceptions $exceptions) {
+        $exceptions->render(function (TokenMismatchException $e, Request $request) {
+
+                if ($request->expectsJson()) {
+                    return response()->json([
+                        'status' => 'error',
+                        'message' => 'Token mismatch',
+                        'data' => null,
+                        'error' => 'Your session has expired. Please login and try again.',
+                    ], 419);
+                }
+
+        });
+
+        $exceptions->render( function(NotFoundHttpException $e, Request $request){
+
+
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Resource not found',
+                    'data' => NULL,
+                    'error' => 'The requested resource could not be found on this server.',
+                ], 404);
+            }
+        });
+
+
+        $exceptions->render( function(ModelNotFoundException $e, Request $request){
+
+
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'No record found',
+                    'data' => NULL,
+                    'error' => 'No records found for the query',
+                ], 404);
+            }
+        });
+
+
+        $exceptions->render( function(QueryException $e, Request $request){
+
+
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Query error',
+                    'data' => null,
+                    'error' => 'There was an error executing a query.',
+                ], 500);
+            }
+        });
+
+
+        $exceptions->render( function(PDOException $e, Request $request){
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Database connection error',
+                    'data' => null,
+                    'error' => 'There was an error establishing connection.',
+                ], 500);
+            }
+        });
+
+        $exceptions->render( function(MethodNotAllowedHttpException $e, Request $request){
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Method not allowed',
+                    'data' => null,
+                    'error' => 'The requested HTTP method is not allowed for this URL.',
+                ], 405);
+            }
+        });
+
+
+    })
+    ->create();
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+require __DIR__.'/api/v1.php';
+
