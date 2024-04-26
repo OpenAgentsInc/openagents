eff05883ad5472b1e657c59684a725a9750f53ae
Christopher David <chris@arcadelabs.co>
2024-04-26 13:34:39 -0500

    assert thar

--- tests/Feature/GrpcTest.php
+++ tests/Feature/GrpcTest.php
@@ -6,5 +6,6 @@ test('can connect to nostr service via grpc', function () {
     $controller = new NostrGrpcController();
     $res = $controller->requestContext('oa.forkforge.net:5000', 'Testing');
 
-    dump($res);
+    // Assert res is a string at least 30 characters long
+    expect($res)->toBeString()->toBeGreaterThan(30);
 });
