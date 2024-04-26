d041e9900f2b4d09fb16ad29c0f7bc4204ae81fe
DJrOf <93095163+DJrOf@users.noreply.github.com>
2024-04-26 16:38:02 +0200

    Nostr grpc connection (#304)
    * WIP Nostr GRPC client
    
    * code clean up
    
    * solved the nostr issue
    
    * created function to be exported to the service class
    
    * added support for encrypted custom provider
    
    * check if the documents array exists
    
    * add empty array  variable
    
    ---------
    
    Co-authored-by: Moneya <moneya59@gmail.com>

---
+++ app/Grpc/nostr/PoolConnectorClient.php
@@ -0,0 +1,382 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace App\Grpc\nostr;
+
+/**
+ */
+class PoolConnectorClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * job management
+     * @param \App\Grpc\nostr\RpcRequestJob $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function requestJob(\App\Grpc\nostr\RpcRequestJob $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/requestJob',
+        $argument,
+        ['\App\Grpc\nostr\Job', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcGetJob $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function getJob(\App\Grpc\nostr\RpcGetJob $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/getJob',
+        $argument,
+        ['\App\Grpc\nostr\Job', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcGetPendingJobs $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function getPendingJobs(\App\Grpc\nostr\RpcGetPendingJobs $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/getPendingJobs',
+        $argument,
+        ['\App\Grpc\nostr\PendingJobs', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcGetJob $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function isJobDone(\App\Grpc\nostr\RpcGetJob $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/isJobDone',
+        $argument,
+        ['\App\Grpc\nostr\RpcIsJobDone', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcAcceptJob $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function acceptJob(\App\Grpc\nostr\RpcAcceptJob $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/acceptJob',
+        $argument,
+        ['\App\Grpc\nostr\Job', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcCancelJob $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function cancelJob(\App\Grpc\nostr\RpcCancelJob $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/cancelJob',
+        $argument,
+        ['\App\Grpc\nostr\Job', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcJobOutput $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function outputForJob(\App\Grpc\nostr\RpcJobOutput $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/outputForJob',
+        $argument,
+        ['\App\Grpc\nostr\Job', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcJobComplete $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function completeJob(\App\Grpc\nostr\RpcJobComplete $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/completeJob',
+        $argument,
+        ['\App\Grpc\nostr\Job', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcJobLog $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function logForJob(\App\Grpc\nostr\RpcJobLog $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/logForJob',
+        $argument,
+        ['\App\Grpc\nostr\Job', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * discovery
+     * @param \App\Grpc\nostr\RpcAnnounceNodeRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function announceNode(\App\Grpc\nostr\RpcAnnounceNodeRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/announceNode',
+        $argument,
+        ['\App\Grpc\nostr\RpcAnnounceNodeResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcAnnounceTemplateRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function announceEventTemplate(\App\Grpc\nostr\RpcAnnounceTemplateRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/announceEventTemplate',
+        $argument,
+        ['\App\Grpc\nostr\RpcAnnounceTemplateResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * generic nostr events
+     * @param \App\Grpc\nostr\RpcSendSignedEventRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function sendSignedEvent(\App\Grpc\nostr\RpcSendSignedEventRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/sendSignedEvent',
+        $argument,
+        ['\App\Grpc\nostr\RpcSendSignedEventResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcSubscribeToEventsRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function subscribeToEvents(\App\Grpc\nostr\RpcSubscribeToEventsRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/subscribeToEvents',
+        $argument,
+        ['\App\Grpc\nostr\RpcSubscribeToEventsResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcUnsubscribeFromEventsRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function unsubscribeFromEvents(\App\Grpc\nostr\RpcUnsubscribeFromEventsRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/unsubscribeFromEvents',
+        $argument,
+        ['\App\Grpc\nostr\RpcUnsubscribeFromEventsResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcGetEventsRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function getEvents(\App\Grpc\nostr\RpcGetEventsRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/getEvents',
+        $argument,
+        ['\App\Grpc\nostr\RpcGetEventsResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * blob storage
+     * @param \App\Grpc\nostr\RpcCreateDiskRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function createDisk(\App\Grpc\nostr\RpcCreateDiskRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/createDisk',
+        $argument,
+        ['\App\Grpc\nostr\RpcCreateDiskResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcOpenDiskRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function openDisk(\App\Grpc\nostr\RpcOpenDiskRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/openDisk',
+        $argument,
+        ['\App\Grpc\nostr\RpcOpenDiskResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcCloseDiskRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function closeDisk(\App\Grpc\nostr\RpcCloseDiskRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/closeDisk',
+        $argument,
+        ['\App\Grpc\nostr\RpcCloseDiskResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcDiskDeleteFileRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function diskDeleteFile(\App\Grpc\nostr\RpcDiskDeleteFileRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/diskDeleteFile',
+        $argument,
+        ['\App\Grpc\nostr\RpcDiskDeleteFileResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcDiskListFilesRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function diskListFiles(\App\Grpc\nostr\RpcDiskListFilesRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/diskListFiles',
+        $argument,
+        ['\App\Grpc\nostr\RpcDiskListFilesResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcDiskReadFileRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\ServerStreamingCall
+     */
+    public function diskReadFile(\App\Grpc\nostr\RpcDiskReadFileRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_serverStreamRequest('/PoolConnector/diskReadFile',
+        $argument,
+        ['\App\Grpc\nostr\RpcDiskReadFileResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcDiskReadFileRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function diskReadSmallFile(\App\Grpc\nostr\RpcDiskReadFileRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/diskReadSmallFile',
+        $argument,
+        ['\App\Grpc\nostr\RpcDiskReadFileResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\ClientStreamingCall
+     */
+    public function diskWriteFile($metadata = [], $options = []) {
+        return $this->_clientStreamRequest('/PoolConnector/diskWriteFile',
+        ['\App\Grpc\nostr\RpcDiskWriteFileResponse','decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcDiskWriteFileRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function diskWriteSmallFile(\App\Grpc\nostr\RpcDiskWriteFileRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/PoolConnector/diskWriteSmallFile',
+        $argument,
+        ['\App\Grpc\nostr\RpcDiskWriteFileResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * cache
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\ClientStreamingCall
+     */
+    public function cacheSet($metadata = [], $options = []) {
+        return $this->_clientStreamRequest('/PoolConnector/cacheSet',
+        ['\App\Grpc\nostr\RpcCacheSetResponse','decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \App\Grpc\nostr\RpcCacheGetRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\ServerStreamingCall
+     */
+    public function cacheGet(\App\Grpc\nostr\RpcCacheGetRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_serverStreamRequest('/PoolConnector/cacheGet',
+        $argument,
+        ['\App\Grpc\nostr\RpcCacheGetResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
---
+++ app/Http/Controllers/NostrGrpcController.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Grpc\nostr\JobInput;
+use App\Grpc\nostr\JobParam;
+use Illuminate\Http\Request;
+use App\Grpc\nostr\RpcRequestJob;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
+use Google\Protobuf\Internal\GPBType;
+use App\Grpc\nostr\PoolConnectorClient;
+use Google\Protobuf\Internal\RepeatedField;
+use App\Grpc\nostr\RpcSendSignedEventRequest;
+use Grpc\Internal\InterceptorChannel;
+
+
+
+class NostrGrpcController extends Controller
+{
+
+    public function requestContext($poolAddress, $query, $documents = [], $k = 1, $max_tokens = 512, $overlap = 128, $encryptFor = "")
+    {
+
+        $currentime =  now();
+        $expiresAt = $currentime->addMinutes(10);
+
+        // Create a new instance of RpcRequestJob
+        $requestJob = new RpcRequestJob();
+
+        // Set the runOn field
+        $requestJob->setRunOn('openagents/embeddings');
+
+        // Set the expireAfter field (e.g., 10 minutes from now)
+        $requestJob->setExpireAfter($expiresAt);
+
+        $inputs = [];
+
+        if ($documents != []) {
+
+            // Set the input field
+            foreach ($documents as $document) {
+                $input = new JobInput();
+                $input->setData($document);
+                $input->setType('url');
+                $input->setMarker('passage');
+                $inputs[] = $input;
+            }
+        }
+
+
+        $inputq = new JobInput();
+        $inputq->setData($query);
+        $inputq->setType('text');
+        $inputq->setMarker('query');
+        $inputs[] = $inputq;
+
+
+        $requestJob->setInput($inputs);
+
+        // Set the param field
+        $param1 = new JobParam();
+        $param1->setKey('max-tokens');
+        $param1->setValue(["$max_tokens"]);
+
+        $param2 = new JobParam();
+        $param2->setKey('overlap');
+        $param2->setValue(["$overlap"]);
+
+        $param3 = new JobParam();
+        $param3->setKey('quantize');
+        $param3->setValue(['true']);
+
+        $param4 = new JobParam();
+        $param4->setKey('k');
+        $param4->setValue(["$k"]);
+
+
+        // Set the RepeatedField to the 'param' field of the requestJob message
+        $requestJob->setParam([$param1, $param2, $param3, $param4]);
+
+        // Set the description field
+        $requestJob->setDescription('RAG pipeline');
+
+        // Set the kind field (optional)
+        $requestJob->setKind(5003);
+
+        // Set the outputFormat field
+        $requestJob->setOutputFormat('application/json');
+
+        // encrypt for a specific provider
+        if ($encryptFor != null) {
+            $requestJob->setEncrypted(true);
+            $requestJob->setRequestProvider($encryptFor);
+        }
+
+        $opts = [
+            'credentials' => \Grpc\ChannelCredentials::createSsl(),
+        ];
+        $hostname = $poolAddress;
+        $res = new PoolConnectorClient($hostname, $opts);
+        $metadata = [];
+        $options = [];
+        $Jobres = $res->requestJob($requestJob, $metadata, $options);
+        $result = $Jobres->wait();
+        $status = $result[1]->code;
+        if ($status !== 0) {
+            throw new \Exception($result[1]->details);
+        }
+        // get the thread_id and job_id to nostrJob model
+        $job_id = $result[0]->id;
+
+        // return $result;
+        return $job_id;
+    }
+
+
+    public function handleJobRequest(Request $request)
+    {
+
+
+        // Create a new instance of RpcRequestJob
+        $requestJob = new RpcRequestJob();
+
+        // Set the runOn field
+        $requestJob->setRunOn('openagents/embeddings');
+
+        // Set the expireAfter field (e.g., 1 hour)
+        $requestJob->setExpireAfter(3600);
+
+        // Set the input field
+        $input1 = new JobInput();
+        $input1->setData('What is the color of the fox?');
+        $input1->setType('text');
+        $input1->setMarker('query');
+
+        $input2 = new JobInput();
+        $input2->setData('The quick brown fox jumps over the lazy dog.');
+        $input1->setType('text');
+        $input2->setMarker('passage');
+
+
+        $requestJob->setInput([$input1, $input2]);
+
+        // Set the param field
+        $param1 = new JobParam();
+        $param1->setKey('max-tokens');
+        $param1->setValue(['512']);
+
+        $param2 = new JobParam();
+        $param2->setKey('overlap');
+        $param2->setValue(['128']);
+
+        $param3 = new JobParam();
+        $param3->setKey('quantize');
+        $param3->setValue(['true']);
+
+
+        // Set the RepeatedField to the 'param' field of the requestJob message
+        $requestJob->setParam([$param1, $param2, $param3]);
+
+        // Set the description field
+        $requestJob->setDescription('Embedding generation job');
+
+        // Set the kind field (optional)
+        $requestJob->setKind(5003);
+
+        // Set the outputFormat field
+        $requestJob->setOutputFormat('application/json');
+
+
+
+
+        try {
+            $opts = [
+                'credentials' => \Grpc\ChannelCredentials::createSsl(),
+            ];
+            $hostname = "openagents.forkforge.net:5000";
+            $res = new PoolConnectorClient($hostname, $opts);
+            // $response->sendSignedEvent($requestEvent);
+            $metadata = [];
+            $options = [];
+            $Jobres = $res->requestJob($requestJob, $metadata, $options);
+            $result = $Jobres->wait();
+            $status = $result[1]->code;
+            if ($status !== 0) {
+                throw new \Exception($result[1]->details);
+            }
+            // get the thread_id and job_id to nostrJob model
+            $job_id = $result[0]->id;
+
+            return $result;
+
+            // return $result[1]->code;
+
+        } catch (\Exception $e) {
+            Log::error($e);
+        }
+    }
+}
