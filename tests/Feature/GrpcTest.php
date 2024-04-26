bd67d71fc994085a4bd8be2ef3a8138424b428c0
Moneya <moneya59@gmail.com>
2024-04-27 00:26:24 +0100

    split tests assertions from one liner

--- tests/Feature/GrpcTest.php
+++ tests/Feature/GrpcTest.php
@@ -7,13 +7,17 @@ test('can connect to nostr service via grpc', function () {
     $res = $service->requestContext('oa.forkforge.net:5000', 'Testing');
 
     // Assert res is a string at least 30 characters long
-    expect($res)->toBeString()->toBeGreaterThan(30);
+    // expect($res)->toBeString()->toBeGreaterThan(30);
+    expect($res)->toBeString();
+    expect(strlen($res))->toBeGreaterThan(30);
 });
 
 test('testing for RAG request', function () {
     $documents = ['https://bitcoin.org/bitcoin.pdf'];
     $service = new NostrService();
-    $res = $service->requestContext('oa.forkforge.net:5000', 'Who is satoshi', $documents, 1,256, 128,'');
-     // Assert res is a string at least 30 characters long
-     expect($res)->toBeString()->toBeGreaterThan(30);
+    $res = $service->requestContext('oa.forkforge.net:5000', 'Who is satoshi', $documents, 1, 256, 128, '');
+    // Assert res is a string at least 30 characters long
+    // expect($res)->toBeString()->toBeGreaterThan(30);
+    expect($res)->toBeString();
+    expect(strlen($res))->toBeGreaterThan(30);
 });
