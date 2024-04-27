7024f601035fc62f1d2b6eb7ec903d97da44713e
moneya <moneya@users.noreply.github.com>
2024-04-27 18:06:52 +0100

    NostrGrpc service and Test updated (#307)
    * added nostr service class
    
    * imported class
    
    * split tests assertions from one liner

--- tests/Feature/GrpcTest.php
+++ tests/Feature/GrpcTest.php
@@ -1,11 +1,23 @@
 <?php
 
-use App\Http\Controllers\NostrGrpcController;
+use App\Services\NostrService;
 
 test('can connect to nostr service via grpc', function () {
-    $controller = new NostrGrpcController();
-    $res = $controller->requestContext('oa.forkforge.net:5000', 'Testing');
+    $service = new NostrService();
+    $res = $service->requestContext('oa.forkforge.net:5000', 'Testing');
 
     // Assert res is a string at least 30 characters long
-    expect($res)->toBeString()->toBeGreaterThan(30);
-})->skip();
+    // expect($res)->toBeString()->toBeGreaterThan(30);
+    expect($res)->toBeString();
+    expect(strlen($res))->toBeGreaterThan(30);
+});
+
+test('testing for RAG request', function () {
+    $documents = ['https://bitcoin.org/bitcoin.pdf'];
+    $service = new NostrService();
+    $res = $service->requestContext('oa.forkforge.net:5000', 'Who is satoshi', $documents, 1, 256, 128, '');
+    // Assert res is a string at least 30 characters long
+    // expect($res)->toBeString()->toBeGreaterThan(30);
+    expect($res)->toBeString();
+    expect(strlen($res))->toBeGreaterThan(30);
+});
