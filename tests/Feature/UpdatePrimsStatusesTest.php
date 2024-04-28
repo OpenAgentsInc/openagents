da1bce67dc439b3a74e2a0c6b9f9e5c1f509c3f2
Svemir Brkic <svemir@symplicity.com>
2024-04-28 17:25:05 -0400

    fix the test mock (#312)

--- tests/Feature/UpdatePrimsStatusesTest.php
+++ tests/Feature/UpdatePrimsStatusesTest.php
@@ -2,21 +2,21 @@
 
 use App\Models\PrismSinglePayment;
 use Mockery;
+use Mockery\MockInterface;
 
-it('updates Prism payment statuses', function () {
-    $mockPayment = Mockery::mock('overload:'.PrismSinglePayment::class);
+it('updates Prism payment status', function () {
+    $this->instance(PrismSinglePayment::class, function (MockInterface $mock) {
+        $payment = new PrismSinglePayment(['payment_id' => 1, 'status' => 'sending']);
+        $payment->payment_id = 1;
+        $payment->status = 'sending';
+        $testPayments = collect([$payment]);
 
-    $payment = new PrismSinglePayment(['payment_id' => 1, 'status' => 'sending']);
-    $payment->payment_id = 1;
-    $payment->status = 'sending';
-
-    // Set up a fake response for the where call
-    $mockPayments = collect([$payment]);
-    $mockPayment->shouldReceive('where')
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('get')
-        ->andReturn($mockPayments);
+        $mock->shouldReceive('where')
+            ->once()
+            ->andReturnSelf()
+            ->shouldReceive('get')
+            ->andReturn($testPayments);
+    });
 
     // Call the command and assert it runs successfully
     $this->artisan('prism:update')->assertExitCode(0);
