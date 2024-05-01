4136f37d35002234bca35685d9617432a895acca
Christopher David <chris@arcadelabs.co>
2024-05-01 12:43:39 -0500

    cleanup

--- tests/Feature/LoggingTest.php
+++ tests/Feature/LoggingTest.php
@@ -3,13 +3,6 @@
 use App\Services\OpenObserveLogger;
 
 test('sends a log entry to OpenObserve', function () {
-    // Set up the OpenObserve logger with dummy credentials
-    config(['openobserve.username' => 'dummy-username']);
-    config(['openobserve.password' => 'dummy-password']);
-    config(['openobserve.base_url' => 'https://example.com']);
-    config(['openobserve.org' => 'my-org']);
-    config(['openobserve.stream' => 'my-stream']);
-
     // Create an instance of the OpenObserve logger
     $logger = new OpenObserveLogger([
         'baseUrl' => 'https://pool.openagents.com:5080',
