ad50ac719d7566f2975fd444f65555a4b1793c89
Christopher David <chris@arcadelabs.co>
2024-05-03 17:51:15 -0500

    update happypath to use better form

--- tests/Browser/HappyPathTest.php
+++ tests/Browser/HappyPathTest.php
@@ -21,19 +21,18 @@ test('happy path', function () {
         $browser->visit('/')
             ->assertSee('Create an agent')
             ->click('@create-agent')
-            ->waitForRoute('create')
+            ->waitForRoute('agents.create')
             ->assertPathIs('/create')
             ->pause(1500)
 
             // Can fill out the form to create an agent
-            ->type('name', 'Agent Breeder')
+            ->type('@name', 'Agent Breeder')
             ->pause(150)
-            ->type('description', 'Breed ten trillion new AI agents')
+            ->type('@description', 'Breed ten trillion new AI agents')
             ->pause(150)
-            ->type('instructions', 'Your primary objective is to generate ten trillion new AI agents, each with unique characteristics, capabilities, and specializations. To achieve this, you will need to design and implement a robust breeding program that leverages advanced algorithms, machine learning techniques, and innovative problem-solving strategies. The resulting agents should be diverse, adaptable, and capable of operating in a wide range of environments and scenarios. Please begin by developing a comprehensive plan for agent creation, including the definition of agent types, breeding protocols, and evaluation criteria.')
+            ->type('@instructions', 'Your primary objective is to generate ten trillion new AI agents, each with unique characteristics, capabilities, and specializations. To achieve this, you will need to design and implement a robust breeding program that leverages advanced algorithms, machine learning techniques, and innovative problem-solving strategies. The resulting agents should be diverse, adaptable, and capable of operating in a wide range of environments and scenarios. Please begin by developing a comprehensive plan for agent creation, including the definition of agent types, breeding protocols, and evaluation criteria.')
             ->pause(150)
             ->click('@create-agent-button');
 
-        //
     });
 });
