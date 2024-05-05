ddc9447dafc73e10e39eaeb6e47e9ef052d16d6d
Christopher David <chris@arcadelabs.co>
2024-05-05 15:22:29 -0500

    restrict to pro users

--- app/Livewire/IndexedCodebaseList.php
+++ app/Livewire/IndexedCodebaseList.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class IndexedCodebaseList extends Component
 {
+    public function mount()
+    {
+        // If user is not logged in or is not pro, redirect to login page
+        if (! auth()->check() || ! auth()->user()->isPro()) {
+            return redirect('/');
+        }
+    }
+
     public function render()
     {
         return view('livewire.indexed-codebase-list');
