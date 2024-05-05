b6a6e786175327bdc8cf138156e4ab38311821f1
Christopher David <chris@arcadelabs.co>
2024-05-05 13:48:17 -0500

    Indexer test - create and send message to agent

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -174,6 +174,7 @@
                                      wireModel="message_input"
                                      :image-upload="auth()->check() && auth()->user()->isPro"
                                      wire:ignore
+                                     dusk="message-input"
                                      onkeydown="if(event.keyCode == 13 && !event.shiftKey) { event.preventDefault(); document.getElementById('send-message').click(); }"
                                      class="flex h-[48px] w-full rounded-md border-2 bg-transparent p-3 pr-10 text-[16px] placeholder:text-[#777A81] focus-visible:outline-none focus-visible:ring-0 focus-visible:border-white focus-visible:ring-white"/>
                     <button dusk="send-message" class="hidden" id="send-message" type="submit"></button>
---
+++ tests/Browser/IndexerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Models\Agent;
+use App\Models\Thread;
+use App\Models\User;
+use Laravel\Dusk\Browser;
+
+test('can interact with paid indexer agent', function () {
+    $this->browse(function (Browser $browser) {
+
+        $currentAgentCount = Agent::count();
+
+        // Homepage loads and shows title and Join button
+        $browser
+            ->move(150, 50)
+            ->resize(1400, 1000)
+            ->visit('/')
+            ->assertSee('OpenAgents')
+            ->assertSee('How can we help you')
+            ->assertSee('Join');
+
+        // Mock a login
+        $browser->loginAs(User::factory()->create());
+
+        // Can click on Create Agent and go to new create page
+        $browser->visit('/')
+            ->waitFor('@model-dropdown')
+            ->click('@model-dropdown')
+            ->waitFor('@create-agent')
+            ->click('@create-agent')
+            ->waitForRoute('agents.create')
+            ->assertPathIs('/create')
+            ->waitFor('@name')
+
+            // Can fill out the form to create an agent
+            ->type('@name', 'OA Codebase Indexer')
+            ->type('@description', 'Queries an index of the OpenAgents.com codebase on GitHub.')
+            ->type('@instructions', 'You consult the index and provide summaries of codebase sections.')
+            ->click('@create-agent-button')
+            ->pause(500);
+
+        // Assert we have one more agent in the database
+        $this->assertEquals($currentAgentCount + 1, Agent::count());
+
+        // Get the ID of the most recent thread
+        $threadId = Thread::latest()->first()->id;
+
+        // User is redirected to chat with the agent
+        $browser
+            ->waitForRoute('chat.id', ['id' => $threadId])
+            ->assertSee('OA Codebase Indexer')
+            ->waitFor('@message-input')
+            ->type('@message-input', 'Summarize this codebase')
+            ->click('@send-message-button')
+            ->pause(25000)
+            ->screenshot('noice');
+    });
+});
