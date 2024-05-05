79f1f7ef845f203a9ce55f7480a07f31cda2c791
Riccardo Balbo <riccardo0blb@gmail.com>
2024-05-05 16:47:49 +0200

    Fix some issues with pool implementation, support insecure connection

--- config/nostr.php
+++ config/nostr.php
@@ -2,6 +2,7 @@
 
 return [
     'pool' => env('NOSTR_POOL'),
+    'pool_ssl' => env('NOSTR_POOL_SSL')=="true",
     'webhook_secret' => env('NOSTR_WEBHOOK_SECRET'),
     'encrypt_for' => env('NOSTR_ENCRYPT'),
     'node_token' => env('NOSTR_NODE_TOKEN'),
