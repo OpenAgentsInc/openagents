46be0ab5b30e83133aca33ef485d2db2a8fa57b6
Christopher David <chris@arcadelabs.co>
2024-05-05 15:28:41 -0500

    initial codebase screen

--- resources/views/livewire/indexed-codebase-list.blade.php
+++ resources/views/livewire/indexed-codebase-list.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<div class="w-full lg:-ml-[25px] px-3">
+    <div class="sm:w-[584px] lg:w-[768px] mx-auto mt-12">
+        <h1>Codebases</h1>
+
+        <div class="text-gray">
+            <p>A list of codebases that have been indexed.</p>
+            <p>These indexes can be used by any OpenAgents Pro user.</p>
+        </div>
+    </div>
 </div>
