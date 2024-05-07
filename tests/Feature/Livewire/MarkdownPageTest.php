370af7585312e91f57cb2718238a198f72a9111c
Christopher David <chris@arcadelabs.co>
2024-05-07 16:13:51 -0500

    axe markdown page test that relies on path

--- tests/Feature/Livewire/MarkdownPageTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\MarkdownPage;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(MarkdownPage::class)
-        ->assertStatus(200);
-});
