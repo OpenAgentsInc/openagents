8d839706ca6bd714be91488a0065612a03c94f66
Riccardo Balbo <riccardo0blb@gmail.com>
2024-05-10 19:41:16 +0200

    Fix some rag issues (#359)
    * fix: wrong param name causing encryption to be always off
    
    * fix: warm-up process needs to have the same params as post-warm-up requests
    
    * Bypass rag if not available or not needed
    
    * remove dead code
    
    * close connection after the job result

--- config/nostr.php
+++ config/nostr.php
@@ -4,6 +4,6 @@ return [
     'pool' => env('NOSTR_POOL'),
     'pool_ssl' => env('NOSTR_POOL_SSL', true),
     'webhook_secret' => env('NOSTR_WEBHOOK_SECRET'),
-    'encrypt_for' => env('NOSTR_ENCRYPT'),
+    'encrypt' => env('NOSTR_ENCRYPT'),
     'node_token' => env('NOSTR_NODE_TOKEN'),
 ];
