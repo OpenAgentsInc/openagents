8a71b91fa4946971ac90ed453f3bd62902567826
Christopher David <chris@arcadelabs.co>
2024-05-10 08:47:40 -0400

    add featured agents to store

--- app/Livewire/Store.php
+++ app/Livewire/Store.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Store extends Component
 {
+    public $agents;
+
+    public function mount()
+    {
+        //        $this->agents = Agent::where('featured', true)->get();
+    }
+
     public function render()
     {
         return view('livewire.store');
--- resources/views/livewire/store.blade.php
+++ resources/views/livewire/store.blade.php
@@ -1,3 +1,5 @@
 <div>
     {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+
+    <livewire:featured-agents/>
 </div>
