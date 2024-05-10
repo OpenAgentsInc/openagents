d34274ccf8c197d348951c77a3a2074219ffe4f8
Christopher David <chris@arcadelabs.co>
2024-05-10 12:28:30 -0400

    Add SN community link to sidebar

--- resources/views/livewire/sidebar-content.blade.php
+++ resources/views/livewire/sidebar-content.blade.php
@@ -67,6 +67,27 @@
                     </div>
                 </div>
             </li>
+
+
+            <li>
+                <div class="relative z-[15]">
+                    <div class="group relative rounded-lg active:opacity-90 px-3">
+                        <a class="flex flex-row items-center gap-2 py-2" target="_blank"
+                           href="https://stacker.news/~openagents">
+                            <div class="select-none cursor-pointer relative grow overflow-hidden whitespace-nowrap">
+                                Community
+                            </div>
+                            <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                 stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                <polyline points="15 3 21 3 21 9"></polyline>
+                                <line x1="10" y1="14" x2="21" y2="3"></line>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </li>
+
             <li>
                 <div class="relative z-[15]">
                     <div class="group relative rounded-lg active:opacity-90 px-3">
