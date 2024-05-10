36fa1c7fe86bceb1a30101ff10225b6b5abd4873
Christopher David <chris@arcadelabs.co>
2024-05-10 08:53:20 -0400

    Add new store link (#357)
    * storesummary links to agent store
    
    * rename store to featuredagents
    
    * axe storetest
    
    * ok
    
    * php artisan make:livewire --pest Store and routesweb
    
    * add featured agents to store
    
    * remove headline from featuredagents, put in chat
    
    * only show this explore link if current path is not /store

--- app/Livewire/Store.php
+++ app/Livewire/Store.php
@@ -2,28 +2,19 @@
 
 namespace App\Livewire;
 
-use App\AI\Models;
-use App\Models\Agent;
 use Livewire\Component;
 
 class Store extends Component
 {
-    public $models = Models::MODELS;
-
     public $agents;
 
     public function mount()
     {
-        // Only grab the latest 2 agents
-        //        $this->agents = Agent::latest()->take(2)->get();
-
-        // Grab the agents where featured is true
-        $this->agents = Agent::where('featured', true)->get();
+        //        $this->agents = Agent::where('featured', true)->get();
     }
 
     public function render()
     {
-        return view('livewire.store')
-            ->layout('components.layouts.store');
+        return view('livewire.store');
     }
 }
--- tests/Feature/Livewire/StoreTest.php
+++ tests/Feature/Livewire/StoreTest.php
@@ -5,15 +5,5 @@ use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(Store::class)
-        ->assertStatus(200)
-        ->assertSeeInOrder([
-            'How can we',
-            'Discover, try, and create AI chat agents...',
-            'LLMs',
-            'Llama 3 8B', 'Free',
-            'Mistral Large', 'Pro',
-            'GPT-4', 'Pro',
-            'Command-R', 'Free',
-            'Satoshi 7B', 'Free',
-        ]);
-})->skip();
+        ->assertStatus(200);
+});
