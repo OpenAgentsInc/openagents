5096007cb01b75e4e835f6c3281fee2aa0b3653e
Christopher David <chris@arcadelabs.co>
2024-05-10 09:45:42 -0400

    Test agent stats: unique threads and users (#358)

--- app/Livewire/FeaturedAgents.php
+++ app/Livewire/FeaturedAgents.php
@@ -9,9 +9,20 @@ class FeaturedAgents extends Component
 {
     public $agents;
 
+    //    public function mount()
+    //    {
+    //        //        $this->agents = Agent::where('featured', true)->get();
+    //        $this->agents = Agent::withCount(['threads as unique_threads_count'])->get();
+    //
+    //    }
+
     public function mount()
     {
-        $this->agents = Agent::where('featured', true)->get();
+        $this->agents = Agent::withCount(['threads as unique_threads_count'])
+            ->get()
+            ->each(function ($agent) {
+                $agent->unique_users_count = $agent->getUniqueUsersCountAttribute();
+            });
     }
 
     public function render()
