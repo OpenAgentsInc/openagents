a844033c87cf56321f5d9e48a36c3cb86c0d6700
Christopher David <chris@arcadelabs.co>
2024-05-10 08:43:29 -0400

    ok

--- resources/views/livewire/featured-agents.blade.php
+++ resources/views/livewire/featured-agents.blade.php
@@ -3,7 +3,6 @@
         <x-logomark :size="1"></x-logomark>
         <h3 class="mt-[36px] text-center leading-relaxed">How can we help you today?</h3>
 
-
         @if (count($agents) > 0)
             <div class="hidden sm:block md:w-[800px] border border-offblack p-8 mt-12 rounded">
                 <div>
@@ -23,7 +22,22 @@
                 </div>
 
                 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-                    <!-- ... -->
+                    @foreach($agents as $agent)
+                        <a class="pointer-events-auto select-auto" href="/chat?agent={{ $agent["id"] }}"
+                           wire:navigate>
+                            <div class="p-4 rounded-lg relative">
+                                <div class="flex">
+                                    <div class="mt-1 w-[20px] h-[20px] sm:w-[60px] sm:h-[60px]">
+                                        <img src="{{ $agent->image_url }}" alt="Agent" class="w-full h-full rounded">
+                                    </div>
+                                    <div class="flex-1 pl-4">
+                                        <h4 class="text-lg font-bold">{{ $agent['name'] }}</h4>
+                                        <span class="text-gray">{{ $agent['about'] }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
                 </div>
             </div>
         @endif
