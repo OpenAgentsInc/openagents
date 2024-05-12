4c54b698d5233b21b79063495128ed653ef07b0c
Moneya <moneya59@gmail.com>
2024-05-12 05:33:35 +0100

    aaded agent status to chat UI

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -203,6 +203,8 @@ class Chat extends Component
             'output_tokens' => null,
         ];
 
+
+
         // Clear the input
         $this->message_input = '';
         $this->pending = true;
