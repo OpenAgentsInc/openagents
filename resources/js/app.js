c4dbbebc1d5e4e79cf588934857eb64f548e9533
Moneya <moneya59@gmail.com>
2024-05-12 10:06:22 +0100

    Filepond wire:navigate white screen flash

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,21 @@ import './bootstrap';
 import {webln} from "@getalby/sdk";
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 import Tooltip from "@ryangjchandler/alpine-tooltip";
+import * as FilePond from 'filepond';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
 
 window.Swal = Swal;
+
+window.FilePond = FilePond;
+
+// Register the plugin
+FilePond.registerPlugin(FilePondPluginImagePreview);
+FilePond.registerPlugin(FilePondPluginFileValidateType);
+FilePond.registerPlugin(FilePondPluginFileValidateSize);
+
 
 
 async function experiment() {
