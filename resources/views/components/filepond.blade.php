b105349edc4a44f614aec62a1997a7c222a513f1
moneya <moneya@users.noreply.github.com>
2024-05-13 08:17:26 +0100

    Fixed filepond issue (#375)

--- resources/views/components/filepond.blade.php
+++ resources/views/components/filepond.blade.php
@@ -1,26 +1,28 @@
-<div wire:ignore x-data x-init="() => {
-            document.addEventListener('livewire:init', () => {
-                const post = FilePond.create($refs.{{ $attributes->get('ref') ?? 'input' }});
-                post.setOptions({
-                    allowMultiple: {{ $attributes->has('multiple') ? 'true' : 'false' }},
-                    server: {
-                        process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
-                            @this.upload('{{ $attributes->whereStartsWith('wire:model')->first() }}', file, load, error, progress)
-                        },
-                        revert: (filename, load) => {
-                            @this.removeUpload('{{ $attributes->whereStartsWith('wire:model')->first() }}', filename, load)
-                        },
-                    },
-                    allowImagePreview: {{ $attributes->has('allowFileTypeValidation') ? 'true' : 'false' }},
-                    imagePreviewMaxHeight: {{ $attributes->has('imagePreviewMaxHeight') ? $attributes->get('imagePreviewMaxHeight') : '256' }},
-                    allowFileTypeValidation: {{ $attributes->has('allowFileTypeValidation') ? 'true' : 'false' }},
-                    acceptedFileTypes: {!! $attributes->get('acceptedFileTypes') ?? 'null' !!},
-                    allowFileSizeValidation: {{ $attributes->has('allowFileSizeValidation') ? 'true' : 'false' }},
-                    maxFileSize: {!! $attributes->has('maxFileSize') ? "'" . $attributes->get('maxFileSize') . "'" : 'null' !!}
-                });
-            });
-}">
-    <input type="file" x-ref="{{ $attributes->get('ref') ?? 'input' }}" />
+<div
+        wire:ignore
+        x-data
+        x-init="() => {
+        const post = FilePond.create($refs.{{ $attributes->get('ref') ?? 'input' }});
+        post.setOptions({
+            allowMultiple: {{ $attributes->has('multiple') ? 'true' : 'false' }},
+            server: {
+                process:(fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+                    @this.upload('{{ $attributes->whereStartsWith('wire:model')->first() }}', file, load, error, progress)
+                },
+                revert: (filename, load) => {
+                    @this.removeUpload('{{ $attributes->whereStartsWith('wire:model')->first() }}', filename, load)
+                },
+            },
+            allowImagePreview: {{ $attributes->has('allowFileTypeValidation') ? 'true' : 'false' }},
+            imagePreviewMaxHeight: {{ $attributes->has('imagePreviewMaxHeight') ? $attributes->get('imagePreviewMaxHeight') : '256' }},
+            allowFileTypeValidation: {{ $attributes->has('allowFileTypeValidation') ? 'true' : 'false' }},
+            acceptedFileTypes: {!! $attributes->get('acceptedFileTypes') ?? 'null' !!},
+            allowFileSizeValidation: {{ $attributes->has('allowFileSizeValidation') ? 'true' : 'false' }},
+            maxFileSize: {!! $attributes->has('maxFileSize') ? "'".$attributes->get('maxFileSize')."'" : 'null' !!}
+        });
+    }"
+>
+    <input type="file" x-ref="{{ $attributes->get('ref') ?? 'input' }}"/>
 </div>
 
 
