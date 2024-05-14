cc27f56e2e9054392a65d302d7d4a2cdc4bc34a8
Christopher David <chris@arcadelabs.co>
2024-05-14 13:17:02 -0500

    card w 400

--- resources/views/livewire/agents/partials/card.blade.php
+++ resources/views/livewire/agents/partials/card.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full h-[70vh] flex flex-col justify-center">
         <div class="pointer-events-none select-none flex flex-col justify-center items-center px-8 sm:w-[584px] lg:w-[768px] mx-auto">
             <p class="text-[16px] text-gray">Now speaking with...</p>
-            <div class="max-w-[400px]">
+            <div class="w-[400px]">
                 <livewire:agent-card-static :agent="$agent" :key="$agent->id"/>
             </div>
         </div>
