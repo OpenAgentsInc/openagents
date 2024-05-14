3af66e34d1ddb77df692072e079a4298538aafe8
Christopher David <chris@arcadelabs.co>
2024-05-14 11:05:41 -0500

    swap featured and popular agents w agent cards

--- app/Livewire/AgentCard.php
+++ app/Livewire/AgentCard.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class AgentCard extends Component
 {
+    public $agent;
+
+    public function mount($agent)
+    {
+        $this->agent = $agent;
+    }
+
     public function render()
     {
         return view('livewire.agent-card');
