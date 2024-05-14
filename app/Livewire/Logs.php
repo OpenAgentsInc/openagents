ad1d610577674fa179c79878434e73bef0b89e78
Christopher David <chris@arcadelabs.co>
2024-05-13 19:52:37 -0500

    php artisan make:model Log -mf etc

--- app/Livewire/Logs.php
+++ app/Livewire/Logs.php
@@ -2,16 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Log;
 use Livewire\Component;
 
 class Logs extends Component
 {
+    public $logs = [];
+
     public function mount()
     {
         // Redirect to the homepage if the user is not an admin
         if (! auth()->check() || ! auth()->user()->isAdmin()) {
             return redirect()->route('home');
         }
+
+        $this->logs = Log::latest()->get();
     }
 
     public function render()
