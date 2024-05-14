e40ec2666c1c35c05765aaa42a41d299fbd352ab
Christopher David <chris@arcadelabs.co>
2024-05-14 13:10:51 -0500

    agent card static

--- resources/views/livewire/agents/partials/card.blade.php
+++ resources/views/livewire/agents/partials/card.blade.php
@@ -3,7 +3,7 @@
         <div class="pointer-events-none select-none flex flex-col justify-center items-center px-8 sm:w-[584px] lg:w-[768px] mx-auto">
             <p class="text-[16px] text-gray">Now speaking with...</p>
             <div class="max-w-[400px]">
-                <livewire:agent-card :agent="$agent" :key="$agent->id"/>
+                <livewire:agent-card-static :agent="$agent" :key="$agent->id"/>
             </div>
         </div>
     </div>
