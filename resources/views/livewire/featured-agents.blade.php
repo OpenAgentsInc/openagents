47999623792632f3f1921aff8fc2d25d84b937f4
Christopher David <chris@arcadelabs.co>
2024-05-14 12:27:40 -0500

    agent store noiceeeee

--- resources/views/livewire/featured-agents.blade.php
+++ resources/views/livewire/featured-agents.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if (count($agents) > 0)
-        <div class="hidden sm:block md:w-[800px] p-8 -mt-4">
+        <div class="hidden sm:block md:w-[800px] p-8">
             <div>
                 <div class="flex justify-between items-center">
                     <h4 class="font-bold">Featured</h4>
@@ -13,13 +13,6 @@
                     @endif
                 </div>
                 <p class="text-gray leading-relaxed">Curated top agents</p>
-                @if (request()->path() !== 'store')
-                    <p class="text-gray leading-relaxed">Chat Agents combine instructions, extra knowledge, and any LLM
-                        on OpenAgents to create custom AI agents. Watch our <a
-                                class="text-white select-auto pointer-events-auto hover:underline"
-                                href="https://twitter.com/OpenAgentsInc/status/1786675616437190707" target="_blank">Agent
-                            Builder video</a> to learn more.</p>
-                @endif
             </div>
 
             <div class="z-10 grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6">
