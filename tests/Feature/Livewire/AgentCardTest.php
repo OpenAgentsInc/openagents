b7e2ea9a9535dc397e83818c16a8be3de6adf2ba
Christopher David <chris@arcadelabs.co>
2024-05-14 12:28:50 -0500

    fix test

--- tests/Feature/Livewire/AgentCardTest.php
+++ tests/Feature/Livewire/AgentCardTest.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Livewire\AgentCard;
+use App\Models\Agent;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
-    Livewire::test(AgentCard::class)
+    $agent = Agent::factory()->create();
+    Livewire::test(AgentCard::class, ['agent' => $agent])
         ->assertStatus(200);
 });
