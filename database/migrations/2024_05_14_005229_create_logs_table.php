61cba31da959d64c680232e148c7f893287af313
Christopher David <chris@arcadelabs.co>
2024-05-13 19:56:36 -0500

    logs table

--- database/migrations/2024_05_14_005229_create_logs_table.php
+++ database/migrations/2024_05_14_005229_create_logs_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
+            $table->json('data');
             $table->timestamps();
         });
     }
--- resources/views/livewire/logs.blade.php
+++ resources/views/livewire/logs.blade.php
@@ -1,3 +1,32 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="p-12">
+    <div class="flex flex-row justify-between">
+        <h2 class="mb-4">Logs ({{ count($logs) }})</h2>
+    </div>
+
+    <div class="flex flex-col gap-8">
+        <table class="min-w-full table-fixed divide-y divide-offblack text-white">
+            <thead>
+            <tr>
+                <th class="p-2 text-left text-sm font-semibold text-gray">
+                    <div>ID</div>
+                </th>
+                <th class="p-2 text-left text-sm font-semibold text-gray">
+                    <div>Data</div>
+                </th>
+                <th class="p-2 text-left text-sm font-semibold text-gray">
+                    <div>Created At</div>
+                </th>
+            </tr>
+            </thead>
+            <tbody class="divide-y divide-offblack bg-black text-gray">
+            @foreach($logs as $log)
+                <tr>
+                    <td class="whitespace-nowrap p-2 text-sm">{{ $log->id }}</td>
+                    <td class="whitespace-nowrap p-2 text-sm">{{ json_encode($log->data) }}</td>
+                    <td class="whitespace-nowrap p-2 text-sm">{{ $log->created_at }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
