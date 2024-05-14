6bf7b63662604be67c3ae2631b5be55ad0b3ce18
Christopher David <chris@arcadelabs.co>
2024-05-13 19:51:37 -0500

    me

--- app/Livewire/Logs.php
+++ app/Livewire/Logs.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class Logs extends Component
 {
+    public function mount()
+    {
+        // Redirect to the homepage if the user is not an admin
+        if (! auth()->check() || ! auth()->user()->isAdmin()) {
+            return redirect()->route('home');
+        }
+    }
+
     public function render()
     {
         return view('livewire.logs');
--- tests/Feature/Livewire/LogsTest.php
+++ tests/Feature/Livewire/LogsTest.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Livewire\Logs;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
-    Livewire::test(Logs::class)
+    Livewire::actingAs(User::factory()->create(['username' => 'AtlantisPleb']))
+        ->test(Logs::class)
         ->assertStatus(200);
+});
+
+// It redirects to the homepage if the user is not isAdmin
+it('redirects to the homepage if the user is not isAdmin', function () {
+    Livewire::actingAs(User::factory()->create())
+        ->test(Logs::class)
+        ->assertRedirect(route('home'));
 });
