d0aeaf783166419f2ef676790de9911013cf1b48
Christopher David <chris@arcadelabs.co>
2024-05-14 13:06:09 -0500

    agent card on speaking with

--- app/Livewire/Agents/Partials/Card.php
+++ app/Livewire/Agents/Partials/Card.php
@@ -2,14 +2,20 @@
 
 namespace App\Livewire\Agents\Partials;
 
+use App\Models\Agent;
 use Livewire\Component;
 
 class Card extends Component
 {
     public $selectedAgent;
 
+    public $agent;
+
     public function mount($selectedAgent)
     {
+        $this->agent = Agent::find($selectedAgent['id']);
+
+        //        dd($selectedAgent['id']); // This will output the selected agent (if you passed it in from the parent component
         $this->selectedAgent = $selectedAgent;
     }
 
