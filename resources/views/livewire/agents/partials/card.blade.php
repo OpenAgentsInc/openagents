50fcfc0785c32ce23d9c7f2aa4dddd5eba3467b5
Christopher David <chris@arcadelabs.co>
2024-05-13 20:33:37 -0500

    say Learning not Building

--- resources/views/livewire/agents/partials/card.blade.php
+++ resources/views/livewire/agents/partials/card.blade.php
@@ -10,7 +10,7 @@
                 <div>
                     <h3 class="mt-4">{{ $selectedAgent['name'] }}</h3>
                     <span class="inline-flex items-center my-1 px-1 py-1 {{ $selectedAgent['is_rag_ready'] == false && $selectedAgent['created_at']->diffInMinutes() > 30 ? 'bg-red text-white' : ($selectedAgent['is_rag_ready'] ? 'bg-white text-black' : 'bg-yellow-500 text-black') }}    text-xs font-bold rounded-md">
-                        {{ $selectedAgent['is_rag_ready'] == false && $selectedAgent['created_at']->diffInMinutes() > 30 ? 'Error' : ($selectedAgent['is_rag_ready'] ? 'Ready' : 'Building') }}
+                        {{ $selectedAgent['is_rag_ready'] == false && $selectedAgent['created_at']->diffInMinutes() > 30 ? 'Error' : ($selectedAgent['is_rag_ready'] ? 'Ready' : 'Learning') }}
                     </span>
                 </div>
                 <p class="text-[14px] text-gray mb-0">{{ $selectedAgent['description'] }}</p>
