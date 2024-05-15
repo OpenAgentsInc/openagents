ab8048177460fd3330bb9f49acef186e592b234d
Christopher David <chris@arcadelabs.co>
2024-05-15 10:23:17 -0500

    php artisan make:command PayAgentCreators

---
+++ app/Console/Commands/PayAgentCreators.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PayAgentCreators extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:pay-agent-creators';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
