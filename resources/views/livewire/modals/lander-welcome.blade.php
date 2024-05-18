653ee1c05915fd86579a4ac199a03641c16b973f
Christopher David <chris@arcadelabs.co>
2024-05-17 19:22:35 -0500

    lol

--- resources/views/livewire/modals/lander-welcome.blade.php
+++ resources/views/livewire/modals/lander-welcome.blade.php
@@ -5,12 +5,12 @@
 
     <div class="p-4 sm:px-7 pb-6">
         <p class="text-left my-2 text-md lg:text-lg text-[#D7D8E5]">
-            Welcome! Try for free. Then log in with X for better models. Upgrade to Pro when you're ready.
+            Welcome! Chat for free. Use the dropdown to pick the model. Upgrade to Pro when you're ready.
         </p>
 
         <div class="mt-8 mb-2">
-            <x-button wire:click="$dispatch('closeModal')" class="w-full justify-center font-medium">Thanks you are
-                awesome!
+            <x-button wire:click="$dispatch('closeModal')" class="w-full justify-center font-medium">I love you
+                OpenAgents!
             </x-button>
         </div>
     </div>
