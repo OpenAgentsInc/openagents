df67acecde8b76329d758e4a08a4f1db4756dc6f
Christopher David <chris@arcadelabs.co>
2024-05-18 22:33:39 -0500

    basic agent profile eheheh

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,8 @@
 <x-htmx-layout>
-    <main class="p-12">
+    <main class="p-12 relative"> <!-- Add 'relative' class to the main element -->
+        <!-- X icon positioned top right -->
+
+
         <div class="flex flex-row gap-x-6">
             @if($user->profile_photo_path)
                 <img src="{{ str_replace('_normal', '', $user->profile_photo_path) }}"
@@ -16,13 +19,21 @@
                     <h2><span class="mr-[4px] text-text">@</span>{{ $user->username }}</h2>
                 @endif
             </div>
-        </div>
-        <div class="mx-2 my-4 inline-block">
             @if ($user->username)
-                <a href="https://x.com/{{ $user->username }}" target="_blank">
-                    <x-icon.x class="h-6 w-6"/>
-                </a>
+                <div class="flex flex-col justify-center">
+                    <a href="https://x.com/{{ $user->username }}" target="_blank"
+                       class="p-1.5 border border-offblack hover:bg-offblack rounded">
+                        <x-icon.x class="h-6 w-6"/>
+                    </a>
+                </div>
             @endif
+        </div>
+
+        <h3 class="mt-12 mb-6">Agents</h3>
+        <div class="grid grid-cols-2 sm:grid-cols-3 gap-6">
+            @foreach($user->agents as $agent)
+                <livewire:agent-card :agent="$agent" :key="$agent->id"/>
+            @endforeach
         </div>
     </main>
 </x-htmx-layout>
