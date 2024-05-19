9bcf9f9fdd9a31aa09dae7081b1a9732a27b141d
Christopher David <chris@arcadelabs.co>
2024-05-18 23:12:07 -0500

    test htmx route

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Thread;
+use Illuminate\Support\Facades\Session;
+
 // An experimental HTMX refactor of our Livewire Chat.php component
 
 class ChatController extends Controller
 {
     public function index()
     {
-        return view('chat');
+        $threads = $this->getThreadsForUser();
+
+        return view('chat', compact('threads'));
+    }
+
+    private function getThreadsForUser()
+    {
+        if (auth()->guest()) {
+            $sessionId = Session::getId();
+
+            return Thread::whereSessionId($sessionId)->orderBy('created_at', 'desc')->get();
+        }
+
+        return auth()->user()->threads()->orderBy('created_at', 'desc')->get();
     }
 }
