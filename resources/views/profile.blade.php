b1d17f1ddbfc440a9749130c1e9a1adbd5e8a815
Christopher David <chris@arcadelabs.co>
2024-05-18 19:08:51 -0500

    nicer

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,21 +3,24 @@
 <head>
     <title>OpenAgents</title>
     <script src="https://unpkg.com/htmx.org@1.9.12"></script>
-    <style>
-        html, body {
-            background-color: black;
-            color: white;
-            font-family: monospace !important;
-        }
-    </style>
+    <script defer src="https://unpkg.com/alpinejs@3/dist/cdn.min.js"></script>
+    @include('partials.vite')
 </head>
 <body hx-boost="true">
-<main>
-    <header>
-        <h1>{{ auth()->user()->name }}</h1>
-        <h2>{{ $username }}</h2>
-    </header>
-    <p>Hello.</p>
+<main class="p-12">
+    <div class="flex flex-row gap-x-6">
+        <img src="https://pbs.twimg.com/profile_images/1607882836740120576/3Tg1mTYJ.jpg"
+             alt="{{ auth()->user()->name }}"
+             class="rounded-xl w-[120px] h-[120px]"/>
+        <div>
+            <h1>{{ auth()->user()->name }}</h1>
+            <h2>{{ $username }}</h2>
+        </div>
+    </div>
+
+    <a href="https://x.com/{{ auth()->user()->username }}" target="_blank">
+        <x-icon.x class="h-6 w-6"/>
+    </a>
 </main>
 </body>
 </html>
