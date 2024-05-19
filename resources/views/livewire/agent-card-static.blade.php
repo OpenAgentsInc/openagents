f3fa1e75ee6cc3783d851f2208c0d2fe6b1987eb
Christopher David <chris@arcadelabs.co>
2024-05-18 22:02:38 -0500

    refresh paths vite update

--- resources/views/livewire/agent-card-static.blade.php
+++ resources/views/livewire/agent-card-static.blade.php
@@ -26,7 +26,6 @@
         @endauth
     </div>
 
-
     <div class="mt-1 mb-3 w-[20px] h-[20px] sm:w-[60px] sm:h-[60px]">
         <img src="{{ $agent->image_url }}" alt="Agent" class="w-full h-full rounded">
     </div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,9 +1,16 @@
 <x-htmx-layout>
     <main class="p-12">
         <div class="flex flex-row gap-x-6">
-            <img src="{{ str_replace('_normal', '', $user->profile_photo_path) }}"
-                 alt="{{ $user->name }}"
-                 class="rounded-xl w-[120px] h-[120px]"/>
+
+            @if($user->profile_photo_path)
+                <img src="{{ str_replace('_normal', '', $user->profile_photo_path) }}"
+                     alt="{{ $user->name }}"
+                     class="rounded-xl w-[120px] h-[120px]"/>
+            @else
+                <x-icon.user class="w-[32px] h-[32px] mt-4"/>
+            @endif
+            
+
             <div>
                 <h1>{{ $user->name }}</h1>
                 <h2><span class="mr-[4px] text-text">@</span>{{ $user->username }}</h2>
