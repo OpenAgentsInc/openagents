c142fb9a710bc18613cd04b10d58466db0d859a9
Christopher David <chris@arcadelabs.co>
2024-05-19 19:10:33 -0500

    changelog

--- resources/views/livewire/changelog.blade.php
+++ resources/views/livewire/changelog.blade.php
@@ -9,7 +9,8 @@
         <div class="w-full sm:max-w-2xl mt-6 p-6 bg-black shadow-md overflow-hidden sm:rounded-lg prose prose-invert">
             <div class="mt-6 grid grid-cols-1 gap-10">
                 <x-pane title="May 19, 2024" borderColor="border-darkgray">
-                    <x-changelog-item>
+                    <x-changelog-item post="https://x.com/OpenAgentsInc/status/1792346743805558985"
+                                      code="https://github.com/OpenAgentsInc/openagents/commit/4692e7b68d259ac3253ac5338d7f2bd129f4ac46">
                         Improved Markdown styling
                     </x-changelog-item>
                 </x-pane>
