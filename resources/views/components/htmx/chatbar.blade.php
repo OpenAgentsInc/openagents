5af9002d172af8256e87623eefd91cc291b5daa5
Christopher David <chris@arcadelabs.co>
2024-05-21 15:58:45 -0500

    button

--- resources/views/components/htmx/chatbar.blade.php
+++ resources/views/components/htmx/chatbar.blade.php
@@ -1,12 +1,19 @@
 <div class="w-full px-3">
-    <div class="sm:w-[584px] lg:w-[768px] mx-auto">
+    <div class="sm:w-[584px] lg:w-[768px] mx-auto relative">
         <textarea id="message-input" minRows="1" default="Message OpenAgents..."
                   :showIcon="true" iconName="send" min-rows="1" max-rows="12"
                   wire:model="message_input"
-                  wireModel="message_input"
                   :image-upload="auth()->check() && auth()->user()->isPro"
                   wire:ignore dusk="message-input"
                   onkeydown="if(event.keyCode === 13 && !event.shiftKey && window.innerWidth > 768) { event.preventDefault(); document.getElementById('send-message').click(); }"
-                  class="flex h-[48px] w-full rounded-md border-2 bg-transparent p-3 pr-10 text-[16px] placeholder:text-[#777A81] focus-visible:outline-none focus-visible:ring-0 focus-visible:border-white focus-visible:ring-white"></textarea>
+                  class="flex h-[48px] w-full rounded-md border-2 bg-transparent p-3 pr-10 text-[16px] placeholder:text-[#777A81]
+                   focus-visible:outline-none focus-visible:ring-0 focus-visible:border-white focus-visible:ring-white
+                   resize-none"></textarea>
+        <button
+                class="absolute bottom-[10px] right-[10px] text-black shadow rounded bg-white hover:bg-white/90"
+                dusk="send-message-button"
+        >
+            <x-icon name="send" class="w-[24px] h-[24px] m-0.5 flex flex-col justify-center items-center"/>
+        </button>
     </div>
 </div>
--- resources/views/htmx/chat.blade.php
+++ resources/views/htmx/chat.blade.php
@@ -3,11 +3,12 @@
 <x-htmx-layout>
     <div class="flex h-full">
         <x-htmx.sidebar/>
-        <div class="flex flex-col overflow-hidden w-full">
+        <div class="flex flex-col w-full">
             <div class="flex-grow w-full overflow-y-auto flex flex-col items-center">
                 <x-htmx.messages-list :messages="$messages ?? null" :thread="$thread ?? null"/>
             </div>
             <x-htmx.chatbar/>
+            <x-htmx.messages-remaining/>
         </div>
     </div>
 </x-htmx-layout>
