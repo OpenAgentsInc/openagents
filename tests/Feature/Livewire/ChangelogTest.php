5d94b93d07042376669805ea42dd80955e814d72
Svemir Brkic <svemir@symplicity.com>
2024-05-21 22:12:40 -0400

    add more livewire tests with real assertions (#409)

--- tests/Feature/Livewire/ChangelogTest.php
+++ tests/Feature/Livewire/ChangelogTest.php
@@ -5,5 +5,7 @@ use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(Changelog::class)
-        ->assertStatus(200);
+        ->assertStatus(200)
+        ->assertSee('Changelog')
+        ->assertSeeHtml('See our <a href="https://github.com/OpenAgentsInc/openagents/commits/main/" target="_blank">GitHub');
 });
