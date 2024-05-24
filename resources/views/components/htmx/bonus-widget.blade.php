18717380a9f935df9838dacd087c347148228efa
Christopher David <chris@arcadelabs.co>
2024-05-23 19:58:18 -0500

    zero bitcorns

--- resources/views/components/htmx/bonus-widget.blade.php
+++ resources/views/components/htmx/bonus-widget.blade.php
@@ -1,5 +1,5 @@
 @pro
-<div class="z-[9999] fixed top-[10px] right-[65px] p-2 border-offblack text-text border shadow-lg rounded-lg">
-    bonus
+<div class="z-[9999] fixed top-[10px] right-[65px] p-2 border-offblack text-gray border shadow-lg rounded-lg">
+    ₿ <span id="amount">0</span>
 </div>
 @endpro
