c19b4c4800f858382ed200deca17cf49220457c1
Christopher David <chris@arcadelabs.co>
2024-05-23 20:04:45 -0500

    SSE balance

--- resources/views/components/htmx/bonus-widget.blade.php
+++ resources/views/components/htmx/bonus-widget.blade.php
@@ -1,5 +1,10 @@
 @pro
-<div class="z-[9999] fixed top-[10px] right-[65px] p-2 border-offblack text-gray border shadow-lg rounded-lg">
-    ₿ <span id="amount">0</span>
+<script src="https://unpkg.com/htmx.org@2.0.0-beta4/dist/htmx.min.js"></script>
+<script src="https://unpkg.com/htmx-ext-sse@2.0.0/sse.js"></script>
+<div class="select-none z-[9999] fixed top-[10px] right-[65px] p-2 border-offblack text-darkgray border shadow-lg rounded-lg">
+    <div hx-ext="sse" sse-connect="/stream">
+        ₿
+        <span sse-swap="SatsBalance">0</span>
+    </div>
 </div>
 @endpro
