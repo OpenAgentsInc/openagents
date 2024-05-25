23cdfadcca3b8c8bf1d1826d324782904ba3e318
Christopher David <chris@arcadelabs.co>
2024-05-25 09:41:26 -0500

    axe unused jetstream provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,5 @@ return [
     App\Providers\CustomBladeDirectivesProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
