3fea67f3165c3d3504e0e9a5ab8d078486673e33
Christopher David <chris@arcadelabs.co>
2024-05-27 16:54:48 -0500

    all agent/user pay combos work

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -23,3 +23,23 @@ test('user can pay a user', function () {
     expect($user->checkBalance(Currency::BTC))->toBe(0)
         ->and($user2->checkBalance(Currency::BTC))->toBe(1000 * 1000);
 });
+
+test('agent can pay a user', function () {
+    $agent = Agent::factory()->withBalance(1000 * 1000, Currency::BTC)->create();
+    $user = User::factory()->withBalance(0, Currency::BTC)->create();
+
+    $agent->payUser($user, 1000 * 1000, Currency::BTC);
+
+    expect($agent->checkBalance(Currency::BTC))->toBe(0)
+        ->and($user->checkBalance(Currency::BTC))->toBe(1000 * 1000);
+});
+
+test('agent can pay an agent', function () {
+    $agent = Agent::factory()->withBalance(1000 * 1000, Currency::BTC)->create();
+    $agent2 = Agent::factory()->withBalance(0, Currency::BTC)->create();
+
+    $agent->payAgent($agent2, 1000 * 1000, Currency::BTC);
+
+    expect($agent->checkBalance(Currency::BTC))->toBe(0)
+        ->and($agent2->checkBalance(Currency::BTC))->toBe(1000 * 1000);
+});
