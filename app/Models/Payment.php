6b64461419c886ffd99851a2a5dd90221f5533b8
Christopher David <chris@arcadelabs.co>
2024-05-27 16:21:40 -0500

    payment and balance have currency

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Currency;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,4 +10,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Payment extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $casts = [
+        'currency' => Currency::class,
+    ];
 }
