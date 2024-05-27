8e7e6d2865a49c866a3e4230c084d77659577482
Christopher David <chris@arcadelabs.co>
2024-05-27 18:02:05 -0500

    clean up payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -16,25 +16,4 @@ class Payment extends Model
     protected $casts = [
         'currency' => Currency::class,
     ];
-
-    // Define relationships
-    public function sources()
-    {
-        return $this->morphedByMany(User::class, 'source', 'payment_sources')
-            ->withPivot('created_at', 'updated_at')
-            ->using(PaymentSource::class)
-            ->morphedByMany(Agent::class, 'source', 'payment_sources')
-            ->withPivot('created_at', 'updated_at')
-            ->using(PaymentSource::class);
-    }
-
-    public function destinations()
-    {
-        return $this->morphedByMany(User::class, 'destination', 'payment_destinations')
-            ->withPivot('created_at', 'updated_at')
-            ->using(PaymentDestination::class)
-            ->morphedByMany(Agent::class, 'destination', 'payment_destinations')
-            ->withPivot('created_at', 'updated_at')
-            ->using(PaymentDestination::class);
-    }
 }
