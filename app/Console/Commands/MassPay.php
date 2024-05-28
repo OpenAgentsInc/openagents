76f7c0f66c3d75c76d1ff73f022506889a4df669
Christopher David <chris@arcadelabs.co>
2024-05-28 11:55:33 -0500

    start masspay command

---
+++ app/Console/Commands/MassPay.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MassPay extends Command
+{
+    protected $signature = 'masspay';
+
+    protected $description = 'Pay all users';
+
+    public function handle()
+    {
+
+    }
+}
