55daefaeb88d8f46d3e6e779ed4dadbaa71cabcb
Christopher David <chris@arcadelabs.co>
2024-05-27 22:11:36 -0500

    test passes

--- tests/Feature/Livewire/WalletScreenTest.php
+++ tests/Feature/Livewire/WalletScreenTest.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Livewire\WalletScreen;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
-    Livewire::test(WalletScreen::class)
+    Livewire::actingAs(User::factory()->create())
+        ->test(WalletScreen::class)
         ->assertStatus(200);
 });
