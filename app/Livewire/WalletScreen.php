0d10a781b6c19e8fb42b279a53d82e4d199e885e
Christopher David <chris@arcadelabs.co>
2024-05-27 22:03:46 -0500

    show btc balance

--- app/Livewire/WalletScreen.php
+++ app/Livewire/WalletScreen.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Enums\Currency;
+use App\Models\User;
 use Livewire\Component;
 
 class WalletScreen extends Component
 {
+    public $balance_btc = 1;
+
+    // On mount, grab the user's bitcoin balance
+    public function mount()
+    {
+        // If the user is not logged in, redirect to the homepage
+        if (! auth()->check()) {
+            return redirect()->route('home');
+        }
+
+        /** @var User $user */
+        $user = auth()->user();
+        $this->balance_btc = $user->checkBalance(Currency::BTC);
+    }
+
     public function render()
     {
         return view('livewire.wallet-screen');
--- resources/views/livewire/wallet-screen.blade.php
+++ resources/views/livewire/wallet-screen.blade.php
@@ -1,5 +1,6 @@
 <div class="p-4 md:p-12 mx-auto flex flex-col justify-center w-full items-center" x-data="{ dropdown: false }">
     <div class="w-full md:max-w-3xl md:min-w-[600px]">
         <h3 class="mb-16 font-bold text-3xl text-center select-none">Wallet</h3>
+        <p class="text-center text-lg">Balance: {{ $this->balance_btc / 1000 }} sats</p>
     </div>
 </div>
