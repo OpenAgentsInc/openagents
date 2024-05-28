1ec7928e5eb5f9b9b3bbefe0069509f63563999d
Christopher David <chris@arcadelabs.co>
2024-05-27 21:53:38 -0500

    Wallet screen test

--- resources/views/livewire/wallet-screen.blade.php
+++ resources/views/livewire/wallet-screen.blade.php
@@ -1,3 +1,5 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
+<div class="p-4 md:p-12 mx-auto flex flex-col justify-center w-full items-center" x-data="{ dropdown: false }">
+    <div class="w-full md:max-w-3xl md:min-w-[600px]">
+        <h3 class="mb-16 font-bold text-3xl text-center select-none">Wallet</h3>
+    </div>
 </div>
