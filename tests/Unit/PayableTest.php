9c2b2336ada7da0f3211a124501de4ecf9b35a6d
Christopher David <chris@arcadelabs.co>
2024-05-29 09:37:30 -0500

    user

--- tests/Unit/PayableTest.php
+++ tests/Unit/PayableTest.php
@@ -2,10 +2,11 @@
 
 use App\Enums\Currency;
 use App\Models\Agent;
+use App\Models\User;
 
 test('sats_earned is calculated correctly', function () {
     $agent = Agent::factory()->withBalance(800000, Currency::BTC)->create();
-    $user = Agent::factory()->withBalance(300000, Currency::BTC)->create();
+    $user = User::factory()->withBalance(300000, Currency::BTC)->create();
 
     expect($agent->sats_earned)->toBe(800)
         ->and($user->sats_earned)->toBe(300);
