181fd232cc570ab132c073f2c35d5342d6f8dbac
Christopher David <chris@arcadelabs.co>
2024-05-29 08:23:43 -0500

    user can pay agent for message - passes

--- tests/Feature/PayAgentsTest.php
+++ tests/Feature/PayAgentsTest.php
@@ -6,8 +6,9 @@ use App\Models\User;
 use App\Services\PaymentService;
 
 test('user can pay agent for message', function () {
-    $user = User::factory()->withBalance(100000, Currency::BTC)->create();
     $agent = Agent::factory()->create(['sats_per_message' => 3]);
+    $user = User::factory()->withBalance(100000, Currency::BTC)->create();
+    $this->actingAs($user);
 
     $payService = new PaymentService();
     $res = $payService->payAgentForMessage($agent->id, $agent->sats_per_message);
@@ -18,7 +19,7 @@ test('user can pay agent for message', function () {
     $user->refresh();
 
     // Assert there is a payment record
-    expect($user->payments()->count())->toBe(1);
+    expect($user->sentPayments()->count())->toBe(1);
 
     expect($user->sats_balance)->toBe(97);
 });
