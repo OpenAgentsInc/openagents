92287c23e9df436fdc7b4261986065ced01b9034
Christopher David <chris@arcadelabs.co>
2024-05-28 20:42:47 -0500

    explorer page shows the most recent payments - test fails

--- tests/Feature/ExplorerTest.php
+++ tests/Feature/ExplorerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Payment;
+
 test('explorer route loads', function () {
     $this->get('/explorer')
         ->assertStatus(200)
@@ -10,7 +12,16 @@ test('explorer page shows the most recent payments', function () {
     // Run PaymentSeeder
     $this->artisan('db:seed', ['--class' => 'PaymentSeeder']);
 
-    $this->get('/explorer')
-        ->assertStatus(200)
+    // Fetch the 10 most recent payments
+    $recentPayments = Payment::latest()->take(10)->get();
+
+    $response = $this->get('/explorer');
+
+    $response->assertStatus(200)
         ->assertSee('Recent payments');
+
+    // Assert we see the amounts of 10 recent payments
+    foreach ($recentPayments as $payment) {
+        $response->assertSee($payment->amount);
+    }
 });
