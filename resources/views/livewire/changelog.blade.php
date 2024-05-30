43106d75592203c98847656bc5d252231b192c9b
Christopher David <chris@arcadelabs.co>
2024-05-30 13:26:24 -0500

    changelog

--- resources/views/livewire/changelog.blade.php
+++ resources/views/livewire/changelog.blade.php
@@ -9,6 +9,9 @@
         <div class="w-full sm:max-w-2xl mt-6 p-6 bg-black shadow-md overflow-hidden sm:rounded-lg prose prose-invert">
             <div class="mt-6 grid grid-cols-1 gap-10">
                 <x-pane title="May 30, 2024" borderColor="border-darkgray">
+                    <x-changelog-item code="https://github.com/OpenAgentsInc/openagents/pull/443">
+                        Editable agent sats per message (max 3000)
+                    </x-changelog-item>
                     <x-changelog-item code="https://github.com/OpenAgentsInc/openagents/pull/441">
                         Sweep agent balances to users every minute
                     </x-changelog-item>
