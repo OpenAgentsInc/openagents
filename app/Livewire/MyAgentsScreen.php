f4fe5253f4f534813db232de91a3b2884d5c2dd1
Christopher David <chris@arcadelabs.co>
2024-05-30 06:15:27 -0500

    my agents screen

--- app/Livewire/MyAgentsScreen.php
+++ app/Livewire/MyAgentsScreen.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class MyAgentsScreen extends Component
 {
+    public function mount()
+    {
+        // If user is unauthed, redirect to home
+        if (! auth()->check()) {
+            return redirect()->route('home');
+        }
+    }
+
     public function render()
     {
         return view('livewire.my-agents-screen');
