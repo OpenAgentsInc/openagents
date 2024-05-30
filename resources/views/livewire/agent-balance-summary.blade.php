a5481fb7126ce92f4f0314f74efcf825e87b8cbd
Christopher David <chris@arcadelabs.co>
2024-05-30 08:05:24 -0500

    yeah that's clear

--- resources/views/livewire/agent-balance-summary.blade.php
+++ resources/views/livewire/agent-balance-summary.blade.php
@@ -4,6 +4,7 @@
             <h4 class="text-center">{{ $satsBalance }} sats</h4>
         </div>
     </x-pane>
-    <p class="mt-4 text-sm text-center text-gray">Agent balances are transferred once per minute: 80% to agent
-        creators, 20% to OpenAgents</p>
+    <p class="mt-4 text-sm text-center text-gray">Agent balances are transferred once per minute: 80% to the agent
+        creator, 20% to OpenAgents (or when plugins are used, 60% to the agent creator, 20% to the plugin authors, 20%
+        to OA)</p>
 </div>
