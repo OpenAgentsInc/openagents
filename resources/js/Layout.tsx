72761907dd6fd78508fb8c15d25b34e41b748a04
Christopher David <chris@arcadelabs.co>
2024-06-14 17:50:02 -0500

    Layout w TopNav looks good

--- resources/js/Layout.tsx
+++ resources/js/Layout.tsx
@@ -1,3 +1,51 @@
+import React, { useCallback, useState } from 'react';
+import { Frame, TopBar } from "@shopify/polaris";
+import { ArrowLeftIcon } from '@shopify/polaris-icons';
+
 export default function Layout({ children }) {
-  return children
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+
+  const toggleIsUserMenuOpen = useCallback(
+    () => setIsUserMenuOpen((isUserMenuOpen) => !isUserMenuOpen),
+    [],
+  );
+
+  const userMenuMarkup = (
+    <TopBar.UserMenu
+      actions={[
+        {
+          items: [{ content: 'Back to OpenAgents', icon: ArrowLeftIcon }],
+        },
+        {
+          items: [{ content: 'Community forums' }],
+        },
+      ]}
+      name="TestUser"
+      // detail="Agent Builder"
+      initials="T"
+      open={isUserMenuOpen}
+      onToggle={toggleIsUserMenuOpen}
+    />
+  );
+
+  const topBar = (
+    <TopBar
+      showNavigationToggle
+      userMenu={userMenuMarkup}
+    />
+  )
+
+  const logo = {
+    topBarSource:
+      '/images/oarect.png',
+    width: 146,
+    url: '/',
+    accessibilityLabel: 'OpenAgents',
+  };
+
+  return (
+    <Frame logo={logo} topBar={topBar}>
+      {children}
+    </Frame>
+  )
 }
