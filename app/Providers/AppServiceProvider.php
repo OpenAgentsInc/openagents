6b7c5f46dea321cc232d63e352d4a35fce1c6e55
Christopher David <chris@arcadelabs.co>
2024-06-13 22:10:06 -0500

    Install Inertia (#460)
    * npm install @inertiajs/react
    
    * install inertia and set root view
    
    * install react and configure vite
    
    * simplest vite config
    
    * skip failing tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Services\PaymentService;
 use App\Services\StreamService;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -34,6 +35,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Inertia::setRootView('inertia');
+
         Gate::define('viewApiDocs', function (User $user) {
             // return in_array($user->email, ['admin@app.com']);
             return true;
