d1539f1becd2112ff4b40dd3f6acbfd2f686e0d5
Christopher David <chris@arcadelabs.co>
2024-06-14 00:21:18 -0500

    Add demo Polaris components (#461)
    * npm install @shopify/polaris --save
    
    * polaris provider
    
    * sample page
    
    * and our font
    
    * darker input bgs
    
    * hacky theme but decent
    
    * dropzone

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig(({command}) => {
             laravel({
                 input: [
                     'resources/css/app.css',
+                    'resources/css/inertia.css',
                     'resources/js/app.js',
                     'resources/js/inertia.jsx',
                 ],
