3f85ac26751fc27575e438fb8f95059354b19e95
Christopher David <chris@arcadelabs.co>
2024-06-14 17:01:47 -0500

    set up inertia layout

--- resources/js/inertia.jsx
+++ resources/js/inertia.jsx
@@ -4,11 +4,14 @@ import { createRoot } from 'react-dom/client'
 import '@shopify/polaris/build/esm/styles.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
 import {AppProvider} from '@shopify/polaris';
+import Layout from './Layout'
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.{jsx,tsx}', { eager: true });
-    return pages[`./Pages/${name}.jsx`] || pages[`./Pages/${name}.tsx`];
+    let page = pages[`./Pages/${name}.jsx`] || pages[`./Pages/${name}.tsx`];
+    page.default.layout = page.default.layout || (page => <Layout children={page} />)
+    return page
   },
   setup({ el, App, props }) {
     createRoot(el).render(
