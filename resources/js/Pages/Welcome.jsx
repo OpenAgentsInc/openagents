dd3ed7c6fcd1fd358f38d297ee3d9942ef0067a5
Christopher David <chris@arcadelabs.co>
2024-06-13 22:14:18 -0500

    sample page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Head } from '@inertiajs/react'
+import {Page, LegacyCard, Button} from '@shopify/polaris';
 
 export default function Welcome({ user }) {
   return (
     <>
       <Head title="Welcome" />
-      <h1>Welcome</h1>
-      <p>Hello {user.name}, welcome to your first Inertia app!</p>
+      <Page title="Example app">
+      <LegacyCard sectioned>
+        <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+      </LegacyCard>
+    </Page>
     </>
   )
 }
