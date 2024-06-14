7662f7c2ca44e4d681b202440d87331b917a321d
Christopher David <chris@arcadelabs.co>
2024-06-14 11:07:56 -0500

    layouttest

--- resources/js/Pages/NavTest.jsx
+++ resources/js/Pages/NavTest.jsx
@@ -1,16 +1,124 @@
-import React from 'react'
-import {Frame, Navigation} from '@shopify/polaris';
+import React, { useCallback, useState } from 'react'
+import { Frame, Navigation, TopBar, ActionList, Icon, Text } from '@shopify/polaris';
 import {
   HomeIcon,
   PlusCircleIcon,
   ProductIcon,
   MinusCircleIcon,
   OrderIcon,
+  ArrowLeftIcon, QuestionCircleIcon
 } from '@shopify/polaris-icons';
 
 export default function NavTest({ user }) {
+  return <TopBarExample />
+}
+
+function TopBarExample() {
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+  const [isSecondaryMenuOpen, setIsSecondaryMenuOpen] = useState(false);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  const toggleIsUserMenuOpen = useCallback(
+    () => setIsUserMenuOpen((isUserMenuOpen) => !isUserMenuOpen),
+    [],
+  );
+
+  const toggleIsSecondaryMenuOpen = useCallback(
+    () => setIsSecondaryMenuOpen((isSecondaryMenuOpen) => !isSecondaryMenuOpen),
+    [],
+  );
+
+  const handleSearchResultsDismiss = useCallback(() => {
+    setIsSearchActive(false);
+    setSearchValue('');
+  }, []);
+
+  const handleSearchChange = useCallback((value) => {
+    setSearchValue(value);
+    setIsSearchActive(value.length > 0);
+  }, []);
+
+  const handleNavigationToggle = useCallback(() => {
+    console.log('toggle navigation visibility');
+  }, []);
+  const logo = {
+    topBarSource:
+      '/images/oarect.png',
+    width: 146,
+    url: '/',
+    accessibilityLabel: 'OpenAgents',
+  };
+
+  const userMenuMarkup = (
+    <TopBar.UserMenu
+      actions={[
+        {
+          items: [{ content: 'Back to Shopify', icon: ArrowLeftIcon }],
+        },
+        {
+          items: [{ content: 'Community forums' }],
+        },
+      ]}
+      name="Dharma"
+      detail="Jaded Pixel"
+      initials="D"
+      open={isUserMenuOpen}
+      onToggle={toggleIsUserMenuOpen}
+    />
+  );
+
+  const searchResultsMarkup = (
+    <ActionList
+      items={[{ content: 'Shopify help center' }, { content: 'Community forums' }]}
+    />
+  );
+
+  const searchFieldMarkup = (
+    <TopBar.SearchField
+      onChange={handleSearchChange}
+      value={searchValue}
+      placeholder="Search"
+      showFocusBorder
+    />
+  );
+
+  const secondaryMenuMarkup = (
+    <TopBar.Menu
+      activatorContent={
+        <span>
+          <Icon source={QuestionCircleIcon} />
+          <Text as="span" visuallyHidden>
+            Secondary menu
+          </Text>
+        </span>
+      }
+      open={isSecondaryMenuOpen}
+      onOpen={toggleIsSecondaryMenuOpen}
+      onClose={toggleIsSecondaryMenuOpen}
+      actions={[
+        {
+          items: [{ content: 'Community forums' }],
+        },
+      ]}
+    />
+  );
+
+  const topBarMarkup = (
+    <TopBar
+      showNavigationToggle
+      userMenu={userMenuMarkup}
+      secondaryMenu={secondaryMenuMarkup}
+      searchResultsVisible={isSearchActive}
+      searchField={searchFieldMarkup}
+      searchResults={searchResultsMarkup}
+      onSearchResultsDismiss={handleSearchResultsDismiss}
+      onNavigationToggle={handleNavigationToggle}
+    />
+  );
+
   return (
-    <Frame>
+    <Frame topBar={topBarMarkup} logo={logo}>
       <Navigation location="/">
         <Navigation.Section
           items={[
@@ -55,7 +163,7 @@ export default function NavTest({ user }) {
                 {
                   accessibilityLabel: 'Remove a product',
                   icon: MinusCircleIcon,
-                  onClick: () => {},
+                  onClick: () => { },
                   tooltip: {
                     content: 'Remove a product',
                   },
@@ -66,5 +174,5 @@ export default function NavTest({ user }) {
         />
       </Navigation>
     </Frame>
-  )
+  );
 }
