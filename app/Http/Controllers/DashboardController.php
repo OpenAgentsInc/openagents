8dd5ae0ef1157ab6345969b20eb0f1f96479c809
Christopher David <chris@arcadelabs.co>
2024-06-14 20:49:20 -0500

    Start new dashboard (#463)
    * dashboard page
    
    * clean up routes
    
    * set up inertia layout
    
    * Layout w TopNav looks good
    
    * shh
    
    * Clean dashboard page
    
    * log out nice
    
    * sidebar nav hover
    
    * nav
    
    * head
    
    * icons

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    public function dashboard () {
+        return Inertia::render('Dashboard');
+    }
+
+    public function test () {
+        return Inertia::render('Welcome', [
+            'user' => ['name' => 'Test Man']
+        ]);
+    }
+
+    public function test2 () {
+        return Inertia::render('NavTest');
+    }
+}
---
+++ resources/js/Layout.tsx
@@ -0,0 +1,121 @@
+import React, { useCallback, useState } from 'react';
+import { Frame, Navigation, TopBar } from "@shopify/polaris";
+import {
+  HomeIcon,
+  ExitIcon,
+  AppExtensionIcon,
+  AffiliateIcon,
+  GaugeIcon,
+} from '@shopify/polaris-icons';
+import { Head } from '@inertiajs/react';
+
+export default function Layout({ children }) {
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+
+  const toggleIsUserMenuOpen = useCallback(
+    () => setIsUserMenuOpen((isUserMenuOpen) => !isUserMenuOpen),
+    [],
+  );
+
+  const userMenuMarkup = (
+    <TopBar.UserMenu
+      actions={[
+        {
+          items: [{
+            content: 'Log out', icon: ExitIcon, onAction: () => {
+              console.log("log out placeholder")
+            }
+          }],
+        },
+      ]}
+      name="TestUser"
+      initials="TU"
+      avatar="https://i.pravatar.cc/150?img=14"
+      open={isUserMenuOpen}
+      onToggle={toggleIsUserMenuOpen}
+    />
+  );
+
+  const topBar = (
+    <TopBar
+      showNavigationToggle
+      userMenu={userMenuMarkup}
+    />
+  )
+
+  const logo = {
+    topBarSource:
+      '/images/oarect.png',
+    width: 146,
+    url: '/',
+    accessibilityLabel: 'OpenAgents',
+  };
+
+  return (
+    <>
+      <Head title="OpenAgents" />
+      <Frame logo={logo} topBar={topBar} navigation={<NavigationIs />}>
+        {children}
+      </Frame>
+    </>
+  )
+}
+
+function NavigationIs() {
+  return (
+    <Navigation location="/dashboard">
+      <Navigation.Section
+        items={[
+          {
+            url: '/',
+            label: 'Home',
+            icon: HomeIcon,
+            selected: false,
+          },
+          {
+            url: '/dashboard',
+            label: 'Dashboard',
+            icon: GaugeIcon,
+            selected: true,
+          },
+          {
+            url: '/agents',
+            label: 'Agents',
+            icon: AffiliateIcon,
+            badge: '2',
+          },
+          {
+            url: '/plugins',
+            label: 'Plugins',
+            icon: AppExtensionIcon,
+            badge: '4',
+          },
+          // {
+          //   url: '#',
+          //   excludePaths: ['#'],
+          //   label: 'Products',
+          //   icon: ProductIcon,
+          //   secondaryActions: [
+          //     {
+          //       url: '#',
+          //       accessibilityLabel: 'Add a product',
+          //       icon: PlusCircleIcon,
+          //       tooltip: {
+          //         content: 'Add a product',
+          //       },
+          //     },
+          //     {
+          //       accessibilityLabel: 'Remove a product',
+          //       icon: MinusCircleIcon,
+          //       onClick: () => { },
+          //       tooltip: {
+          //         content: 'Remove a product',
+          //       },
+          //     },
+          //   ],
+          // },
+        ]}
+      />
+    </Navigation>
+  )
+}
