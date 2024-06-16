47cf223b4d34cf04f08870e81f0a320f7347f14c
Christopher David <chris@arcadelabs.co>
2024-06-16 15:40:01 -0500

    Start login page nice

--- resources/js/Pages/Login.tsx
+++ resources/js/Pages/Login.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import AuthLayout from '../Layouts/AuthLayout'
 
-export default function LoginPage () {
-  return <p>Log in</p>
+function LoginPage () {
+  return (
+    <div className="h-full flex flex-col items-center justify-center" style={{ background: 'linear-gradient(rgb(12, 13, 14) 0%, rgb(8, 8, 8) 50%)' }}>
+      <div className="mb-8 gap-y-6 w-full flex flex-col items-center justify-center">
+        <img className="w-20 h-20" src="/images/sqlogo-t.png" />
+        <h1 className="text-xl font-bold">Log in to OpenAgents</h1>
+      </div>
+    </div>
+  );
 }
+
+LoginPage.layout = page => <AuthLayout children={page} title="Log in to OpenAgents" />
+
+export default LoginPage
