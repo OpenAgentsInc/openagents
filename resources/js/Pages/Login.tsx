bbaefcebe2978d911df2c8cf9e9a12da71a3cd58
Christopher David <chris@arcadelabs.co>
2024-06-17 09:03:06 -0500

    form structure nice

--- resources/js/Pages/Login.tsx
+++ resources/js/Pages/Login.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-// import { XIcon } from "../Components/XIcon";
-import { Icon } from "@shopify/polaris";
-import { LogoGoogleIcon, LogoXIcon } from "@shopify/polaris-icons";
+import React, { useState, useCallback } from "react";
+import { Icon, TextField } from "@shopify/polaris";
+import { EmailIcon, LogoGoogleIcon, LogoXIcon } from "@shopify/polaris-icons";
 import AuthLayout from "../Layouts/AuthLayout";
 
 function LoginPage() {
+  const [value, setValue] = useState("");
+
+  const handleChange = useCallback((e) => setValue(e.target.value), []);
   return (
     <div
       className="h-full w-full fixed flex flex-col items-center justify-center"
@@ -15,21 +17,49 @@ function LoginPage() {
       <div className="mb-8 gap-y-6 w-full flex flex-col items-center justify-center">
         <img className="w-20 h-20" src="/images/sqlogo-t.png" />
         <h1 className="text-xl font-bold">Log in to OpenAgents</h1>
+
         <div className="flex flex-col gap-y-5 mt-2 w-[350px]">
-          <a href="/login/google">
-            <button className="inline-flex items-center justify-center text-[16px] w-full h-[48px] border border-white rounded-md gap-2">
+          <form>
+            <label for="email" class="pl-1">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              autoFocus
+              label="Email"
+              value={value}
+              onChange={handleChange}
+              autoComplete="off"
+              className="mt-1 flex h-[48px] w-full rounded-md border-2 bg-transparent p-3 pr-10 text-[16px] placeholder:text-[#777A81] focus-visible:outline-none focus-visible:ring-0 focus:visible:border-white focus-visible:ring-white"
+            />
+            <button
+              type="submit"
+              className="mt-3 inline-flex items-center justify-center text-[16px] w-full h-[48px] border border-white rounded-md gap-2"
+            >
               <div>
-                <Icon source={LogoGoogleIcon} className="h-5 w-5" />
+                <Icon source={EmailIcon} className="h-5 w-5" />
               </div>
-              <span>Continue with Google</span>
+              <span>Continue with email</span>
             </button>
-          </a>
+          </form>
+
+          <p class="text-center">or</p>
+
           <a href="/login/x">
             <button className="inline-flex items-center justify-center text-[16px] w-full h-[48px] border border-white rounded-md gap-2">
               <div>
                 <Icon source={LogoXIcon} className="h-5 w-5" />
               </div>
               <span>Continue with X</span>
+            </button>
+          </a>
+          <a href="/login/google">
+            <button className="inline-flex items-center justify-center text-[16px] w-full h-[48px] border border-white rounded-md gap-2">
+              <div>
+                <Icon source={LogoGoogleIcon} className="h-5 w-5" />
+              </div>
+              <span>Continue with Google</span>
             </button>
           </a>
         </div>
