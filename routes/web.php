f4b34ee10053565a4274b5ae823eeb596c76f1ec
Christopher David <chris@arcadelabs.co>
2024-06-29 14:08:24 -0500

    Start new workspace layout

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Demo2');
+    return Inertia::render('AutoDev');
+    // return Inertia::render('Demo2');
 //    return Inertia::render('Welcome', [
 //        'canLogin' => Route::has('login'),
 //        'canRegister' => Route::has('register'),
