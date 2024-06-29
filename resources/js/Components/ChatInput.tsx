bf30e6bb0bafd8c02005ca6c07daaaf4fe4152da
Christopher David <chris@arcadelabs.co>
2024-06-29 14:52:12 -0500

    class w classname

--- resources/js/Components/ChatInput.tsx
+++ resources/js/Components/ChatInput.tsx
@@ -2,24 +2,24 @@ export const ChatInput = () => {
   return (
     <fieldset className="flex w-full min-w-0 flex-col-reverse">
       <div className="flex  flex-col  bg-zinc-900  gap-1.5  border-0.5  border-border-300  pl-4  pt-2.5  pr-2.5  pb-2.5  -mx-1  sm:mx-0  items-stretch  transition-all  duration-200  relative  shadow-[0_0.25rem_1.25rem_rgba(0,0,0,0.035)]  focus-within:shadow-[0_0.25rem_1.25rem_rgba(0,0,0,0.075)]  hover:border-border-200  focus-within:border-border-200  cursor-text  z-10 rounded-t-2xl border-b-0">
-        <div class="flex gap-2">
+        <div className="flex gap-2">
           <div
             aria-label="Write your prompt to Claude"
-            class="mt-1 max-h-96 w-full overflow-y-auto break-words"
+            className="mt-1 max-h-96 w-full overflow-y-auto break-words"
           >
             <div
               contenteditable="true"
               translate="no"
               enterkeyhint="enter"
               tabindex="0"
-              class="focus:outline-none ProseMirror break-words max-w-[60ch]"
+              className="focus:outline-none ProseMirror break-words max-w-[60ch]"
               autofocus=""
             >
               <p
                 data-placeholder="Reply to Claude..."
-                class="is-empty is-editor-empty before:!text-text-500 before:whitespace-nowrap"
+                className="is-empty is-editor-empty before:!text-text-500 before:whitespace-nowrap"
               >
-                <br class="ProseMirror-trailingBreak" />
+                <br className="ProseMirror-trailingBreak" />
               </p>
             </div>
           </div>
@@ -27,14 +27,14 @@ export const ChatInput = () => {
             data-testid="file-upload"
             aria-hidden="true"
             tabindex="-1"
-            class="absolute -z-10 h-0 w-0 overflow-hidden opacity-0"
+            className="absolute -z-10 h-0 w-0 overflow-hidden opacity-0"
             accept=".pdf,.doc,.docx,.rtf,.epub,.odt,.odp,.pptx,.txt,.py,.ipynb,.js,.jsx,.html,.css,.java,.cs,.php,.c,.cpp,.cxx,.h,.hpp,.rs,.R,.Rmd,.swift,.go,.rb,.kt,.kts,.ts,.tsx,.m,.scala,.rs,.dart,.lua,.pl,.pm,.t,.sh,.bash,.zsh,.csv,.log,.ini,.config,.json,.proto,.yaml,.yml,.toml,.lua,.sql,.bat,.md,.coffee,.tex,.latex,.jpg,.jpeg,.png,.gif,.webp"
             multiple=""
             aria-label="Upload files"
             type="file"
           />
           <button
-            class="inline-flex
+            className="inline-flex
   items-center
   justify-center
   relative
@@ -72,7 +72,7 @@ export const ChatInput = () => {
               height="20"
               fill="currentColor"
               viewBox="0 0 256 256"
-              class="text-text-300"
+              className="text-text-300"
             >
               <path d="M209.66,122.34a8,8,0,0,1,0,11.32l-82.05,82a56,56,0,0,1-79.2-79.21L147.67,35.73a40,40,0,1,1,56.61,56.55L105,193A24,24,0,1,1,71,159L154.3,74.38A8,8,0,1,1,165.7,85.6L82.39,170.31a8,8,0,1,0,11.27,11.36L192.93,81A24,24,0,1,0,159,47L59.76,147.68a40,40,0,1,0,56.53,56.62l82.06-82A8,8,0,0,1,209.66,122.34Z"></path>
             </svg>
