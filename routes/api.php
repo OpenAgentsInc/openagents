daa05c1bf0d5ae8b067f4d4f2ea50661eacef2e9
Christopher David <chris@openagents.com>
2024-10-15 21:25:33 -0500

    Deprecate v1 (#468)
    * delete stuff
    
    * readme
    
    * readme
    
    * ok
    
    * vanillavel
    
    * repo link

--- routes/api.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-require __DIR__.'/api/v1.php';
--- tests/Feature/Livewire/AgentUsageTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\AgentUsage;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(AgentUsage::class)
-        ->assertStatus(200);
-})->skip();
