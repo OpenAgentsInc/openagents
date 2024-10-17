2e458117eaa7a62e65170629a1ff0938c6b31cda
Christopher David <chris@openagents.com>
2024-10-17 10:23:14 -0500

    Enhanced ThreadFactory to generate 'title' and 'project_id', linking threads to projects.

--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Project;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class ThreadFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence,
+            'project_id' => Project::factory(),
         ];
     }
 }
