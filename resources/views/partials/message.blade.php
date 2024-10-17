2bd562d3beb86ff76e2d7173ff808ee43fe88d79
Christopher David <chris@openagents.com>
2024-10-17 16:14:35 -0500

    Add class attribute to paragraph in message.blade.php to improve styling control.

--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -1,4 +1,4 @@
 <div class="message {{ $message->is_system_message ? 'system-message' : 'user-message' }}">
     <strong>{{ $message->user ? $message->user->name : 'System' }}:</strong>
-    <p>{{ $message->content }}</p>
+    <p class="message-content">{{ $message->content }}</p>
 </div>
