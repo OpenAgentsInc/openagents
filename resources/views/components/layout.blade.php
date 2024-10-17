c8d62650d7229a518933ff0970904f0143db2132
Christopher David <chris@openagents.com>
2024-10-16 21:33:09 -0500

    fixed layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" class="dark">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +10,9 @@
     <script src="{{ asset('js/tailwind.min.js') }}"></script>
     <script src="{{ asset('js/tailwind-config.js') }}"></script>
 </head>
-<body class="bg-background text-foreground font-mono">
+
+<body class="bg-background text-foreground font-mono fixed w-screen">
     {{ $slot }}
 </body>
+
 </html>
