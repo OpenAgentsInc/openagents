4b9f379c08ef196385c631757138687fd8b9a1fc
Christopher David <chris@openagents.com>
2024-10-16 20:46:05 -0500

    homepage loads homepage view

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('homepage');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
