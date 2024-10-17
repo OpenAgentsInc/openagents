d1c12fd48e50b8d65c1f3449ba7f9e7c1d9a045a
Christopher David <chris@openagents.com>
2024-10-17 10:31:58 -0500

    Refactor API routing to use groups and add routes for messages, threads, and projects in api.php.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\ThreadController;
+use App\Http\Controllers\ProjectController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Message routes
+    Route::post('/messages', [MessageController::class, 'store']);
+
+    // Thread routes
+    Route::post('/threads/{thread}/messages', [ThreadController::class, 'addMessage']);
+    Route::post('/threads/{thread}/process', [ThreadController::class, 'process']);
+
+    // Project routes
+    Route::get('/projects/{project}/threads', [ProjectController::class, 'threads']);
 });
