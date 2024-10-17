8f676dfd198bce1cdd83c87a0223ec947ee55506
Christopher David <chris@openagents.com>
2024-10-17 10:35:29 -0500

    Added TeamController and updated routing for team-related functionality.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ThreadController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TeamController;
 
 Route::view('/', 'homepage');
 Route::view('/components', 'components')->name('components');
@@ -11,13 +12,16 @@ Route::view('/components', 'components')->name('components');
 Route::middleware(['auth'])->group(function () {
     // Message routes
     Route::post('/messages', [MessageController::class, 'store']);
+    Route::post('/threads/{thread}/messages', [MessageController::class, 'storeInThread']);
 
     // Thread routes
-    Route::post('/threads/{thread}/messages', [ThreadController::class, 'addMessage']);
     Route::post('/threads/{thread}/process', [ThreadController::class, 'process']);
 
     // Project routes
     Route::get('/projects/{project}/threads', [ProjectController::class, 'threads']);
+
+    // Team routes
+    Route::get('/teams/{team}/threads', [TeamController::class, 'threads']);
 });
 
 require __DIR__ . '/auth.php';
