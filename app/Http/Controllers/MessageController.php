3d7c9b8a8bbbf4a100c5b9fe98f4f75e9270ae35
Christopher David <chris@openagents.com>
2024-10-17 10:35:40 -0500

    Add `Thread` model import in `MessageController` for future enhancements.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
+use App\Models\Thread;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
@@ -17,6 +18,20 @@ class MessageController extends Controller
         $message = Message::create([
             'thread_id' => $validatedData['thread_id'],
             'user_id' => auth()->id(),
+            'content' => $validatedData['content'],
+        ]);
+
+        return response()->json($message, 201);
+    }
+
+    public function storeInThread(Request $request, Thread $thread)
+    {
+        $validatedData = $request->validate([
+            'content' => 'required|string',
+        ]);
+
+        $message = $thread->messages()->create([
+            'user_id' => $request->input('user_id', auth()->id()),
             'content' => $validatedData['content'],
         ]);
 
