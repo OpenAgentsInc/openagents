e5da9af12dbacba03a30bf8d45c38b32b2e42335
Christopher David <chris@openagents.com>
2024-10-16 21:32:15 -0500

    component

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,8 +1,10 @@
 <x-layout>
     <main class="flex flex-col items-center justify-center min-h-screen p-4 bg-black text-white overflow-hidden">
         <h1 class="text-5xl font-bold mb-8 pointer-events-none select-none">OpenAgents</h1>
-        <x-button variant="outline" size="lg">
-            Sign up
-        </x-button>
+        <a href="{{ route('components') }}">
+            <x-button variant="outline" size="lg">
+                Component Library
+            </x-button>
+        </a>
     </main>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'homepage');
-Route::view('/components', 'components');
+Route::view('/components', 'components')->name('components');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
