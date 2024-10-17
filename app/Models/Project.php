3974877a2b21d551b629a1b5df698a1a17f71158
Christopher David <chris@openagents.com>
2024-10-17 10:11:08 -0500

    php artisan make:model -mf (Message,Project,Team,Thread)

---
+++ app/Models/Project.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProjectFactory> */
+    use HasFactory;
+}
