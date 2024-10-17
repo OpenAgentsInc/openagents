dfabe4d66e0f9bd8bebe9bb1f0cd5ae6ff42958d
Christopher David <chris@openagents.com>
2024-10-16 21:01:24 -0500

    Refactor Button component to include variant, size, and child props, enhancing flexibility and customization.

--- app/View/Components/Button.php
+++ app/View/Components/Button.php
@@ -2,25 +2,46 @@
 
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class Button extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
+    public $variant;
+    public $size;
+    public $asChild;
+
+    public function __construct($variant = 'default', $size = 'default', $asChild = false)
     {
-        //
+        $this->variant = $variant;
+        $this->size = $size;
+        $this->asChild = $asChild;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
+    public function render()
     {
         return view('components.button');
+    }
+
+    public function classes()
+    {
+        $baseClasses = 'button';
+        $variantClasses = [
+            'default' => 'button--default',
+            'destructive' => 'button--destructive',
+            'outline' => 'button--outline',
+            'secondary' => 'button--secondary',
+            'ghost' => 'button--ghost',
+            'link' => 'button--link',
+        ];
+        $sizeClasses = [
+            'default' => 'button--size-default',
+            'sm' => 'button--size-sm',
+            'lg' => 'button--size-lg',
+            'icon' => 'button--size-icon',
+        ];
+
+        return $baseClasses . ' ' . 
+               ($variantClasses[$this->variant] ?? '') . ' ' . 
+               ($sizeClasses[$this->size] ?? '');
     }
 }
