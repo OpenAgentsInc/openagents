998b34b488a63996dad650272f82a34e14864767
Christopher David <chris@openagents.com>
2024-10-17 10:37:31 -0500

    Add HasManyThrough import in Team.php to prepare for new relational methods.

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Team extends Model
 {
@@ -23,5 +24,10 @@ class Team extends Model
     public function users(): HasMany
     {
         return $this->hasMany(User::class);
+    }
+
+    public function threads(): HasManyThrough
+    {
+        return $this->hasManyThrough(Thread::class, Project::class);
     }
 }
