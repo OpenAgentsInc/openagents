d23beb6bfa071140d6c1bf696eb4121ca26832cf
Christopher David <chris@openagents.com>
2024-10-17 10:52:51 -0500

    Renamed section comment to 'Custom Authentication Routes' for clarity.

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,9 @@ Route::get('/', function () {
 
 Route::view('/components', 'components')->name('components');
 
-// Authentication Routes
+// Custom Authentication Routes
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     // Message routes
