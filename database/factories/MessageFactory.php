7505ecd43f92d87f11f2158ef14c2bcf7f67140b
Christopher David <chris@openagents.com>
2024-10-17 10:18:30 -0500

    Add User and Thread relationships to MessageFactory definition for dynamic data seeding.

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Thread;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,25 @@ class MessageFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'thread_id' => Thread::factory(),
+            'content' => $this->faker->paragraph,
+            'is_system_message' => false,
         ];
+    }
+
+    /**
+     * Indicate that the message is a system message.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function system()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'user_id' => null,
+                'is_system_message' => true,
+            ];
+        });
     }
 }
