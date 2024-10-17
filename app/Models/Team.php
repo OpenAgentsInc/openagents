f745f7677306890432909e58cf8b2c236b79a0e4
Christopher David <chris@openagents.com>
2024-10-17 10:23:36 -0500

    Add `fillable` attribute and `projects` relationship to `Team` model for improved data handling and association management.

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
     /** @use HasFactory<\Database\Factories\TeamFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
