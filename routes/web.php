45980f8072a4aa872ed5f71a7b8bbf09e2a8bb57
Christopher David <chris@openagents.com>
2024-10-17 10:34:37 -0500

    Added Message, Thread, and Project controllers with auth middleware for new routes in web.php.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\ThreadController;
+use App\Http\Controllers\ProjectController;
 
 Route::view('/', 'homepage');
 Route::view('/components', 'components')->name('components');
+
+Route::middleware(['auth'])->group(function () {
+    // Message routes
+    Route::post('/messages', [MessageController::class, 'store']);
+
+    // Thread routes
+    Route::post('/threads/{thread}/messages', [ThreadController::class, 'addMessage']);
+    Route::post('/threads/{thread}/process', [ThreadController::class, 'process']);
+
+    // Project routes
+    Route::get('/projects/{project}/threads', [ProjectController::class, 'threads']);
+});
 
 require __DIR__ . '/auth.php';
