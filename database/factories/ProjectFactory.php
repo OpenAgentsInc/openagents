7c4b9f801ab6315fbd72e1ebe20fe4c99f1aadb3
Christopher David <chris@openagents.com>
2024-10-17 10:18:43 -0500

    Update ProjectFactory to include 'name' and 'description' with faker data.

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,25 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->sentence,
+            'user_id' => User::factory(),
+            'team_id' => null,
         ];
+    }
+
+    /**
+     * Indicate that the project belongs to a team.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function forTeam()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'user_id' => null,
+                'team_id' => Team::factory(),
+            ];
+        });
     }
 }
