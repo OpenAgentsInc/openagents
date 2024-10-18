7e04251880f6703e327382be5cdfecb89623e5c5
Christopher David <chris@openagents.com>
2024-10-18 05:14:30 -0500

    center dat

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,6 +3,7 @@
     .sidebar-init #sidebarContent {
         transition: none !important;
     }
+
     #sidebarContent {
         opacity: var(--sidebar-content-opacity, 1);
         visibility: var(--sidebar-content-visibility, visible);
@@ -10,12 +11,12 @@
     }
 </style>
 <div id="sidebar" class="h-full overflow-hidden flex flex-col transition-all duration-300 ease-in-out"
-     style="width: var(--sidebar-width, 270px);">
+    style="width: var(--sidebar-width, 270px);">
     <div class="bg-background flex-grow border-r border-border flex flex-col">
         <div class="p-4">
             <button
                 id="sidebarToggle"
-                class="btn btn-square btn-sm btn-ghost rounded ml-0.5"
+                class="btn btn-square btn-sm btn-ghost rounded ml-1.5"
                 aria-label="Toggle sidebar">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
