a615a9c5eaa5dbe7e3000705d6bb25cb386689e5
Christopher David <chris@openagents.com>
2024-10-18 14:52:51 -0400

    homepagechattest

--- tests/Feature/HomepageChatTest.php
+++ tests/Feature/HomepageChatTest.php
@@ -2,9 +2,6 @@
 
 use App\Models\User;
 use App\Models\Thread;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-
-uses(RefreshDatabase::class);
 
 test('authenticated user can send a message from homepage and is redirected to new chat thread', function () {
     $user = User::factory()->create();
@@ -15,7 +12,7 @@ test('authenticated user can send a message from homepage and is redirected to n
         ]);
 
     $response->assertStatus(302); // Assert that there's a redirect
-    
+
     $thread = Thread::latest()->first();
     $response->assertRedirect("/chat/{$thread->id}");
 
