0356356ad4bd20ed5964915375a9d273f2545df0
Christopher David <chris@openagents.com>
2024-10-18 05:23:48 -0500

    Create file resources/views/partials/scripts.blade.php

---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,7 @@
+<script src="{{ asset('js/tailwind.min.js') }}"></script>
+<script src="{{ asset('js/tailwind-config.js') }}"></script>
+<script src="{{ asset('js/htmx.min.js') }}"></script>
+<script src="{{ asset('js/htmx-sse.js') }}"></script>
+<script src="{{ asset('js/sidebar.js') }}"></script>
+<script src="{{ asset('js/stream.js') }}"></script>
+@stack('scripts')
