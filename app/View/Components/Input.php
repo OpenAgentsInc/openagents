0ee665396318f8d3285e1be86a82faf23aa37f1f
Christopher David <chris@openagents.com>
2024-10-18 08:35:53 -0500

    hm

--- app/View/Components/Input.php
+++ app/View/Components/Input.php
@@ -39,7 +39,7 @@ class Input extends Component
                         value="{{ $value }}"
                         placeholder=" "
                         @if($required) required @endif
-                        {{ $attributes->merge(['class' => 'peer flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 h-14' . ($icon ? ' pl-10' : '')]) }}
+                        {{ $attributes->merge(['class' => 'peer flex w-full rounded-md border border-input bg-transparent px-3 py-2 pt-3 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 h-14' . ($icon ? ' pl-10' : '')]) }}
                     >
                     @if($label)
                         <label
