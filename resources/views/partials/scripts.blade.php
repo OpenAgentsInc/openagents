869824f6bfe3e0e5a7d1070092ac0230ac2aa7d1
Christopher David <chris@openagents.com>
2024-10-18 13:59:52 -0400

    fathom prod only

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -3,7 +3,9 @@
 <script src="{{ asset('js/htmx.min.js') }}"></script>
 <script src="{{ asset('js/htmx-sse.js') }}"></script>
 <script src="{{ asset('js/alpine.min.js') }}" defer></script>
+@production
 <script src="https://cdn.usefathom.com/script.js" data-site="COZQPXXY" defer></script>
+@endproduction
 @auth
 <script src="{{ asset('js/sidebar.js') }}"></script>
 @endauth
