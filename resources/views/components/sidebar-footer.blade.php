150c31ad2e0531c8aa468a4a657efe8abd4b0bb6
Christopher David <chris@openagents.com>
2024-10-18 17:23:24 -0400

    nice

--- resources/views/components/sidebar-footer.blade.php
+++ resources/views/components/sidebar-footer.blade.php
@@ -3,11 +3,11 @@
         <button @click="open = !open" class="flex items-center space-x-3 w-full cursor-pointer">
             <div class="avatar">
                 <div class="w-10 rounded-full">
-                    <img src="https://pbs.twimg.com/profile_images/1607882836740120576/3Tg1mTYJ_400x400.jpg" alt="User avatar" />
+                    <img class="rounded-full" src="https://pbs.twimg.com/profile_images/1607882836740120576/3Tg1mTYJ_400x400.jpg" alt="User avatar" />
                 </div>
             </div>
             <div class="flex-grow text-left">
-                <p class="font-medium">Christopher David</p>
+                <p class="text-sm font-medium">Christopher David</p>
                 <p class="text-xs text-muted-foreground">chris@openagents.com</p>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 transition duration-200 ease-in-out" :class="{ 'transform rotate-180': open }">
