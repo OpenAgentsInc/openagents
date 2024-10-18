69c512e8b2542cafcba2455dc057b8bae6b9a55a
Christopher David <chris@openagents.com>
2024-10-18 06:18:28 -0500

    Refactor Input component to use PHP 8 constructor property promotion.

--- app/View/Components/Input.php
+++ app/View/Components/Input.php
@@ -39,12 +39,12 @@ class Input extends Component
                         placeholder="{{ $placeholder }}"
                         value="{{ $value }}"
                         @if($required) required @endif
-                        {{ $attributes->merge(['class' => 'peer flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50' . ($icon ? ' pl-10' : '')]) }}
+                        {{ $attributes->merge(['class' => 'peer flex h-14 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50' . ($icon ? ' pl-12' : '')]) }}
                     >
                     @if($label)
                         <label
                             for="{{ $uuid }}"
-                            class="absolute left-2 -top-2 z-10 px-2 text-xs text-muted-foreground bg-background"
+                            class="absolute text-sm text-muted-foreground duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-background px-2 peer-focus:px-2 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                         >
                             {{ $label }}
                         </label>
