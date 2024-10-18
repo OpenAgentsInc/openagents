2700c32f26477f08d0e674ce0b8ee5e0f47e6563
Christopher David <chris@openagents.com>
2024-10-18 15:09:24 -0400

    fix chat show layout

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -1,28 +1,22 @@
-@extends('layouts.app')
+<x-layout>
+    <div class="container">
+        <h1>Chat Thread: {{ $thread->title }}</h1>
 
-@section('content')
-<div class="container">
-    <h1>Chat Thread: {{ $thread->title }}</h1>
-    
-    <div id="chat-messages" hx-ext="sse" sse-connect="{{ route('chat.stream', ['thread' => $thread->id]) }}">
-        <div sse-swap="message">
-            <!-- Existing messages will be loaded here -->
-            @foreach ($thread->messages as $message)
+        <div id="chat-messages" hx-ext="sse" sse-connect="{{ route('chat.stream', ['thread' => $thread->id]) }}">
+            <div sse-swap="message">
+                <!-- Existing messages will be loaded here -->
+                @foreach ($thread->messages as $message)
                 @include('partials.message', ['message' => $message])
-            @endforeach
+                @endforeach
+            </div>
         </div>
+
+        <form hx-post="{{ route('messages.store', ['thread' => $thread->id]) }}" hx-target="#chat-messages" hx-swap="beforeend">
+            @csrf
+            <div class="form-group">
+                <textarea name="content" class="form-control" rows="3" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Send</button>
+        </form>
     </div>
-
-    <form hx-post="{{ route('messages.store', ['thread' => $thread->id]) }}" hx-target="#chat-messages" hx-swap="beforeend">
-        @csrf
-        <div class="form-group">
-            <textarea name="content" class="form-control" rows="3" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Send</button>
-    </form>
-</div>
-@endsection
-
-@push('scripts')
-<script src="https://unpkg.com/htmx-ext-sse@2.2.2/sse.js"></script>
-@endpush
+</x-layout>
