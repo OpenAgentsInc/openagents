5a4dead821118dc77991c9ef5d03ab55b75908c8
Christopher David <chris@openagents.com>
2024-10-18 06:13:28 -0500

    Refactor Input component: Remove unused 'uuid' property to simplify code.

--- app/View/Components/Input.php
+++ app/View/Components/Input.php
@@ -29,9 +29,7 @@ class Input extends Component
                 <div class="relative">
                     @if($icon)
                         <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg class="h-5 w-5 text-muted-foreground" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="{{ self::getIconPath($icon) }}" clip-rule="evenodd" />
-                            </svg>
+                            {!! $icon !!}
                         </div>
                     @endif
                     <input
@@ -54,16 +52,5 @@ class Input extends Component
                 </div>
             </div>
         blade;
-    }
-
-    private static function getIconPath($icon)
-    {
-        $paths = [
-            'user' => 'M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z',
-            'envelope' => 'M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z',
-            'lock-closed' => 'M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z',
-        ];
-
-        return $paths[$icon] ?? '';
     }
 }
