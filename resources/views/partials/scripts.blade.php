788b50b66aecf703b2b998691ec3be61a4f72d12
Christopher David <chris@openagents.com>
2024-10-18 10:22:48 -0500

    Add Fathom analytics

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -2,6 +2,7 @@
 <script src="{{ asset('js/tailwind-config.js') }}"></script>
 <script src="{{ asset('js/htmx.min.js') }}"></script>
 <script src="{{ asset('js/htmx-sse.js') }}"></script>
+<script src="https://cdn.usefathom.com/script.js" data-site="COZQPXXY" defer></script>
 @auth
 <script src="{{ asset('js/sidebar.js') }}"></script>
 @endauth
