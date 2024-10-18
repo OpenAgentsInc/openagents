67437de72c6badbb0d6e704c11d5e936436fd3e0
Christopher David <chris@openagents.com>
2024-10-18 13:57:11 -0400

    install alpine

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -2,6 +2,7 @@
 <script src="{{ asset('js/tailwind-config.js') }}"></script>
 <script src="{{ asset('js/htmx.min.js') }}"></script>
 <script src="{{ asset('js/htmx-sse.js') }}"></script>
+<script src="{{ asset('js/alpine.min.js') }}" defer></script>
 <script src="https://cdn.usefathom.com/script.js" data-site="COZQPXXY" defer></script>
 @auth
 <script src="{{ asset('js/sidebar.js') }}"></script>
