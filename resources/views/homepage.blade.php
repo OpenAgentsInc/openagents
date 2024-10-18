cf6888c3341935c5fe5852642d098eb51207f183
Christopher David <chris@openagents.com>
2024-10-18 05:52:28 -0500

    cool

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -20,6 +20,9 @@
                         <a href="https://stage2.openagents.com" target="_blank" rel="noopener noreferrer" class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 w-full mt-6 pointer-events-auto">
                             Access previous version
                         </a>
+                        <a href="/register" rel="noopener noreferrer" class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 w-full mt-4 pointer-events-auto">
+                            Explore unfinished v3
+                        </a>
                     </x-card-content>
                 </x-card>
             </div>
