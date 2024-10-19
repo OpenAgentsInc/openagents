f33d9243cc7216f9342555584f2e390f32938ace
Christopher David <chris@openagents.com>
2024-10-19 15:49:28 -0400

    Refactor error display in register.blade.php for better readability and maintenance.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,12 +36,14 @@
                     name="password"
                     type="password"
                     required
+                    autocomplete="new-password"
                     :icon="'<svg class=\'h-5 w-5 text-muted-foreground\' xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' fill=\'none\' stroke=\'currentColor\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-linejoin=\'round\'><rect x=\'3\' y=\'11\' width=\'18\' height=\'11\' rx=\'2\' ry=\'2\'></rect><path d=\'M7 11V7a5 5 0 0 1 10 0v4\'></path></svg>'" />
                 <x-input
                     label="Confirm Password"
                     name="password_confirmation"
                     type="password"
                     required
+                    autocomplete="new-password"
                     :icon="'<svg class=\'h-5 w-5 text-muted-foreground\' xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' fill=\'none\' stroke=\'currentColor\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-linejoin=\'round\'><rect x=\'3\' y=\'11\' width=\'18\' height=\'11\' rx=\'2\' ry=\'2\'></rect><path d=\'M7 11V7a5 5 0 0 1 10 0v4\'></path></svg>'" />
 
                 <div class="flex justify-end items-center space-x-4">
