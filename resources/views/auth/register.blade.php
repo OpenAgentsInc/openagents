50a56e2672f160e8fff8b5e7991810156699440a
Christopher David <chris@openagents.com>
2024-10-19 15:54:15 -0400

    Refactor error display in register.blade.php for enhanced readability and maintenance.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,6 +29,7 @@
                     name="email"
                     type="email"
                     required
+                    autocomplete="username"
                     :value="old('email')"
                     :icon="'<svg class=\'h-5 w-5 text-muted-foreground\' xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' fill=\'none\' stroke=\'currentColor\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-linejoin=\'round\'><path d=\'M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z\'></path><polyline points=\'22,6 12,13 2,6\'></polyline></svg>'" />
                 <x-input
