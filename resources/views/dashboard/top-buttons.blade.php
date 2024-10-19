5d93c2710bd8a60823f9864a45dcbdc29015a9a4
Christopher David <chris@openagents.com>
2024-10-19 15:16:11 -0400

    clean buttons

--- resources/views/dashboard/top-buttons.blade.php
+++ resources/views/dashboard/top-buttons.blade.php
@@ -1,17 +1,17 @@
 <div class="flex space-x-2">
     @guest
-        <x-button tag="a" href="{{ route('login') }}" variant="secondary">
-            Log in
-        </x-button>
-        <x-button tag="a" href="{{ route('register') }}">
-            Sign up
-        </x-button>
+    <x-button tag="a" href="{{ route('login') }}">
+        Log in
+    </x-button>
+    <x-button tag="a" href="{{ route('register') }}" variant="secondary">
+        Sign up
+    </x-button>
     @else
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <x-button type="submit" variant="destructive">
-                Logout
-            </x-button>
-        </form>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <x-button type="submit" variant="destructive">
+            Logout
+        </x-button>
+    </form>
     @endguest
 </div>
