473d43fbd1c53ae9f2cec5bf3b7754c2a1d3849e
Christopher David <chris@openagents.com>
2024-10-19 19:35:02 -0400

    Refactor test to check teams and projects for active team only in LoadTeamsAndProjectsTest.

--- tests/Feature/LoadTeamsAndProjectsTest.php
+++ tests/Feature/LoadTeamsAndProjectsTest.php
@@ -12,7 +12,7 @@ class LoadTeamsAndProjectsTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_getTeamsAndProjects_returns_actual_teams_and_projects()
+    public function test_getTeamsAndProjects_returns_teams_and_projects_for_active_team()
     {
         // Create a user
         $user = User::factory()->create();
@@ -23,6 +23,10 @@ class LoadTeamsAndProjectsTest extends TestCase
 
         // Associate user with teams
         $user->teams()->attach([$team1->id, $team2->id]);
+
+        // Set active team
+        $user->currentTeam = $team1;
+        $user->save();
 
         // Create projects
         $project1 = Project::factory()->create(['team_id' => $team1->id]);
@@ -40,7 +44,7 @@ class LoadTeamsAndProjectsTest extends TestCase
         $response->assertSee($team2->name);
         $response->assertSee($project1->name);
         $response->assertSee($project2->name);
-        $response->assertSee($project3->name);
+        $response->assertDontSee($project3->name);
 
         // Assert that the response doesn't contain teams not associated with the user
         $team3 = Team::factory()->create();
@@ -49,5 +53,32 @@ class LoadTeamsAndProjectsTest extends TestCase
         // Assert that the response doesn't contain projects from teams not associated with the user
         $project4 = Project::factory()->create(['team_id' => $team3->id]);
         $response->assertDontSee($project4->name);
+    }
+
+    public function test_getTeamsAndProjects_returns_personal_projects_when_no_active_team()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Create teams
+        $team1 = Team::factory()->create();
+
+        // Associate user with teams
+        $user->teams()->attach([$team1->id]);
+
+        // Create projects
+        $personalProject = Project::factory()->create(['team_id' => null, 'user_id' => $user->id]);
+        $teamProject = Project::factory()->create(['team_id' => $team1->id]);
+
+        // Make request to getTeamsAndProjects endpoint
+        $response = $this->actingAs($user)->get('/teams');
+
+        // Assert response status
+        $response->assertStatus(200);
+
+        // Assert that the response contains the correct teams and projects
+        $response->assertSee($team1->name);
+        $response->assertSee($personalProject->name);
+        $response->assertDontSee($teamProject->name);
     }
 }
