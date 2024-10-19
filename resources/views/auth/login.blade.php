890c268d1dbcb88d23612ec405ad268f0d4d702f
Christopher David <chris@openagents.com>
2024-10-19 15:55:24 -0400

    Refactor login form UI in login.blade.php for improved error display and alignment.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,6 +22,7 @@
                     name="email"
                     type="email"
                     required
+                    autocomplete="username"
                     :value="old('email')"
                     :icon="'<svg class=\'h-5 w-5 text-muted-foreground\' xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' fill=\'none\' stroke=\'currentColor\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-linejoin=\'round\'><path d=\'M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z\'></path><polyline points=\'22,6 12,13 2,6\'></polyline></svg>'" />
                 <x-input
@@ -29,6 +30,7 @@
                     name="password"
                     type="password"
                     required
+                    autocomplete="current-password"
                     :icon="'<svg class=\'h-5 w-5 text-muted-foreground\' xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' fill=\'none\' stroke=\'currentColor\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-linejoin=\'round\'><rect x=\'3\' y=\'11\' width=\'18\' height=\'11\' rx=\'2\' ry=\'2\'></rect><path d=\'M7 11V7a5 5 0 0 1 10 0v4\'></path></svg>'" />
 
                 <div class="flex justify-end items-center space-x-4">
