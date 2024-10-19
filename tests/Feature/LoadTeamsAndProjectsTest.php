5c4390f6bb3392cc63c2fa7a575ba3a1dd1c49e0
Christopher David <chris@openagents.com>
2024-10-19 19:24:01 -0400

    Refactor LoadTeamsAndProjectsTest to remove hardcoded team names in team creation.

--- tests/Feature/LoadTeamsAndProjectsTest.php
+++ tests/Feature/LoadTeamsAndProjectsTest.php
@@ -18,16 +18,16 @@ class LoadTeamsAndProjectsTest extends TestCase
         $user = User::factory()->create();
 
         // Create teams
-        $team1 = Team::factory()->create(['name' => 'Team 1']);
-        $team2 = Team::factory()->create(['name' => 'Team 2']);
+        $team1 = Team::factory()->create();
+        $team2 = Team::factory()->create();
 
         // Associate user with teams
         $user->teams()->attach([$team1->id, $team2->id]);
 
         // Create projects
-        $project1 = Project::factory()->create(['name' => 'Project A', 'team_id' => $team1->id]);
-        $project2 = Project::factory()->create(['name' => 'Project B', 'team_id' => $team1->id]);
-        $project3 = Project::factory()->create(['name' => 'Project C', 'team_id' => $team2->id]);
+        $project1 = Project::factory()->create(['team_id' => $team1->id]);
+        $project2 = Project::factory()->create(['team_id' => $team1->id]);
+        $project3 = Project::factory()->create(['team_id' => $team2->id]);
 
         // Make request to getTeamsAndProjects endpoint
         $response = $this->actingAs($user)->get('/teams');
@@ -36,18 +36,18 @@ class LoadTeamsAndProjectsTest extends TestCase
         $response->assertStatus(200);
 
         // Assert that the response contains the correct teams and projects
-        $response->assertSee('Team 1');
-        $response->assertSee('Team 2');
-        $response->assertSee('Project A');
-        $response->assertSee('Project B');
-        $response->assertSee('Project C');
+        $response->assertSee($team1->name);
+        $response->assertSee($team2->name);
+        $response->assertSee($project1->name);
+        $response->assertSee($project2->name);
+        $response->assertSee($project3->name);
 
         // Assert that the response doesn't contain teams not associated with the user
-        $team3 = Team::factory()->create(['name' => 'Team 3']);
-        $response->assertDontSee('Team 3');
+        $team3 = Team::factory()->create();
+        $response->assertDontSee($team3->name);
 
         // Assert that the response doesn't contain projects from teams not associated with the user
-        $project4 = Project::factory()->create(['name' => 'Project D', 'team_id' => $team3->id]);
-        $response->assertDontSee('Project D');
+        $project4 = Project::factory()->create(['team_id' => $team3->id]);
+        $response->assertDontSee($project4->name);
     }
 }
