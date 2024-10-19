1a514a272f48b5c4c7ff45f3fa3ab676d3592773
Christopher David <chris@openagents.com>
2024-10-19 18:57:10 -0400

    Create file resources/views/components/sidebar/team-switcher-content.blade.php

---
+++ resources/views/components/sidebar/team-switcher-content.blade.php
@@ -0,0 +1,5 @@
+<!-- Team Dropdown -->
+<x-dropdown label="Team" :items="$teams" id="teamSwitcher" class="w-full" />
+
+<!-- Project Dropdown -->
+<x-dropdown label="Project" :items="$projects" id="projectSwitcher" class="w-full" />
