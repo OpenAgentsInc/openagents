e277b21dc2f5afc5a7e6acf3061e08150890600e
Christopher David <chris@openagents.com>
2024-10-19 15:45:26 -0400

    fix sidebar layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,10 +25,8 @@
     <div x-data="{ sidebarCollapsed: localStorage.getItem('sidebarCollapsed') === 'true' }" class="flex h-full">
         @auth <x-sidebar /> @endauth
         <div
-            x-bind:class="{ 'ml-[60px]': sidebarCollapsed, 'ml-[270px]': !sidebarCollapsed }"
             class="flex-1 flex flex-col h-full relative transition-all duration-300 ease-in-out invisible"
-            x-init="$nextTick(() => $el.classList.remove('invisible'))"
-        >
+            x-init="$nextTick(() => $el.classList.remove('invisible'))">
             <header class="absolute top-0 left-0 right-0 flex justify-between items-center bg-transparent border-b border-border md:border-none p-0 sm:p-3 z-[9999]">
                 <div class="flex flex-row items-center gap-x-4 px-3 py-1">
                     <x-app-brand />
