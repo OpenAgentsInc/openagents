7aa2cf1325ad856037292954024a42711a47618a
Christopher David <chris@openagents.com>
2024-10-19 19:08:45 -0400

    Add caching import and simulate delay in getTeamsAndProjects method in TeamController.

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Team;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class TeamController extends Controller
 {
@@ -15,9 +16,12 @@ class TeamController extends Controller
 
     public function getTeamsAndProjects()
     {
+        // Simulate a 1-second delay
+        sleep(1);
+
         // TODO: Fetch actual teams and projects from the database
         $teams = ['OpenAgents', 'Atlantis Ports', 'RoA'];
-        $projects = [''];
+        $projects = ['Project A', 'Project B', 'Project C'];
 
         return view('components.sidebar.team-switcher-content', compact('teams', 'projects'));
     }
