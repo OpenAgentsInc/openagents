512f7ab93fa52a9cb3d7a26115e535f9199eeb69
Christopher David <chris@openagents.com>
2024-10-19 19:25:20 -0400

    Add 'name' column to 'teams' table in migration.

--- database/migrations/2024_10_17_151011_create_teams_table.php
+++ database/migrations/2024_10_17_151011_create_teams_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
