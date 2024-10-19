358f0d2fefdb87242777bc751647f5389f989fad
Christopher David <chris@openagents.com>
2024-10-19 15:43:39 -0400

    Refactor layout.blade.php to remove redundant CSS link.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,9 +22,13 @@
 </head>
 
 <body hx-boost="true" class="bg-background text-foreground font-mono h-screen overflow-hidden antialiased">
-    <div class="flex h-full">
+    <div x-data="{ sidebarCollapsed: localStorage.getItem('sidebarCollapsed') === 'true' }" class="flex h-full">
         @auth <x-sidebar /> @endauth
-        <div class="flex-1 flex flex-col h-full relative">
+        <div
+            x-bind:class="{ 'ml-[60px]': sidebarCollapsed, 'ml-[270px]': !sidebarCollapsed }"
+            class="flex-1 flex flex-col h-full relative transition-all duration-300 ease-in-out invisible"
+            x-init="$nextTick(() => $el.classList.remove('invisible'))"
+        >
             <header class="absolute top-0 left-0 right-0 flex justify-between items-center bg-transparent border-b border-border md:border-none p-0 sm:p-3 z-[9999]">
                 <div class="flex flex-row items-center gap-x-4 px-3 py-1">
                     <x-app-brand />
