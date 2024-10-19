8679d8b7dfc090506cc5ae9c6066b724660b069f
Christopher David <chris@openagents.com>
2024-10-19 19:34:45 -0400

    Update team-switcher-content.blade.php to preselect active team and add JS to reload page on team change.

--- resources/views/components/sidebar/team-switcher-content.blade.php
+++ resources/views/components/sidebar/team-switcher-content.blade.php
@@ -1,5 +1,12 @@
 <!-- Team Dropdown -->
-<x-dropdown label="Team" :items="$teams" id="teamSwitcher" class="w-full" />
+<x-dropdown label="Team" :items="$teams" id="teamSwitcher" class="w-full" :selected="$activeTeam ? $activeTeam->id : null" />
 
 <!-- Project Dropdown -->
 <x-dropdown label="Project" :items="$projects" id="projectSwitcher" class="w-full" />
+
+<script>
+    document.getElementById('teamSwitcher').addEventListener('change', function(event) {
+        // Reload the page with the new team context
+        window.location.href = '/switch-team/' + event.target.value;
+    });
+</script>
