3876b94296072b5ba5ab50a33dc7b4f5056db841
Christopher David <chris@openagents.com>
2024-10-19 19:06:31 -0400

    Add `loading` parameter to Dropdown component constructor for state management.

--- app/View/Components/Dropdown.php
+++ app/View/Components/Dropdown.php
@@ -15,7 +15,8 @@ class Dropdown extends Component
         public ?string $id = null,
         public bool $checkable = false,
         public ?string $icon = null,
-        public ?string $class = ''
+        public ?string $class = '',
+        public bool $loading = false
     ) {
         $this->uuid = $id ?? Str::uuid();
     }
@@ -23,22 +24,36 @@ class Dropdown extends Component
     public function render()
     {
         return <<<'BLADE'
-            <div x-data="{ open: false }" class="relative inline-block text-left {{ $class }}">
+            <div x-data="{ open: false, loading: @js($loading) }" 
+                 class="relative inline-block text-left {{ $class }}"
+                 x-bind:class="{ 'opacity-50 cursor-not-allowed': loading }"
+                 id="{{ $uuid }}">
                 <div>
-                    <button @click="open = !open" type="button" class="inline-flex justify-between w-full rounded-md border border-sidebar-border bg-transparent px-4 py-2 text-sm font-medium text-sidebar-foreground shadow-sm hover:bg-sidebar-accent focus:outline-none focus:ring-0 focus:ring-sidebar-ring" id="{{ $uuid }}" aria-haspopup="true" aria-expanded="false">
+                    <button @click="!loading && (open = !open)" type="button" 
+                            class="inline-flex justify-between w-full rounded-md border border-sidebar-border bg-transparent px-4 py-2 text-sm font-medium text-sidebar-foreground shadow-sm hover:bg-sidebar-accent focus:outline-none focus:ring-0 focus:ring-sidebar-ring" 
+                            aria-haspopup="true" x-bind:aria-expanded="open.toString()"
+                            x-bind:disabled="loading">
                         <span class="flex items-center">
                             @if($icon)
                                 <span class="mr-2">{!! $icon !!}</span>
                             @endif
-                            {{ $label }}
+                            <span x-text="loading ? 'Loading...' : '{{ $label }}'"></span>
                         </span>
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
+                        <template x-if="loading">
+                            <svg class="animate-spin h-5 w-5 text-sidebar-foreground" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
+                        </template>
+                        <template x-if="!loading">
+                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </template>
                     </button>
                 </div>
 
-                <div x-show="open" @click.away="open = false" class="origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg bg-sidebar-background ring-1 ring-sidebar-border ring-opacity-5 divide-y divide-sidebar-border z-50">
+                <div x-show="open && !loading" @click.away="open = false" class="origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg bg-sidebar-background ring-1 ring-sidebar-border ring-opacity-5 divide-y divide-sidebar-border z-50">
                     <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="{{ $uuid }}">
                         @foreach($items as $item)
                             @if($checkable)
