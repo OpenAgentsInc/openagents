e306308eb07f55d77373d966e91ea664f0ce87c3
Christopher David <chris@openagents.com>
2024-10-21 04:04:19 -0400

    Refactor chat message display to use a partial template for better maintainability.

--- resources/views/partials/chat_messages.blade.php
+++ resources/views/partials/chat_messages.blade.php
@@ -1,10 +1,5 @@
 @forelse($messages as $message)
-    <div class="mb-4 {{ $message->user_id == auth()->id() ? 'text-right' : 'text-left' }}">
-        <div class="inline-block bg-zinc-800 text-white rounded px-4 py-2">
-            <p class="text-sm">{{ $message->content }}</p>
-            <p class="text-xs text-zinc-400">{{ $message->created_at->format('M d, Y H:i') }}</p>
-        </div>
-    </div>
+    @include('partials.message', ['message' => $message])
 @empty
     <x-empty-message-list />
 @endforelse
