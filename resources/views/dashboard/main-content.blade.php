ad378ce91a7b6b56cd6ead8e39feb77403ca07e0
Christopher David <chris@openagents.com>
2024-10-21 01:25:11 -0400

    Add conditional rendering for `$thread` variable in `main-content.blade.php` to enhance content flexibility.

--- resources/views/dashboard/main-content.blade.php
+++ resources/views/dashboard/main-content.blade.php
@@ -3,13 +3,17 @@
         <div class="h-full w-full lg:py-[18px]">
             <div class="m-auto text-base px-3 md:px-4 w-full md:px-5 lg:px-4 xl:px-5 h-full">
                 <div id="main-content-inner" class="mx-auto flex h-full w-full flex-col text-base justify-center md:max-w-3xl">
-                    <div class="mb-7 text-center">
-                        <div class="select-none pointer-events-none inline-flex justify-center text-2xl font-semibold leading-9">
-                            <h1>How can we help?</h1>
+                    @if(isset($thread) && $thread->messages->isNotEmpty())
+                        @include('chat.message-list', ['messages' => $thread->messages])
+                    @else
+                        <div class="mb-7 text-center">
+                            <div class="select-none pointer-events-none inline-flex justify-center text-2xl font-semibold leading-9">
+                                <h1>How can we help?</h1>
+                            </div>
                         </div>
-                    </div>
-                    @include('dashboard.message-form')
-                    @include('dashboard.terms-privacy')
+                        @include('dashboard.message-form')
+                        @include('dashboard.terms-privacy')
+                    @endif
                 </div>
             </div>
         </div>
