49e6f07041be65d28de6484dd625dc7035ddb594
Christopher David <chris@openagents.com>
2024-10-21 05:38:10 -0400

    php artisan dusk:install

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,9 @@
 <?php
 
+pest()->extend(Tests\DuskTestCase::class)
+//  ->use(Illuminate\Foundation\Testing\DatabaseMigrations::class)
+    ->in('Browser');
+
 /*
 |--------------------------------------------------------------------------
 | Test Case
