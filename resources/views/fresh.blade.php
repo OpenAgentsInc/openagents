28ad64f3cd9205f9da2015608edcdbf4086ab704
Christopher David <chris@openagents.com>
2024-10-21 03:33:10 -0400

    testing fresh view

--- resources/views/fresh.blade.php
+++ resources/views/fresh.blade.php
@@ -1,12 +1,12 @@
 <x-layouts.empty>
     <div class="h-screen w-screen flex bg-background">
         <!-- Sidebar -->
-        <div class="w-1/4 bg-gray-800 text-white p-4">
-            <h2 class="text-xl font-bold mb-4">Chats</h2>
+        <div class="w-1/4 bg-zinc-800 text-white p-4">
+            <h2 class="pt-12 text-xl font-bold mb-4">Chats</h2>
             <ul>
-                <li class="mb-2"><a href="/chat/1" class="hover:text-gray-300">Chat 1</a></li>
-                <li class="mb-2"><a href="/chat/2" class="hover:text-gray-300">Chat 2</a></li>
-                <li class="mb-2"><a href="/chat/3" class="hover:text-gray-300">Chat 3</a></li>
+                <li class="mb-2"><a href="/chat/1" class="hover:text-zinc-300">Chat 1</a></li>
+                <li class="mb-2"><a href="/chat/2" class="hover:text-zinc-300">Chat 2</a></li>
+                <li class="mb-2"><a href="/chat/3" class="hover:text-zinc-300">Chat 3</a></li>
             </ul>
         </div>
 
