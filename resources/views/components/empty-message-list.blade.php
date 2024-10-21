69f8efa135bd903abdab849bae3de5c4d33264a4
Christopher David <chris@openagents.com>
2024-10-21 03:59:11 -0400

    Remove conditional rendering of default text in empty-message-list.blade.php to always display "Send your first message".

--- resources/views/components/empty-message-list.blade.php
+++ resources/views/components/empty-message-list.blade.php
@@ -1,3 +1,3 @@
 <div class="flex items-center justify-center h-full">
-    <p class="text-gray-500 text-lg">{{ $slot ?? 'Send your first message' }}</p>
+    <p class="text-gray-500 text-lg">Send your first message</p>
 </div>
