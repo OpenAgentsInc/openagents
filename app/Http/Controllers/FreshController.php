2049bea0255a7b4c3f71f94247f5f38d00348ae2
Christopher David <chris@openagents.com>
2024-10-21 03:39:51 -0400

    "Add Message model import in FreshController for enhanced message handling."

--- app/Http/Controllers/FreshController.php
+++ app/Http/Controllers/FreshController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Thread;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class FreshController extends Controller
@@ -17,5 +18,21 @@ class FreshController extends Controller
     {
         $messages = $thread->messages()->orderBy('created_at', 'asc')->get();
         return view('partials.chat_messages', compact('messages'));
+    }
+
+    public function sendMessage(Request $request, Thread $thread)
+    {
+        $request->validate([
+            'content' => 'required|string',
+        ]);
+
+        $message = new Message([
+            'content' => $request->content,
+            'user_id' => auth()->id(),
+        ]);
+
+        $thread->messages()->save($message);
+
+        return view('partials.chat_messages', ['messages' => [$message]]);
     }
 }
