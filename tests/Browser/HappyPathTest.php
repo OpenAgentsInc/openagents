0be059984d6ced1c7a1395ca68e202b40f5a59fb
Christopher David <chris@openagents.com>
2024-10-21 05:58:58 -0400

    frontpage nah nothin

--- tests/Browser/HappyPathTest.php
+++ tests/Browser/HappyPathTest.php
@@ -10,21 +10,8 @@ test('Happy Path', function () {
             ->waitForText('OpenAgents')
             ->assertSee('OpenAgents')
 
-            // I see the how can we help section with form
-            ->assertSee('How can we help?')
-
-            // I can submit the form
-            ->type('content', 'What does this do')
-            ->click('button[type="submit"]')
-
-            // The URL updates to chat/{id}
-            ->waitForLocation('/chat/1')
-
-            // I see the message I sent on this page
-            ->assertSee('What does this do')
-
-            // And I see a loading indicator that my message is being responded to
-            ->assertSee('Typing...')
+            // I see the homepage
+            ->assertSee('OpenAgents will return')
 
             // I see login buttons
             ->assertSee('Log in')
@@ -49,6 +36,19 @@ test('Happy Path', function () {
             // I see the form with prompt
             ->waitForText('How can we help?')
             ->assertSee('How can we help?')
+
+            // I can submit the form
+            ->type('content', 'What does this do')
+            ->click('button[type="submit"]')
+
+            // The URL updates to chat/{id}
+            ->waitForLocation('/chat/1')
+
+            // I see the message I sent on this page
+            ->assertSee('What does this do')
+
+            // And I see a loading indicator that my message is being responded to
+            ->assertSee('Typing...')
 
             ->screenshot('done');
     });
