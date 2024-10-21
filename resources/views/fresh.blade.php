143ebcffc3c626f6ab8099a9bb53b093bc024112
Christopher David <chris@openagents.com>
2024-10-21 04:27:20 -0400

    messageform

--- resources/views/fresh.blade.php
+++ resources/views/fresh.blade.php
@@ -5,23 +5,23 @@
             <h2 class="pt-12 text-xl font-bold mb-4">Chats</h2>
             <ul>
                 @foreach($threads as $thread)
-                    <li class="mb-2">
-                        <a href="#" class="hover:text-zinc-300" hx-get="{{ route('chat.messages', $thread) }}" hx-target="#message-list" hx-trigger="click">
-                            {{ $thread->title }}
-                        </a>
-                    </li>
+                <li class="mb-2">
+                    <a href="#" class="hover:text-zinc-300" hx-get="{{ route('chat.messages', $thread) }}" hx-target="#message-list" hx-trigger="click">
+                        {{ $thread->title }}
+                    </a>
+                </li>
                 @endforeach
             </ul>
         </div>
 
         <!-- Main Content -->
-        <div class="w-3/4 flex flex-col h-screen">
+        <div class="w-3/4 flex flex-col h-screen justify-center items-center">
             <div id="main-content" class="flex-grow overflow-y-auto p-4">
                 <div id="message-list" class="space-y-4">
                     <x-empty-message-list />
                 </div>
             </div>
-            <div class="p-4 bg-zinc-700">
+            <div class="p-4 w-[650px]">
                 @include('dashboard.message-form', ['thread' => $threads->first()])
             </div>
         </div>
