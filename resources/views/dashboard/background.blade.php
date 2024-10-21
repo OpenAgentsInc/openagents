083cc1283492f0d0e35dd1e563a17f3bd2d30e9b
GitHub API <noreply@github.com>
2024-10-21 09:51:38 -0400

    Delete resources/views/dashboard/background.blade.php

--- resources/views/dashboard/background.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="absolute inset-0 z-0">
-    <div class="absolute inset-0 bg-grid opacity-20"></div>
-    <div class="absolute inset-0 overflow-hidden">
-        <div class="absolute inset-0 neural-network"></div>
-    </div>
-</div>
