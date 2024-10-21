c6f392932cf02896e435247014bb272f0275dad8
Christopher David <chris@openagents.com>
2024-10-21 03:00:40 -0400

    Refactor `web.php` to improve readability by removing redundant `use` statements.

--- routes/web.php
+++ routes/web.php
@@ -28,34 +28,18 @@ Route::view('/plans', function () {
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
-    Route::get('/chat/{id}', [ChatController::class, 'show'])->name('chat.show');
+    Route::get('/chat/{thread}', [ChatController::class, 'show'])->name('chat.show');
     Route::post('/chat/{thread}/send', [ChatController::class, 'send'])->name('chat.send');
 
-
     // Message routes
-    // Route::post('/messages', [MessageController::class, 'store']);
-    // Route::post('/threads/{thread}/messages', [MessageController::class, 'storeInThread'])->name('messages.store');
     Route::post('/send-message', [MessageController::class, 'sendMessage'])->name('send-message');
 
-    // // Thread routes
+    // Thread routes
     Route::get('/threads', [ThreadController::class, 'index'])->name('threads.index');
-    // Route::get('/threads/{thread}/messages', [ThreadController::class, 'messages'])->name('threads.messages');
-    // Route::post('/threads/{thread}/process', [ThreadController::class, 'process']);
-    // Route::get('/chat/{thread}', [ThreadController::class, 'show'])->name('chat.show');
-    // Route::post('/threads/{thread}/add-message', [ThreadController::class, 'addMessage'])->name('threads.addMessage');
     Route::post('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
 
-    // // Project routes
-    // Route::get('/projects/{project}/threads', [ProjectController::class, 'threads']);
-
-    // // Team routes
-    // Route::get('/teams/{team}/threads', [TeamController::class, 'threads']);
+    // Team routes
     Route::get('/teams', [TeamController::class, 'getTeamsAndProjects'])->name('teams.get');
-    // Route::post('/switch-team/{team}', [TeamController::class, 'switchTeam'])->name('switch-team');
-    // Route::post('/switch-project/{project}', [TeamController::class, 'switchProject'])->name('switch-project');
-
-    // // New SSE route
-    // Route::get('/chat/{thread}/stream', [MessageController::class, 'streamResponse'])->name('chat.stream');
 });
 
 require __DIR__ . '/auth.php';
