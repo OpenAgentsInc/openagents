88fe6741701ce09d2de4fccce54847cfd3770dca
Christopher David <chris@openagents.com>
2024-10-21 04:08:29 -0400

    Added JavaScript to enhance UI responsiveness in message-form.blade.php.

--- resources/views/dashboard/message-form.blade.php
+++ resources/views/dashboard/message-form.blade.php
@@ -3,3 +3,10 @@
     <input type="text" name="content" placeholder="Type your message..." class="flex-grow p-2 border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
     <button type="submit" class="bg-blue-500 text-white p-2 rounded-r-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Send</button>
 </form>
+<script>
+    document.body.addEventListener('htmx:afterRequest', function(event) {
+        if (event.detail.elt.getAttribute('hx-post') === "{{ route('chat.send', $thread ?? 0) }}") {
+            event.detail.elt.reset();
+        }
+    });
+</script>
