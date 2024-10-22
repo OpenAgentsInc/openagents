070f485fa20bfc56484d3d6b73b25430101da677
Christopher David <chris@openagents.com>
2024-10-22 11:11:11 -0400

    Add project data binding to sidebar projects section.

--- resources/views/components/sidebar/sidebar-content.blade.php
+++ resources/views/components/sidebar/sidebar-content.blade.php
@@ -1,5 +1,5 @@
 <div class="text-white">
     <x-sidebar.section-threads :recentThreads="$recentThreads" />
-    <x-sidebar.section-projects />
+    <x-sidebar.section-projects :projects="$projects" />
     <x-sidebar.section-platform />
 </div>
