9fd751d3dab353091536e52f879c80e8c56241df
Christopher David <chris@openagents.com>
2024-10-22 10:13:32 -0500

    nice

--- resources/views/components/sidebar/sidebar-content.blade.php
+++ resources/views/components/sidebar/sidebar-content.blade.php
@@ -1,5 +1,4 @@
 <div class="text-white">
-    <x-sidebar.section-threads :recentThreads="$recentThreads" />
     <x-sidebar.section-projects :projects="$projects" />
-    <x-sidebar.section-platform />
+    <x-sidebar.section-threads :recentThreads="$recentThreads" />
 </div>
