af75341b7a2a585507dbf4df459227b7ff7f3413
Christopher David <chris@openagents.com>
2024-10-22 11:16:22 -0400

    Refactor `section-projects.blade.php` for readability and maintenance by updating CSS classes and properties.

--- resources/views/components/sidebar/section-projects.blade.php
+++ resources/views/components/sidebar/section-projects.blade.php
@@ -3,6 +3,22 @@
 <div data-sidebar="group" class="relative flex w-full min-w-0 flex-col p-2 group-data-[collapsible=icon]:hidden">
     <div data-sidebar="group-label" class="duration-200 flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opa] ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0 group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0">Projects</div>
     <ul data-sidebar="menu" class="flex w-full min-w-0 flex-col gap-1">
+        <li data-sidebar="menu-item" class="group/menu-item relative">
+            <a href="{{ route('project.create') }}"
+               data-sidebar="menu-button"
+               data-size="default"
+               class="flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground h-8 text-sm"
+               hx-post="{{ route('project.create') }}"
+               hx-swap="none"
+               hx-push-url="true"
+               hx-trigger="click">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus">
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5v14"></path>
+                </svg>
+                <span>New project</span>
+            </a>
+        </li>
         @foreach ($projects as $project)
         <li data-sidebar="menu-item" class="group/menu-item relative">
             <a href="#" data-sidebar="menu-button" data-size="default" data-active="false" class="peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground h-8 text-sm">
