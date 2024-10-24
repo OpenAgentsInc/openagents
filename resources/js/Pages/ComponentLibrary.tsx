417fa97d892932c6c77c91284e09e2f9f5076b55
Christopher David <chris@openagents.com>
2024-10-24 17:18:36 -0500

    Add `Inputs` component and adjust styling in `ComponentLibrary.tsx`.

--- resources/js/Pages/ComponentLibrary.tsx
+++ resources/js/Pages/ComponentLibrary.tsx
@@ -1,13 +1,16 @@
-import { Buttons } from "@/Components/ComponentLibrary"
 import { Head } from "@inertiajs/react"
+import { Buttons, Inputs } from "@/Components/ComponentLibrary"
 
 export default function ComponentLibrary() {
   return (
     <>
       <Head title="Component Library" />
-      <div className='dark font-mono bg-black text-white min-h-screen w-screen p-8'>
+      <div className='font-mono bg-black text-white min-h-screen w-screen p-8'>
         <h1 className="text-3xl font-bold mb-8">Component Library</h1>
-        <Buttons />
+        <div className="space-y-16">
+          <Buttons />
+          <Inputs />
+        </div>
       </div>
     </>
   );
