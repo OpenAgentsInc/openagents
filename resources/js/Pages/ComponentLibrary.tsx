17b11d06fc7552577433a5ad1ce3634098a1055a
Christopher David <chris@openagents.com>
2024-10-24 17:17:32 -0500

    dark

--- resources/js/Pages/ComponentLibrary.tsx
+++ resources/js/Pages/ComponentLibrary.tsx
@@ -1,11 +1,11 @@
-import { Head } from "@inertiajs/react"
 import { Buttons } from "@/Components/ComponentLibrary"
+import { Head } from "@inertiajs/react"
 
 export default function ComponentLibrary() {
   return (
     <>
       <Head title="Component Library" />
-      <div className='font-mono bg-black text-white min-h-screen w-screen p-8'>
+      <div className='dark font-mono bg-black text-white min-h-screen w-screen p-8'>
         <h1 className="text-3xl font-bold mb-8">Component Library</h1>
         <Buttons />
       </div>
