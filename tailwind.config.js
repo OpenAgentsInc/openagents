552adf9bd9700b954c153082862c42f911bd911f
Christopher David <chris@openagents.com>
2024-10-24 17:03:12 -0500

    use font

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ export default {
     theme: {
     	extend: {
     		fontFamily: {
-    			sans: ['Figtree', ...defaultTheme.fontFamily.sans]
+    			sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+          mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
     		},
     		borderRadius: {
     			lg: 'var(--radius)',
