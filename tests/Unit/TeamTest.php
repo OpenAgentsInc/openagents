1f7b55fa658ef6ec22dd21b41cb5947e9f7ff2d7
Christopher David <chris@openagents.com>
2024-10-24 22:30:39 -0500

    Refactor TeamTest.php: Update user-team relationship tests and add multi-team membership test for User model.

--- tests/Unit/TeamTest.php
+++ tests/Unit/TeamTest.php
@@ -5,12 +5,24 @@ use App\Models\User;
 use App\Models\Thread;
 use App\Models\Project;
 
-test('a team has many users', function () {
+test('a team can have many users', function () {
     $team = Team::factory()->create();
-    $users = User::factory()->count(3)->create(['team_id' => $team->id]);
+    $users = User::factory()->count(3)->create();
+
+    $team->users()->attach($users->pluck('id'));
 
     expect($team->users)->toHaveCount(3);
     expect($team->users->first())->toBeInstanceOf(User::class);
+});
+
+test('a user can be a member of multiple teams', function () {
+    $teams = Team::factory()->count(3)->create();
+    $user = User::factory()->create();
+
+    $user->teams()->attach($teams->pluck('id'));
+
+    expect($user->teams)->toHaveCount(3);
+    expect($user->teams->first())->toBeInstanceOf(Team::class);
 });
 
 test('a team has many projects', function () {
@@ -26,6 +38,22 @@ test('a team has many threads through projects', function () {
     $project = Project::factory()->create(['team_id' => $team->id]);
     $threads = Thread::factory()->count(3)->create(['project_id' => $project->id]);
 
-    expect($team->projects->first()->threads)->toHaveCount(3);
-    expect($team->projects->first()->threads->first())->toBeInstanceOf(Thread::class);
+    expect($team->threads)->toHaveCount(3);
+    expect($team->threads->first())->toBeInstanceOf(Thread::class);
+});
+
+test('a user can have a current team', function () {
+    $team = Team::factory()->create();
+    $user = User::factory()->create(['current_team_id' => $team->id]);
+
+    expect($user->currentTeam)->toBeInstanceOf(Team::class);
+    expect($user->currentTeam->id)->toBe($team->id);
+});
+
+test('a team can have many users with it as their current team', function () {
+    $team = Team::factory()->create();
+    $users = User::factory()->count(3)->create(['current_team_id' => $team->id]);
+
+    expect($team->currentUsers)->toHaveCount(3);
+    expect($team->currentUsers->first())->toBeInstanceOf(User::class);
 });
