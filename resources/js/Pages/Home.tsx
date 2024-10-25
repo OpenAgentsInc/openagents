ec3f6cf5b6b22fc5c45da91c83204edd24dcb4db
Christopher David <chris@openagents.com>
2024-10-24 19:17:15 -0500

    sidebar7 home page

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,11 +1,19 @@
 import MainLayout from "@/Layouts/MainLayout"
 import { Head } from "@inertiajs/react"
+import Page from "../../../app/dashboard/page"
 
 export default function Home() {
   return (
-    <MainLayout>
-      <Head title="Home" />
-      <p>Home</p>
-    </MainLayout>
+    <Page />
   )
 }
+
+
+// export default function Home() {
+//   return (
+//     <MainLayout>
+//       <Head title="Home" />
+//       <p>Home</p>
+//     </MainLayout>
+//   )
+// }
