6559a5f987f9342406ece8a1f0d68f5974dc664b
Christopher David <chris@openagents.com>
2024-10-24 22:08:56 -0500

    header constant height

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -16,7 +16,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
         <SidebarProvider>
           <AppSidebar />
           <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <header className="flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-14">
               <div className="flex items-center gap-2 px-4">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="mr-2 h-4" />
