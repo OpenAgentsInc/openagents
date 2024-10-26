497d3c50efec05d7c18f7eb876ec8040144763d6
Christopher David <chris@openagents.com>
2024-10-26 16:42:02 -0500

    Add email and comment fields to InquiryFactory definition.

--- database/factories/InquiryFactory.php
+++ database/factories/InquiryFactory.php
@@ -17,7 +17,8 @@ class InquiryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'email' => fake()->safeEmail(),
+            'comment' => fake()->paragraph(),
         ];
     }
 }
