4fc37d9ff6cb354a184b2efe7d1c676682f12227
Christopher David <chris@openagents.com>
2024-10-26 16:40:58 -0500

    php artisan make:model -mf Inquiry

---
+++ app/Models/Inquiry.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inquiry extends Model
+{
+    /** @use HasFactory<\Database\Factories\InquiryFactory> */
+    use HasFactory;
+}
