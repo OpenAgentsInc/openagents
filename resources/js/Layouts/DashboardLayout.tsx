0b67eb124a3f9d1cede8a5fe54237f610a68c882
Christopher David <chris@openagents.com>
2024-10-27 14:43:34 -0700

    Add Footer component and adjust padding in DashboardLayout.

--- resources/js/Layouts/DashboardLayout.tsx
+++ resources/js/Layouts/DashboardLayout.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from "react"
 import { Container } from "@/components/lander/container"
 import { Navbar } from "@/components/lander/navbar"
 import { GradientBackground } from "@/components/lander/gradient"
+import { Footer } from "@/components/lander/footer"
 
 export function DashboardLayout({ children }: PropsWithChildren) {
   return (
@@ -9,9 +10,10 @@ export function DashboardLayout({ children }: PropsWithChildren) {
       <GradientBackground />
       <Container>
         <Navbar />
-        <div className="mt-16">
+        <div className="mt-16 pb-24">
           {children}
         </div>
+        <Footer />
       </Container>
     </main>
   )
