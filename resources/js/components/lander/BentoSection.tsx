ac0d7ce63807a1fc9bbadb7bc23ed636f316dc27
Christopher David <chris@openagents.com>
2024-10-27 08:58:03 -0500

    Create file resources/js/components/lander/BentoSection.tsx

---
+++ resources/js/components/lander/BentoSection.tsx
@@ -0,0 +1,65 @@
+import { Container } from "@/components/lander/container"
+import { BentoCard } from "@/components/lander/bento-card"
+import { Keyboard } from "@/components/lander/keyboard"
+import { LogoCluster } from "@/components/lander/logo-cluster"
+import { Map } from "@/components/lander/map"
+import { Heading, Subheading } from "@/components/lander/text"
+
+export function BentoSection() {
+  return (
+    <Container>
+      <Subheading>Sales</Subheading>
+      <Heading as="h3" className="mt-2 max-w-3xl">
+        Know more about your customers than they do.
+      </Heading>
+
+      <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+        <BentoCard
+          eyebrow="Insight"
+          title="Get perfect clarity"
+          description="Radiant uses social engineering to build a detailed financial picture of your leads. Know their budget, compensation package, social security number, and more."
+          graphic={
+            <div className="h-80 bg-[url(/screenshots/profile.png)] bg-[size:1000px_560px] bg-[left_-109px_top_-112px] bg-no-repeat" />
+          }
+          fade={['bottom']}
+          className="max-lg:rounded-t-4xl lg:col-span-3 lg:rounded-tl-4xl"
+        />
+        <BentoCard
+          eyebrow="Analysis"
+          title="Undercut your competitors"
+          description="With our advanced data mining, you'll know which companies your leads are talking to and exactly how much they're being charged."
+          graphic={
+            <div className="absolute inset-0 bg-[url(/screenshots/competitors.png)] bg-[size:1100px_650px] bg-[left_-38px_top_-73px] bg-no-repeat" />
+          }
+          fade={['bottom']}
+          className="lg:col-span-3 lg:rounded-tr-4xl"
+        />
+        <BentoCard
+          eyebrow="Speed"
+          title="Built for power users"
+          description="It's never been faster to cold email your entire contact list using our streamlined keyboard shortcuts."
+          graphic={
+            <div className="flex size-full pl-10 pt-10">
+              <Keyboard highlighted={['LeftCommand', 'LeftShift', 'D']} />
+            </div>
+          }
+          className="lg:col-span-2 lg:rounded-bl-4xl"
+        />
+        <BentoCard
+          eyebrow="Source"
+          title="Get the furthest reach"
+          description="Bypass those inconvenient privacy laws to source leads from the most unexpected places."
+          graphic={<LogoCluster />}
+          className="lg:col-span-2"
+        />
+        <BentoCard
+          eyebrow="Limitless"
+          title="Sell globally"
+          description="Radiant helps you sell in locations currently under international embargo."
+          graphic={<Map />}
+          className="max-lg:rounded-b-4xl lg:col-span-2 lg:rounded-br-4xl"
+        />
+      </div>
+    </Container>
+  )
+}
