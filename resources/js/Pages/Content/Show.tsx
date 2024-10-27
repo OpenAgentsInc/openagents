0edb46f23f3bf6545967d48d2a848ad1075d9445
Christopher David <chris@openagents.com>
2024-10-27 06:19:14 -0500

    tailwinc config etc

--- resources/js/Pages/Content/Show.tsx
+++ resources/js/Pages/Content/Show.tsx
@@ -12,7 +12,7 @@ export default function Show({ content, title }: Props) {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-background overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 prose prose-sm sm:prose lg:prose-lg xl:prose-xl max-w-none">
+            <div className="p-6 prose dark:prose-invert prose-sm sm:prose lg:prose-lg xl:prose-xl max-w-none">
               <div dangerouslySetInnerHTML={{ __html: content }} />
             </div>
           </div>
