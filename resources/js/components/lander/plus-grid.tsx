fd8b5e6202b5dcd8957e3c62bd3f717fb9ca3df4
Christopher David <chris@openagents.com>
2024-10-27 09:19:17 -0500

    Refactored PlusGridRow to utilize clsx for dynamic className bindings for better maintainability and readability.

--- resources/js/components/lander/plus-grid.tsx
+++ resources/js/components/lander/plus-grid.tsx
@@ -28,10 +28,10 @@ export function PlusGridRow({
         aria-hidden="true"
         className="absolute inset-y-0 left-1/2 -z-10 w-screen -translate-x-1/2"
       >
-        <div className="absolute inset-x-0 top-0 border-t border-black/5"></div>
-        <div className="absolute inset-x-0 top-2 border-t border-black/5"></div>
-        <div className="absolute inset-x-0 bottom-0 hidden border-b border-black/5 group-last/row:block"></div>
-        <div className="absolute inset-x-0 bottom-2 hidden border-b border-black/5 group-last/row:block"></div>
+        <div className="absolute inset-x-0 top-0 border-t border-border"></div>
+        <div className="absolute inset-x-0 top-2 border-t border-border"></div>
+        <div className="absolute inset-x-0 bottom-0 hidden border-b border-border group-last/row:block"></div>
+        <div className="absolute inset-x-0 bottom-2 hidden border-b border-border group-last/row:block"></div>
       </div>
       {children}
     </div>
@@ -83,7 +83,7 @@ export function PlusGridIcon({
       aria-hidden="true"
       className={clsx(
         className,
-        'absolute size-[15px] fill-black/10',
+        'absolute size-[15px] fill-border',
         yClass,
         xClass,
       )}
