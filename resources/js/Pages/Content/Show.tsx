5c36c3217f9d9a866689f23f1da78b8f3f68ab40
Christopher David <chris@openagents.com>
2024-10-27 12:26:14 -0500

    Refactor: Removed unused imports and streamlined Show component in Show.tsx.

--- resources/js/Pages/Content/Show.tsx
+++ resources/js/Pages/Content/Show.tsx
@@ -40,10 +40,11 @@ export default function Show({ content, title }: Props) {
         <Container>
           <Navbar />
           <Subheading className="mt-16 text-center">
-            {title === 'Thesis' ? 'Our Thesis' : 'Legal'}
+            {/* {dayjs(Date.now()).format('dddd, MMMM D, YYYY')} */}
+            Our Thesis
           </Subheading>
           <Heading as="h1" className="mt-2 text-center">
-            {title === 'Thesis' ? 'The Case for Open Agents' : 'Terms of Service'}
+            The Case for Open Agents
           </Heading>
           <div className="mt-16 grid grid-cols-1 gap-8 pb-24 lg:grid-cols-[15rem_1fr] xl:grid-cols-[15rem_1fr_15rem]">
             <div className="hidden lg:block">
