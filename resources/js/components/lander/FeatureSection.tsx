3ce68fa1e9e6686afa11405f6cc987c8a2da417f
Christopher David <chris@openagents.com>
2024-10-27 08:57:44 -0500

    Create file resources/js/components/lander/FeatureSection.tsx

---
+++ resources/js/components/lander/FeatureSection.tsx
@@ -0,0 +1,21 @@
+import { Container } from "@/components/lander/container"
+import { Screenshot } from "@/components/lander/screenshot"
+import { Heading } from "@/components/lander/text"
+
+export function FeatureSection() {
+  return (
+    <div className="overflow-hidden">
+      <Container className="pb-24">
+        <Heading as="h2" className="max-w-3xl">
+          A snapshot of your entire sales pipeline.
+        </Heading>
+        <Screenshot
+          width={1216}
+          height={768}
+          src="/screenshots/app.png"
+          className="mt-16 h-[36rem] sm:h-auto sm:w-[76rem]"
+        />
+      </Container>
+    </div>
+  )
+}
