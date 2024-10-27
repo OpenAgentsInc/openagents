f40787615d0b93ddde30879de649d4997d0043ec
Christopher David <chris@openagents.com>
2024-10-27 13:03:33 -0500

    Refactor `Link` component to handle external URLs using new `isExternalUrl` function.

--- resources/js/components/lander/link.tsx
+++ resources/js/components/lander/link.tsx
@@ -2,18 +2,24 @@ import { forwardRef } from "react"
 import * as Headless from "@headlessui/react"
 import { InertiaLinkProps, Link as InertiaLink } from "@inertiajs/react"
 
+const isExternalUrl = (url: string) => {
+  return url.startsWith('http://') || url.startsWith('https://')
+}
+
 export const Link = forwardRef(function Link(
   props: InertiaLinkProps & React.ComponentPropsWithoutRef<'a'>,
   ref: React.ForwardedRef<HTMLAnchorElement>,
 ) {
-  const { target, ...otherProps } = props
+  const { target, href, ...otherProps } = props
 
-  if (target === '_blank') {
+  // Handle external URLs or _blank targets with regular anchor tags
+  if (target === '_blank' || (href && isExternalUrl(href))) {
     return (
       <Headless.DataInteractive>
         <a 
           ref={ref} 
-          target="_blank" 
+          href={href}
+          target={target || '_blank'} 
           rel="noopener noreferrer" 
           {...otherProps} 
         />
@@ -23,7 +29,7 @@ export const Link = forwardRef(function Link(
 
   return (
     <Headless.DataInteractive>
-      <InertiaLink ref={ref} {...props} />
+      <InertiaLink ref={ref} href={href} {...otherProps} />
     </Headless.DataInteractive>
   )
 })
