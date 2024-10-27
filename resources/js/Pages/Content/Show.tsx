e53c74da2344fc11117e958e9cb1a25e7959d10b
Christopher David <chris@openagents.com>
2024-10-27 06:22:00 -0500

    cool

--- resources/js/Pages/Content/Show.tsx
+++ resources/js/Pages/Content/Show.tsx
@@ -1,5 +1,5 @@
-import { Head } from '@inertiajs/react';
-import React from 'react';
+import React from "react"
+import { Head } from "@inertiajs/react"
 
 interface Props {
   content: string;
@@ -13,7 +13,7 @@ export default function Show({ content, title }: Props) {
       <div className="min-h-screen py-12 bg-background">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="bg-card shadow-sm rounded-lg">
-            <article className="p-8 prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto">
+            <article className="p-8 dark:prose-invert prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto">
               <div dangerouslySetInnerHTML={{ __html: content }} />
             </article>
           </div>
