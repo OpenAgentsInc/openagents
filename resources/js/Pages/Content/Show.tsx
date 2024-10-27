07df955d6d3dd94d72d90cf5b058d9b43a0d107e
Christopher David <chris@openagents.com>
2024-10-27 06:16:53 -0500

    nice

--- resources/js/Pages/Content/Show.tsx
+++ resources/js/Pages/Content/Show.tsx
@@ -1,5 +1,4 @@
-import { Head } from '@inertiajs/react';
-import React from 'react';
+import { Head } from "@inertiajs/react"
 
 interface Props {
   content: string;
@@ -12,7 +11,7 @@ export default function Show({ content, title }: Props) {
       <Head title={title} />
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div className="bg-background overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 prose prose-sm sm:prose lg:prose-lg xl:prose-xl max-w-none">
               <div dangerouslySetInnerHTML={{ __html: content }} />
             </div>
