64ad48fce85330a4a3312607058c6f817d683644
Christopher David <chris@openagents.com>
2024-10-27 14:38:16 -0700

    Refactor DashboardLayout to remove Sidebar and simplify structure for a cleaner UI.

--- resources/js/Layouts/DashboardLayout.tsx
+++ resources/js/Layouts/DashboardLayout.tsx
@@ -1,5 +1,4 @@
 import { PropsWithChildren } from "react"
-import { Sidebar } from "@/components/dashboard/Sidebar"
 import { Header } from "@/components/nav/Header"
 
 // import { useKeystrokes } from "@/lib/useKeystrokes"
@@ -7,8 +6,7 @@ import { Header } from "@/components/nav/Header"
 export function DashboardLayout({ children }: PropsWithChildren) {
   // useKeystrokes()
   return (
-    <div className="fixed grid min-h-screen w-full md:grid-cols-[240px_1fr]">
-      <Sidebar />
+    <div className="fixed min-h-screen w-full">
       <div className="flex flex-col h-screen">
         <Header />
         <main className="flex-1 overflow-hidden">
