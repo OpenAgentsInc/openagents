83e1867eb7c551335bf5e727640b924740c3f83c
Christopher David <chris@openagents.com>
2024-10-27 09:31:49 -0500

    Refactor `Link` component to handle `_blank` target prop by rendering a native `a` tag.

--- resources/js/components/lander/link.tsx
+++ resources/js/components/lander/link.tsx
@@ -6,6 +6,21 @@ export const Link = forwardRef(function Link(
   props: InertiaLinkProps & React.ComponentPropsWithoutRef<'a'>,
   ref: React.ForwardedRef<HTMLAnchorElement>,
 ) {
+  const { target, ...otherProps } = props
+
+  if (target === '_blank') {
+    return (
+      <Headless.DataInteractive>
+        <a 
+          ref={ref} 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          {...otherProps} 
+        />
+      </Headless.DataInteractive>
+    )
+  }
+
   return (
     <Headless.DataInteractive>
       <InertiaLink ref={ref} {...props} />
