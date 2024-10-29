62a8f1da7804b78edae1bdbd5aadc57167461788
Christopher David <chris@openagents.com>
2024-10-29 16:48:29 -0700

    Refactor MainLayout.tsx by removing unused imports to clean up the code.

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -15,19 +15,15 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
         <SidebarProvider>
           <MainSidebar />
           <SidebarInset>
-            <div className="fixed">
-              <header className="flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-14">
-                <div className="font-bold flex items-center gap-2 px-4">
-                  <Link href="/" className="flex flex-row gap-x-2 items-center">
-                    <IconOpenAgents className="h-4 w-4" />
-                    <span className="text-lg">OpenAgents</span>
-                  </Link>
-                  {/* <Separator orientation="vertical" className="mr-2 h-4" />
-                  <MainBreadcrumb /> */}
-                </div>
-              </header>
-            </div>
-            <main className="flex flex-1 flex-col gap-4 px-4 py-0">
+            <header className="fixed top-0 left-0 right-0 flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-14 bg-background z-10">
+              <div className="font-bold flex items-center gap-2 px-4">
+                <Link href="/" className="flex flex-row gap-x-2 items-center">
+                  <IconOpenAgents className="h-4 w-4" />
+                  <span className="text-lg">OpenAgents</span>
+                </Link>
+              </div>
+            </header>
+            <main className="pt-14 h-screen">
               {children}
             </main>
           </SidebarInset>
