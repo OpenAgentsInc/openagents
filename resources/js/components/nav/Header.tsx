d1cb73a8530d8803601864d86c1042f22c2614be
Christopher David <chris@openagents.com>
2024-10-29 17:35:15 -0700

    "Refactor Header.tsx to include IconOpenAgents and update layout for improved navigation and aesthetics."

--- resources/js/components/nav/Header.tsx
+++ resources/js/components/nav/Header.tsx
@@ -1,22 +1,25 @@
 import { UserMenu } from "@/components/dashboard/UserMenu"
 import { usePage } from "@inertiajs/react"
-// import { LoginButton } from "./LoginButton"
-// import { MobileNav } from "./MobileNav"
 import TeamSwitcher from "./TeamSwitcher"
+import IconOpenAgents from "@/components/IconOpenAgents"
+import { Link } from "@inertiajs/react"
 
 export function Header() {
   const props = usePage().props as any
   return (
-    <header className="flex h-14 items-center gap-4 border-b border-border bg-background px-4 lg:h-[60px] lg:px-6">
-      {/* <MobileNav /> */}
-      <div className="w-full flex-1"></div>
-      {!!props.auth.user && (
-        <div className="ml-auto flex items-center space-x-4">
-          <TeamSwitcher />
-          <UserMenu />
-          {/* <LoginButton /> */}
-        </div>
-      )}
+    <header className="h-14 shrink-0 items-center gap-2 bg-sidebar border-b">
+      <div className="font-bold flex items-center justify-between gap-2 px-4 h-full">
+        <Link href="/" className="flex flex-row gap-x-2 items-center">
+          <IconOpenAgents className="h-4 w-4" />
+          <span className="text-lg">OpenAgents</span>
+        </Link>
+        {!!props.auth.user && (
+          <div className="flex items-center space-x-4">
+            <TeamSwitcher />
+            <UserMenu />
+          </div>
+        )}
+      </div>
     </header>
   )
 }
