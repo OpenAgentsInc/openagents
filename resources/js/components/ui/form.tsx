221b8e042c0289b191dd1f0e963e7382d7390b1c
Christopher David <chris@openagents.com>
2024-10-30 12:47:19 -0500

    no useclient

--- resources/js/components/ui/form.tsx
+++ resources/js/components/ui/form.tsx
@@ -1,19 +1,14 @@
-"use client"
+
 
 import * as React from "react"
+import {
+  Controller, ControllerProps, FieldPath, FieldValues, FormProvider,
+  useFormContext
+} from "react-hook-form"
+import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { Slot } from "@radix-ui/react-slot"
-import {
-  Controller,
-  ControllerProps,
-  FieldPath,
-  FieldValues,
-  FormProvider,
-  useFormContext,
-} from "react-hook-form"
-
-import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
 
 const Form = FormProvider
 
--- resources/js/components/ui/select.tsx
+++ resources/js/components/ui/select.tsx
@@ -1,15 +1,11 @@
-"use client"
+
 
 import * as React from "react"
+import { cn } from "@/lib/utils"
 import {
-  CaretSortIcon,
-  CheckIcon,
-  ChevronDownIcon,
-  ChevronUpIcon,
+  CaretSortIcon, CheckIcon, ChevronDownIcon, ChevronUpIcon
 } from "@radix-ui/react-icons"
 import * as SelectPrimitive from "@radix-ui/react-select"
-
-import { cn } from "@/lib/utils"
 
 const Select = SelectPrimitive.Root
 
@@ -82,7 +78,7 @@ const SelectContent = React.forwardRef<
       className={cn(
         "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         position === "popper" &&
-          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
+        "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className
       )}
       position={position}
@@ -93,7 +89,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           "p-1",
           position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+          "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
         )}
       >
         {children}
