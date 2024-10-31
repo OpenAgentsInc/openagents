7e4b54c30e06be62f803b07a4b4b59f58b2e0f18
Christopher David <chris@openagents.com>
2024-10-30 21:31:41 -0700

    Add default company attributes in CompanyFactory definition.

--- database/factories/CRM/CompanyFactory.php
+++ database/factories/CRM/CompanyFactory.php
@@ -17,7 +17,10 @@ class CompanyFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->company(),
+            'website' => fake()->url(),
+            'industry' => fake()->randomElement(['Technology', 'Finance', 'Healthcare', 'Retail', 'Manufacturing']),
+            'description' => fake()->paragraph(),
         ];
     }
 }
