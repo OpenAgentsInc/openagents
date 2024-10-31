a7c9418b1cb2ef3fbb5688a9aab8848d656765e8
Christopher David <chris@openagents.com>
2024-10-30 21:30:48 -0700

    Add 'name', 'website', and 'industry' columns to 'companies' table in migration.

--- database/migrations/2024_10_31_042821_create_companies_table.php
+++ database/migrations/2024_10_31_042821_create_companies_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('website')->nullable();
+            $table->string('industry')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
