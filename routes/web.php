1ca11f9229a01ee787e17e5568fff3516292e65a
Christopher David <chris@openagents.com>
2024-10-31 09:10:01 -0700

    Refactor `web.php` to remove unused `UseChatController` import.

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
 Route::middleware('auth')->group(function () {
     // CHAT
     Route::get('/chat', [ChatController::class, 'index'])->name('chat');
-    Route::get('/chat/create', [ChatController::class, 'create'])->name('chat.create');
+    Route::post('/chat/create', [ChatController::class, 'create'])->name('chat.create');
     Route::get('/chat/{id}', [ChatController::class, 'show'])->name('chat.id');
     Route::post('/chat', [UseChatController::class, 'chat']);
     Route::delete('/chat/{id}', [ChatController::class, 'destroy'])->name('chat.destroy');
