4cfcf3ada49df937c2185e47f8f54727bf9d7d5e
Christopher David <chris@openagents.com>
2024-10-31 12:30:23 -0700

    Create file app/Providers/AuthServiceProvider.php

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Project;
+use App\Policies\ProjectPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Project::class => ProjectPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
