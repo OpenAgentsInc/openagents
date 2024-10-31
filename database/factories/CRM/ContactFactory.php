717179c8fe70d2615ab03370538754d71b3bdfb6
Christopher David <chris@openagents.com>
2024-10-30 21:31:53 -0700

    Enhance ContactFactory with default state definitions for 'contact_id' and include User and Company models.

--- database/factories/CRM/ContactFactory.php
+++ database/factories/CRM/ContactFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories\CRM;
 
+use App\Models\CRM\Company;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,15 @@ class ContactFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'contact_id' => 'CT' . str_pad(fake()->unique()->numberBetween(1, 999999), 6, '0', STR_PAD_LEFT),
+            'company_id' => Company::factory(),
+            'created_by' => User::factory(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->numerify('##########'),
+            'title' => fake()->jobTitle(),
+            'notes' => fake()->paragraph(),
         ];
     }
 }
