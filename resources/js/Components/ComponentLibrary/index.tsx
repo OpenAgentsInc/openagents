ab6c9ab3897900103c71efd43f916212189be46b
Christopher David <chris@openagents.com>
2024-12-08 11:44:46 -0600

    update readme, move rest to branch

--- resources/js/Components/ComponentLibrary/index.tsx
+++
@@ -1,2 +0,0 @@
-export { Buttons } from './Buttons';
-export { Inputs } from './Inputs';
--- tests/Feature/ComponentLibraryTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-it('returns a successful response', function () {
-    $response = $this->get('/components');
-
-    $response->assertStatus(200);
-});
