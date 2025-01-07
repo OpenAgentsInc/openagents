d0a24be210a1c919ceb57950ef25744760ac2bcd
Christopher David <chris@openagents.com>
2025-01-07 10:39:11 -0600

    Enable file listing for static files directory in `config.rs`.

--- src/server/config.rs
+++ src/server/config.rs
@@ -1,6 +1,5 @@
 use actix_files as fs;
 use actix_web::web;
-use std::path::Path;
 
 use super::routes;
 
@@ -13,13 +12,7 @@ pub fn configure_app(cfg: &mut web::ServiceConfig) {
                 .index_file("index.html")
                 .use_hidden_files()
                 .prefer_utf8(true)
-                .mime_override(|path| {
-                    let path_str = path.to_str().unwrap_or("");
-                    if path_str.ends_with(".js") {
-                        Some(mime::APPLICATION_JAVASCRIPT)
-                    } else {
-                        None
-                    }
-                })
+                .show_files_listing()
+                .default_handler(fs::NamedFile::open)
         );
 }
