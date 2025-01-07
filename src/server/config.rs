932c35d50a7d2931e603f69b344b75e984e9bb9a
Christopher David <chris@openagents.com>
2025-01-07 09:49:44 -0600

    Refactor `config.rs`: Remove unused imports and middleware, streamline `configure_app` function.

--- src/server/config.rs
+++ src/server/config.rs
@@ -1,7 +1,5 @@
-use actix_cors::Cors;
 use actix_files as fs;
 use actix_web::web;
-use actix_web::middleware::{Logger, DefaultHeaders};
 
 use super::routes;
 
@@ -13,19 +11,5 @@ pub fn configure_app(cfg: &mut web::ServiceConfig) {
             fs::Files::new("/", "./static")
                 .index_file("index.html")
                 .use_hidden_files()
-        )
-        // Add middleware
-        .wrap(
-            Cors::default()
-                .allow_any_origin()
-                .allow_any_method()
-                .allow_any_header()
-        )
-        .wrap(Logger::default())
-        .wrap(
-            DefaultHeaders::new()
-                .add(("X-Content-Type-Options", "nosniff"))
-                .add(("X-Frame-Options", "DENY"))
-                .add(("X-XSS-Protection", "1; mode=block"))
         );
 }
