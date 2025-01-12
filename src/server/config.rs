9add7654317e2d40b73deb8160c65a98e14a6f69
Christopher David <chris@openagents.com>
2025-01-11 20:57:56 -0500

    Refactor non-admin route configurations into `routes::configure_routes` for cleaner code organization.

--- src/server/config.rs
+++ src/server/config.rs
@@ -12,14 +12,14 @@ pub fn configure_app(cfg: &mut web::ServiceConfig) {
     );
 
     // Configure non-admin routes
-    cfg.service(routes::health_check)
-        .service(routes::new_page)
-        // Serve static files from the static directory
-        .service(
-            fs::Files::new("/", "./static")
-                .index_file("index.html")
-                .use_hidden_files()
-                .prefer_utf8(true)
-                .show_files_listing(),
-        );
+    routes::configure_routes(cfg);
+
+    // Serve static files from the static directory
+    cfg.service(
+        fs::Files::new("/", "./static")
+            .index_file("index.html")
+            .use_hidden_files()
+            .prefer_utf8(true)
+            .show_files_listing(),
+    );
 }
