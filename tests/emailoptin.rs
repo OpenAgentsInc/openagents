b5688cad2b35ed68243ea8acda5dcb902837fe02
Christopher David <chris@openagents.com>
2025-01-11 19:11:47 -0500

    emailoptin test placeholders

---
+++ tests/emailoptin.rs
@@ -0,0 +1,11 @@
+use actix_web::{test, web, App};
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // todo
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // todo
+}
