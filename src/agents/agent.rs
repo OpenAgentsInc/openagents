2a3700627dc3db0b6f564aa07b430bf34cd6e593
Christopher David <chris@openagents.com>
2025-01-11 19:48:24 -0500

    agent/instance types

---
+++ src/agents/agent.rs
@@ -0,0 +1,10 @@
+pub struct Agent {
+    pub name: string,
+    pub description: string,
+    pub created_at: i64, 
+}
+
+pub struct AgentInstance {
+    pub created_at: i64, 
+    pub ended_at: i64, 
+}
