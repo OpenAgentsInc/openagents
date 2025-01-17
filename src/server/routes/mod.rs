497173c8a45060bef7d3b8745e73c7c63a31148c
GitHub API <noreply@github.com>
2025-01-16 19:03:17 -0600

    Delete src/server/routes/mod.rs

--- src/server/routes/mod.rs
+++
@@ -1,8 +0,0 @@
-use actix_web::web;
-
-pub mod repomap;
-
-pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(repomap::get_repomap)
-       .service(repomap::generate_repomap);
-}
