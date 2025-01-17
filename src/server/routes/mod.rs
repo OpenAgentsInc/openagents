bee7f13b1c90131f3fe6559e209c132a1a3bdd84
Christopher David <chris@openagents.com>
2025-01-16 18:46:04 -0600

    Update mod.rs

--- src/server/routes/mod.rs
+++ src/server/routes/mod.rs
@@ -1,3 +1,8 @@
+use actix_web::web;
+
 pub mod repomap;
 
-pub use repomap::{get_repomap, generate_repomap};
+pub fn configure_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(repomap::get_repomap)
+       .service(repomap::generate_repomap);
+}
