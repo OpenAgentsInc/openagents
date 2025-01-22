61ad5ac3d2c15b78fc1b2eabf5341be4649bbe0e
Christopher David <chris@openagents.com>
2025-01-22 14:05:27 -0600

    Update mod.rs

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,11 +1,15 @@
 pub mod admin;
 pub mod config;
+pub mod routes;
 pub mod services;
+pub mod tools;
 pub mod ws;
 
 use axum::{routing::get, Router};
 
 pub fn app_router() -> Router {
     // Create base router
-    Router::new().route("/ws", get(ws::ws_handler))
+    Router::new()
+        .route("/ws", get(ws::ws_handler))
+        .merge(routes::routes())
 }
