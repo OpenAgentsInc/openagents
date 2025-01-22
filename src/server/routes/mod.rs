ee52fd505de13f3b272406a70e95ea7281ea9236
Christopher David <chris@openagents.com>
2025-01-22 15:00:58 -0600

    Update mod.rs

--- src/server/routes/mod.rs
+++ src/server/routes/mod.rs
@@ -1,8 +1,8 @@
-pub mod chat;
-
 use axum::Router;
+
+pub mod chat;
 
 pub fn routes() -> Router {
     Router::new()
-        .nest("/chat", chat::routes())
+        .nest("/chat", chat::chat_routes())
 }
