8350790380f7b2396174cacc1d330260c536c38f
Christopher David <chris@openagents.com>
2025-01-23 16:37:46 -0600

    shh

--- src/server/services/mod.rs
+++ src/server/services/mod.rs
@@ -2,11 +2,9 @@ pub mod deepseek;
 pub mod github;
 pub mod github_issue;
 pub mod github_types;
-pub mod openrouter;
 pub mod repomap;
 
 pub use deepseek::{DeepSeekService, StreamUpdate};
 pub use github::GitHubService;
 pub use github_issue::GitHubService as GitHubIssueService;
-pub use openrouter::OpenRouterService;
 pub use repomap::RepomapService;
