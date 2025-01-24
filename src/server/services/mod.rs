a2e2710f9de81d5511429343e95b34a93d96ca48
Christopher David <chris@openagents.com>
2025-01-24 14:35:27 -0600

    Update mod.rs

--- src/server/services/mod.rs
+++ src/server/services/mod.rs
@@ -4,6 +4,7 @@ pub mod github_types;
 pub mod model_router;
 pub mod repomap;
 
-pub use deepseek::DeepSeekService;
+pub use deepseek::{DeepSeekService, StreamUpdate};
 pub use github_issue::GitHubService;
 pub use model_router::ModelRouter;
+pub use repomap::RepomapService;
