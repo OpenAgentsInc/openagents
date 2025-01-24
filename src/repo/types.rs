79fc7e9e7dfda8573f0a445de32c019165e38e37
Christopher David <chris@openagents.com>
2025-01-23 21:39:22 -0600

    Create file src/repo/types.rs

---
+++ src/repo/types.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+use std::path::PathBuf;
+
+pub struct RepoContext {
+    pub temp_dir: PathBuf,
+    pub api_key: String,
+    pub github_token: Option<String>,
+}
+
+impl RepoContext {
+    pub fn new(temp_dir: PathBuf, api_key: String, github_token: Option<String>) -> Self {
+        Self {
+            temp_dir,
+            api_key,
+            github_token,
+        }
+    }
+}
