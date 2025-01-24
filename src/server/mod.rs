a9f28f0c428fdcc81020ea08d2da33838c4188d7
Christopher David <chris@openagents.com>
2025-01-23 19:41:50 -0600

    More cleanup and fix websocket init (#593)
    * local repos doc
    
    * doc
    
    * fix ws init
    
    * sh
    
    * axe dupe template filters
    
    * shh

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,10 +1,3 @@
 pub mod config;
 pub mod services;
 pub mod ws;
-
-use axum::{routing::get, Router};
-
-pub fn app_router() -> Router {
-    // Create base router
-    Router::new().route("/ws", get(ws::ws_handler))
-}
