2c5deda73f4c1f07eab9da5455e0ee61662d6f06
Christopher David <chris@openagents.com>
2025-01-24 19:58:15 -0600

    Create file tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js}",
+    "./static/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
