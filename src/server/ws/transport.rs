28c73fa605746cb5385267f438fe532783da5481
Christopher David <chris@openagents.com>
2025-01-24 18:33:46 -0600

    Update transport.rs

--- src/server/ws/transport.rs
+++ src/server/ws/transport.rs
@@ -169,8 +169,8 @@ impl WebSocketState {
 
 // Test helper methods
 #[cfg(test)]
-impl WebSocketState {
-    pub async fn add_test_connection(self: &Arc<Self>, conn_id: &str) -> mpsc::UnboundedReceiver<Message> {
+impl Arc<WebSocketState> {
+    pub async fn add_test_connection(&self, conn_id: &str) -> mpsc::UnboundedReceiver<Message> {
         let (tx, rx) = mpsc::unbounded_channel();
         let mut conns = self.connections.write().await;
         conns.insert(conn_id.to_string(), tx);
