2eed03bd4b0bc1867fc0977496898b206d971666
Christopher David <chris@openagents.com>
2025-01-26 00:03:51 -0600

    Update filters.rs

--- src/filters.rs
+++ src/filters.rs
@@ -1,7 +1,6 @@
-use askama::Result;
 use pulldown_cmark::{html, Options, Parser};
 
-pub fn render_markdown(s: &str) -> Result<String> {
+pub fn render_markdown(s: &str) -> String {
     let mut options = Options::empty();
     options.insert(Options::ENABLE_STRIKETHROUGH);
     options.insert(Options::ENABLE_TABLES);
@@ -10,5 +9,5 @@ pub fn render_markdown(s: &str) -> Result<String> {
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
 
-    Ok(html_output)
+    html_output
 }
