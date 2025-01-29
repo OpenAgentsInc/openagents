5ef6c54bc870c35107bf8b726f7c174468fe1cce
Christopher David <chris@openagents.com>
2025-01-29 17:10:52 -0600

    Update mod.rs

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,15 +1,21 @@
 mod changes;
+mod cli;
 mod config;
 mod context;
 mod display;
+mod file_list;
 mod github;
 mod planning;
 mod streaming;
+mod types;
 
 pub use changes::*;
+pub use cli::*;
 pub use config::*;
 pub use context::*;
 pub use display::*;
+pub use file_list::*;
 pub use github::*;
 pub use planning::*;
 pub use streaming::*;
+pub use types::*;
