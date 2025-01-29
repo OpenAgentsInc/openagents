9673b2d8ddb4479cf78d98ffed8f1a4e6d142f8c
Christopher David <chris@openagents.com>
2025-01-29 13:28:53 -0600

    Update mod.rs

--- src/repo/mod.rs
+++ src/repo/mod.rs
@@ -1,11 +1,7 @@
 mod git;
 mod types;
+mod test;
 
 pub use git::{cleanup_temp_dir, clone_repository, commit_changes, checkout_branch, push_changes_with_token};
 pub use types::RepoContext;
-
-// Re-export test module only in test configuration
-#[cfg(test)]
 pub use test::run_cargo_tests;
-#[cfg(test)]
-mod test;
