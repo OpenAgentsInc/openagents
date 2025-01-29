d5c564eaa9feb5885ba913c920c008528702cadd
Christopher David <chris@openagents.com>
2025-01-29 00:01:22 -0600

    Update mod.rs

--- src/server/ws/handlers/mod.rs
+++ src/server/ws/handlers/mod.rs
@@ -1,16 +1,10 @@
+use anyhow::Result;
 use async_trait::async_trait;
-use std::error::Error;
-
-pub mod chat;
 
 #[async_trait]
 pub trait MessageHandler {
     type Message;
 
-    async fn handle_message(
-        &self,
-        msg: Self::Message,
-        conn_id: String,
-    ) -> Result<(), Box<dyn Error + Send + Sync>>;
-    async fn broadcast(&self, msg: Self::Message) -> Result<(), Box<dyn Error + Send + Sync>>;
+    async fn handle_message(&self, msg: Self::Message, conn_id: String) -> Result<()>;
+    async fn broadcast(&self, msg: Self::Message) -> Result<()>;
 }
