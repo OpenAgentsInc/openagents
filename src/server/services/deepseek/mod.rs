15eca5d5011712de3256e4644b1187b31ab8bf25
Christopher David <chris@openagents.com>
2025-01-28 23:54:00 -0600

    Update mod.rs

--- src/server/services/deepseek/mod.rs
+++ src/server/services/deepseek/mod.rs
@@ -1,10 +1,8 @@
 pub mod methods;
-pub mod types;
+pub mod service;
 pub mod streaming;
+pub mod types;
 
-pub use methods::chat::chat;
-pub use methods::chat_stream::chat_stream;
-pub use methods::chat_with_tool_response::chat_with_tool_response;
-pub use methods::chat_with_tools::chat_with_tools;
+pub use service::DeepSeekService;
 pub use streaming::StreamUpdate;
 pub use types::*;
