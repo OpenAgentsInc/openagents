e7a3243af3c3e10cd189e805040ee06afe55ec92
Christopher David <chris@openagents.com>
2025-01-29 23:43:08 -0600

    Update mod.rs

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -9,3 +9,17 @@ pub mod planning;
 pub mod solution;
 pub mod streaming;
 pub mod types;
+
+pub use changes::*;
+pub use cli::*;
+pub use config::*;
+pub use context::*;
+pub use display::*;
+pub use file_list::*;
+pub use github::*;
+pub use planning::*;
+pub use streaming::*;
+pub use types::*;
+
+// Re-export octocrab types that we use
+pub use octocrab::models::issues::{Comment, Issue};
