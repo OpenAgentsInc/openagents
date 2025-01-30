bf2956974b4647c998c93170abcdf24d3c342a9a
Christopher David <chris@openagents.com>
2025-01-29 19:37:44 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
+pub mod filters;
+pub mod repo;
+pub mod repomap;
+pub mod routes;
+pub mod server;
 pub mod solver;
+
+pub use filters::*;
+pub use repo::*;
+pub use repomap::*;
+pub use routes::*;
+
+// Re-export specific items from server and solver as needed
+pub use server::services;
+pub use solver::Cli;
 
 // Re-export dependencies that our public API uses
 pub use octocrab;
