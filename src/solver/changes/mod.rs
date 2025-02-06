081245d960049a96af06c47949133221ea58a910
Christopher David <chris@openagents.com>
2025-02-05 21:42:53 -0600

    Update mod.rs

--- src/solver/changes/mod.rs
+++ src/solver/changes/mod.rs
@@ -1,9 +1,7 @@
-pub mod apply;
-pub mod generation;
-pub mod parsing;
-pub mod types;
+mod apply;
+mod generation;
+mod parsing;
 
-pub use apply::apply_changes;
-pub use generation::generate_changes;
+pub use apply::apply_file_changes;
+pub use generation::{extract_keywords, generate_changes, validate_changes_relevance};
 pub use parsing::parse_search_replace;
-pub use types::{ChangeBlock, ChangeResponse};
