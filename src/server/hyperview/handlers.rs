26497c92f974eda2426dc715eb1b4d4a4ef36c7b
Christopher David <chris@openagents.com>
2025-02-06 05:23:53 -0600

    fmt

--- src/server/hyperview/handlers.rs
+++ src/server/hyperview/handlers.rs
@@ -1,15 +1,16 @@
-use axum::{
-    response::Response,
-    http::{header, StatusCode},
-    extract::State,
-};
 use crate::server::config::AppState;
+use axum::{
+    extract::State,
+    http::{header, StatusCode},
+    response::Response,
+};
 
 pub async fn hello_world(State(_state): State<AppState>) -> Response {
     Response::builder()
         .status(StatusCode::OK)
         .header(header::CONTENT_TYPE, "application/vnd.hyperview+xml")
-        .body(r#"<?xml version="1.0" encoding="UTF-8"?>
+        .body(
+            r#"<?xml version="1.0" encoding="UTF-8"?>
 <doc xmlns="https://hyperview.org/hyperview">
   <screen>
     <styles>
@@ -21,6 +22,9 @@ pub async fn hello_world(State(_state): State<AppState>) -> Response {
       </view>
     </body>
   </screen>
-</doc>"#.to_string().into())
+</doc>"#
+                .to_string()
+                .into(),
+        )
         .unwrap()
 }
