45ee794c5d25565404f378c0980efc9b785920b7
Christopher David <chris@openagents.com>
2025-02-05 22:10:01 -0600

    Update issue.rs

--- src/bin/solver_impl/issue.rs
+++ src/bin/solver_impl/issue.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use openagents::server::services::github_issue::GitHubService;
-use openagents::server::services::github_issue::types::{GitHubIssue, GitHubComment};
 use octocrab::models::issues::{Issue, Comment};
 
 pub async fn handle_issue(
@@ -9,14 +8,7 @@ pub async fn handle_issue(
     name: &str,
     issue_num: i32,
 ) -> Result<(Issue, Vec<Comment>)> {
-    let issue: GitHubIssue = github.get_issue(owner, name, issue_num).await?;
-    let comments: Vec<GitHubComment> = github.get_issue_comments(owner, name, issue_num).await?;
-    
-    // Convert types
-    let issue = Issue::try_from(issue)?;
-    let comments = comments.into_iter()
-        .map(Comment::try_from)
-        .collect::<Result<Vec<_>, _>>()?;
-
+    let issue = github.get_issue(owner, name, issue_num).await?;
+    let comments = github.get_issue_comments(owner, name, issue_num).await?;
     Ok((issue, comments))
 }
