ac6c1a88212a8f4cc7448158dd0b4f93c036e68b
Christopher David <chris@openagents.com>
2025-02-06 05:21:25 -0600

    format

--- src/server/hyperview/handlers.rs
+++ src/server/hyperview/handlers.rs
@@ -1,15 +1,17 @@
-use axum::{
-    response::Response,
-    http::{header, StatusCode},
-    extract::State,
-};
 use crate::server::config::AppState;
+use axum::{
+    extract::State,
+    http::{header, StatusCode},
+    response::Response,
+};
 
 pub async fn hello_world(State(_state): State<AppState>) -> Response {
     Response::builder()
         .status(StatusCode::OK)
         .header(header::CONTENT_TYPE, "application/vnd.hyperview+xml")
-        .body(format!(r#"<?xml version="1.0" encoding="UTF-8"?>
+        .body(
+            format!(
+                r#"<?xml version="1.0" encoding="UTF-8"?>
 <doc xmlns="https://hyperview.org/hyperview">
   <screen>
     <styles>
@@ -21,6 +23,9 @@ pub async fn hello_world(State(_state): State<AppState>) -> Response {
       </view>
     </body>
   </screen>
-</doc>"#).into())
+</doc>"#
+            )
+            .into(),
+        )
         .unwrap()
 }
--- src/server/hyperview/routes.rs
+++ src/server/hyperview/routes.rs
@@ -1,11 +1,7 @@
-use axum::{
-    Router,
-    routing::get,
-};
-use crate::server::config::AppState;
 use super::handlers;
+use crate::server::config::AppState;
+use axum::{routing::get, Router};
 
 pub fn hyperview_routes() -> Router<AppState> {
-    Router::new()
-        .route("/hyperview", get(handlers::hello_world))
+    Router::new().route("/hyperview", get(handlers::hello_world))
 }
