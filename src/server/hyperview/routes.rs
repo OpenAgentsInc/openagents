6cc556f5c6b46e12925ca2f5cdfe42a3c61293da
Christopher David <chris@openagents.com>
2025-02-06 05:19:56 -0600

    Update routes.rs

--- src/server/hyperview/routes.rs
+++ src/server/hyperview/routes.rs
@@ -2,9 +2,10 @@ use axum::{
     Router,
     routing::get,
 };
+use crate::server::config::AppState;
 use super::handlers;
 
-pub fn hyperview_routes() -> Router {
+pub fn hyperview_routes() -> Router<AppState> {
     Router::new()
         .route("/hyperview", get(handlers::hello_world))
 }
