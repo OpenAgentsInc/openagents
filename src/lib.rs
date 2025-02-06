119b71c204b393c162792e8521f96ca836786fbf
Christopher David <chris@openagents.com>
2025-02-05 22:03:19 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod configuration;
 pub mod database;
 pub mod filters;
+pub mod repo;
 pub mod repomap;
 pub mod routes;
 pub mod server;
@@ -9,6 +10,7 @@ pub mod solver;
 pub use configuration::get_configuration;
 pub use database::get_connection_pool;
 pub use filters::render_markdown;
+pub use repo::analysis::analyze_repository;
 pub use repomap::generate_repo_map;
 pub use routes::*;
 pub use server::services;
