25ecd8a08b5923f6d67241602d3326ccc74fbcc3
Christopher David <chris@openagents.com>
2025-02-10 18:11:12 -0600

    Update user.rs

--- src/server/models/user.rs
+++ src/server/models/user.rs
@@ -1,12 +1,16 @@
+use axum::extract::{FromRef, FromRequestParts};
+use axum::http::{request::Parts, StatusCode};
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
-use sqlx::types::JsonValue;
+use serde_json::Value;
+use sqlx::PgPool;
 use time::OffsetDateTime;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
     pub scramble_id: String,
-    pub metadata: Option<JsonValue>,
+    pub metadata: Option<Value>,
     #[serde(with = "time::serde::timestamp::option")]
     pub last_login_at: Option<OffsetDateTime>,
     #[serde(with = "time::serde::timestamp::option")]
@@ -15,8 +19,36 @@ pub struct User {
     pub updated_at: Option<OffsetDateTime>,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct CreateUser {
-    pub scramble_id: String,
-    pub metadata: Option<JsonValue>,
+#[async_trait]
+impl<'a, S> FromRequestParts<S> for User
+where
+    S: Send + Sync,
+    PgPool: FromRef<S>,
+{
+    type Rejection = StatusCode;
+
+    async fn from_request_parts<'b>(
+        _parts: &'b mut Parts,
+        _state: &'b S,
+    ) -> Result<Self, Self::Rejection> {
+        // TODO: Get user from session/token
+        // For now, return a mock user for testing
+        Ok(User {
+            id: 1,
+            scramble_id: "test_user".to_string(),
+            metadata: Some(serde_json::json!({
+                "github": {
+                    "id": 123456,
+                    "login": "test",
+                    "name": "Test User",
+                    "email": "test@example.com",
+                    "access_token": "gho_test",
+                    "scope": "repo,user"
+                }
+            })),
+            last_login_at: Some(OffsetDateTime::now_utc()),
+            created_at: Some(OffsetDateTime::now_utc()),
+            updated_at: Some(OffsetDateTime::now_utc()),
+        })
+    }
 }
