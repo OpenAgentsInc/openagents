c79efe6a30dcb28c8f22a73785efb3f98ae19a9e
Christopher David <chris@openagents.com>
2025-02-10 20:12:01 -0600

    Update user.rs

--- src/server/models/user.rs
+++ src/server/models/user.rs
@@ -1,38 +1,10 @@
 use serde::{Deserialize, Serialize};
 use sqlx::types::JsonValue;
 use time::OffsetDateTime;
-use axum::extract::FromRequestParts;
-use axum::http::request::Parts;
-use std::sync::Arc;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct User {
     pub id: i32,
     pub scramble_id: String,
     pub metadata: Option<JsonValue>,
-    #[serde(with = "time::serde::timestamp::option")]
-    pub last_login_at: Option<OffsetDateTime>,
-    #[serde(with = "time::serde::timestamp::option")]
-    pub created_at: Option<OffsetDateTime>,
-    #[serde(with = "time::serde::timestamp::option")]
-    pub updated_at: Option<OffsetDateTime>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct CreateUser {
-    pub scramble_id: String,
-    pub metadata: Option<JsonValue>,
-}
-
-#[async_trait::async_trait]
-impl<S> FromRequestParts<S> for User
-where
-    S: Send + Sync,
-{
-    type Rejection = axum::response::Response;
-
-    async fn from_request_parts(_parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        // Implement your user extraction logic here
-        todo!("Implement user extraction")
-    }
 }
