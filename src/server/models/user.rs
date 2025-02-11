a6d9a86737aebb29559fde281ed72bde54384db0
Christopher David <chris@openagents.com>
2025-02-10 20:14:13 -0600

    Update user.rs

--- src/server/models/user.rs
+++ src/server/models/user.rs
@@ -7,4 +7,16 @@ pub struct User {
     pub id: i32,
     pub scramble_id: String,
     pub metadata: Option<JsonValue>,
+    #[serde(with = "time::serde::timestamp::option")]
+    pub last_login_at: Option<OffsetDateTime>,
+    #[serde(with = "time::serde::timestamp::option")]
+    pub created_at: Option<OffsetDateTime>,
+    #[serde(with = "time::serde::timestamp::option")]
+    pub updated_at: Option<OffsetDateTime>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateUser {
+    pub scramble_id: String,
+    pub metadata: Option<JsonValue>,
 }
