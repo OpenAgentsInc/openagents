47d85af8ca604b1a9653ca33302f35c3ecedac84
Christopher David <chris@openagents.com>
2025-02-11 21:37:02 -0600

    Update mod.rs

--- src/server/hyperview/handlers/mod.rs
+++ src/server/hyperview/handlers/mod.rs
@@ -1,5 +1,15 @@
+mod auth;
+mod content;
+mod pages;
 mod repomap;
 mod repos;
+mod status;
+mod user;
 
-pub use repomap::*;
-pub use repos::*;
+pub use auth::mobile_logout;
+pub use content::content;
+pub use pages::{login_page, main_page};
+pub use repomap::generate_repomap;
+pub use repos::github_repos;
+pub use status::{connected_status, disconnected_status};
+pub use user::user_info;
