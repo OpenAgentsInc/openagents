58c9813020cbf76095e362f51c79de71cbf3059a
Christopher David <chris@openagents.com>
2025-02-11 21:31:37 -0600

    Update routes.rs

--- src/server/hyperview/routes.rs
+++ src/server/hyperview/routes.rs
@@ -1,28 +1,13 @@
-use super::{handlers, ws};
+use super::handlers;
 use crate::server::config::AppState;
 use axum::{routing::get, Router};
 
 pub fn hyperview_routes() -> Router<AppState> {
     Router::new()
-        .route("/hyperview/main", get(handlers::main_page))
-        .route("/hyperview/fragments/user-info", get(handlers::user_info))
         .route(
             "/hyperview/fragments/github-repos",
             get(handlers::github_repos),
         )
-        .route(
-            "/hyperview/fragments/connected-status",
-            get(handlers::connected_status),
-        )
-        .route(
-            "/hyperview/fragments/disconnected-status",
-            get(handlers::disconnected_status),
-        )
-        .route("/hyperview/ws", get(ws::hyperview_ws_handler))
-        .route("/hyperview/fragments/content", get(handlers::content))
-        .route("/templates/pages/auth/login.xml", get(handlers::login_page))
-        .route("/templates/pages/main.xml", get(handlers::main_page))
-        .route("/hyperview/auth/logout", get(handlers::mobile_logout))
         .route(
             "/hyperview/repo/:owner/:repo/repomap",
             get(handlers::generate_repomap),
