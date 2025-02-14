021a3860dd154176f92f6c9306be63b7334ad711
Christopher David <chris@openagents.com>
2025-02-14 10:25:21 -0600

    hm

--- src/server/hyperview/routes.rs
+++ src/server/hyperview/routes.rs
@@ -31,6 +31,101 @@ pub fn hyperview_routes() -> Router<AppState> {
         .route("/templates/pages/auth/login.xml", get(handlers::login_page))
         .route("/templates/pages/main.xml", get(handlers::main_page))
         .route("/hyperview/solve-demo-modal", get(solve_demo_modal))
+        .route("/hyperview/screen2", get(demo_screen2))
+        .route("/hyperview/screen2-redirect", get(screen2_redirect))
+        .route("/hyperview/modal-redirect", get(modal_redirect))
+}
+
+pub async fn demo_home() -> impl IntoResponse {
+    let xml = r###"<?xml version="1.0" encoding="UTF-8"?>
+<doc xmlns="https://hyperview.org/hyperview">
+  <screen>
+    <styles>
+      <style id="container" flex="1" backgroundColor="black" paddingTop="48" />
+      <style id="userInfoText" color="white" fontSize="16" />
+      <style fontSize="16" id="Description" margin="24" marginBottom="0" />
+      <style fontFamily="HKGrotesk-Medium" color="white" fontSize="24" id="Basic" margin="24" />
+      <style fontFamily="HKGrotesk-Bold" color="white" fontSize="16" id="Bold" margin="24" />
+      <style backgroundColor="#63CB76" color="white" fontSize="32" id="Color" margin="24" padding="16" />
+    </styles>
+
+    <body scroll="true" style="container">
+      <text style="Basic">Home Screen</text>
+      <text style="Color">Welcome to Demo!</text>
+
+      <view>
+        <behavior trigger="press" action="push" href="/screen2" />
+        <text style="Basic">Go to Screen 2</text>
+      </view>
+    </body>
+  </screen>
+</doc>"###;
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/vnd.hyperview+xml")
+        .body(Body::from(xml))
+        .unwrap()
+}
+
+pub async fn demo_screen2() -> impl IntoResponse {
+    let xml = r###"<?xml version="1.0" encoding="UTF-8"?>
+<doc xmlns="https://hyperview.org/hyperview">
+  <screen>
+    <styles>
+      <style id="container" flex="1" backgroundColor="#2C3E50" paddingTop="48" />
+      <style fontFamily="HKGrotesk-Medium" color="white" fontSize="24" id="Basic" margin="24" />
+      <style backgroundColor="#E74C3C" color="white" fontSize="32" id="Color" margin="24" padding="16" />
+      <style id="button" backgroundColor="#444" padding="16" marginBottom="16" borderRadius="8" alignItems="center" />
+      <style id="buttonText" color="white" fontSize="16" />
+    </styles>
+
+    <body scroll="true" style="container">
+      <text style="Basic">Screen 2</text>
+      <text style="Color">Second Demo Screen!</text>
+
+      <view style="button">
+        <behavior trigger="press" action="back" />
+        <text style="buttonText">Go Back</text>
+      </view>
+    </body>
+  </screen>
+</doc>"###;
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/vnd.hyperview+xml")
+        .body(Body::from(xml))
+        .unwrap()
+}
+
+pub async fn demo_screen3() -> impl IntoResponse {
+    let xml = r###"<?xml version="1.0" encoding="UTF-8"?>
+<doc xmlns="https://hyperview.org/hyperview">
+  <screen>
+    <styles>
+      <style id="container" flex="1" backgroundColor="#8E44AD" paddingTop="48" />
+      <style fontFamily="HKGrotesk-Medium" color="white" fontSize="24" id="Basic" margin="24" />
+      <style backgroundColor="#2ECC71" color="white" fontSize="32" id="Color" margin="24" padding="16" />
+    </styles>
+
+    <body scroll="true" style="container">
+      <text style="Basic">Screen 3 (Modal)</text>
+      <text style="Color">Modal Demo Screen!</text>
+
+      <view>
+        <behavior trigger="press" action="back" />
+        <text style="Basic">Go Back</text>
+      </view>
+    </body>
+  </screen>
+</doc>"###;
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/vnd.hyperview+xml")
+        .body(Body::from(xml))
+        .unwrap()
 }
 
 pub async fn solve_demo_modal() -> impl IntoResponse {
@@ -82,5 +177,25 @@ pub async fn solve_demo_modal() -> impl IntoResponse {
         .status(StatusCode::OK)
         .header("Content-Type", "application/vnd.hyperview+xml")
         .body(Body::from(modal_xml))
+        .unwrap()
+}
+
+pub async fn screen2_redirect() -> impl IntoResponse {
+    let xml = r###"<behavior xmlns="https://hyperview.org/hyperview" trigger="load" action="push" href="/hyperview/screen2" />"###;
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/vnd.hyperview+xml")
+        .body(Body::from(xml))
+        .unwrap()
+}
+
+pub async fn modal_redirect() -> impl IntoResponse {
+    let xml = r###"<behavior xmlns="https://hyperview.org/hyperview" trigger="load" action="new" href="/hyperview/solve-demo-modal" />"###;
+
+    Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/vnd.hyperview+xml")
+        .body(Body::from(xml))
         .unwrap()
 }
