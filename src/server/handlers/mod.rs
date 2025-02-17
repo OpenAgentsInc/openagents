d4bdb61abcd8c4b6ea750a04a73f6c561fb1566c
Christopher David <chris@openagents.com>
2025-02-16 20:09:51 -0600

    Update mod.rs

--- src/server/handlers/mod.rs
+++ src/server/handlers/mod.rs
@@ -1,9 +1,12 @@
 pub mod auth;
+pub mod oauth;
+pub mod user;
 
 pub use auth::{
     callback,
     login::{handle_login, login_page},
     session::clear_session_and_redirect,
     signup::{handle_signup, signup_page},
-    AuthState,
 };
+pub use oauth::github::{github_callback, github_login};
+pub use oauth::scramble::{scramble_callback, scramble_login, scramble_signup};
