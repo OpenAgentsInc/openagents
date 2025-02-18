9bbac9efcf84e40db2de1e4c7e441a91f0a85fd6
Christopher David <chris@openagents.com>
2025-02-17 23:06:58 -0600

    CI fix and delete old (#717)
    * shh
    
    * axe frontendold

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-animate'),
+    require('@tailwindcss/typography'),
+  ],
+}
