2615f740593381a0b33b5a24569282f2035e8012
Christopher David <chris@openagents.com>
2025-02-17 21:44:23 -0600

    Update onyx.tsx

--- frontend/app/routes/onyx.tsx
+++ frontend/app/routes/onyx.tsx
@@ -9,16 +9,9 @@ export function meta({}: Route.MetaArgs) {
 
 export default function MobileApp() {
   return (
-    <div className="flex justify-center mx-2 md:mx-6">
-      <div className="w-[60rem] max-w-full my-6 px-4 py-6 border border-white">
-        <div className="grid grid-cols-3 lg:grid-cols-6 gap-4 mb-6">
-          {/* Navigation buttons will go here */}
-        </div>
-        <div id="content">
-          <h1>Mobile App - Onyx</h1>
-          {/* Add your mobile app content here */}
-        </div>
-      </div>
+    <div id="content">
+      <h1>Mobile App - Onyx</h1>
+      {/* Add your mobile app content here */}
     </div>
   );
 }
