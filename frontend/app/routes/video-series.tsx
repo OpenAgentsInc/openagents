cdf336e0dbb1b955a3c2573c66e6b87f53f3dd59
Christopher David <chris@openagents.com>
2025-02-17 21:32:36 -0600

    Update video-series.tsx

--- frontend/app/routes/video-series.tsx
+++ frontend/app/routes/video-series.tsx
@@ -10,16 +10,12 @@ export const meta: MetaFunction = () => {
 
 export default function VideoSeries() {
   return (
-    <div className="min-h-screen w-screen bg-black text-white font-mono overflow-x-hidden">
-      <div className="flex justify-center mx-2 md:mx-6">
-        <div className="w-[60rem] max-w-full my-6 px-4 py-6 border border-white">
-          <Navigation />
-          <div id="content">
-            <h1>Video Series</h1>
-            {/* Add your video series content here */}
-          </div>
-        </div>
+    <>
+      <Navigation />
+      <div id="content">
+        <h1>Video Series</h1>
+        {/* Add your video series content here */}
       </div>
-    </div>
+    </>
   );
 }
