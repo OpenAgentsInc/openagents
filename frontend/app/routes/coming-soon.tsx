6d43d77d16d2e99015a3cd9b2e0980ef58741fa4
Christopher David <chris@openagents.com>
2025-02-17 21:33:30 -0600

    Update coming-soon.tsx

--- frontend/app/routes/coming-soon.tsx
+++ frontend/app/routes/coming-soon.tsx
@@ -10,16 +10,12 @@ export const meta: MetaFunction = () => {
 
 export default function ComingSoon() {
   return (
-    <div className="min-h-screen w-screen bg-black text-white font-mono overflow-x-hidden">
-      <div className="flex justify-center mx-2 md:mx-6">
-        <div className="w-[60rem] max-w-full my-6 px-4 py-6 border border-white">
-          <Navigation />
-          <div id="content">
-            <h1>Coming Soon</h1>
-            {/* Add your coming soon content here */}
-          </div>
-        </div>
+    <>
+      <Navigation />
+      <div id="content">
+        <h1>Coming Soon</h1>
+        {/* Add your coming soon content here */}
       </div>
-    </div>
+    </>
   );
 }
