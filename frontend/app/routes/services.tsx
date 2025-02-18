d816ee7feceab510aeb015a971c2139c35cd4f51
Christopher David <chris@openagents.com>
2025-02-17 21:45:03 -0600

    Update services.tsx

--- frontend/app/routes/services.tsx
+++ frontend/app/routes/services.tsx
@@ -9,16 +9,9 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Services() {
   return (
-    <div className="flex justify-center mx-2 md:mx-6">
-      <div className="w-[60rem] max-w-full my-6 px-4 py-6 border border-white">
-        <div className="grid grid-cols-3 lg:grid-cols-6 gap-4 mb-6">
-          {/* Navigation buttons will go here */}
-        </div>
-        <div id="content">
-          <h1>Services</h1>
-          {/* Add your services content here */}
-        </div>
-      </div>
+    <div id="content">
+      <h1>Services</h1>
+      {/* Add your services content here */}
     </div>
   );
 }
