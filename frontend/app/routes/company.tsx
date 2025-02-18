3f378a5be4126baab1263212bc94c82cf9ba248b
Christopher David <chris@openagents.com>
2025-02-17 21:33:12 -0600

    Update company.tsx

--- frontend/app/routes/company.tsx
+++ frontend/app/routes/company.tsx
@@ -10,16 +10,12 @@ export const meta: MetaFunction = () => {
 
 export default function Company() {
   return (
-    <div className="min-h-screen w-screen bg-black text-white font-mono overflow-x-hidden">
-      <div className="flex justify-center mx-2 md:mx-6">
-        <div className="w-[60rem] max-w-full my-6 px-4 py-6 border border-white">
-          <Navigation />
-          <div id="content">
-            <h1>Company</h1>
-            {/* Add your company content here */}
-          </div>
-        </div>
+    <>
+      <Navigation />
+      <div id="content">
+        <h1>Company</h1>
+        {/* Add your company content here */}
       </div>
-    </div>
+    </>
   );
 }
