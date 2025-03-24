01b561049176d2881cb3a328ce3b1fe2f320c06f
Christopher David <chris@openagents.com>
2025-03-23 23:04:36 -0500

    pull in button

--- apps/coder/src/pages/HomePage.tsx
+++ apps/coder/src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@openagents/ui";
 import React from "react";
 
 export default function HomePage() {
   return (
     <div className="flex h-full flex-col">
-
+      <Button label="Noice" />
     </div>
   );
 }
