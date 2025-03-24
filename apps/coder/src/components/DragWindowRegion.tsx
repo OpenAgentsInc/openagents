0df0da436c4d5526b4684ab8a7566734cb60dd37
Christopher David <chris@openagents.com>
2025-03-23 23:00:42 -0500

    shh

--- apps/coder/src/components/DragWindowRegion.tsx
+++ apps/coder/src/components/DragWindowRegion.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DragWindowRegion() {
   return (
-    <div className="bg-black flex w-screen items-stretch justify-between">
+    <div className="flex w-screen items-stretch justify-between">
       <div className="draglayer w-full h-[40px]">
       </div>
     </div>
