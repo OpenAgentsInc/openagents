b205261cb40d20c0d0f6ba1dae9d688faeb62af4
Christopher David <chris@openagents.com>
2025-03-24 21:29:19 -0500

    ai sdk pasta

--- apps/chatserver/src/index.ts
+++ apps/chatserver/src/index.ts
@@ -1,9 +1,22 @@
-import { Hono } from 'hono'
+import { openai } from '@ai-sdk/openai';
+import { serve } from '@hono/node-server';
+import { streamText } from 'ai';
+import { Hono } from 'hono';
+import { stream } from 'hono/streaming';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post('/', async c => {
+  const result = streamText({
+    model: openai('gpt-4o'),
+    prompt: 'Invent a new holiday and describe its traditions.',
+  });
 
-export default app
+  // Mark the response as a v1 data stream:
+  c.header('X-Vercel-AI-Data-Stream', 'v1');
+  c.header('Content-Type', 'text/plain; charset=utf-8');
+
+  return stream(c, stream => stream.pipe(result.toDataStream()));
+});
+
+serve({ fetch: app.fetch, port: 8080 });
