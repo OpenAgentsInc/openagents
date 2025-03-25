9186c9fa397063bc944179d2be2710dfb795e45c
Christopher David <chris@openagents.com>
2025-03-24 21:47:18 -0500

    [Chat] Return demo chat response via Workers AI (#776)
    * readme
    
    * ai sdk pasta
    
    * types and wranglin
    
    * types and cleanup
    
    * copy

--- apps/chatserver/src/index.ts
+++ apps/chatserver/src/index.ts
@@ -1,9 +1,28 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { stream } from 'hono/streaming';
+import { streamText } from "ai";
+import { createWorkersAI } from "workers-ai-provider";
 
-const app = new Hono()
+interface Env {
+  AI: typeof AI;
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Env }>();
 
-export default app
+app.get('/', c => c.text('200'));
+
+app.post('/', async c => {
+  const workersai = createWorkersAI({ binding: c.env.AI });
+  const result = streamText({
+    model: workersai("@cf/meta/llama-3.3-70b-instruct-fp8-fast"),
+    prompt: 'Write one paragraph speculating about collaborative, multiplayer, asynchronous AI agents for coding and more',
+  });
+
+  // Mark the response as a v1 data stream:
+  c.header('X-Vercel-AI-Data-Stream', 'v1');
+  c.header('Content-Type', 'text/plain; charset=utf-8');
+
+  return stream(c, stream => stream.pipe(result.toDataStream()));
+});
+
+export default app;
