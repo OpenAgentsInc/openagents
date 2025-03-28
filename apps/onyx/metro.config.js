64be913f475e4b8b705716697ff5bbadcb4aed74
Christopher David <chris@openagents.com>
2025-03-28 10:08:57 -0400

    Switch Onyx to useChat from core package; remove MCP client from Coder (#794)
    * Add openagents/core
    
    * axe demo shaz
    
    * axe client shaz from coder
    
    * use our usechat

--- apps/onyx/metro.config.js
+++ apps/onyx/metro.config.js
@@ -10,6 +10,7 @@ const config = getDefaultConfig(projectRoot);
 // If your monorepo tooling can give you the list of monorepo workspaces linked
 // in your app workspace, you can automate this list instead of hardcoding them.
 const monorepoPackages = {
+  '@openagents/core': path.resolve(monorepoRoot, 'packages/core'),
   '@openagents/ui': path.resolve(monorepoRoot, 'packages/ui'),
 };
 
@@ -17,16 +18,17 @@ const monorepoPackages = {
 // Note how we change this from `monorepoRoot` to `projectRoot`. This is part of the optimization!
 config.watchFolders = [projectRoot, ...Object.values(monorepoPackages)];
 
-// Add the monorepo workspaces as `extraNodeModules` to Metro.
-// If your monorepo tooling creates workspace symlinks in the `node_modules` directory,
-// you can either add symlink support to Metro or set the `extraNodeModules` to avoid the symlinks.
-// See: https://metrobundler.dev/docs/configuration/#extranodemodules
-config.resolver.extraNodeModules = monorepoPackages;
+// Add the monorepo workspaces and MCP SDK as extraNodeModules
+config.resolver.extraNodeModules = {
+  ...monorepoPackages,
+  '@modelcontextprotocol/sdk': path.resolve(monorepoRoot, 'node_modules/@modelcontextprotocol/sdk/dist/esm')
+};
 
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'packages/core/node_modules'),
 ];
 
 module.exports = config;
