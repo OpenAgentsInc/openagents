5556a905f09f638ff106229b5b000307201932e2
Christopher David <chris@openagents.com>
2025-03-30 13:58:49 -0400

    shh

--- apps/coder/src/pages/HomePage.tsx
+++ apps/coder/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function HomePage() {
   return (
     <div className="font-mono flex flex-col h-full text-white">
-      <div className="flex flex-row justify-between items-center p-2">
+      <div className="mx-8 flex flex-row justify-between items-center p-2">
         Coder
+        <Button variant="outline">Test</Button>
       </div>
     </div>
   );
