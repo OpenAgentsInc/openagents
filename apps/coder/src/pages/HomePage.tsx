99560675d64d34ef67524d8a13e29a8dc58a4193
Christopher David <chris@openagents.com>
2025-03-30 14:06:55 -0400

    [Coder] Add basic shadui AI chat (#810)
    * npx shadcn@latest add --all
    
    * shh
    
    * npx shadcn@latest add https://shadcn-chatbot-kit.vercel.app/r/chat.json
    
    * typefix and chat is there
    
    * no bullshit
    
    * shhh

--- apps/coder/src/pages/HomePage.tsx
+++ apps/coder/src/pages/HomePage.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { useChat } from "@ai-sdk/react"
+import { Chat } from "@/components/ui/chat"
 
 export default function HomePage() {
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
+    useChat({
+      api: "https://chat.openagents.com"
+    })
   return (
+
     <div className="font-mono flex flex-col h-full text-white">
-      <div className="flex flex-row justify-between items-center p-2">
-        Coder
+      <div>
+        <Chat
+          messages={messages}
+          input={input}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+          isGenerating={isLoading}
+          stop={stop}
+        />
       </div>
     </div>
   );
