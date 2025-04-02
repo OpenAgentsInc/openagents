8688ddee01b19cd35713788235152be9e2b54ff9
Christopher David <chris@openagents.com>
2025-04-02 06:59:11 -0500

    hm

--- apps/coder/src/components/ui/sonner.tsx
+++ apps/coder/src/components/ui/sonner.tsx
@@ -7,7 +7,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
+      className="toaster group !font-mono"
       style={
         {
           "--normal-bg": "var(--popover)",
