3a16e126c8ab9bec0008b4d28b8fe9b9396d727a
Christopher David <chris@openagents.com>
2025-04-04 20:08:59 -0500

    hmm

--- apps/coder/src/components/DragWindowRegion.tsx
+++ apps/coder/src/components/DragWindowRegion.tsx
@@ -2,7 +2,7 @@ export default function DragWindowRegion() {
   return (
     <div className="fixed top-0 left-0 right-0 z-50 border-b">
       <div className="flex w-screen items-stretch justify-between">
-        <div className="draglayer w-full h-[30px]">
+        <div className="draglayer w-full h-[30px] bg-transparent">
         </div>
       </div>
     </div>
