04886138a3143c52e24338ffe2701e9898c22dc2
Christopher David <chris@openagents.com>
2025-04-07 21:35:00 -0500

    mcp client maybe

--- packages/agents/src/server.ts
+++ packages/agents/src/server.ts
@@ -12,6 +12,27 @@ import { AsyncLocalStorage } from "node:async_hooks";
 import { createGoogleGenerativeAI } from '@ai-sdk/google';
 import { env } from "cloudflare:workers";
 
+import { MCPClientManager } from "agents/mcp/client";
+import type {
+  Tool,
+  Prompt,
+  Resource,
+} from "@modelcontextprotocol/sdk/types.js";
+
+export type Server = {
+  url: string;
+  state: "authenticating" | "connecting" | "ready" | "discovering" | "failed";
+  authUrl?: string;
+};
+
+export type State = {
+  servers: Record<string, Server>;
+  tools: (Tool & { serverId: string })[];
+  prompts: (Prompt & { serverId: string })[];
+  resources: (Resource & { serverId: string })[];
+};
+
+
 const google = createGoogleGenerativeAI({
   apiKey: env.GOOGLE_API_KEY,
 });
@@ -24,7 +45,8 @@ export const agentContext = new AsyncLocalStorage<Coder>();
 /**
  * Chat Agent implementation that handles real-time AI chat interactions
  */
-export class Coder extends AIChatAgent<Env> {
+export class Coder extends AIChatAgent<Env, State> {
+
   githubToken?: string;
   combinedTools?: Record<string, any>;
 
@@ -32,6 +54,91 @@ export class Coder extends AIChatAgent<Env> {
    * MCP STUFF
    */
 
+  initialState = {
+    servers: {},
+    tools: [],
+    prompts: [],
+    resources: [],
+  };
+
+  private mcp_: MCPClientManager | undefined;
+
+  get mcp(): MCPClientManager {
+    if (!this.mcp_) {
+      throw new Error("MCPClientManager not initialized");
+    }
+
+    return this.mcp_;
+  }
+
+  onStart() {
+    this.mcp_ = new MCPClientManager("coder", "0.0.1", {
+      baseCallbackUri: `https://agents.openagents.com/agents/coder/${this.name}/callback`,
+      storage: this.ctx.storage,
+    });
+    console.log("MCPClientManager initialized");
+  }
+
+  setServerState(id: string, state: Server) {
+    this.setState({
+      ...this.state,
+      servers: {
+        ...this.state.servers,
+        [id]: state,
+      },
+    });
+  }
+
+  async refreshServerData() {
+    this.setState({
+      ...this.state,
+      prompts: this.mcp.listPrompts(),
+      tools: this.mcp.listTools(),
+      resources: this.mcp.listResources(),
+    });
+  }
+
+  async addMcpServer(url: string): Promise<string> {
+    console.log(`Registering server: ${url}`);
+    const { id, authUrl } = await this.mcp.connect(url);
+    this.setServerState(id, {
+      url,
+      authUrl,
+      state: this.mcp.mcpConnections[id].connectionState,
+    });
+    return authUrl ?? "";
+  }
+
+  async onRequest(request: Request): Promise<Response> {
+    if (this.mcp.isCallbackRequest(request)) {
+      try {
+        const { serverId } = await this.mcp.handleCallbackRequest(request);
+        this.setServerState(serverId, {
+          url: this.state.servers[serverId].url,
+          state: this.mcp.mcpConnections[serverId].connectionState,
+        });
+        await this.refreshServerData();
+        // Hack: autoclosing window because a redirect fails for some reason
+        // return Response.redirect('http://localhost:5173/', 301)
+        return new Response("<script>window.close();</script>", {
+          status: 200,
+          headers: { "content-type": "text/html" },
+        });
+        // biome-ignore lint/suspicious/noExplicitAny: just bubbling an error up
+      } catch (e: any) {
+        return new Response(e, { status: 401 });
+      }
+    }
+
+    const reqUrl = new URL(request.url);
+    if (reqUrl.pathname.endsWith("add-mcp") && request.method === "POST") {
+      const mcpServer = (await request.json()) as { url: string };
+      const authUrl = await this.addMcpServer(mcpServer.url);
+      return new Response(authUrl, { status: 200 });
+    }
+
+    return new Response("Not found", { status: 404 });
+  }
 
 
   /**
@@ -164,7 +271,7 @@ export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
     // Route the request to our agent or return 404 if not found
     return (
-      (await routeAgentRequest(request, env)) ||
+      (await routeAgentRequest(request, env, { cors: true })) ||
       new Response("Not found", { status: 404 })
     );
   },
