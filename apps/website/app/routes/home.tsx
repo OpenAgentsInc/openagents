eeca9e6ba9c3eb41e6eac8a1a140a72c8d860d63
Christopher David <chris@openagents.com>
2025-04-09 18:23:57 -0500

    axe value

--- apps/website/app/routes/home.tsx
+++ apps/website/app/routes/home.tsx
@@ -8,10 +8,6 @@ export function meta({ }: Route.MetaArgs) {
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+export default function Home() {
+  return <Welcome />;
 }
