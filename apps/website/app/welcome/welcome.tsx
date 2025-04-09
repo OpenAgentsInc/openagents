982652216914d0702feefb6ab4d5ea3a5f180a4a
Christopher David <chris@openagents.com>
2025-04-09 18:46:20 -0500

    button and lock etc

--- apps/website/app/welcome/welcome.tsx
+++ apps/website/app/welcome/welcome.tsx
@@ -1,8 +1,11 @@
+import { Button } from "~/components/ui/button";
+
 export function Welcome() {
   return (
     <main className="fixed w-screen min-h-screen gap-4 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold select-none">OpenAgents</h1>
       <p className="text-lg select-none opacity-50">New site coming soon</p>
+      <Button>Get Started</Button>
     </main>
   );
 }
