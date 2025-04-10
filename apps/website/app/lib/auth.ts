01fbb67f100da82490864eeab68e19bf528e1427
Christopher David <chris@openagents.com>
2025-04-10 07:31:22 -0500

    better sqlite w auth

---
+++ apps/website/app/lib/auth.ts
@@ -0,0 +1,6 @@
+import { betterAuth } from "better-auth";
+import Database from "better-sqlite3";
+
+export const auth: ReturnType<typeof betterAuth> = betterAuth({
+  database: new Database("./sqlite.db"),
+})
