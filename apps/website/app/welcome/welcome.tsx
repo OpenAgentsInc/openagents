2958c91f87f41a9ad78fb7fa518e63846515dadc
Christopher David <chris@openagents.com>
2025-04-09 19:02:44 -0500

    noice

--- apps/website/app/welcome/welcome.tsx
+++ apps/website/app/welcome/welcome.tsx
@@ -4,8 +4,8 @@ export function Welcome() {
   return (
     <main className="fixed w-screen min-h-screen gap-4 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold select-none">OpenAgents</h1>
-      <p className="text-lg select-none opacity-50">New site coming soon</p>
-      <Button>Get Started</Button>
+      <p className="text-lg select-none opacity-50">Under construction</p>
+      <Button variant="outline">Get Started</Button>
     </main>
   );
 }
