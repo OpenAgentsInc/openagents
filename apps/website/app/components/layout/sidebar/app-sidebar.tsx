f614533aeea5c64e75708607905700838ff7d59f
Christopher David <chris@openagents.com>
2025-04-14 21:03:41 -0500

    wow

--- apps/website/app/components/layout/sidebar/app-sidebar.tsx
+++ apps/website/app/components/layout/sidebar/app-sidebar.tsx
@@ -39,41 +39,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarContent>
       <SidebarFooter>
         <div className="w-full flex flex-col gap-2">
-          {open && (
-            <div className="group/sidebar relative flex flex-col gap-2 rounded-lg border p-4 text-sm w-full">
-              <div
-                className="absolute top-2.5 right-2 z-10 cursor-pointer"
-                onClick={() => setOpen(!open)}
-                role="button"
-              >
-                <X className="size-4" />
-              </div>
-              <div className="text-balance text-lg font-semibold leading-tight group-hover/sidebar:underline">
-                Open-source layouts by lndev-ui
-              </div>
-              <div>
-                Collection of beautifully crafted open-source layouts UI built with
-                shadcn/ui.
-              </div>
-              <Link
-                to="https://square.lndev.me"
-                target="_blank"
-                rel="noreferrer"
-                className="absolute inset-0"
-              >
-                <span className="sr-only">Square by lndev-ui</span>
-              </Link>
-              <Button size="sm" className="w-full">
-                <Link
-                  to="https://square.lndev.me"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  square.lndev.me
-                </Link>
-              </Button>
-            </div>
-          )}
+
           <div className="w-full flex items-center justify-between">
             <HelpButton />
             <Button size="icon" variant="secondary" asChild>
