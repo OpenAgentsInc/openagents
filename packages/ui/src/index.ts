48441fa15cf797b259198a922a29b30012eb3cb2
Christopher David <chris@openagents.com>
2025-06-04 13:50:46 -0500

    fix: rename kebab-case files to camelCase for valid JS exports
    - Renamed pane-menu.ts to paneMenu.ts
    - Renamed pane-store.ts to paneStore.ts
    - Updated imports in playground App.tsx
    - Fixed codegen exports to use valid JavaScript identifiers

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,14 @@
+
 export * as pane from "./core/types/pane.js"
-export * as paneMenu from "./core/types/pane-menu.js"
+
+
+export * as paneMenu from "./core/types/paneMenu.js"
+
+
 export * as cn from "./core/utils/cn.js"
+
+
 export * as os from "./core/utils/os.js"
-export * as paneStore from "./web/stores/pane-store.js"
+
+
+export * as paneStore from "./web/stores/paneStore.js"
