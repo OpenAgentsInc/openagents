99123acf4d08227f2369666964b3f63d9e44a96e
Christopher David <chris@openagents.com>
2025-06-14 22:27:57 -0400

    Fix linting for CSS styles

--- packages/ui/src/web/components/button.ts
+++ packages/ui/src/web/components/button.ts
@@ -15,16 +15,20 @@ export const Button = (props: ButtonProps): any => {
 
   const variantStyles = {
     default: "background-color: rgb(15 23 42); color: rgb(248 250 252); box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);",
-    destructive: "background-color: rgb(239 68 68); color: rgb(248 250 252); box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);",
-    outline: "border-color: rgb(226 232 240); background-color: rgb(255 255 255); color: rgb(15 23 42); box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);",
+    destructive:
+      "background-color: rgb(239 68 68); color: rgb(248 250 252); box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);",
+    outline:
+      "border-color: rgb(226 232 240); background-color: rgb(255 255 255); color: rgb(15 23 42); box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);",
     secondary: "background-color: rgb(241 245 249); color: rgb(15 23 42); box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);",
     ghost: "color: rgb(15 23 42); background-color: transparent;",
-    link: "color: rgb(15 23 42); text-decoration-line: underline; text-underline-offset: 4px; background-color: transparent;"
+    link:
+      "color: rgb(15 23 42); text-decoration-line: underline; text-underline-offset: 4px; background-color: transparent;"
   }
 
   const sizeStyles = {
     default: "height: 2.25rem; padding-left: 1rem; padding-right: 1rem; padding-top: 0.5rem; padding-bottom: 0.5rem;",
-    sm: "height: 2rem; border-radius: 0.375rem; padding-left: 0.75rem; padding-right: 0.75rem; font-size: 0.75rem; line-height: 1rem;",
+    sm:
+      "height: 2rem; border-radius: 0.375rem; padding-left: 0.75rem; padding-right: 0.75rem; font-size: 0.75rem; line-height: 1rem;",
     lg: "height: 2.75rem; border-radius: 0.375rem; padding-left: 2rem; padding-right: 2rem;",
     icon: "height: 2.25rem; width: 2.25rem;"
   }
