be36f7f2cf4ee25bcd13e832dc5c536f87a2672d
Christopher David <chris@openagents.com>
2025-11-01 11:17:43 -0500

    chore(storybook): add on-device Storybook for Expo with USE_STORYBOOK toggle; non-default opt-in scripts

---
+++ expo/storybook/stories/placeholder.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const meta = {
+  title: 'Example/Placeholder',
+}
+export default meta
+
+export const Basic = {
+  render: () => (
+    <View style={{ padding: 16 }}>
+      <Text>Hello Storybook</Text>
+    </View>
+  ),
+}
+
