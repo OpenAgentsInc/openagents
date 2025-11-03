2e42820e1ff2a5a8b5604a22e4c6190303894a1e
Christopher David <chris@openagents.com>
2025-11-03 10:46:50 -0600

    Tauri: add desktop entrypoint and use Bun for dev
    - Render minimal themed "Hello world" screen
    - Switch Tauri to Vite via Bun; stop loading Expo web
    - Update tauri.conf to use bun run dev/build and point to Vite

--- tauri/src/main.tsx
+++ tauri/src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import "./App.css";
+import HelloDesktop from "./desktop/HelloDesktop";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HelloDesktop />
   </React.StrictMode>,
 );
