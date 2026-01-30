4c044038a2ba2c840faaa81746c569a6dfeb6ea3
Christopher David <chris@openagents.com>
2026-01-30 11:36:02 -0600

    website: restore blog, add Hello world post (Jan 30 2026)

---
+++ apps/website/src/content/blog/hello-world.md
@@ -0,0 +1,7 @@
+---
+title: Hello world
+description: Welcome to the new site, redesigned to be friendly to agents and humans.
+pubDate: 2026-01-30
+---
+
+Welcome to the new site, redesigned to be friendly to agents and humans.
--- apps/website/src/pages/rss.xml.js
+++ apps/website/src/pages/rss.xml.js
@@ -3,19 +3,19 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	let posts = [];
-	try {
-		posts = await getCollection('blog');
-	} catch {
-		// Blog collection may be empty or not yet used.
-	}
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  let posts = [];
+  try {
+    posts = await getCollection('blog');
+  } catch {
+    // Blog collection may be empty or not yet used.
+  }
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.id}/`,
+    })),
+  });
 }
