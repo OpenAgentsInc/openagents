d5097d0d298aee585f5d317ab38bf117db8900d3
Christopher David <chris@openagents.com>
2026-01-30 11:06:12 -0600

    init log

---
+++ apps/website/astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    },
+
+    imageService: "cloudflare"
+  }),
+});
