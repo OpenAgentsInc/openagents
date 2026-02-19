862400925c5c53760f1e813cec4b65bd37169b96
Christopher David <chris@openagents.com>
2026-02-18 20:31:51 -0600

    Add server-side PostHog pageview capture for DAU reliability

--- apps/openagents.com/bootstrap/app.php
+++ apps/openagents.com/bootstrap/app.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\EnsureAdminEmail;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\CapturePostHogPageview;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -28,6 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->web(append: [
+            CapturePostHogPageview::class,
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
