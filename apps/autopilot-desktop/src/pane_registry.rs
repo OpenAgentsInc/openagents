f05afd50259dd966db88455e60e2bbeb6abf4acb
Christopher David <chris@openagents.com>
2026-03-06 10:11:53 -0600

    ui: show 4 for command palette hotbar slot

--- apps/autopilot-desktop/src/pane_registry.rs
+++ apps/autopilot-desktop/src/pane_registry.rs
@@ -9,9 +9,9 @@ pub const HOTBAR_SLOT_NOSTR_IDENTITY: u8 = 2;
 pub const HOTBAR_SLOT_SPARK_WALLET: u8 = 3;
 pub const HOTBAR_SLOT_COMMAND_PALETTE: u8 = 4;
 
-pub const HOTBAR_COMMAND_PALETTE_ICON: &str = "K";
+pub const HOTBAR_COMMAND_PALETTE_ICON: &str = "4";
 pub const HOTBAR_COMMAND_PALETTE_TOOLTIP: &str = "Command palette";
-pub const HOTBAR_COMMAND_PALETTE_SHORTCUT: &str = "K";
+pub const HOTBAR_COMMAND_PALETTE_SHORTCUT: &str = "4";
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct PaneCommandSpec {
