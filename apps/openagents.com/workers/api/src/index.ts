93b225ebc513a38d491168b4fef789b327edce04
AtlantisPleb <chris@openagents.com>
2026-06-29 06:04:14 -0500

    fix(khala): add serving rate monitor

--- apps/openagents.com/workers/api/src/index.ts
+++ apps/openagents.com/workers/api/src/index.ts
@@ -466,6 +466,7 @@ import {
   matchMirrorCodeRunByIdRequest,
 } from './inference/gym/mirrorcode-routes'
 import { makeD1MirrorCodeRunStore } from './inference/gym/mirrorcode-store'
+import { runServingRateMonitorScheduled } from './inference/serving-rate-monitor'
 import {
   handleOperatorKhalaHeadToHeadApi,
   handlePublicKhalaHeadToHeadApi,
@@ -13951,6 +13952,21 @@ export default {
             { scheduledTimeMs: event.scheduledTime },
             (line, fields) =>
               logWorkerRouteWarning('fleet_burn_stall_watchdog', {
+                line,
+                ...fields,
+              }),
+          ),
+        ),
+      ),
+      observedEffect(
+        'ServingRateMonitor.tick',
+        Effect.promise(() =>
+          runServingRateMonitorScheduled(
+            openAgentsDatabase(env),
+            env,
+            { scheduledTimeMs: event.scheduledTime },
+            (line, fields) =>
+              logWorkerRouteWarning('serving_rate_monitor', {
                 line,
                 ...fields,
               }),
