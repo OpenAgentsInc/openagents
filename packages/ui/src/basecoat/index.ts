e7902af148ca369eb48c4e8810eda90fcdf69070
Christopher David <chris@openagents.com>
2026-06-30 10:32:44 -0500

    Add Basecoat form components (#7725)

--- packages/ui/src/basecoat/index.ts
+++ packages/ui/src/basecoat/index.ts
@@ -3,6 +3,7 @@ export * from './button'
 export * from './card'
 export * from './containers'
 export * from './display'
+export * from './form'
 export * from './input'
 export * from './selection'
 export {
