ed0de439feb00d52fe782d0c664ff5ff451fd919
AtlantisPleb <chris@openagents.com>
2026-06-30 10:33:28 -0500

    Add Basecoat native select components

--- packages/ui/src/basecoat/index.ts
+++ packages/ui/src/basecoat/index.ts
@@ -3,6 +3,7 @@ export * from './button'
 export * from './card'
 export * from './display'
 export * from './input'
+export * from './native-select'
 export * from './selection'
 export {
   basecoatAttrs,
