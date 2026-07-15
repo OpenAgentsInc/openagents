2be54142c6681582115129287928db02bf934779
AtlantisPleb <chris@openagents.com>
2026-07-15 17:04:47 -0500

    chore(release): prepare desktop rc17

--- apps/openagents.com/apps/start/src/lib/public-site.ts
+++ apps/openagents.com/apps/start/src/lib/public-site.ts
@@ -6,13 +6,13 @@ export const DOCS_URL = '/docs'
 export const INSTALL_URL = '/install'
 
 export const MAC_RELEASE = {
-  version: '0.1.0-rc.12',
+  version: '0.1.0-rc.17',
   platform: 'macOS',
   architecture: 'Apple silicon',
   size: '290 MB',
   downloadUrl:
-    'https://github.com/OpenAgentsInc/openagents/releases/download/openagents-desktop-v0.1.0-rc.12/OpenAgents-0.1.0-rc.12-arm64.dmg',
-  releaseUrl: `${GITHUB_REPOSITORY_URL}/releases/tag/openagents-desktop-v0.1.0-rc.12`,
+    'https://github.com/OpenAgentsInc/openagents/releases/download/openagents-desktop-v0.1.0-rc.17/OpenAgents-0.1.0-rc.17-arm64.dmg',
+  releaseUrl: `${GITHUB_REPOSITORY_URL}/releases/tag/openagents-desktop-v0.1.0-rc.17`,
 } as const
 
 export const PRODUCT_BOUNDARIES = [
